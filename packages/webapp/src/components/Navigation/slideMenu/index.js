22d8e20ed11f17d8040c09e102c40057565f2a1f
Aliya <aliyamohamed13@gmail.com>
2020-07-23 16:48:08 -0700

    nav menu styling

--- packages/webapp/src/components/Navigation/slideMenu/index.js
+++ packages/webapp/src/components/Navigation/slideMenu/index.js
@@ -1,6 +1,7 @@
 import { slide as Menu } from 'react-burger-menu';
 import React from 'react';
-import logo from '../../../assets/images/logo2x.png';
+// import logo from '../../../assets/images/logo2x.png';
+import logo from '../../../../public/favicon-96x96.png';
 import styles from './styles.scss';
 import history from '../../../history';
 import {connect} from 'react-redux';
@@ -11,10 +12,19 @@ class slideMenu extends React.Component {
     super(props);
     this.state = {
       menuOpen: false,
+        supportOpen: false,
+        manageOpen: false
     };
 
     this.handleClick = this.handleClick.bind(this);
     this.handleStateChange = this.handleStateChange.bind(this);
+  }
+
+    toggleSupport = () => {
+      this.setState({supportOpen : !this.state.supportOpen})
+  }
+  toggleManage = () => {
+      this.setState({manageOpen: !this.state.manageOpen}, () => console.log(this.state.manageOpen))
   }
 
   render () {
@@ -25,38 +35,54 @@ class slideMenu extends React.Component {
       {isAuthenticated() && users && farm && farm.has_consent &&
       <Menu isOpen={this.state.menuOpen}
             width={ 180 }
-            onStateChange={(state) => this.handleStateChange(state)}>
+            onStateChange={(state) => this.handleStateChange(state)}
+            className={styles["menu-body"]}>
         <div className={styles["top-nav-bar"]}>
-          <img width="60%" height="60%" src={logo} alt={"logo"}/>
+          <img src={logo} alt={"logo"}/>
         </div>
-        <a id="home" className="menu-item" onClick={() => this.handleClick("/")}><span>Home</span></a>
-        <a id="profile" className="menu-item" onClick={() => this.handleClick("/Profile")}><span>Profile</span></a>
-        {
-          (Number(farm.role_id) === 1 || Number(farm.role_id) === 2) &&
-          <a id="field" className="menu-item" onClick={() => this.handleClick("/Field")}><span>Fields</span></a>
-        }
-        <a id="log" className="menu-item" onClick={() => this.handleClick("/Log")}><span>Log</span></a>
+      {
+      <a id="manage" className="menu-item" onClick={() => this.toggleManage()}><span>Manage</span></a>
+      }
+      {this.state.manageOpen &&
+      <div className={styles["sub-menu"]}>
+      {
+          (Number(farm.role_id) === 1 || Number(farm.role_id) === 2)
+       &&
+      <a id="field" className="menu-item" onClick={() => this.handleClick("/Field")}><span>Fields</span></a>
+    }
+      <a id="crops" className="menu-item" onClick={() => console.log("clicked crops")}><span>Crops</span></a>
+      <a id="log" className="menu-item" onClick={() => this.handleClick("/Log")}><span>Logs</span></a>
+      <a id="tasks" className="menu-item" ><span>Tasks</span></a>
+      <a id="inventory" className="menu-item" ><span>Inventory</span></a>
+      <a id="profile" className="menu-item" onClick={() => this.handleClick("/Profile")}><span>Users</span></a>
+     </div>
+          }
+      {
+      (Number(farm.role_id) === 1) &&
+      <a id="finances" className="menu-item" onClick={() => this.handleClick("/Finances")}><span>Finances</span></a>
+  }
         <a id="shift" className="menu-item" onClick={() => this.handleClick("/Shift")}><span>Shift</span></a>
-        {
-          (Number(farm.role_id) === 1) &&
-          <a id="finances" className="menu-item" onClick={() => this.handleClick("/Finances")}><span>Finances</span></a>
-        }
+
         {
           (Number(farm.role_id) === 1 || Number(farm.role_id) === 2) &&
           <a id="insights" className="menu-item" onClick={() => this.handleClick("/Insights")}><span>Insights</span></a>
         }
-        <a onClick={logout} id="logout" className="menu-item"><span>Log Out</span></a>
-        <div className={styles["hyper-link"]}>
+      {
+        <a id="support" className="menu-item" onClick={() => this.toggleSupport()} ><span>Support</span></a>
+      }
+      { this.state.supportOpen &&
+         <div className={styles["sub-menu"]}>
           {
-            (Number(farm.role_id) === 1 || Number(farm.role_id) === 2) &&
-            <a id="demo" onClick={() => this.handleClick("/Intro")}>Demos</a>
-          }
-          <a id="contact" onClick={() => this.handleClick("/Contact")}>Contact Us</a>
-          <a id="terms" onClick={() => this.handleClick("/Consent")}>Terms</a>
-
-        </div>
-
-      </Menu>}
+          (Number(farm.role_id) === 1 || Number(farm.role_id) === 2) &&
+          <a id="demo" className="menu-item" onClick={() => this.handleClick("/Intro")}><span>Demos</span></a>
+      }
+      <a id="contact" className="menu-item" onClick={() => this.handleClick("/Contact")}><span>Contact Us</span></a>
+          <a id="terms" className="menu-item" onClick={() => this.handleClick("/Consent")}><span>Terms</span></a>
+          </div>
+       }
+      <a onClick={logout} id="logout" className="menu-item"><span>Log Out</span></a>
+      </Menu>
+      }
       </div>
     );
   }

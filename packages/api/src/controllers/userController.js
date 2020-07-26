270ea7a75bfc6a718725776d8d9afb5d578d1337
Candice Pang <candicekhpang@hotmail.com.hk>
2020-07-25 20:23:52 -0700

    Updated comment to describe what the add pseudo endpoint does

--- packages/api/src/controllers/userController.js
+++ packages/api/src/controllers/userController.js
@@ -1,12 +1,12 @@
-/* 
- *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>   
+/*
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
  *  This file (userController.js) is part of LiteFarm.
- *  
+ *
  *  LiteFarm is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  LiteFarm is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -47,7 +47,7 @@ class userController extends baseController {
   }
 
   static addPseudoUser() {
-    // Add user endpoint
+    // Add pseudo user endpoint
     return async (req, res) => {
       const trx = await transaction.start(Model.knex());
       try {

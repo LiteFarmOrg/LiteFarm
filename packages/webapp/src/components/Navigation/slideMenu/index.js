68d637cd1da2f8d9b059202e4064466d23c90985
Aliya <aliyamohamed13@gmail.com>
2020-07-27 19:26:15 -0700

    more styling for slide in menu, profile logo on top moved right, logos clickable to home page

--- packages/webapp/src/components/Navigation/slideMenu/index.js
+++ packages/webapp/src/components/Navigation/slideMenu/index.js
@@ -1,7 +1,9 @@
 import { slide as Menu } from 'react-burger-menu';
 import React from 'react';
 // import logo from '../../../assets/images/logo2x.png';
-import logo from '../../../../public/favicon-96x96.png';
+import logo from '../../../assets/images/nav-logo.svg';
+import vectorUp from '../../../assets/images/vector-up.svg';
+import vectorDown from '../../../assets/images/vector-down.svg';
 import styles from './styles.scss';
 import history from '../../../history';
 import {connect} from 'react-redux';
@@ -20,11 +22,11 @@ class slideMenu extends React.Component {
     this.handleStateChange = this.handleStateChange.bind(this);
   }
 
-    toggleSupport = () => {
+  toggleSupport = () => {
       this.setState({supportOpen : !this.state.supportOpen})
   }
   toggleManage = () => {
-      this.setState({manageOpen: !this.state.manageOpen}, () => console.log(this.state.manageOpen))
+      this.setState({manageOpen: !this.state.manageOpen})
   }
 
   render () {
@@ -34,17 +36,17 @@ class slideMenu extends React.Component {
       <div>
       {isAuthenticated() && users && farm && farm.has_consent &&
       <Menu isOpen={this.state.menuOpen}
-            width={ 180 }
+            width={ 204 }
             onStateChange={(state) => this.handleStateChange(state)}
             className={styles["menu-body"]}>
         <div className={styles["top-nav-bar"]}>
-          <img src={logo} alt={"logo"}/>
+          <img src={logo} alt={"logo"} onClick={() => this.handleClick("/")}/>
         </div>
       {
-      <a id="manage" className="menu-item" onClick={() => this.toggleManage()}><span>Manage</span></a>
+      <a id="manage" className="menu-item" onClick={() => this.toggleManage()}><span>Manage </span><img src={this.state.manageOpen ? vectorUp : vectorDown} alt={"logo"}/></a>
       }
       {this.state.manageOpen &&
-      <div className={styles["sub-menu"]}>
+      <div className={styles["sub-menu"]} style={{'display':'grid'}}>
       {
           (Number(farm.role_id) === 1 || Number(farm.role_id) === 2)
        &&
@@ -59,7 +61,7 @@ class slideMenu extends React.Component {
           }
       {
       (Number(farm.role_id) === 1) &&
-      <a id="finances" className="menu-item" onClick={() => this.handleClick("/Finances")}><span>Finances</span></a>
+      <a id="finances" className="menu-item" onClick={() => this.handleClick("/Finances")}><span>Finance</span></a>
   }
         <a id="shift" className="menu-item" onClick={() => this.handleClick("/Shift")}><span>Shift</span></a>
 
@@ -68,19 +70,19 @@ class slideMenu extends React.Component {
           <a id="insights" className="menu-item" onClick={() => this.handleClick("/Insights")}><span>Insights</span></a>
         }
       {
-        <a id="support" className="menu-item" onClick={() => this.toggleSupport()} ><span>Support</span></a>
+        <a id="support" className="menu-item" onClick={() => this.toggleSupport()}><span>Support</span><img src={this.state.supportOpen ? vectorUp : vectorDown} alt={"logo"}/></a>
       }
       { this.state.supportOpen &&
-         <div className={styles["sub-menu"]}>
+         <div className={styles["sub-menu"]} style={{'display':'grid'}}>
           {
           (Number(farm.role_id) === 1 || Number(farm.role_id) === 2) &&
           <a id="demo" className="menu-item" onClick={() => this.handleClick("/Intro")}><span>Demos</span></a>
       }
-      <a id="contact" className="menu-item" onClick={() => this.handleClick("/Contact")}><span>Contact Us</span></a>
+      <a id="contact" className="menu-item" onClick={() => this.handleClick("/Contact")}><span>Contact us</span></a>
           <a id="terms" className="menu-item" onClick={() => this.handleClick("/Consent")}><span>Terms</span></a>
           </div>
        }
-      <a onClick={logout} id="logout" className="menu-item"><span>Log Out</span></a>
+      <a onClick={logout} id="logout" className="menu-item"><span>Log out</span></a>
       </Menu>
       }
       </div>

709c4262e8578b5fb40e7676671b6a07cba57aaf
Orangel Marquez (Kike) <kikemarquez18@gmail.com>
2020-09-10 11:09:57 -0300

    Adding middlewares for user authorized and farm authorization
    Deprecating farm get

---
+++ packages/api/src/middleware/acl/isOwnerOrAssigne.js
@@ -0,0 +1,32 @@
+const Knex = require('knex');
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile')[environment];
+const knex = Knex(config);
+
+module.exports = async (req, res, next) => {
+  const data = req.body || req.params;
+  const headers = req.headers;
+  const { user_id } = headers;
+
+  if(data.activity_id) {
+    const activity = await fromActivity(data.activity_id);
+    return sameUser(activity, user_id) ? next() : notAuthorizedResponse(res);
+  }
+  if(data.user_id) {
+    return sameUser(data, user_id) ? next() : notAuthorizedResponse(res);
+  }
+  // No user relationship
+  next();
+}
+
+function sameUser(object, user) {
+  return object.user_id === user;
+}
+
+async function fromActivity(activityId) {
+  return await knex('activityLog').where({ activity_id: activityId }).first();
+}
+
+function notAuthorizedResponse(res) {
+  res.status(403).send('user not authorized to access record from other user');
+}
--- packages/api/src/routes/farmRoute.js
+++ packages/api/src/routes/farmRoute.js
@@ -1,12 +1,12 @@
-/* 
- *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>   
+/*
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
  *  This file (farmRoute.js) is part of LiteFarm.
- *  
+ *
  *  LiteFarm is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  LiteFarm is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -18,9 +18,6 @@ const router = express.Router();
 const farmController = require('../controllers/farmController');
 const authFarmId = require('../middleware/acl/authFarmId');
 const checkScope = require('../middleware/acl/checkScope');
-
-// Get the crop on a bed
-router.get('/', farmController.getAllFarms());
 
 router.get('/:farm_id', authFarmId, farmController.getFarmByID());
 

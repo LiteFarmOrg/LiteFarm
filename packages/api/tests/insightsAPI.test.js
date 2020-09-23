91b28de529599980b1414048c4539838ca91a582
Orangel Marquez (Kike) <kikemarquez18@gmail.com>
2020-09-23 12:39:32 -0300

    Adding insights permissions tests

--- packages/api/tests/insightsAPI.test.js
+++ packages/api/tests/insightsAPI.test.js
@@ -19,155 +19,358 @@ chai.use(chaiHttp);
 const chai_assert = chai.assert;    // Using Assert style
 const chai_expect = chai.expect;    // Using Expect style
 const chai_should = chai.should();  // Using Should style
-const server = 'http://localhost:5000';
-const dummy = require('./dummy');
-//const nitrogenBalance = require('../src/jobs/nitrogenBalance/nitrogenBalance');
+const server = require('./../src/server');
+const mocks = require('./mock.factories');
+jest.mock('jsdom')
+jest.mock('../src/middleware/acl/checkJwt')
 
-beforeAll(() => {
-  // beforeAll is set before each test
-  // global.token is set in testEnvironment.js
-  token = global.token;
+
+describe('insights test', () => {
+  let middleware;
+  beforeAll(() => {
+    middleware = require('../src/middleware/acl/checkJwt');
+    middleware.mockImplementation((req, res, next) => {
+      next()
+    });
+  })
+  describe('People Fed', () => {
+    test('Should get people fed endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'people_fed', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get people fed endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'people_fed', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+      test('Should get people fed endpoint if Im on my farm as a worker',async  (done) => {
+        const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+            promisedFarm: mocks.farmFactory(),
+            promisedUser: mocks.usersFactory()
+          },
+          { role_id: 3, status: 'Active' });
+        getInsight(farm_id, user_id, 'people_fed', (err, res) => {
+          expect(res.status).toBe(200);
+          done();
+        });
+      });
+  });
+
+  describe('Soil Om', () => {
+    test('Should get soil om endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'soil_om', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get soil om endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'soil_om', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get soil om endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'soil_om', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('labour happiness', () => {
+    test('Should get labour happiness endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'labour_happiness', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get labour happiness endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'labour_happiness', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get labour happiness endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'labour_happiness', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('biodiversity', () => {
+    test('Should get biodiversity endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'biodiversity', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get biodiversity endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'biodiversity', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get biodiversity endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'biodiversity', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('prices distance', () => {
+    test('Should get prices distance endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'prices/distance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get prices distance endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'prices/distance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get prices distance endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'prices/distance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('waterbalance', () => {
+    test('Should get waterbalance endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'waterbalance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get waterbalance endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'waterbalance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get waterbalance endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'waterbalance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('waterbalance schedule', () => {
+    test('Should get waterbalance schedule endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'waterbalance/schedule', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get waterbalance schedule endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'waterbalance/schedule', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get waterbalance schedule endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'waterbalance/schedule', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('nitrogenbalance', () => {
+    test('Should get nitrogenbalance endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'nitrogenbalance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get nitrogenbalance endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'nitrogenbalance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get nitrogenbalance endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'nitrogenbalance', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  })
+
+  describe('nitrogenbalance schedule', () => {
+    test('Should get nitrogenbalance schedule endpoint if Im on my farm as an owner',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 1, status: 'Active' });
+      getInsight(farm_id, user_id, 'nitrogenbalance/schedule', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+    test('Should get nitrogenbalance schedule endpoint if Im on my farm as a manager',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 2, status: 'Active' });
+      getInsight(farm_id, user_id, 'nitrogenbalance/schedule', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    })
+
+    test('Should get nitrogenbalance schedule endpoint if Im on my farm as a worker',async  (done) => {
+      const [{ user_id, farm_id }] = await mocks.userFarmFactory({
+          promisedFarm: mocks.farmFactory(),
+          promisedUser: mocks.usersFactory()
+        },
+        { role_id: 3, status: 'Active' });
+      getInsight(farm_id, user_id, 'nitrogenbalance/schedule', (err, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+  })
+
 });
 
-xdescribe('Testing Insights People Fed API', () => {
-
-  let farm_id = null;
-  let sale_id;
-  let field_crop_id;
-  let field_id;
-  let crop_id;
-
-  test('POST farm happy land to DB to setup test', (done) => {
-    chai.request(server).post('/farm')
-      .set('content-type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .send(dummy.mockFarm)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(201);
-        chai.expect(res.body).to.have.property('farm_id');
-        farm_id = res.body.farm_id;
-        done();
-      });
-  });
-
-  test('POST a new crop with farm id', (done) => {
-    let crop = dummy.mockCrop;
-    crop.farm_id = farm_id;
-    chai.request(server).post('/crop')
-      .set('content-type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .send(crop)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(201);
-        chai.expect(res.body).to.have.property('crop_common_name');
-        chai.expect(res.body).to.have.property('crop_id');
-        chai.expect(res.body.crop_common_name).to.equal('Tide pods');
-        crop_id = parseInt(res.body.crop_id);
-        done();
-      });
-  });
-
-  test('POST a field to DB', (done) => {
-    let field = dummy.mockField;
-    field.farm_id = farm_id;
-    chai.request(server).post('/field')
-      .set('content-type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .send(field)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(201);
-        chai.expect(res.body).to.have.property('field_id');
-        field_id = res.body.field_id;
-        done();
-      });
-  });
-
-  test('POST fieldCrop to DB to setup test', (done) => {
-    chai.request(server).post('/field_crop')
-      .set('content-type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .send({field_id, crop_id})
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(201);
-        chai.expect(res.body).to.have.property('field_crop_id');
-        field_crop_id = res.body.field_crop_id;
-        done();
-      });
-  });
-
-  test('POST sale and cropSale', (done) => {
-    const mockSaleAndCropSale = Object.assign(dummy.mockSaleAndCropSale, { farm_id, cropSale: { crop_id }});
-    chai.request(server).post('/sale')
-      .set('content-type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .send(mockSaleAndCropSale)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(201);
-        chai.expect(res.body).to.have.property('sale_id');
-        sale_id = res.body.sale_id;
-        done();
-      });
-  });
-
-  test('GET People Fed with farm_id from DB', (done) => {
-    chai.request(server).get('/insight/people_fed/' + farm_id)
-      .set('content-type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(200);
-        chai_expect(res.body.data.length).to.equal(5);
-        chai_expect(res.body.preview).to.equal(0);
-        done();
-      });
-  });
-
-  test('DELETE sale and cropSale', (done) => {
-    chai.request(server).del('/sale/' + sale_id)
-      .set('content-type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(200);
-        done();
-      });
-  });
-
-  test('DELETE fieldCrop that was associated to this test', (done) => {
-    chai.request(server).del('/field_crop/' + field_crop_id)
-      .set('Content-Type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(200);
-        done();
-      });
-  });
-
-  test('DELETE field associated to this test', (done) => {
-    chai.request(server).del('/field/' + field_id)
-      .set('Content-Type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(200);
-        done();
-      })
-  });
-
-  test('DELETE crop associated to this test', (done) => {
-    chai.request(server).del('/crop/' + crop_id)
-      .set('Content-Type', 'application/json')
-      .set('Authorization', 'Bearer ' + token)
-      .end((err, res) => {
-        chai_expect(err).to.be.null;
-        chai_expect(res.status).to.equal(200);
-        done();
-      })
-  });
-});
+  function getInsight(farmId, userId, route, callback) {
+    chai.request(server).get(`/insight/${route}/${farmId}`)
+      .set('farm_id', farmId)
+      .set('user_id', userId)
+      .end(callback)
+  }

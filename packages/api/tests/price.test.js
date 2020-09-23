c3c923f7680ae38f1f610a8e0d71f1cbf559058b
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-09-23 14:44:24 -0700

    Delete tests passing

--- packages/api/tests/price.test.js
+++ packages/api/tests/price.test.js
@@ -72,6 +72,11 @@ describe('Price Tests', () => {
 			.end(callback);
 	}
 
+	function deleteRequest(data, { user_id = newOwner.user_id, farm_id = farm.farm_id }, callback) {
+		const { price_id } = data;
+		chai.request(server).delete(`/price/${price_id}`).set('user_id', user_id).set('farm_id', farm_id).end(callback);
+	}
+
 	function fakeUserFarm(role = 1) {
 		return { ...mocks.fakeUserFarm(), role_id: role };
 	}
@@ -124,7 +129,7 @@ describe('Price Tests', () => {
 
 	// POST TESTS
 
-	describe('Post yield tests', () => {
+	describe('Post price tests', () => {
 		test('Owner should post price', async (done) => {
 			const { mainFarm, user } = await returnUserFarms(1);
 			const { crop } = await returnCrop(mainFarm);
@@ -279,6 +284,64 @@ describe('Price Tests', () => {
 				expect(res.error.text).toBe('User does not have the following permission(s): get:prices');
 				done();
 			});
+		});
+	});
+
+	// DELETE TESTS
+
+	describe('Delete price tests', () => {
+		test('Owner should delete their price', async (done) => {
+			const { mainFarm, user } = await returnUserFarms(1);
+			const { price } = await returnPrice(mainFarm);
+
+			deleteRequest(price, { user_id: user.user_id, farm_id: mainFarm.farm_id }, async (err, res) => {
+				expect(res.status).toBe(200);
+				const [ deletedPrice ] = await priceModel.query().where('price_id', price.price_id);
+				expect(deletedPrice.deleted).toBe(true);
+				done();
+			});
+		});
+
+		test('Manager should delete their price', async (done) => {
+			const { mainFarm, user } = await returnUserFarms(2);
+			const { price } = await returnPrice(mainFarm);
+
+			deleteRequest(price, { user_id: user.user_id, farm_id: mainFarm.farm_id }, async (err, res) => {
+				expect(res.status).toBe(200);
+				const [ deletedPrice ] = await priceModel.query().where('price_id', price.price_id);
+				expect(deletedPrice.deleted).toBe(true);
+				done();
+			});
+		});
+
+		test('Should return 403 if a worker tries to delete a price', async (done) => {
+			const { mainFarm, user } = await returnUserFarms(3);
+			const { price } = await returnPrice(mainFarm);
+
+			deleteRequest(price, { user_id: user.user_id, farm_id: mainFarm.farm_id }, async (err, res) => {
+				expect(res.status).toBe(403);
+				expect(res.error.text).toBe('User does not have the following permission(s): delete:prices');
+				done();
+			});
+		});
+
+		test('Should get status 403 if an unauthorizedUser tries to delete price', async (done) => {
+			const { mainFarm, user } = await returnUserFarms(1);
+			const { price } = await returnPrice(mainFarm);
+			const [ unAuthorizedUser ] = await mocks.usersFactory();
+
+			deleteRequest(
+				price,
+				{
+					user_id: unAuthorizedUser.user_id,
+					farm_id: mainFarm.farm_id
+				},
+				async (err, res) => {
+					expect(res.status).toBe(403);
+					expect(res.error.text).toBe('User does not have the following permission(s): delete:prices');
+					done();
+				}
+			);
 		});
 	});
 });

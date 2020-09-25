b7cd5799d9a366b9a2eef6c839ed4a48543425c3
Jimmy Rowland <tian_yu@student.smc.edu>
2020-09-25 10:34:32 -0700

    Mark failed tests

--- packages/api/tests/logs.test.js
+++ packages/api/tests/logs.test.js
@@ -608,6 +608,7 @@ describe('Log Tests', () => {
             })
           });
 
+          //TODO fail
           test('Should return 403 if body.user_id is different from header.user_id', async (done) => {
             sampleRequestBody.user_id = worker.user_id;
             putRequest(sampleRequestBody, {user_id: manager.user_id}, async (err, res) => {
@@ -763,7 +764,7 @@ describe('Log Tests', () => {
               fertilizer_id: fertilizer.fertilizer_id
             }
           });
-
+          //TODO fail
           test('Should return 403 if field references a new farm', async (done) => {
             sampleRequestBody.fields = [sampleRequestBody.fields[0], {field_id: field1.field_id}];
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -783,7 +784,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 403 if field and fieldCrop reference 2 farms', async (done) => {
             sampleRequestBody.crops = [sampleRequestBody.crops[0], {field_crop_id: fieldCrop1.field_crop_id}];
             sampleRequestBody.fields = [sampleRequestBody.fields[0], {field_id: field1.field_id}];
@@ -793,7 +794,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 403 if fertilizer references a new farm', async (done) => {
             sampleRequestBody.fertilizer_id = fertilizer1.fertilizer_id;
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -802,7 +803,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 403 if field_crop references a new farm', async (done) => {
             sampleRequestBody.crops = [sampleRequestBody.crops[0], {field_crop_id: fieldCrop1.field_crop_id}];
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -863,7 +864,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 400 if field_crops reference a field that is not in fields array', async (done) => {
             sampleRequestBody.field = [sampleRequestBody.fields[0]]
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -872,7 +873,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 400 if field_crops reference a field that is not in fields in the database', async (done) => {
             sampleRequestBody.crops = [{field_crop_id: fieldCrop1.field_crop_id}];
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -881,7 +882,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 400 if field reference a field that is not in fieldCrop array', async (done) => {
             sampleRequestBody.crops = [sampleRequestBody.crops[0]]
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -890,7 +891,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 400 if field reference a field that is not in fieldCrop in the database', async (done) => {
             sampleRequestBody.fields = [{field_id: field1.field_id}];
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -899,7 +900,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 400 if body.crops is empty1', async (done) => {
             sampleRequestBody.crops = [{}];
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -909,7 +910,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 400 if body.crops is empty2', async (done) => {
             sampleRequestBody.crops = [];
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -919,7 +920,7 @@ describe('Log Tests', () => {
               done();
             })
           });
-
+          //TODO fail
           test('Should return 400 if body.fields is empty1[{}]', async (done) => {
             sampleRequestBody.fields = [{}];
             putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
@@ -1506,7 +1507,7 @@ describe('Log Tests', () => {
             done();
           })
         });
-
+        //TODO fail
         test('Should return 400 when fieldCrops is not empty', async (done) => {
           putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
             console.log(fakefieldWorkLog, res.error);
@@ -1638,7 +1639,7 @@ describe('Log Tests', () => {
             done();
           })
         });
-
+        //TODO fail
         test('Should return 400 when fieldCrops is not empty', async (done) => {
           putRequest(sampleRequestBody, {user_id: owner.user_id}, async (err, res) => {
             console.log(fakeSoilDataLog, res.error);
@@ -2058,7 +2059,7 @@ describe('Log Tests', () => {
           fertilizer_id: fertilizer.fertilizer_id
         }
       })
-
+      //TODO fail
       test('Should return 400 when activity_kind does not fit req.body shape', async (done) => {
         sampleRequestBody.activity_kind = "soilData";
         postRequest(sampleRequestBody, {}, async (err, res) => {
@@ -2067,7 +2068,7 @@ describe('Log Tests', () => {
           done();
         })
       });
-
+      //TODO fail
       test('Should return 400 when activity_kind does not fit req.body shape2', async (done) => {
         sampleRequestBody.activity_kind = "fieldWork";
         postRequest(sampleRequestBody, {}, async (err, res) => {
@@ -2114,7 +2115,7 @@ describe('Log Tests', () => {
           done();
         })
       });
-
+      //TODO fail
       test('Should return 403 when 1 of the 2 fields references a farm that the user does have access to', async (done) => {
         const [newField] = await mocks.fieldFactory();
         sampleRequestBody.fields = [{field_id: newField.field_id}, sampleRequestBody.fields[0]];
@@ -2134,7 +2135,7 @@ describe('Log Tests', () => {
           done();
         })
       });
-
+      //TODO fail
       test('Should return 400 when 1 fieldCrop references a field that is not in body.fields', async (done) => {
         const [newFieldCrop] = await mocks.fieldCropFactory({promisedField: mocks.fieldFactory({promisedFarm: [farm]})})
         sampleRequestBody.crops = [{field_crop_id: newFieldCrop.field_crop_id}, sampleRequestBody.crops[0]];
@@ -2144,7 +2145,7 @@ describe('Log Tests', () => {
           done();
         })
       });
-
+      //TODO fail
       test('Should return 403 when 1 fieldCrop references a field that user does not have access to', async (done) => {
         const [newFieldCrop] = await mocks.fieldCropFactory();
         sampleRequestBody.crops = [{field_crop_id: newFieldCrop.field_crop_id}, sampleRequestBody.crops[0]];
@@ -2263,7 +2264,7 @@ describe('Log Tests', () => {
             done();
           })
         });
-
+        //TODO fail
         test('Should return 403 when a manager tries to post a log for a worker', async (done) => {
           sampleRequestBody.user_id = workder.user_id;
           postRequest(sampleRequestBody, {user_id: manager.user_id}, async (err, res) => {

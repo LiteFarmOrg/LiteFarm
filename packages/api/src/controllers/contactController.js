7cb0e7fc9d95cede8afeb9ce70bf567e48d6093c
Orangel Marquez (Kike) <kikemarquez18@gmail.com>
2020-10-01 13:03:10 -0300

    changing contact controller

--- packages/api/src/controllers/contactController.js
+++ packages/api/src/controllers/contactController.js
@@ -1,12 +1,12 @@
-/* 
- *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>   
+/*
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
  *  This file (contactController.js) is part of LiteFarm.
- *  
+ *
  *  LiteFarm is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  LiteFarm is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -16,6 +16,7 @@
 const baseController = require('../controllers/baseController');
 const nodeMailer = require('nodemailer');
 const credentials = require('../credentials');
+const sendEmailTemplate = require('./../templates/sendEmailTemplate')
 
 class contactController extends baseController {
   constructor() {
@@ -23,30 +24,17 @@ class contactController extends baseController {
   }
 
   static sendContactForm() {
-    const smptTransport = nodeMailer.createTransport({
-      service: 'Gmail',
-      port: 465,
-      auth: {
-        user: credentials.CONTACT_FORM.user,
-        pass: credentials.CONTACT_FORM.pass,
-      },
-    });
     return async (req, res) => {
       try {
         const mailData = req.body;
-        const mailOptions = {
-          'from': mailData.email,
-          to: credentials.CONTACT_FORM.user,
-          subject: mailData.email + ' ' + mailData.name + ': ' + mailData.subject,
-          text: 'FARM ID: ' + mailData.farm_id + '\n--\n' + mailData.body,
-        };
-
-        await smptTransport.sendMail(mailOptions, (err) => {
-          if (err) res.send(err);
-          else {
-            res.status(200).send('Success')
-          }
-        })
+        await sendEmailTemplate.sendEmail('../templates/contact.html',
+          mailData.email + ' ' + mailData.name + ': ' + mailData.subject,
+          {
+            farm_id: mailData.farm_id,
+            body: mailData.body,
+          }, credentials.CONTACT_FORM.user, credentials.CONTACT_FORM.user
+        )
+        res.status(200).send('Success')
       } catch (error) {
         res.status(400).json({
           error,

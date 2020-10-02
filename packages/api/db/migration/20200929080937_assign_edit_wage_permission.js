f47591be36135f2b5944e3837d2a55526c9910c2
Brandon <brandon.viro@gmail.com>
2020-10-02 11:31:27 -0700

    adjust migrations to fix merging tests

--- packages/api/db/migration/20200929080937_assign_edit_wage_permission.js
+++ packages/api/db/migration/20200929080937_assign_edit_wage_permission.js
@@ -2,15 +2,15 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex('rolePermissions').insert([
-      { role_id: 1, permission_id: 83 },
-      { role_id: 2, permission_id: 83 },
+      { role_id: 1, permission_id: 84 },
+      { role_id: 2, permission_id: 84 },
     ])
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex('rolePermissions').where({ role_id:1, permission_id:83 }).del(),
-    knex('rolePermissions').where({ role_id:2, permission_id:83 }).del(),
+    knex('rolePermissions').where({ role_id:1, permission_id:84 }).del(),
+    knex('rolePermissions').where({ role_id:2, permission_id:84 }).del(),
   ]);
 };

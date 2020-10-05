ac8e2fec85df1bdc69292f0af47b139622291a3a
Orangel Marquez <kike_marquez8@hotmail.com>
2020-10-05 12:11:55 -0300

    Update 20201004094857_revoke_user_farm_data_permissions.js
    edited migration to remove Promise parameter

--- packages/api/db/migration/20201004094857_revoke_user_farm_data_permissions.js
+++ packages/api/db/migration/20201004094857_revoke_user_farm_data_permissions.js
@@ -1,11 +1,11 @@
-exports.up = function (knex, Promise) {
+exports.up = function (knex) {
     return Promise.all([
       knex('rolePermissions').where({ role_id: 3, permission_id: 8 }).del(),
       knex('rolePermissions').where({ role_id: 3, permission_id: 65 }).del(),
     ])
   };
   
-  exports.down = function (knex, Promise) {
+  exports.down = function (knex) {
     return Promise.all([
       knex('rolePermissions').insert({ role_id: 3, permission_id: 8 }),
       knex('rolePermissions').insert({ role_id: 3, permission_id: 65 }),

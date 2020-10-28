5978607e073aa7bed87de9620de6cfc5e871fb35
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2020-10-28 08:45:36 -0300

    Outro splash story added to story books

--- packages/webapp/src/containers/Outro/index.js
+++ packages/webapp/src/containers/Outro/index.js
@@ -1,55 +1,36 @@
-import OutroImg from '../../assets/images/outro/outro.svg';
-import React, {Component} from "react";
-import styles from "./styles.scss";
-import Footer from '../../stories/Footer/index';
-import Button from '../../stories/Button/index'
-import {connect} from 'react-redux';
-import {farmSelector} from '../selector';
+import React  from "react";
+import { connect } from 'react-redux';
+import { farmSelector } from '../selector';
 import history from '../../history';
-import {finishOnboarding} from './actions';
+import { finishOnboarding } from './actions';
+import PureOutroSplash from "../../stories/Pages/Intro/Outro";
 
-function Outro ({farm, dispatch}) {
+function Outro({ farm, dispatch }) {
 
-    const redirectFinish = () => {
-        if (farm) {
-            // TODO replace with Brandon's Splotlight component
-            dispatch(finishOnboarding())
-            history.push('/home')
-        }
-        // TODO: add else case wih Jimmy's organic farm 
-      }
+  const redirectFinish = () => {
+    if (farm) {
+      // TODO replace with Brandon's Splotlight component
+      dispatch(finishOnboarding())
+      history.push('/home')
+    }
+    // TODO: add else case wih Jimmy's organic farm
+  }
 
 
-    return (
-        <div className={styles.home}> 
-            <div className={styles.lander}>
-                    <div className={styles.greetContainer}>
-                    <img src={OutroImg}/>
-                        <div className={styles.description}> 
-                            <h3>{`And finally, let us show you a couple of important things!`}</h3>
-                        </div>
-                    </div>
-                    <Footer
-                        children={<><Button fullLength color="secondary" children='Go back'/>
-                        
-                                    <Button fullLength children='Finish' onClick={redirectFinish}/>
-                                    </>}
-                    >
-                    </Footer>
-                </div>
-            </div>
-    )
-    
+  return (
+    <PureOutroSplash redirectFinish={redirectFinish} />
+  )
+
 }
 
 const mapStateToProps = (state) => {
-    return {
-      farm: farmSelector(state),
-    }
-  };
+  return {
+    farm: farmSelector(state),
+  }
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {dispatch};
+  return { dispatch };
 }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Outro);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Outro);

cb5335652ab837c00ceb92fbe85e003d63155802
Jimmy Rowland <tian_yu@student.smc.edu>
2020-11-15 14:37:02 -0800

    fix link line height and margin

--- packages/webapp/src/components/ChooseFarm/index.js
+++ packages/webapp/src/components/ChooseFarm/index.js
@@ -11,11 +11,13 @@ import Input from '../Form/Input';
 
 export default function PureChooseFarmScreen({ onClick, farms = [], isSearchable, isOnBoarding, ...props }) {
   return <Layout buttonGroup={<Button onClick={onClick} fullLength>Let's get started</Button>}>
-    <Title style={{marginBottom: '16px'}}>Choose your farm</Title>
-    <Link style={{marginBottom: '16px'}} to={'/add_farm'}>+ <Underlined>Add new farm</Underlined></Link>
-    {isSearchable && <Input style={{marginBottom: '16px'}} placeholder={'Search'} isSearchBar={true} {...props}/>}
+    <Title style={{ marginBottom: '16px' }}>Choose your farm</Title>
+    <Link style={{ marginBottom: '24px', width: 'fit-content', fontSize: '16px', color: 'var(--iconActive)', lineHeight: '16px' }}
+          to={'/add_farm'}>+ <Underlined>Add new farm</Underlined></Link>
+    {isSearchable && <Input style={{ marginBottom: '16px' }} placeholder={'Search'} isSearchBar={true} {...props}/>}
     {farms.map((farm) => {
-      return <ChooseFarmMenuItem style={{marginBottom: '16px'}} farmName={farm.name} address={farm.address} color={farm.color} onClick={onClick}
+      return <ChooseFarmMenuItem style={{ marginBottom: '16px' }} farmName={farm.name} address={farm.address}
+                                 color={farm.color} onClick={onClick}
                                  coordinate={farm.coordinate} ownerName={farm.ownerName}/>
     })}
   </Layout>
@@ -25,5 +27,5 @@ PureChooseFarmScreen.prototype = {
   isSearchable: PropTypes.bool,
   farms: PropTypes.array,
   onClick: PropTypes.func,
-  isOnBoarding: PropTypes.bool
+  isOnBoarding: PropTypes.bool,
 }

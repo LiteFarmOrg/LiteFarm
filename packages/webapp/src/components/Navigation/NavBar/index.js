627d3b0df80313dac98c4b9b76b25d2f92ccc332
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2020-11-16 13:09:10 -0300

    Making style changes to profile and adding functionality to open one window at a time

--- packages/webapp/src/components/Navigation/NavBar/index.js
+++ packages/webapp/src/components/Navigation/NavBar/index.js
@@ -2,18 +2,27 @@ import styles from "./styles.scss";
 import MyFarmIcon from "../../../assets/images/my-farm.svg";
 import NotifIcon from "../../../assets/images/notif.svg";
 import HelpIcon from "../../../assets/images/help.svg";
-import React from "react";
+import React, { useState } from "react";
 import ReactJoyride, { STATUS } from 'react-joyride';
 import ProfileFloater from "../../../containers/ProfileFloater";
 
 export default function PureNavBar({ logo, children, steps, resetSpotlight, auth }) {
-
+  const initialState = { profile: false };
+  const [tooltipInteraction, setTooltipInteraction] = useState(initialState);
+  const [isOneTooltipOpen, setOneTooltipOpen] = useState(false);
   const resetSpotlightStatus = (data) => {
     const { action, status } = data;
 
     if ([STATUS.FINISHED, STATUS.SKIPPED].includes(status) || action === 'close') {
       resetSpotlight();
     }
+  }
+
+  const changeInteraction = (tooltipName, onOverlay=false) => {
+    const newInteraction = onOverlay ? initialState : {...initialState, [tooltipName]: !tooltipInteraction[tooltipName]};
+    setTooltipInteraction(newInteraction);
+    setOneTooltipOpen(Object.keys(newInteraction).some((k) => newInteraction[k]));
+    console.log(newInteraction, Object.keys(newInteraction).some((k) => newInteraction[k]));
   }
 
 
@@ -58,10 +67,19 @@ export default function PureNavBar({ logo, children, steps, resetSpotlight, auth
 
         <input id="firstStep" type="image" src={MyFarmIcon} className={styles.actionItem}/>
         <input id="secondStep" type="image" src={NotifIcon} className={styles.actionItem}/>
-        <ProfileFloater auth={auth}>
-          <input id="thirdStep" type="image" src={HelpIcon} className={styles.actionItem}/>
+        <ProfileFloater auth={auth} openProfile={tooltipInteraction.profile}>
+          <input id="thirdStep" type="image" src={HelpIcon} className={styles.actionItem} onClick={() =>changeInteraction('profile')} />
         </ProfileFloater>
-
+        {
+          isOneTooltipOpen && <div style={{
+            position: "fixed",
+            zIndex: 100,
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.01)"}} onClick={() => changeInteraction('', true)} />
+        }
       </div>
       <div className={styles.itemContainer}>
         {logo}

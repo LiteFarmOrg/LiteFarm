e65cd71079d64ef7e17d1f73e7e590cd681554ae
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2020-11-18 09:26:34 -0300

    Adding docker configuration and docker compose

---
+++ packages/api/scripts/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [ $1 == 'testing' ]
+then
+  echo "--- Seeding DB with test data ---"
+  for i in `seq 1 10`;
+    do
+      nc -z litefarm-db 5433 && echo Success && break
+      echo -n .
+      sleep 1
+    done
+  # echo Failed waiting for Postgres && exit 1
+  npm run migrate:dev:db
+  nodemon --exec npm start
+fi

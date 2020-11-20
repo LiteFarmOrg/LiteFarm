23b5ab546f81bc5f9fc2bbbc6c9155bd838124b5
Brandon <brandon.viro@gmail.com>
2020-11-20 11:24:46 -0800

    restore access email

--- packages/api/src/controllers/userFarmController.js
+++ packages/api/src/controllers/userFarmController.js
@@ -441,8 +441,8 @@ class userFarmController extends baseController {
           subject = 'You\'ve lost access to ' + targetUser.farm_name + ' on LiteFarm!';
         } else if (currentStatus === 'Inactive') {
           // TODO: LF-506
-          template_path = '';
-          subject = '';
+          template_path = '../templates/restoration_of_access_to_farm_email.html';
+          subject = 'Your access to ' + targetUser.farm_name + ' has been restored!'; // TODO: CHANGE THIS
         }
         const isPatched = await userFarmModel.query(trx).where('farm_id', farm_id).andWhere('user_id', user_id)
           .patch({

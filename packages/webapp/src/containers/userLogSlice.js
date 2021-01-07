827b3ccac0b3ab6ca994e2302130e98d160d9af9
Jimmy Rowland <tian_yu@student.smc.edu>
2021-01-07 14:00:04 -0800

    fix migration

--- packages/webapp/src/containers/userLogSlice.js
+++ packages/webapp/src/containers/userLogSlice.js
@@ -2,14 +2,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const initialState = {
   lastActiveDatetime: undefined,
+  farm_id: undefined,
 };
 
 const userLogSlice = createSlice({
   name: 'userLogReducer',
   initialState,
   reducers: {
-    logUserInfoSuccess: (state) => {
+    logUserInfoSuccess: (state, { payload: farm_id }) => {
       state.lastActiveDatetime = new Date().getTime();
+      state.farm_id = farm_id;
     },
   },
 });
@@ -18,3 +20,4 @@ export const { logUserInfoSuccess } = userLogSlice.actions;
 export default userLogSlice.reducer;
 export const lastActiveDatetimeSelector = (state) =>
   state?.persistedStateReducer[userLogSlice.name].lastActiveDatetime;
+export const userLogReducerSelector = (state) => state?.persistedStateReducer[userLogSlice.name];

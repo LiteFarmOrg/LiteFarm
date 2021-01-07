8d586d638bb84a25dd4b6f60be17d37b9c7a86c1
Jimmy Rowland <tian_yu@student.smc.edu>
2021-01-06 21:10:36 -0800

    fix crop translation key

--- packages/api/db/migration/20210106205453_populate_user_created_crop_crop_translation_key.js
+++ packages/api/db/migration/20210106205453_populate_user_created_crop_crop_translation_key.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     .then((data) => {
       return Promise.all(data.map((crop) => {
         return knex('crop').where({ crop_id: crop.crop_id }).update({
-          crop_translation_key: crop.crop_common_name.replace(/[,()]/g, '').replace(/\s/g, '_').toUpperCase(),
+          crop_translation_key: crop.crop_common_name,
         })
       }))
     })

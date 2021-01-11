6c9f1f919c2e84ae260ff15c7ee2eed8ec976adf
Jimmy Rowland <tian_yu@student.smc.edu>
2021-01-11 11:13:38 -0800

    expired token screen container

--- packages/webapp/src/containers/ExpiredTokenScreen/index.js
+++ packages/webapp/src/containers/ExpiredTokenScreen/index.js
@@ -1,16 +1,21 @@
-import Layout from '../Layout';
-import Button from '../Form/Button';
-import { ReactComponent } from '../../assets/images/expiredToken/expiredToken.svg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { Semibold } from '../Typography';
 import PureExpiredTokenScreen from '../../components/ExpiredTokenScreen';
 
-export default function ExpiredTokenScreen() {
+export default function ExpiredTokenScreen({ history }) {
   const { t } = useTranslation();
+  const [translation_key, setTranslationKey] = useState();
 
-  return <PureExpiredTokenScreen text={t} />;
+  useEffect(() => {
+    const { translation_key } = history.location;
+    if (translation_key) {
+      setTranslationKey(translation_key);
+    } else {
+      history.push('/');
+    }
+  }, []);
+  return <PureExpiredTokenScreen text={t(`EXPIRED_TOKEN.${translation_key || 'DEFAULT'}`)} />;
 }
 
 ExpiredTokenScreen.prototype = {

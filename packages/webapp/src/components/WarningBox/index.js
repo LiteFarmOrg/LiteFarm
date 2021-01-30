e06d8d11f164185a7ce2b739902642f26b468f1b
Brandon <34196752+beetai@users.noreply.github.com>
2021-01-30 08:49:11 -0800

    F21-108 map drawing line not allowed (#480)
    * fix add field endpoint
    
    * refactor warning box
    
    * add warning if field area 0
    
    * removing commented code
    
    Co-authored-by: Orangel Marquez (Kike) <kike_marquez8@hotmail.com>

--- packages/webapp/src/components/WarningBox/index.js
+++ packages/webapp/src/components/WarningBox/index.js
@@ -1,20 +1,12 @@
-import { Label } from '../Typography';
-import PropTypes from 'prop-types';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import styles from './styles.scss';
 import { ReactComponent as WarningIcon } from '../../assets/images/warning.svg';
 
-export default function PureWarningBox({ text, children, ...props }) {
-  const { t } = useTranslation();
+export default function PureWarningBox({ children, ...props }) {
   return (
     <div className={styles.warningBox} {...props}>
-      <WarningIcon className={styles.icon} />
-      <Label>{text}</Label>
+      <WarningIcon className={styles.icon}/>
+      {children}
     </div>
   );
 }
-
-PureWarningBox.prototype = {
-  text: PropTypes.string,
-};

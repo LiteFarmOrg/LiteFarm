e10be98d2b34a75dada147135f14551bd9f887b1
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-02-04 17:26:52 -0300

    Soil log missing translations

--- packages/webapp/src/containers/Log/EditLog/soilData.js
+++ packages/webapp/src/containers/Log/EditLog/soilData.js
@@ -239,7 +239,7 @@ class soilDataLog extends Component {
             />
           </div>
           <div className={styles.defaultFormDropDown}>
-            <label>Texture</label>
+            <label>{this.props.t('LOG_SOIL.TEXTURE')}</label>
             <Control
               model=".texture"
               component={DropDown}
@@ -250,7 +250,7 @@ class soilDataLog extends Component {
           <Unit model=".k" title="K" type="%" />
           <Unit model=".p" title="P" type="%" />
           <Unit model=".n" title="N" type="%" />
-          <Unit model=".om" title="OM" type="%" />
+          <Unit model=".om" title={this.props.t('LOG_SOIL.OM')} type="%" />
           <Unit model=".ph" title="ph" type="%" />
           <Unit
             model=".bulk_density_kg/m3"

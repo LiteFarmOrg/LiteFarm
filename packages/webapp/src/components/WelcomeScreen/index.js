bced8fbbf6ffb77b0396db153074a0627b170557
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-02-04 17:15:53 -0300

    setting language conditionally on welcome screen svg

--- packages/webapp/src/components/WelcomeScreen/index.js
+++ packages/webapp/src/components/WelcomeScreen/index.js
@@ -1,13 +1,20 @@
 import Layout from '../Layout';
 import Button from '../Form/Button';
-import { ReactComponent as Signup2 }  from '../../assets/images/signUp/signUp2.svg';
+import { ReactComponent as SignupEnglish }  from '../../assets/images/signUp/signup_english.svg';
+import { ReactComponent as SignupSpanish }  from '../../assets/images/signUp/signup_spanish.svg';
+import { ReactComponent as SignupPortuguese }  from '../../assets/images/signUp/signup_portuguese.svg';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
 export default function PureWelcomeScreen({ onClick }) {
   const { t } = useTranslation();
-
+  const languageToSvg = {
+    en: (<SignupEnglish/>),
+    es: (<SignupSpanish/>),
+    pt: (<SignupPortuguese/>)
+  }
+  const language = localStorage.getItem('litefarm_lang');
   return (
     <Layout
       isSVG
@@ -17,8 +24,9 @@ export default function PureWelcomeScreen({ onClick }) {
         </Button>
       }
     >
-      <Signup2 id={"tt2"}>
-      </Signup2>
+      {
+        languageToSvg[language]
+      }
 
     </Layout>
   );

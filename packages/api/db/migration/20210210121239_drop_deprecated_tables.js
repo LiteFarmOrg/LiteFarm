f3f9f0b9d3366cbd9f82b369cbdedefda0a0c007
Jimmy Rowland <tian_yu@student.smc.edu>
2021-02-10 12:28:57 -0800

    format

--- packages/api/db/migration/20210210121239_drop_deprecated_tables.js
+++ packages/api/db/migration/20210210121239_drop_deprecated_tables.js
@@ -3,7 +3,6 @@ exports.up = function(knex) {
     knex.schema.dropTable('userTodo'),
     knex.schema.dropTable('todo'),
     knex.schema.dropTable('plan'),
-
     knex.schema.dropTable('cropCommonName'),
     knex.schema.dropTable('notification'),
   ]);
@@ -29,13 +28,13 @@ exports.down = function(knex) {
         .inTable('users');
       table.primary(['todo_id', 'user_id']);
     }),
-    knex.schema.createTable('cropCommonName', function (table) {
+    knex.schema.createTable('cropCommonName', function(table) {
       table.string('crop_name');
       table.bigint('crop_id')
         .references('crop_id')
         .inTable('crop');
     }),
-    knex.schema.createTable('plan', function (table) {
+    knex.schema.createTable('plan', function(table) {
       table.uuid('plan_id').primary();
       table.uuid('farm_id')
         .references('farm_id')
@@ -43,10 +42,10 @@ exports.down = function(knex) {
       table.jsonb('plan_config').notNullable();
       table.boolean('deleted').notNullable().defaultTo(false);
     }),
-    knex.schema.createTable('notification', function (table) {
+    knex.schema.createTable('notification', function(table) {
       table.uuid('notification_id').primary().defaultTo(knex.raw('uuid_generate_v1()'));
       table.string('user_id').references('user_id').inTable('users');
-      table.enu('notification_kind', ['todo_added','alert_weather', 'alert_worker_finish', 'alert_action_after_scouting', 'alert_before_planned_date', 'alert_pest']).notNullable();
+      table.enu('notification_kind', ['todo_added', 'alert_weather', 'alert_worker_finish', 'alert_action_after_scouting', 'alert_before_planned_date', 'alert_pest']).notNullable();
       table.boolean('is_read').notNullable().defaultTo(false);
       table.timestamps(false, true);
     }),

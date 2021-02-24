89bed2a3b28794d3c784840e8c3190565308b768
Brandon <34196752+beetai@users.noreply.github.com>
2021-02-24 11:01:14 -0800

    [LF-784] basic map page (#620)
    * create components and stories
    
    * basic footer
    
    * basic header
    
    * change div to button
    
    * add hover effect
    
    * remove comments
    
    * minor refactor
    
    * adjust stories
    
    * add farm map link and fix routes

---
+++ packages/webapp/src/stories/Pages/Map/Footer/MapFooter.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import decorators from '../../config/decorators';
+import PureMapFooter from '../../../../components/Map/Footer/';
+
+export default {
+  title: 'Components/Map/MapFooter',
+  component: PureMapFooter,
+};
+
+const Template = (args) => <PureMapFooter {...args} />;
+
+export const Admin = Template.bind({});
+Admin.args = {
+  isAdmin: true,
+};
+Admin.parameters = {
+  chromatic: { viewports: [320, 414, 768, 1024, 1800] },
+};
+
+export const Worker = Template.bind({});
+Worker.args = {
+  isAdmin: false,
+};
+Worker.parameters = {
+  chromatic: { viewports: [320, 414, 768, 1024, 1800] },
+};

d82548df9901ab8cd7c6f2e6f1ed0dbeea0feaad
Jimmy Rowland <tian_yu@student.smc.edu>
2021-03-01 10:43:50 -0800

    jest config

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -63,6 +63,12 @@ module.exports = {
       'ecmaVersion': 2019,
     },
     'parser': '@babel/eslint-parser',
+    'ts-jest': {
+      babelConfig: true,
+      tsConfig: {
+        target: 'ES2019',
+      },
+    },
   },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.

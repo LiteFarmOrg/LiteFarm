4582a74069ed3cc8ff528c841da91de60a813073
Jimmy Rowland <tian_yu@student.smc.edu>
2021-03-01 09:53:56 -0800

    download map

--- packages/webapp/src/containers/Map/index.js
+++ packages/webapp/src/containers/Map/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './styles.module.scss';
 import GoogleMap from 'google-map-react';
@@ -9,6 +9,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { userFarmSelector } from '../../containers/userFarmSlice';
 import { chooseFarmFlowSelector, endMapSpotlight } from '../ChooseFarm/chooseFarmFlowSlice';
 import ExportMapModal from '../../components/Modals/ExportMapModal';
+import html2canvas from 'html2canvas';
 
 export default function Map() {
   const { farm_name, grid_points, is_admin, farm_id } = useSelector(userFarmSelector);
@@ -57,58 +58,62 @@ export default function Map() {
   const handleGoogleMapApi = (map, maps) => {
     console.log(map);
     console.log(maps);
-  }
+  };
 
   const resetSpotlight = () => {
     dispatch(endMapSpotlight(farm_id));
-  }
+  };
 
   const handleClickAdd = () => {
     setShowModal(false);
-  }
+  };
 
   const handleClickFilter = () => {
     setShowModal(false);
-  }
+  };
 
   const handleClickExport = () => {
     setShowModal(!showModal);
-  }
+  };
+
+  const mapWrapperRef = useRef();
 
   const handleDownload = () => {
     console.log('download clicked');
-  }
+    html2canvas(mapWrapperRef.current, { useCORS: true }).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = `${new Date().toISOString()}.png`;
+      link.href = canvas.toDataURL();
+      link.click();
+    });
+  };
 
   const handleShare = () => {
     console.log('share clicked');
-  }
+  };
 
   const handleDismiss = () => {
     setShowModal(false);
-  }
+  };
 
   return (
     <div className={styles.pageWrapper}>
-      <PureMapHeader
-        className={styles.mapHeader}
-        farmName={farm_name}
-      />
+      <PureMapHeader className={styles.mapHeader} farmName={farm_name} />
       <div className={styles.mapContainer}>
-        <div className={styles.workaround}>
-        <GoogleMap
-          style={{ flexGrow: 1 }}
-          bootstrapURLKeys={{
-            key: GMAPS_API_KEY,
-            libraries: ['drawing', 'geometry', 'places'],
-            language: localStorage.getItem('litefarm_lang'),
-          }}
-          defaultCenter={grid_points}
-          defaultZoom={DEFAULT_ZOOM}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => handleGoogleMapApi(map, maps)}
-          options={getMapOptions}
-        >
-        </GoogleMap>
+        <div className={styles.workaround} ref={mapWrapperRef}>
+          <GoogleMap
+            style={{ flexGrow: 1 }}
+            bootstrapURLKeys={{
+              key: GMAPS_API_KEY,
+              libraries: ['drawing', 'geometry', 'places'],
+              language: localStorage.getItem('litefarm_lang'),
+            }}
+            defaultCenter={grid_points}
+            defaultZoom={DEFAULT_ZOOM}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => handleGoogleMapApi(map, maps)}
+            options={getMapOptions}
+          ></GoogleMap>
         </div>
       </div>
       <PureMapFooter
@@ -121,11 +126,13 @@ export default function Map() {
         onClickExport={handleClickExport}
         showModal={showModal}
       />
-      {showModal && <ExportMapModal
-        onClickDownload={handleDownload}
-        onClickShare={handleShare}
-        dismissModal={handleDismiss}
-      />}
+      {showModal && (
+        <ExportMapModal
+          onClickDownload={handleDownload}
+          onClickShare={handleShare}
+          dismissModal={handleDismiss}
+        />
+      )}
     </div>
-  )
+  );
 }

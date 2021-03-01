1efb061a21f6e2f1988599e65045d03ac29e2c50
Brandon <brandon.viro@gmail.com>
2021-03-01 07:52:21 -0800

    style zoom buttons

--- packages/webapp/src/components/Map/CustomZoom/index.js
+++ packages/webapp/src/components/Map/CustomZoom/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-// import styles from './styles.module.scss';  
+import styles from './styles.module.scss';
 import { useTranslation } from 'react-i18next';
-import VideoLogo from '../../../assets/images/map/video.svg';
+import { ReactComponent as ZoomInLogo } from '../../../assets/images/map/zoomIn.svg';
+import { ReactComponent as ZoomOutLogo } from '../../../assets/images/map/zoomOut.svg';
 
 export default function CustomZoom({
+  className,
+  style,
   onClickZoomIn,
   onClickZoomOut,
 }) {
@@ -12,16 +15,22 @@ export default function CustomZoom({
 
   return (
     <div
-      // className={[styles.container, className].join(' ')}
-      // style={style}
+      className={[styles.container, className].join(' ')}
+      style={style}
     >
-      <button onClick={onClickZoomIn}>{"fml"}</button>
-      <button onClick={onClickZoomOut}>{"fml 2"}</button>
+      <button className={styles.zoomIn} onClick={onClickZoomIn}>
+        <ZoomInLogo className={styles.svg} />
+      </button>
+      <button className={styles.zoomOut} onClick={onClickZoomOut}>
+        <ZoomOutLogo className={styles.svg} />
+      </button>
     </div>
   );
 }
 
 CustomZoom.prototype = {
+  className: PropTypes.string,
+  style: PropTypes.object,
   onClickZoomIn: PropTypes.func,
   onClickZoomOut: PropTypes.func,
 };

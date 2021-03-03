458a7f3f69eca3a9c8e9cc39b869c609cd75d36f
Kaavya Lakshmanan <kaavya.laks@gmail.com>
2021-03-03 12:31:45 -0800

    Button change

--- packages/webapp/src/components/Map/Footer/index.js
+++ packages/webapp/src/components/Map/Footer/index.js
@@ -143,14 +143,18 @@ export default function PureMapFooter({
             <AddLogo className={svg} />
           </button>
         )}
-        <button className={clsx(button, stepSpotlighted === 1 && spotlighted)} id="mapSecondStep">
+        <button
+          className={clsx(button, (stepSpotlighted === 1 || !showMapFilter) && spotlighted)}
+          id="mapSecondStep"
+          onClick={showMapFilter ? toggleDrawer('bottom', true) : toggleDrawer('bottom', false)}
+        >
           {' '}
           <div>
             {['bottom'].map((anchor) => (
               <React.Fragment key={anchor}>
                 <FilterLogo
                   className={svg}
-                  onClick={showMapFilter ? toggleDrawer(anchor, true) : toggleDrawer(anchor, false)}
+                  // onClick={showMapFilter ? toggleDrawer(anchor, true) : toggleDrawer(anchor, false)}
                 />
                 <MapFilter
                   setRoadview={setRoadview}

fc2e3b644aa6b9e40e7595c4a64261b3dd7942d2
Brandon <34196752+beetai@users.noreply.github.com>
2021-03-05 08:53:51 -0800

    Map drawing (#642)
    * draw area with dotted line
    
    * remove repeated import
    
    * draw fields from reducer
    
    * draw field names
    
    * set up border styles for varying types
    
    * line and point set up
    
    * add icons to map markers
    
    Co-authored-by: Jimmy Rowland <jimmyrowland@protonmail.com>

--- packages/webapp/src/containers/Map/constants.js
+++ packages/webapp/src/containers/Map/constants.js
@@ -6,3 +6,4 @@ export const DEFAULT_CENTER = {
 };
 export const DEFAULT_ZOOM = 15;
 export const GMAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+export const ENVIRONMENT = process.env.NODE_ENV;

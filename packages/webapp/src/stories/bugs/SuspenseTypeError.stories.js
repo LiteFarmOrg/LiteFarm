9aee92fb0f9958252fe9179bf78bf7915ed3d276
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-03-12 09:49:44 -0800

    storybook fix (#672)
    Co-authored-by: Brandon <34196752+beetai@users.noreply.github.com>

--- packages/webapp/src/stories/bugs/SuspenseTypeError.stories.js
+++ packages/webapp/src/stories/bugs/SuspenseTypeError.stories.js
@@ -1,6 +1,5 @@
 import React, { lazy, Suspense } from 'react';
 import text from '../../containers/Consent/locales/en/Owner.Consent.md';
-import { Consent } from '../Consent/Consent.stories';
 
 const Button = lazy(() => import('../../components/Form/Button'));
 const PureConsent = lazy(() => import('../../components/Consent'));
@@ -23,9 +22,15 @@ export const I18Lazyload = ({ ...args }) => (
   </Suspense>
 );
 
-Consent.args = {
+I18Lazyload.args = {
   onSubmit: () => {},
   onGoBack: () => {},
   checkboxArgs: { label: 'I Agree' },
   text: text,
+};
+I18Lazyload.parameters = {
+  chromatic: { disable: true },
+};
+LazyLoadedButton.parameters = {
+  chromatic: { disable: true },
 };

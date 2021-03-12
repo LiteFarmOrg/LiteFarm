0bb9abd4f6fe7a5defe438d31c9773fadae548ff
Kaavya Lakshmanan <kaavya.laks@gmail.com>
2021-03-12 14:03:14 -0800

    Reset form data & push to map

--- packages/webapp/src/containers/AreaDetailsLayout/Field/index.js
+++ packages/webapp/src/containers/AreaDetailsLayout/Field/index.js
@@ -3,12 +3,15 @@ import { useTranslation } from 'react-i18next';
 import PureField from '../../../components/AreaDetailsLayout/Field';
 import { postFieldLocation } from './saga';
 import { useDispatch } from 'react-redux';
+import { resetLocationData } from '../../mapSlice';
 
 function AreaDetailsField({ history }) {
   const dispatch = useDispatch();
 
   const submitForm = (data) => {
     dispatch(postFieldLocation(data));
+    resetLocationData();
+    history.push('/map');
   };
 
   return <PureField history={history} submitForm={submitForm} />;

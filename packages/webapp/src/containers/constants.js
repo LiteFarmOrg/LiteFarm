752fc767baae92475cd6247a4eef902ef18695dc
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-03-15 11:45:40 -0700

    location selectors (#682)
    * location selectors
    
    * fitBounds when location is not empty

--- packages/webapp/src/containers/constants.js
+++ packages/webapp/src/containers/constants.js
@@ -1,21 +1,11 @@
-/*
- *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
- *  This file (constants.js) is part of LiteFarm.
- *
- *  LiteFarm is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  LiteFarm is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
- */
-
-export const GET_FARM_INFO = 'GET_FARM_INFO';
-export const UPDATE_FARM = 'UPDATE_FARM';
-
-export const UPDATE_AGREEMENT = 'UPDATE_AGREEMENT';
-
-export const SHOW_SPOTLIGHT = 'SHOW_SPOTLIGHT';
+export const areaProperties = [
+  'total_area',
+  'total_area_unit',
+  'grid_points',
+  'perimeter',
+  'perimeter_unit',
+];
+export const locationProperties = ['farm_id', 'name'];
+export const lineProperties = ['length', 'width', 'line_points', 'length_unit', 'width_unit'];
+export const pointProperties = ['point'];
+export const figureProperties = ['figure_id', 'type', 'location_id'];

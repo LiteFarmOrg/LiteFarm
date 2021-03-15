a44b14a9d3c9b8564757933a91bf47df800d011d
Kaavya Lakshmanan <kaavya.laks@gmail.com>
2021-03-15 13:45:14 -0700

    Remove unused code

--- packages/webapp/src/components/PointDetailsLayout/WaterValve/index.js
+++ packages/webapp/src/components/PointDetailsLayout/WaterValve/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import PointDetails from '..';
 import { useSelector } from 'react-redux';

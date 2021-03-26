1e146632172ea63412dfa8b58627a566abcf2c2f
Kaavya Lakshmanan <kaavya.laks@gmail.com>
2021-03-26 15:36:28 -0700

    Fix stories

--- packages/webapp/src/stories/LineDetails/Fence/Fence.stories.js
+++ packages/webapp/src/stories/LineDetails/Fence/Fence.stories.js
@@ -14,7 +14,9 @@ export const Primary = Template.bind({});
 Primary.args = {
   history: (data) => {},
   submitForm: (data) => {},
-  system: (data) => {},
+  system: 'metric',
+  line_points: (data) => {},
+  length: 10,
 };
 Primary.parameters = {
   chromatic: { viewports: [320, 414, 768, 1024, 1800] },

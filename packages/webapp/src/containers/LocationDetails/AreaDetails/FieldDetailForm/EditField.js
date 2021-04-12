b386c8e471b65a2360f2a50df030595482cf1c1b
Brandon <brandon.viro@gmail.com>
2021-04-12 11:46:24 -0700

    modal and conditions

--- packages/webapp/src/containers/LocationDetails/AreaDetails/FieldDetailForm/EditField.js
+++ packages/webapp/src/containers/LocationDetails/AreaDetails/FieldDetailForm/EditField.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PureField from '../../../../components/LocationDetailLayout/AreaDetails/Field';
 import { editFieldLocation } from './saga';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,10 +10,18 @@ import {
   setAreaDetailFormData,
 } from '../../../hooks/useHookFormPersist/hookFormPersistSlice';
 import { getFormData, useLocationPageType } from '../../utils';
+import {
+  currentFieldCropsByLocationIdSelector,
+  plannedFieldCropsByLocationIdSelector,
+} from '../../../fieldCropSlice';
+import UnableToRetireModal from '../../../../components/Modals/UnableToRetireModal';
+import RetireConfirmationModal from '../../../../components/Modals/RetireConfirmationModal';
+import { useTranslation } from 'react-i18next';
 
 function EditFieldDetailForm({ history, match }) {
   const dispatch = useDispatch();
   const system = useSelector(measurementSelector);
+  const { t } = useTranslation();
   const submitForm = (data) => {
     isEditLocationPage &&
       dispatch(editFieldLocation({ ...data, ...match.params, figure_id: field.figure_id }));
@@ -26,16 +34,45 @@ function EditFieldDetailForm({ history, match }) {
   const { isCreateLocationPage, isViewLocationPage, isEditLocationPage } = useLocationPageType(
     match,
   );
+  const [showCannotRetireModal, setShowCannotRetireModal] = useState(false);
+  const [showConfirmRetireModal, setShowConfirmRetireModal] = useState(false);
+  const { location_id } = match.params;
+  const activeCrops = useSelector(currentFieldCropsByLocationIdSelector(location_id));
+  const plannedCrops = useSelector(plannedFieldCropsByLocationIdSelector(location_id));
+  const handleRetire = () => {
+    if (activeCrops.length === 0 && plannedCrops.length === 0) {
+      setShowConfirmRetireModal(true);
+    } else {
+      setShowCannotRetireModal(true);
+    }
+  }
+
+  const confirmRetire = () => {
+    console.log("confirm Retire");
+  }
+
   return (
-    <PureField
-      history={history}
-      match={match}
-      submitForm={submitForm}
-      system={system}
-      useHookFormPersist={useHookFormPersist}
-      isEditLocationPage={isEditLocationPage}
-      isViewLocationPage={isViewLocationPage}
-    />
+    <>
+      <PureField
+        history={history}
+        match={match}
+        submitForm={submitForm}
+        system={system}
+        useHookFormPersist={useHookFormPersist}
+        isEditLocationPage={isEditLocationPage}
+        isViewLocationPage={isViewLocationPage}
+        handleRetire={handleRetire}
+      />
+      {isViewLocationPage && showCannotRetireModal && (
+        <UnableToRetireModal dismissModal={() => setShowCannotRetireModal(false)} />
+      )}
+      {showConfirmRetireModal && (
+        <RetireConfirmationModal
+          dismissModal={() => setShowConfirmRetireModal(false)}
+          handleRetire={confirmRetire}
+        />
+      )}
+    </>
   );
 }
 

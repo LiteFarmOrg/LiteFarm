448210ed9113c0d7b12c649a58a70d0fb702fd67
Kaavya Lakshmanan <kaavya.laks@gmail.com>
2021-04-19 14:42:46 -0700

    Add country_id to farm table

---
+++ packages/api/db/migration/20210419133721_populate_farm_table_with_country_id.js
@@ -0,0 +1,46 @@
+exports.up = async function(knex) {
+
+    const currentFarms = await knex('farm');
+    const countries = await knex('countries');
+
+    const newStructure = {
+        country_id: null,
+      }
+    
+
+    const updatedFarms = currentFarms.map((farm) => {
+        const { currency } = farm.units;
+        const farm_id = farm.farm_id;
+        const match = countries.find(({ iso }) => iso === currency );
+        if (match) {
+            return {
+                ...newStructure,
+              country_id: match.id,
+              farm_id
+            }
+        }
+        else {
+            return {
+                ...newStructure,
+                farm_id
+            }
+        }   
+    })
+
+    return Promise.all(updatedFarms.map((updatedFarm) => {
+        const { farm_id, ...data } = updatedFarm;
+        return knex('farm').where({ farm_id }).update(data);
+      }))
+  
+};
+
+exports.down = function(knex) {
+    return Promise.all([
+        knex.schema.dropTable('farm'),
+      ])
+  
+};
+
+
+
+

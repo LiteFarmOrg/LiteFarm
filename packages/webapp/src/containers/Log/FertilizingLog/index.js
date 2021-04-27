0a503666b2182e0c24be3c7ce8adc5de2cc062b1
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-04-27 16:03:07 -0700

    auto fill with empty string when fertilizer is other (#823)

--- packages/webapp/src/containers/Log/FertilizingLog/index.js
+++ packages/webapp/src/containers/Log/FertilizingLog/index.js
@@ -87,12 +87,21 @@ class FertilizingLog extends Component {
         label: this.props.t(`fertilizer:${fert.fertilizer_type}`),
       }),
     );
-    this.props.dispatch(actions.change('logReducer.forms.fertLog.n_percentage', fert.n_percentage));
-    this.props.dispatch(actions.change('logReducer.forms.fertLog.nh4_n_ppm', fert.nh4_n_ppm));
-    this.props.dispatch(actions.change('logReducer.forms.fertLog.k_percentage', fert.k_percentage));
-    this.props.dispatch(actions.change('logReducer.forms.fertLog.p_percentage', fert.p_percentage));
     this.props.dispatch(
-      actions.change('logReducer.forms.fertLog.moisture_percentage', fert.moisture_percentage),
+      actions.change('logReducer.forms.fertLog.n_percentage', fert.n_percentage ?? ''),
+    );
+    this.props.dispatch(actions.change('logReducer.forms.fertLog.nh4_n_ppm', fert.nh4_n_ppm ?? ''));
+    this.props.dispatch(
+      actions.change('logReducer.forms.fertLog.k_percentage', fert.k_percentage ?? ''),
+    );
+    this.props.dispatch(
+      actions.change('logReducer.forms.fertLog.p_percentage', fert.p_percentage ?? ''),
+    );
+    this.props.dispatch(
+      actions.change(
+        'logReducer.forms.fertLog.moisture_percentage',
+        fert.moisture_percentage ?? '',
+      ),
     );
   }
 

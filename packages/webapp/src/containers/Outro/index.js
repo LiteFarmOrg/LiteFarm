f3564a2d1ea9959309db91a09a2a367cfba18777
Jimmy Rowland <tian_yu@student.smc.edu>
2021-04-28 12:08:18 -0700

    send to support even though there is no attachment

--- packages/webapp/src/containers/Outro/index.js
+++ packages/webapp/src/containers/Outro/index.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import history from '../../history';
 import PureOutroSplash from '../../components/Outro';
 import { certifierSurveySelector } from '../OrganicCertifierSurvey/slice';
-import { getCertifiers } from '../OrganicCertifierSurvey/saga';
 import { patchOutroStep } from './saga';
-import { requestedCertifierSelector } from '../OrganicCertifierSurvey/organicCertifierSurveySlice';
+import {
+  finishedSelectingCertificationTypeSelector,
+  requestedCertifierSelector,
+} from '../OrganicCertifierSurvey/organicCertifierSurveySlice';
 
 function Outro() {
   const dispatch = useDispatch();
   const requestCertifierData = useSelector(requestedCertifierSelector);
+  const survey = useSelector(certifierSurveySelector);
+  const selected = useSelector(finishedSelectingCertificationTypeSelector);
   const onGoBack = () => {
     history.push(
-      !survey.interested
+      !survey.interested || !selected
         ? '/interested_in_organic'
         : requestCertifierData
         ? '/requested_certifier'
@@ -22,12 +26,6 @@ function Outro() {
   const onContinue = () => {
     dispatch(patchOutroStep());
   };
-  const survey = useSelector(certifierSurveySelector);
-  useEffect(() => {
-    if (!survey.survey_id) {
-      dispatch(getCertifiers());
-    }
-  }, []);
 
   return <PureOutroSplash onGoBack={onGoBack} onContinue={onContinue} />;
 }

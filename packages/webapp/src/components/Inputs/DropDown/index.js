a58fb86a732e295dda05b6ff34d4d97ffacb5e83
Jimmy Rowland <tian_yu@student.smc.edu>
2021-04-30 14:16:29 -0700

    fix table no data z-index root container height add react select select all header add delete button to edit harvest log page fix shift how do you feel header auto allocate time when there is only one havest use

--- packages/webapp/src/components/Inputs/DropDown/index.js
+++ packages/webapp/src/components/Inputs/DropDown/index.js
@@ -15,6 +15,7 @@ class DropDown extends React.Component {
       value,
       isMulti,
       style,
+      components,
       ...props
     } = this.props;
     const DropdownIndicator = (props) => {
@@ -35,6 +36,7 @@ class DropDown extends React.Component {
         isSearchable={isSearchable ? isSearchable : false}
         placeholder={placeholder}
         onChange={onChange}
+        components={components}
         value={value}
         {...props}
       />

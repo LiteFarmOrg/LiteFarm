fdb33a21fd613fba28eb457d2f9cce5b95ec671b
Jimmy Rowland <tian_yu@student.smc.edu>
2021-05-12 02:04:50 -0700

    Create crop_variety for deleted fieldCrop

--- packages/api/db/migration/20210511012038_crop_variety_table.js
+++ packages/api/db/migration/20210511012038_crop_variety_table.js
@@ -27,7 +27,7 @@ exports.up = async function(knex) {
       updated_by_user_id: 1,
       created_at: defaultDate,
       updated_at: defaultDate,
-      deleted: fieldCrop.deleted,
+      deleted: false,
     };
   });
   await knex.batchInsert('crop_variety', varieties);

30b1acb6bb4cd328287bccebf1b1d84212d2a8c3
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-05-14 13:37:43 -0300

    Taking nuka-carousel off and implementing a simple video view

--- packages/webapp/src/components/Map/Videos/index.js
+++ packages/webapp/src/components/Map/Videos/index.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import Carousel from "nuka-carousel";
 import AreaVideo from '../../../assets/videos/AreaDrawing.mp4';
 import LineVideo from '../../../assets/videos/LineDrawing.mp4';
 import PointVideo from '../../../assets/videos/AddingPoints.mp4';
-import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import './styles.module.scss';
+import styles from './styles.module.scss';
 import TitleLayout from "../../Layout/TitleLayout";
 
 function PureVideoView({ history }) {
   return (
     <TitleLayout title={'Map Tutorials'} onGoBack={() => history.push('/map')}>
-        <Carousel
-          width={'100%'}
-          initialSlideHeight={'first'}
-          renderCenterLeftControls={({previousSlide}) => (
-            <a onClick={previousSlide}>
-              <BsChevronLeft style={{color: 'white'}} />
-            </a>
-            )
-          }
-          renderCenterRightControls={({nextSlide}) => (
-            <a onClick={nextSlide}>
-              <BsChevronRight style={{color: 'white'}} />
-            </a>
-          )}
-        >
-          <video controls style={{width: '100%', height: 'auto'}} loop  muted src={AreaVideo} />
-          <video controls style={{width: '100%', height: 'auto'}} loop muted src={LineVideo} />
-          <video controls style={{width: '100%', height: 'auto'}} loop  muted src={PointVideo} />
-        </Carousel>
+      <div className={styles.videoFlex}>
+        <div style={{flex : '1'}}>
+          <video controls style={{width: '95%', height: 'auto'}} loop  muted src={AreaVideo} />
+        </div>
+        <div className={styles.videoSpace} />
+        <div style={{flex : '1'}}>
+          <video controls style={{width: '95%', height: 'auto'}} loop muted src={LineVideo} />
+        </div>
+        <div className={styles.videoSpace} />
+
+        <div style={{flex : '1'}}>
+          <video controls style={{width: '95%', height: 'auto'}} loop  muted src={PointVideo} />
+        </div>
+      </div>
     </TitleLayout>
   )
 }

d5974be07135f48bf14b3b66853ffe3f095ac1de
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-05-14 13:50:34 -0300

    removing unused css

--- packages/webapp/src/components/Map/Videos/index.js
+++ packages/webapp/src/components/Map/Videos/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import AreaVideo from '../../../assets/videos/AreaDrawing.mp4';
 import LineVideo from '../../../assets/videos/LineDrawing.mp4';
 import PointVideo from '../../../assets/videos/AddingPoints.mp4';
-import './styles.module.scss';
 import styles from './styles.module.scss';
 import TitleLayout from "../../Layout/TitleLayout";
 

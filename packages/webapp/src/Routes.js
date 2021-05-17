bfbb7704233244d3165a6e5c3a02a7c674d75bd7
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-05-17 16:35:59 -0300

    Adding modified view

--- packages/webapp/src/Routes.js
+++ packages/webapp/src/Routes.js
@@ -176,7 +176,7 @@ const PostGreenhouseForm = React.lazy(() =>
   import('./containers/LocationDetails/AreaDetails/GreenhouseDetailForm/PostGreenhouse'),
 );
 const PureCropManagement = React.lazy(() => import('./components/Crop/management'))
-const PureCropDetail = React.lazy(() => import('./components/Crop/detail'))
+const CropDetail = React.lazy(() => import('./containers/Crop/CropDetail/index'))
 
 const GreenhouseDetails = React.lazy(() => import('./routes/GreenhouseDetailsRoutes'));
 const PostFenceForm = React.lazy(() =>
@@ -202,7 +202,7 @@ const EditWatercourseForm = React.lazy(() =>
 );
 
 const CropCatalogue = React.lazy(() => import('./containers/CropCatalogue'));
-const CropVarieties = React.lazy(() => import('./containers/CropVatieties'));
+const CropVarieties = React.lazy(() => import('./containers/CropVarieties'));
 const AddCrop = React.lazy(() => import('./containers/AddCrop/AddCrop'));
 const Routes = () => {
   const userFarm = useSelector(
@@ -290,8 +290,8 @@ const Routes = () => {
             <Route path="/shift_step_one" exact component={ShiftStepOne} />
             <Route path="/shift_step_two" exact component={ShiftStepTwo} />
             <Route path="/my_shift" exact component={MyShift} />
-            <Route path="/crop/:crop_id/detail" component={PureCropDetail}/>
-            <Route path="/crop/:crop_id/management" component={PureCropManagement}/>
+            <Route path="/crop/:variety_id/detail" component={CropDetail}/>
+            <Route path="/crop/:variety_id/management" component={PureCropManagement}/>
             <Route path="/crop_add" exact component={AddCrop}/>
             <Route path="/create_location/garden" exact component={PostGardenForm} />
             <Route path="/garden/:location_id" component={GardenDetails} />
@@ -477,6 +477,8 @@ const Routes = () => {
             <Route path="/barn/:location_id/details" exact component={EditBarnForm} />
             <Route path="/barn/:location_id/edit" exact component={EditBarnForm} />
             <Route path="/create_location/natural_area" exact component={PostNaturalAreaForm} />
+            <Route path="/crop/:variety_id/detail" component={CropDetail}/>
+            <Route path="/crop/:variety_id/management" component={PureCropManagement}/>
             <Route
               path="/natural_area/:location_id/details"
               exact
---
+++ packages/webapp/src/containers/Crop/CropDetail/index.js
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import PureCropDetail from "../../../components/Crop/detail";
+import { cropVarietyByID, cropVarietySelector } from "../../cropVarietySlice";
+import { useEffect, useState } from "react";
+
+function CropDetail ({ history, match }) {
+  const selectedCrop = useSelector(cropVarietySelector(match.params.variety_id));
+  const [isEditing, setIsEditing] = useState(false);
+
+  const submitForm = () => {
+    setIsEditing(false);
+  }
+
+  const goBack = () => {
+    history.push(`/crop_varieties/crop/${selectedCrop.crop_id}`)
+  }
+
+  useEffect(() => {
+    console.log(selectedCrop);
+  } , []);
+  return (
+    <>
+      <PureCropDetail
+        history={history}
+        match={match}
+        crop={selectedCrop}
+        isEditing={isEditing}
+        setIsEditing={setIsEditing}
+        submitForm={submitForm}
+        onBack={goBack}
+      />
+    </>
+  )
+}
+
+export default CropDetail

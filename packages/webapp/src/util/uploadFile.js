cb7aa6bf4fb9d4fabdeafd508f9e296be9f29297
Jimmy Rowland <tian_yu@student.smc.edu>
2021-05-17 12:47:38 -0700

    upload function

---
+++ packages/webapp/src/util/uploadFile.js
@@ -0,0 +1,32 @@
+import AWS from 'aws-sdk';
+
+const DO_URI = 'nyc3.digitaloceanspaces.com';
+const spacesEndpoint = new AWS.Endpoint(DO_URI);
+const s3 = new AWS.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: process.env.DO_ACCESS_KEY_ID,
+  secretAccessKey: process.env.DO_ACCESS_SECRET_KEY,
+});
+
+export default function uploadFile(blob, filename, callback, { isPublic = false, onError }) {
+  const params = {
+    Body: blob,
+    Bucket: `${process.env.DO_BUCKET_NAME}`,
+    Key: filename,
+  };
+  const host = `https://${process.env.DO_BUCKET_NAME}.${DO_URI}`;
+  s3.putObject(params)
+    .on('build', (request) => {
+      request.httpRequest.headers.Host = host;
+      request.httpRequest.headers['Content-Length'] = blob.size;
+      request.httpRequest.headers['Content-Type'] = blob.type;
+      isPublic && (request.httpRequest.headers['x-amz-acl'] = 'public-read');
+    })
+    .send((err) => {
+      if (err) onError?.();
+      else {
+        const imageUrl = `${host}/${filename}`;
+        callback(imageUrl);
+      }
+    });
+}

693686c0470ead7931c1d506aa7463b75f2353d8
Jimmy Rowland <tian_yu@student.smc.edu>
2021-05-19 19:25:14 -0700

    style fix

--- packages/webapp/src/components/Modals/index.js
+++ packages/webapp/src/components/Modals/index.js
@@ -2,7 +2,7 @@ import Floater from 'react-floater';
 import React from 'react';
 import ModalComponent from './ModalComponent/v1';
 
-function Modal({ children, dismissModal }) {
+function Modal({ children, dismissModal, backgroundDiv }) {
   return (
     <>
       <Floater
@@ -11,18 +11,20 @@ function Modal({ children, dismissModal }) {
         styles={{ floaterCentered: { transform: 'translate(-50%, -70%)', zIndex: 1300 } }}
         component={children}
       />
-      <div
-        onClick={dismissModal}
-        style={{
-          position: 'fixed',
-          zIndex: 1300,
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          backgroundColor: 'rgba(25, 25, 40, 0.8)',
-        }}
-      />
+      {backgroundDiv || (
+        <div
+          onClick={dismissModal}
+          style={{
+            position: 'fixed',
+            zIndex: 1300,
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(25, 25, 40, 0.8)',
+          }}
+        />
+      )}
     </>
   );
 }

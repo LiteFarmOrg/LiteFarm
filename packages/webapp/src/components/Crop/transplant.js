b4a682ff8ddfd19c8a53c714560e6a90378a97a3
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-06-02 10:33:39 -0700

    merge with dev and storybook test (#1020)
    * merge with dev and storybook test
    
    * storybook patch

--- packages/webapp/src/components/Crop/transplant.js
+++ packages/webapp/src/components/Crop/transplant.js
@@ -2,132 +2,115 @@ import Button from '../Form/Button';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { Label } from '../Typography';
-import PageTitle from '../PageTitle/v2';
-import styles from './styles.module.scss';
-import ProgressBar from '../ProgressBar';
-import Radio from '../Form/Radio';
+import { Label, Main } from '../Typography';
 import Form from '../Form';
 import { useForm } from 'react-hook-form';
-import Infoi from '../Tooltip/Infoi';
+import MultiStepPageTitle from '../PageTitle/MultiStepPageTitle';
+import RadioGroup from '../Form/RadioGroup';
 
 export default function Transplant({
-    onSubmit,
-    isCoverCrop,
-    onCancel,
-    onGoBack,
-    useHookFormPersist,
-    persistedFormData,
-    match,
+  onSubmit,
+  isCoverCrop,
+  onCancel,
+  onGoBack,
+  useHookFormPersist,
+  persistedFormData,
+  match,
 }) {
+  const { t } = useTranslation();
 
-    const { t } = useTranslation(['translation', 'common', 'crop']);
+  const progress = 12.5;
 
-    const progress = 12.5;
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    control,
+    formState: { isValid },
+  } = useForm({
+    mode: 'onChange',
+    shouldUnregister: true,
+    defaultValues: { ...persistedFormData },
+  });
 
-    const {
-        register,
-        handleSubmit,
-        getValues,
-        formState: { isValid },
-    } = useForm({
-        mode: 'onChange',
-        shouldUnregister: true,
-        defaultValues: { ...persistedFormData },
-    });
+  // TODO - Add path
+  const persistedPath = [`/path`];
 
-    // TODO - Add path
-    const persistedPath = [`/path`]
+  useHookFormPersist(persistedPath, getValues);
 
-    useHookFormPersist(persistedPath, getValues);
+  const disabled = !isValid;
 
-    const disabled = !isValid;
+  const TRANSPLANT = 'needs_transplant';
+  const COVER = 'for_cover';
 
-    const TRANSPLANT = 'needs_transplant';
-    const COVER = 'for_cover';
-
-    const transplant_register = register(TRANSPLANT, { required: true });
-    const cover_register = register(COVER, { required: isCoverCrop ? true : false });
-
-    return (
-        <Form
-            buttonGroup={
-                <Button disabled={disabled} fullLength>
-                    {t('common:CONTINUE')}
-                </Button>
-            }
-            onSubmit={handleSubmit(onSubmit)}
-        >
-            <PageTitle onGoBack={onGoBack} onCancel={onCancel} title={t('CROP_MANAGEMENT.ADD_MANAGEMENT_PLAN')} />
-            <div
-                style={{
-                    marginBottom: '24px',
-                    marginTop: '16px',
-                }}
+  return (
+    <Form
+      buttonGroup={
+        <Button disabled={disabled} fullLength>
+          {t('common:CONTINUE')}
+        </Button>
+      }
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <MultiStepPageTitle
+        onGoBack={onGoBack}
+        onCancel={onCancel}
+        title={t('MANAGEMENT_PLAN.ADD_MANAGEMENT_PLAN')}
+        value={progress}
+        style={{
+          marginBottom: '24px',
+        }}
+      />
+      <div>
+        <div style={{ marginBottom: '18px' }}>
+          <Label
+            style={{
+              paddingRight: '10px',
+              fontSize: '16px',
+              lineHeight: '20px',
+              display: 'inline-block',
+            }}
+          >
+            {t('MANAGEMENT_PLAN.IS_TRANSPLANT')}
+          </Label>
+        </div>
+        <RadioGroup hookFormControl={control} name={TRANSPLANT} required />
+      </div>
+      <div>
+        {isCoverCrop && (
+          <>
+            <Main
+              style={{ marginTop: '16px', marginBottom: '18px' }}
+              tooltipContent={t('MANAGEMENT_PLAN.COVER_INFO')}
             >
-                <ProgressBar value={progress} />
-            </div>
-            <div>
-                <div style={{ marginBottom: '18px' }}>
-                    <Label
-                        style={{
-                            paddingRight: '10px',
-                            fontSize: '16px',
-                            lineHeight: '20px',
-                            display: 'inline-block',
-                        }}
-                    >
-                        {t('CROP_MANAGEMENT.TRANSPLANT')}
-                    </Label>
-                </div>
-                <div>
-                    <Radio label={t('Yes')} value={true} hookFormRegister={transplant_register} />
-                </div>
-                <div>
-                    <Radio label={t('No')} value={false} hookFormRegister={transplant_register} />
-                </div>
-            </div>
-            <div>
-                {isCoverCrop && (
-                    <div>
-                        <div style={{ marginTop: '16px', marginBottom: '18px' }}>
-                            <Label
-                                style={{
-                                    paddingRight: '10px',
-                                    fontSize: '16px',
-                                    lineHeight: '20px',
-                                    display: 'inline-block',
-                                }}
-                            >
-                                {t('CROP_MANAGEMENT.COVER_OR_HARVEST')}
-                                {
-                                    <Infoi
-                                        style={{ marginLeft: '8px' }}
-                                        content={t('CROP_MANAGEMENT.COVER_INFO')}
-                                    />
-                                }
-                            </Label>
-                        </div>
-                        <div>
-                            <Radio label={t('CROP_MANAGEMENT.AS_COVER_CROP')} value={true} hookFormRegister={cover_register} />
-                        </div>
-                        <div>
-                            <Radio label={t('CROP_MANAGEMENT.FOR_HARVEST')} value={false} hookFormRegister={cover_register} />
-                        </div>
-                    </div>
-                )}
-            </div>
-
-        </Form>
-    );
+              {' '}
+              {t('MANAGEMENT_PLAN.COVER_OR_HARVEST')}
+            </Main>
+            <RadioGroup
+              hookFormControl={control}
+              name={COVER}
+              required={!!isCoverCrop}
+              radios={[
+                {
+                  label: t('MANAGEMENT_PLAN.AS_COVER_CROP'),
+                  value: true,
+                },
+                { label: t('MANAGEMENT_PLAN.FOR_HARVEST'), value: false },
+              ]}
+            />
+          </>
+        )}
+      </div>
+    </Form>
+  );
 }
 
 Transplant.prototype = {
-    isCoverCrop: PropTypes.bool,
-    onSubmit: PropTypes.func,
-    onGoBack: PropTypes.func,
-    onCancel: PropTypes.func,
-    persistedFormData: PropTypes.func,
-    useHookFormPersist: PropTypes.func,
-    match: PropTypes.object,
+  isCoverCrop: PropTypes.bool,
+  onSubmit: PropTypes.func,
+  onGoBack: PropTypes.func,
+  onCancel: PropTypes.func,
+  persistedFormData: PropTypes.func,
+  useHookFormPersist: PropTypes.func,
+  match: PropTypes.object,
 };

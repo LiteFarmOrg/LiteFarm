18c471287a8d1bca893cd13f116f0740427d0693
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-06-08 17:40:48 -0700

    set planting depth nullable (#1053)
    * set planting depth nullable
    
    * ADD INDIVIDUAL_CONTAINER  translation.json

--- packages/webapp/src/components/Crop/PlantingMethod/index.js
+++ packages/webapp/src/components/Crop/PlantingMethod/index.js
@@ -76,7 +76,9 @@ export default function PurePlantingMethod({
   );
   pathsToPersist.push(`/crop/${variety_id}/add_management_plan/choose_transplant_location`);
   const submitPath = `/crop/${variety_id}/add_management_plan/${planting_type?.toLowerCase()}`;
-  const goBackPath = `/crop/${variety_id}/add_management_plan/choose_transplant_location`;
+  const goBackPath = `/crop/${variety_id}/add_management_plan/${
+    persistedFormData.needs_transplant ? 'choose_transplant_location' : 'choose_planting_location'
+  }`;
   useHookFormPersist([...pathsToPersist, goBackPath], getValues);
   const onSubmit = () => {
     history?.push(submitPath);
@@ -127,7 +129,7 @@ export default function PurePlantingMethod({
             },
             { label: t('MANAGEMENT_PLAN.BEDS'), value: BEDS },
             {
-              label: t('MANAGEMENT_PLAN.CONTAINER'),
+              label: t('MANAGEMENT_PLAN.INDIVIDUAL_CONTAINER'),
               value: CONTAINER,
             },
             { label: t('MANAGEMENT_PLAN.BROADCAST'), value: BROADCAST },

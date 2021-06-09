9c70c62fe9d192737eeb847f8003059bc0851e90
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-06-09 12:30:24 -0700

    fix hookform persist (#1059)

--- packages/webapp/src/containers/Crop/AddManagementPlan/PlantInContainer/index.js
+++ packages/webapp/src/containers/Crop/AddManagementPlan/PlantInContainer/index.js
@@ -1,19 +1,13 @@
 import PurePlantInContainer from '../../../../components/Crop/PlantInContainer';
 import { useSelector } from 'react-redux';
-import { hookFormPersistSelector } from '../../../hooks/useHookFormPersist/hookFormPersistSlice';
-import useHookFormPersist from '../../../hooks/useHookFormPersist';
 import { measurementSelector } from '../../../userFarmSlice';
+import { HookFormPersistProvider } from '../../../hooks/useHookFormPersist/HookFormPersistProvider';
 
 export default function PlantInContainer({ history, match }) {
-  const persistedFormData = useSelector(hookFormPersistSelector);
   const system = useSelector(measurementSelector);
   return (
-    <PurePlantInContainer
-      useHookFormPersist={useHookFormPersist}
-      persistedFormData={persistedFormData}
-      match={match}
-      history={history}
-      system={system}
-    />
+    <HookFormPersistProvider>
+      <PurePlantInContainer match={match} history={history} system={system} />
+    </HookFormPersistProvider>
   );
 }

51e00c3084739d9dd94e8924285977a8dc6ca43b
audreyylu <audrey_lu@outlook.com>
2021-06-15 10:02:50 -0700

    changed null comparisons, next call case

--- packages/api/src/middleware/validation/organicCertifierCheck.js
+++ packages/api/src/middleware/validation/organicCertifierCheck.js
@@ -6,24 +6,25 @@ async function organicCertifierCheck(req, res, next) { // express
   if (body.farm_id) {
     const isFarmInterestedInOrganic = await organicCertifierModel.query().where({ farm_id: body.farm_id }) // objection: query organicCertifierModel where farm_id == body.farm_id
     if(isFarmInterestedInOrganic.interested) { // farm is interested in organic certification
-      if(body.organic.isNull()){          // crop variety organic field is null but farm is interested in organic certification
+      if(body.organic === null){          // crop variety organic field is null but farm is interested in organic certification
         return res.status(400).send({ message: 'Organic can\'t be null' }); // return error
       }
-      if(!body.organic && (body.searched.isNull() || body.genetically_engineered.isNull())) { // crop is not organic but searched and genetically engineered questions are null
+      if(!body.organic && (body.searched === null || body.genetically_engineered === null)) { // crop is not organic but searched and genetically engineered questions are null
         return res.status(400).send({ message: 'Organic can\'t be null' }); // return error
       }
-      if(body.organic && (body.searched.isNotNull() || body.genetically_engineered.isNotNull())) { // crop is organic but searched and genetically engineered questions are not null (they should be null)
-        return res.status(400).send({ message: 'Organic should be null'});
+      if(body.organic && (body.searched !== null || body.genetically_engineered !== null)) { // crop is organic but searched and genetically engineered questions are not null (they should be null)
+        return res.status(400).send({ message: 'Organic should be null' });
       }
     }
     if(!isFarmInterestedInOrganic.interested) { // farm is not interested in organic certification
-      if(body.organic.isNotNull || body.searched.isNotNull() || body.genetically_engineered.isNotNull()) {
+      if(body.organic !== null || body.searched !== null || body.genetically_engineered !== null) {
         return res.status(400).send({ message: 'This farm is not interested in organic certification, data should be null' });
       }
     }
   } else {
-    return next();
+    return res.status(400).send({ message: 'Did not receive farm_id' });
   }
+  return next();
 }
 
 module.exports = organicCertifierCheck;

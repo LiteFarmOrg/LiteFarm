8fd6792f6568fb9d75dc6e1d85f31aa8f3e655f2
audreyylu <audrey_lu@outlook.com>
2021-06-16 14:30:50 -0700

    changed middleware logic

--- packages/api/src/middleware/validation/activeManagementPlanCheck.js
+++ packages/api/src/middleware/validation/activeManagementPlanCheck.js
@@ -1,18 +1,12 @@
-
 const managementPlanModel = require('../../models/managementPlanModel');
 
 async function activeManagementPlanCheck(req, res, next) {
   const currentDate = new Date();
-  const {body} = req;   // body should be for cropVarietyModel??
+  const {body} = req;
   if (body.crop_variety_id) {
     const doesCropVarietalHaveAManagementPlan = await managementPlanModel.query().where({crop_variety_id: body.crop_variety_id})// check if there is a management plan that has the crop varietal
     if (doesCropVarietalHaveAManagementPlan !== null) { // crop varietal is in a management plan
-      if (body.seed_date === null) { // seed date is null
-        return next();
-      }
-      if ((body.seed_date.getTime() < currentDate.getTime()) &&
-        (body.termination_date.getTime() < currentDate) &&
-        (body.harvest_date.getTime() < currentDate)) { // active crop management plan
+      if ((!!body.harvest_date && body.harvest_date.getTime() > currentDate.getTime()) || (!!body.termination_date && body.termination_date.getTime() > currentDate.getTime())) {
         return res.status(400).send({message: 'Cannot delete! Active or future crop management plans exist for this varietal'});
       }
       if (body.seed_date.getTime() > currentDate.getTime()) { // planned crop management plan
@@ -20,10 +14,8 @@ async function activeManagementPlanCheck(req, res, next) {
       }
     }
   } else { // crop variety not received
-    return res.status(400).send({ message: 'Did not receive crop variety id'});
+    return res.status(400).send({message: 'Did not receive crop variety id'});
   }
   return next();
 }
-
-
 module.exports = activeManagementPlanCheck;

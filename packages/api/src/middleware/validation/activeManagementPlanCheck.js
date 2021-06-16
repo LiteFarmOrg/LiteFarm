7c38e960690005248d894f6c455a28a7a64c5a96
audreyylu <audrey_lu@outlook.com>
2021-06-16 16:52:56 -0700

    changed middleware logic

--- packages/api/src/middleware/validation/activeManagementPlanCheck.js
+++ packages/api/src/middleware/validation/activeManagementPlanCheck.js
@@ -2,15 +2,17 @@ const managementPlanModel = require('../../models/managementPlanModel');
 
 async function activeManagementPlanCheck(req, res, next) {
   const currentDate = new Date();
-  const {body} = req;
-  if (body.crop_variety_id) {
-    const doesCropVarietalHaveAManagementPlan = await managementPlanModel.query().where({crop_variety_id: body.crop_variety_id})// check if there is a management plan that has the crop varietal
-    if (doesCropVarietalHaveAManagementPlan !== null) { // crop varietal is in a management plan
-      if ((!!body.harvest_date && body.harvest_date.getTime() > currentDate.getTime()) || (!!body.termination_date && body.termination_date.getTime() > currentDate.getTime())) {
-        return res.status(400).send({message: 'Cannot delete! Active or future crop management plans exist for this varietal'});
-      }
-      if (body.seed_date.getTime() > currentDate.getTime()) { // planned crop management plan
-        return res.status(400).send({message: 'Cannot delete! Active or future crop management plans exist for this varietal'});
+  const {params} = req;
+  if (params.crop_variety_id) {
+    const cropVarietalManagementPlanResults = await managementPlanModel.query().where({crop_variety_id: params.crop_variety_id})// check if there is a management plan that has the crop varietal
+    if (cropVarietalManagementPlanResults.length > 0) { // this crop varietal is in at least one management plan
+      for (const mp of cropVarietalManagementPlanResults) {
+        if ((!!mp.harvest_date && mp.harvest_date.getTime() > currentDate.getTime()) || (!!mp.termination_date && mp.termination_date.getTime() > currentDate.getTime())) {
+          return res.status(400).send({message: 'Cannot delete! Active or future crop management plans exist for this varietal'});
+        }
+        if (mp.seed_date.getTime() > currentDate.getTime()) {
+          return res.status(400).send({message: 'Cannot delete! Active or future crop management plans exist for this varietal'});
+        }
       }
     }
   } else { // crop variety not received
@@ -18,4 +20,5 @@ async function activeManagementPlanCheck(req, res, next) {
   }
   return next();
 }
+
 module.exports = activeManagementPlanCheck;

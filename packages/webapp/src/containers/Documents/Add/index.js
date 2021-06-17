eeeedf3d3cee0741a8828468cbaf273a509ca488
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-06-16 17:08:07 -0700

    replace hookFormFileSelector with  useHookFormPersist (#1086)

--- packages/webapp/src/containers/Documents/Add/index.js
+++ packages/webapp/src/containers/Documents/Add/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import PureAddDocumentView from '../../../components/Documents/Add';
 import { useDispatch } from 'react-redux';
-import { deleteUploadedFile, uploadFileSuccess } from '../../hooks/useHookFormPersist/hookFormPersistSlice';
+import { deleteUploadedFile } from '../../hooks/useHookFormPersist/hookFormPersistSlice';
 import { postDocumentSaga } from '../saga';
-
+import useHookFormPersist from '../../hooks/useHookFormPersist';
 
 function AddDocument({ history }) {
   const dispatch = useDispatch();
@@ -14,27 +14,27 @@ function AddDocument({ history }) {
     //   url: 'https://litefarmbeta.nyc3.digitaloceanspaces.com/default_crop/default.jpg',
     //   thumbnailUrl: 'https://litefarmbeta.nyc3.digitaloceanspaces.com/default_crop/default.jpg'
     // }]))
-  },[]);
+  }, []);
 
   const handleSubmit = (data) => {
     dispatch(postDocumentSaga(data));
-  }
+  };
 
   const onBack = () => {
     onCancel();
-  }
+  };
 
   const onCancel = () => {
-    history.push('/document')
-  }
+    history.push('/document');
+  };
 
   const upload = () => {
     //TODO: uploading functionality tbd
-  }
+  };
 
   const deleteImage = (url) => {
-    dispatch(deleteUploadedFile({ thumbnailUrl: url }))
-  }
+    dispatch(deleteUploadedFile({ thumbnailUrl: url }));
+  };
 
   return (
     <PureAddDocumentView
@@ -43,10 +43,9 @@ function AddDocument({ history }) {
       submit={handleSubmit}
       uploadImageOrDocument={upload}
       deleteImage={deleteImage}
+      useHookFormPersist={useHookFormPersist}
     />
-  )
+  );
 }
-
-
 
 export default AddDocument;

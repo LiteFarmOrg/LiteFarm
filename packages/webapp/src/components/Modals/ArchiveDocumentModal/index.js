1d93da559e11702fe558d96d4dbf0f345cb2c2d9
audreyylu <audrey_lu@outlook.com>
2021-06-18 13:58:47 -0700

    returns to previous screen when click outside of modal or on "cancel"

--- packages/webapp/src/components/Modals/ArchiveDocumentModal/index.js
+++ packages/webapp/src/components/Modals/ArchiveDocumentModal/index.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Modal } from '../';
 import ModalComponent from '../ModalComponent/v2';
 import styles from './styles.module.scss';
 import Button from '../../Form/Button';
 
-import clsx from 'clsx';
-import { Title } from '../../Typography';
-
-export default function ArchiveDocumentModal({ dismissModal, onCancel, onContinue }) {
+export default function ArchiveDocumentModal({ onContinue }) {
   const { t } = useTranslation();
+  const [showArchiveDocumentModal, setShowArchiveDocumentModal] = useState(true);
 
   return (
     <div className={styles.container}>
-      <ModalComponent
-        dismissModal={dismissModal}
-        title={t('DOCUMENTS.ARCHIVE_DOCUMENT')}
-        buttonGroup={
-          <>
-            <Button onClick={onCancel} className={styles.button} color="secondary" sm>
-              {t('DOCUMENTS.CANCEL')}
-            </Button>
-            <Button onClick={onContinue} className={styles.button} color="primary" sm>
-              {t('DOCUMENTS.ARCHIVE')}
-            </Button>
-          </>
-        }
-      >
-        <div className={styles.stepList}>{t('DOCUMENTS.ARCHIVE_DOCUMENT_TEXT')}</div>
-      </ModalComponent>
+      {showArchiveDocumentModal && (
+        <ModalComponent
+          dismissModal={() => setShowArchiveDocumentModal(false)}
+          title={t('DOCUMENTS.ARCHIVE_DOCUMENT')}
+          buttonGroup={
+            <>
+              {showArchiveDocumentModal && (
+                <Button
+                  onClick={() => setShowArchiveDocumentModal(false)}
+                  className={styles.button}
+                  color="secondary"
+                  sm
+                >
+                  {t('DOCUMENTS.CANCEL')}
+                </Button>
+              )}
+
+              <Button onClick={onContinue} className={styles.button} color="primary" sm>
+                {t('DOCUMENTS.ARCHIVE')}
+              </Button>
+            </>
+          }
+        >
+          <div className={styles.stepList}>{t('DOCUMENTS.ARCHIVE_DOCUMENT_TEXT')}</div>
+        </ModalComponent>
+      )}
     </div>
   );
 }

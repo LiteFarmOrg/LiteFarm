22f8a43cfc3cae7c4f6864f4595930025b54d78f
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-06-28 15:24:51 -0700

    loading component

---
+++ packages/webapp/src/stories/Loading/Loading.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Loading } from '../../components/Loading/Loading';
+import decorator from '../Pages/config/decorators';
+import Form from '../../components/Form';
+import Button from '../../components/Form/Button';
+import PageTitle from '../../components/PageTitle/v2';
+
+export default {
+  title: 'Components/Loading',
+  component: Loading,
+  decorators: decorator,
+};
+
+const Template = (args) => <Loading {...args} />;
+const TemplateWithText = (args) => (
+  <Form
+    buttonGroup={
+      <>
+        <Button fullLength>Submit</Button>
+      </>
+    }
+  >
+    <PageTitle title={'Title'} />
+    <Loading {...args} />
+  </Form>
+);
+export const Primary = Template.bind({});
+Primary.args = {};
+
+export const InjectStyle = TemplateWithText.bind({});
+InjectStyle.args = {};

544afd702bbd4c66a05414c53ccfedb2ba33281a
audreyylu <audrey_lu@outlook.com>
2021-07-05 10:50:37 -0700

    added certifier and certifications for supported and unsupported certification selections in ViewCertification container

--- packages/webapp/src/containers/OrganicCertifierSurvey/ViewCertification/ViewCertification.js
+++ packages/webapp/src/containers/OrganicCertifierSurvey/ViewCertification/ViewCertification.js
@@ -21,6 +21,12 @@ export default function ViewCertification({ history }) {
   const onAddCertification = () => history.push('/certification/interested_in_organic');
   const onChangePreference = onAddCertification;
 
+  // todo: audrey added these props for certifier and certifications for supported and unsupported certification selections
+  const unsupportedCertification = {};
+  const unsupportedCertifier = {};
+  const supportedCertification = {};
+  const supportedCertifier = {};
+
   const certifier = useSelector(certifierSelector);
 
   return (
@@ -31,11 +37,15 @@ export default function ViewCertification({ history }) {
         <PureViewUnsupportedCertification
           onExport={onExport}
           onChangeCertificationPreference={onChangePreference}
+          unsupportedCertification={unsupportedCertification}
+          unsupportedCertifier={unsupportedCertifier}
         />
       ) : (
         <PureViewSupportedCertification
           onExport={onExport}
           onChangeCertificationPreference={onChangePreference}
+          supportedCertification={supportedCertification}
+          supportedCertifier={supportedCertifier}
         />
       )}
     </>

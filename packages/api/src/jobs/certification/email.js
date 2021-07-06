3f2fa69a6c080abf7ee1d01c8b6a0dbafeac1336
Orangel Marquez <kike_marquez8@hotmail.com>
2021-07-06 16:13:17 -0300

    removing bucket from export link string

--- packages/api/src/jobs/certification/email.js
+++ packages/api/src/jobs/certification/email.js
@@ -2,8 +2,10 @@ const { sendEmail, emails } = require('./../../templates/sendEmailTemplate')
 module.exports = (job) => {
   if(!job.data?.fail) {
     const farm_name = job.data.records[0].notes.split('/')[0];
-    const { first_name, email, file } = job.data;
-    const buttonLink = `/export/${Buffer.from(file).toString('base64')}`;
+    const { first_name, email, file, farm_id } = job.data;
+    const fileWithoutBucket = `${farm_id}${file.split(farm_id)[1]}`;
+    const exportLink = `${Buffer.from(fileWithoutBucket).toString('base64')}`
+    const buttonLink = `/export/${exportLink}`;
     return sendEmail(emails.EXPORT_EMAIL, { first_name, farm_name }, email, {
       buttonLink,
     })

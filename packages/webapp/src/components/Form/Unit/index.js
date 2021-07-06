18de207e8d2518e7f187366f269413f716524923
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-07-06 15:02:48 -0700

    set max

--- packages/webapp/src/components/Form/Unit/index.js
+++ packages/webapp/src/components/Form/Unit/index.js
@@ -231,6 +231,9 @@ const Unit = ({
     } else if (e.target.value === '') {
       hookFormSetValue(name, '', { shouldValidate: true });
       setVisibleInputValue('');
+    } else if (e.target.value > max) {
+      const newHookFormValue = convert(max).from(hookFormUnit).to(databaseUnit);
+      hookFormSetHiddenValue(newHookFormValue, { shouldDirty: true });
     } else {
       const newHookFormValue = convert(e.target.value).from(hookFormUnit).to(databaseUnit);
       hookFormSetHiddenValue(newHookFormValue, { shouldDirty: true });

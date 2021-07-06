49d89732ad56cfac57fc15ca321f96acd933541e
Tbrid <33890191+Tbrid@users.noreply.github.com>
2021-07-06 13:08:39 -0700

    LF-1346/Row-method (#1175)
    * Draft
    
    * Add units
    
    * Add calculations and persisting
    
    * Add storybook and translations
    
    * Revert package-lock
    
    Co-authored-by: Shang Heng <shanghengmak@gmail.com>

--- packages/webapp/src/Routes.js
+++ packages/webapp/src/Routes.js
@@ -226,6 +226,9 @@ const PlantBroadcast = React.lazy(() =>
 const ManagementPlanName = React.lazy(() =>
   import('./containers/Crop/AddManagementPlan/ManagementPlanName'),
 );
+const RowMethod = React.lazy(() => 
+  import('./containers/Crop/AddManagementPlan/RowMethod'),
+);
 
 const PlantedAlready = React.lazy(() => 
   import('./containers/Crop/AddManagementPlan/PlantedAlready'),
@@ -410,6 +413,11 @@ const Routes = () => {
               path="/crop/:variety_id/add_management_plan/container"
               exact
               component={PlantInContainer}
+            />
+            <Route
+              path="/crop/:variety_id/add_management_plan/rows"
+              exact
+              component={RowMethod}
             />
             <Route
               path="/crop/:variety_id/add_management_plan/name"
@@ -636,6 +644,11 @@ const Routes = () => {
               path="/crop/:variety_id/add_management_plan/container"
               exact
               component={PlantInContainer}
+            />
+            <Route
+              path="/crop/:variety_id/add_management_plan/rows"
+              exact
+              component={RowMethod}
             />
             <Route path="/crop_catalogue" exact component={CropCatalogue} />
             <Route path="/crop_varieties/crop/:crop_id" exact component={CropVarieties} />

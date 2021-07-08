99244c2ade9068065f65cf9ef6de33f69b25a0f5
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-07-08 08:04:05 -0700

    ContainerWithIcon.js and fix file tile width (#1201)
    * ContainerWithIcon.js and fix file tile width
    
    * fix loading indicator width
    
    * fix icon cursor
    
    Co-authored-by: Orangel Marquez <kike_marquez8@hotmail.com>

--- packages/webapp/src/components/Documents/Main/index.js
+++ packages/webapp/src/components/Documents/Main/index.js
@@ -52,28 +52,25 @@ function MainDocumentView({ onRetire, onUpdate, onGoBack, document, imageCompone
 
       <div
         style={{
-          width: '312px',
           flexGrow: 1,
           display: 'flex',
           flexDirection: 'column',
           rowGap: '24px',
-          margin: 'auto',
           paddingBottom: '32px',
+          alignItems: 'center',
         }}
       >
-        {document.files?.map(({ thumbnail_url, file_name, url }, index) => (
-          <div key={index}>
-            {thumbnail_url ? (
-              imageComponent({
-                width: '100%',
-                style: { position: 'relative', zIndex: 0 },
-                src: thumbnail_url,
-              })
-            ) : (
-              <CertifierSelectionMenuItem certifierName={file_name} />
-            )}
-          </div>
-        ))}
+        {document.files?.map(({ thumbnail_url, file_name, url }, index) =>
+          thumbnail_url ? (
+            imageComponent({
+              style: { width: '100%', maxWidth: '312px', position: 'relative', zIndex: 0 },
+              src: thumbnail_url,
+              key: index,
+            })
+          ) : (
+            <CertifierSelectionMenuItem key={index} certifierName={file_name} />
+          ),
+        )}
       </div>
       <InputAutoSize
         label={t('common:NOTES')}
---
+++ packages/webapp/src/stories/ContainerWithIcon/ContainerWithIcon.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ContainerWithIcon } from '../../components/ContainerWithIcon/ContainerWithIcon';
+import { componentDecorators } from '../Pages/config/decorators';
+import { ReactComponent as TrashIcon } from '../../assets/images/document/trash.svg';
+
+export default {
+  title: 'Components/ContainerWithIcon',
+  component: ContainerWithIcon,
+  decorators: componentDecorators,
+};
+
+const Template = (args) => <ContainerWithIcon {...args} />;
+
+export const Image = Template.bind({});
+Image.args = {
+  icon: <TrashIcon />,
+  onIconClick: () => {
+    console.log('delete');
+  },
+  children: (
+    <img src={`https://litefarm.nyc3.cdn.digitaloceanspaces.com/default_crop/blueberry.jpg`} />
+  ),
+};
+
+export const RedBlock = Template.bind({});
+RedBlock.args = {
+  icon: <TrashIcon />,
+  onIconClick: () => {
+    console.log('delete');
+  },
+  children: <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }} />,
+  iconBackgroundColor: 'blue',
+};

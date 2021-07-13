159d38ddfa928d066b34323b11c4cd70fae401a9
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-07-13 17:55:49 -0300

    Modifying possible status and added start date for the management plan

--- packages/api/db/migration/20210713112811_management_plan_status.js
+++ packages/api/db/migration/20210713112811_management_plan_status.js
@@ -1,8 +1,9 @@
 exports.up = function(knex) {
   return knex.schema.alterTable('management_plan', (t) => {
-    t.enu('status', ['IN_PROGRESS', 'COMPLETED', 'ABANDONED']).defaultTo('IN_PROGRESS');
+    t.enu('status', ['ACTIVE', 'PLANNED', 'COMPLETED', 'ABANDONED']).defaultTo('PLANNED');
     t.date('abandoned_date');
     t.date('completed_date');
+    t.date('start_date');
   })
 };
 
@@ -11,5 +12,6 @@ exports.down = function(knex) {
     t.dropColumn('status');
     t.dropColumn('abandoned_date');
     t.dropColumn('completed_date');
+    t.dropColumn('start_date');
   });
 };

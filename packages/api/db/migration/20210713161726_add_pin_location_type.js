85f8ab9f0a7f0928ea7e4a7ed9f14c2a2f7795bb
audreyylu <audrey_lu@outlook.com>
2021-07-13 16:54:25 -0700

    edit migration file

--- packages/api/db/migration/20210713161726_add_pin_location_type.js
+++ packages/api/db/migration/20210713161726_add_pin_location_type.js
@@ -1,9 +1,10 @@
-exports.up = async function(knex) {
+exports.up = async function (knex) {
   await knex.schema.createTable('pin', t => {
-    t.uuid('location_id').references('location_id').inTable('location');
+    t.uuid('location_id').primary().references('location_id')
+      .inTable('location').unique().onDelete('CASCADE');
   });
 };
 
-exports.down = async function(knex) {
+exports.down = async function (knex) {
   await knex.schema.dropTable('pin')
 };

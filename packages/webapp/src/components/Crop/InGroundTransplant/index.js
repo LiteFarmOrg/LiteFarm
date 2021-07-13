c80dae3c7347704c0ee277c6e79af90d7e543867
Shang Heng <shanghengmak@gmail.com>
2021-07-13 16:39:21 -0700

    Add choosing methods

--- packages/webapp/src/components/Crop/InGroundTransplant/index.js
+++ packages/webapp/src/components/Crop/InGroundTransplant/index.js
@@ -1,5 +1,5 @@
 import Button from '../../Form/Button';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { Label, Main } from '../../Typography';
@@ -7,6 +7,40 @@ import Form from '../../Form';
 import { useForm } from 'react-hook-form';
 import MultiStepPageTitle from '../../PageTitle/MultiStepPageTitle';
 import RadioGroup from '../../Form/RadioGroup';
+import styles from './styles.module.scss';
+import { ReactComponent as Individual } from '../../../assets/images/plantingMethod/Individual.svg';
+import { ReactComponent as Rows } from '../../../assets/images/plantingMethod/Rows.svg';
+import { ReactComponent as Beds } from '../../../assets/images/plantingMethod/Beds.svg';
+import { ReactComponent as Monocrop } from '../../../assets/images/plantingMethod/Monocrop.svg';
+import { DO_CDN_URL } from '../../../util/constants';
+import ImageModal from '../../Modals/ImageModal';
+
+const BROADCAST = 'BROADCAST';
+const CONTAINER = 'CONTAINER';
+const BEDS = 'BEDS';
+const ROWS = 'ROWS';
+const images = {
+  [BROADCAST]: [
+    `${DO_CDN_URL}/planting_method/Broadcast_1.jpg`,
+    `${DO_CDN_URL}/planting_method/Broadcast_2.jpg`,
+    `${DO_CDN_URL}/planting_method/Broadcast_3.jpg`,
+  ],
+  [CONTAINER]: [
+    `${DO_CDN_URL}/planting_method/Individual_1.jpg`,
+    `${DO_CDN_URL}/planting_method/Individual_2.jpg`,
+    `${DO_CDN_URL}/planting_method/Individual_3.jpg`,
+  ],
+  [BEDS]: [
+    `${DO_CDN_URL}/planting_method/Bed_1.jpg`,
+    `${DO_CDN_URL}/planting_method/Bed_2.jpg`,
+    `${DO_CDN_URL}/planting_method/Bed_3.jpg`,
+  ],
+  [ROWS]: [
+    `${DO_CDN_URL}/planting_method/Rows_1.jpg`,
+    `${DO_CDN_URL}/planting_method/Rows_2.jpg`,
+    `${DO_CDN_URL}/planting_method/Rows_3.jpg`,
+  ],
+};
 
 export default function PureInGroundTransplant({
   onCancel,
@@ -16,15 +50,16 @@ export default function PureInGroundTransplant({
   persistedFormData,
 }) {
   const { t } = useTranslation();
-  
+
 
   const progress = 54;
- 
+
 
   const {
     register,
     handleSubmit,
     getValues,
+    watch,
     control,
     formState: { isValid },
   } = useForm({
@@ -32,7 +67,15 @@ export default function PureInGroundTransplant({
     shouldUnregister: true,
   });
 
-  const KNOWS_HOW = 'knows_how_is_crop_planted'; 
+  const PLANTING_TYPE = 'planting_type';
+  const planting_type = watch(PLANTING_TYPE);
+
+  const KNOWS_HOW = 'knows_how_is_crop_planted';
+  const knows_how = watch(KNOWS_HOW);
+
+  const [{ imageModalSrc, imageModalAlt }, setSelectedImage] = useState({});
+  const onImageSelect = (src, alt) => setSelectedImage({ imageModalSrc: src, imageModalAlt: alt });
+  const dismissModal = () => setSelectedImage({});
 
   const disabled = !isValid;
 
@@ -65,6 +108,56 @@ export default function PureInGroundTransplant({
           required
         />
       </div>
+      {knows_how && (
+        <>
+          <Main
+            style={{ marginBottom: '18px' }}
+            tooltipContent={t('MANAGEMENT_PLAN.KNOW_HOW_IS_CROP_PLANTED_INFO')}
+          >
+            {t('MANAGEMENT_PLAN.WHAT_WAS_PLANTING_METHOD')}
+          </Main>
+          <div className={styles.radioGroupContainer}>
+            <RadioGroup
+              hookFormControl={control}
+              name={PLANTING_TYPE}
+              radios={[
+                {
+                  label: t('MANAGEMENT_PLAN.ROWS'),
+                  value: ROWS,
+                },
+                { label: t('MANAGEMENT_PLAN.BEDS'), value: BEDS },
+                {
+                  label: t('MANAGEMENT_PLAN.INDIVIDUAL_CONTAINER'),
+                  value: CONTAINER,
+                },
+                { label: t('MANAGEMENT_PLAN.BROADCAST'), value: BROADCAST },
+              ]}
+              required
+            />
+            <div className={styles.radioIconsContainer}>
+              <Rows />
+              <Beds />
+              <Individual />
+              <Monocrop />
+            </div>
+          </div>
+          {planting_type && (
+            <div className={styles.imageGrid}>
+              {images[planting_type].map((url, index) => (
+                <img
+                  key={index}
+                  src={url}
+                  alt={`${planting_type}${index}`}
+                  onClick={() => onImageSelect(url, planting_type)}
+                />
+              ))}
+            </div>
+          )}
+          {imageModalSrc && (
+            <ImageModal src={imageModalSrc} alt={imageModalAlt} dismissModal={dismissModal} />
+          )}
+        </>
+      )}
     </Form>
   );
 }

8f0770e9e755632f2a68bd25bb5afc2f5b84f585
Shang Heng <shanghengmak@gmail.com>
2021-07-19 18:02:51 -0700

    Add todo

--- packages/webapp/src/containers/Crop/AddManagementPlan/RowMethod/RowGuidance.js
+++ packages/webapp/src/containers/Crop/AddManagementPlan/RowMethod/RowGuidance.js
@@ -2,14 +2,20 @@ import PurePlanGuidance from '../../../../components/Crop/BedPlan/BedPlanGuidanc
 import { useSelector } from 'react-redux';
 import { measurementSelector } from '../../../userFarmSlice';
 import { HookFormPersistProvider } from '../../../hooks/useHookFormPersist/HookFormPersistProvider';
+import { hookFormPersistSelector } from '../../../hooks/useHookFormPersist/hookFormPersistSlice';
 
 export default function RowGuidance({ history, match }) {
   const variety_id = match.params.variety_id;
   const system = useSelector(measurementSelector);
+
+  // TODO - add path for LF-1586
   const onContinuePath = `/crop/${variety_id}/add_management_plan/name`;
   const onGoBackPath = `/crop/${variety_id}/add_management_plan/rows`;
   const persistedPaths = [onContinuePath, onGoBackPath];
+  
+  const persistedFormData = useSelector(hookFormPersistSelector);
 
+  
   const onCancel = () => {
     history.push(`/crop/${variety_id}/management`);
   };

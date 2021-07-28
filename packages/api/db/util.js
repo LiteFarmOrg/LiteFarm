50e0ab58b715225a7cb6ad81fac43494207394bd
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-07-28 10:30:20 -0300

    task migration update

---
+++ packages/api/db/util.js
@@ -0,0 +1,13 @@
+export const formatAlterTableEnumSql = (
+  tableName,
+  columnName,
+  enums,
+) => {
+  const constraintName = `${tableName}_${columnName}_check`;
+  return [
+    `ALTER TABLE ${tableName} DROP CONSTRAINT IF EXISTS ${constraintName};`,
+    `ALTER TABLE ${tableName} ADD CONSTRAINT ${constraintName} CHECK (${columnName} = ANY (ARRAY['${enums.join(
+      "'::text, '" // eslint-disable-line
+    )}'::text]));`,
+  ].join('\n');
+};

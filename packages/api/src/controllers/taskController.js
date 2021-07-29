1a9e2705b5e02262a8c230931707fc5886c5d337
Shang Heng <shanghengmak@gmail.com>
2021-07-29 16:41:46 -0700

    Add draft of controller and route

---
+++ packages/api/src/controllers/taskController.js
@@ -0,0 +1,14 @@
+const TaskModel = require('../models/taskModel');
+
+const taskController = {
+
+  assignTask() {
+    
+  },
+
+  assignAllTaskOnDate() {
+    
+  },
+}
+
+module.exports = taskController;

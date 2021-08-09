740429b5d065728781c6f386af433ce0fa086903
Brandon <brandon.viro@gmail.com>
2021-08-09 04:21:43 -0700

    add translation tags for task status

--- packages/webapp/src/components/TaskCard/StatusLabel/index.js
+++ packages/webapp/src/components/TaskCard/StatusLabel/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { colors } from '../../../assets/theme';
 import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles({
   container: {
@@ -41,8 +42,17 @@ const useStyles = makeStyles({
 
 const StatusLabel = ({ status = 'forReview', task, assignee = null, style, onClick, ...props }) => {
   const classes = useStyles();
+  const { t } = useTranslation();
 
-  return <div className={clsx(classes.container, classes[status])}>"for review"</div>;
+  const statusText = {
+    forReview: t('TASK.STATUS.FOR_REVIEW'),
+    planned: t('TASK.STATUS.PLANNED'),
+    completed: t('TASK.STATUS.COMPLETED'),
+    late: t('TASK.STATUS.LATE'),
+    abandoned: t('TASK.STATUS.ABANDONED'),
+  };
+
+  return <div className={clsx(classes.container, classes[status])}>{statusText[status]}</div>;
 };
 
 StatusLabel.propTypes = {

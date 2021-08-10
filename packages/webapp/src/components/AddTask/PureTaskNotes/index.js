9b9cbd948e3e2e5a8a6401ca546dbb630fd89dfd
audreyylu <audrey_lu@outlook.com>
2021-08-09 22:37:32 -0700

    task notes input field pre filled with input notes when go back from next page

--- packages/webapp/src/components/AddTask/PureTaskNotes/index.js
+++ packages/webapp/src/components/AddTask/PureTaskNotes/index.js
@@ -3,7 +3,7 @@ import Form from '../../../components/Form';
 import MultiStepPageTitle from '../../../components/PageTitle/MultiStepPageTitle';
 import { useTranslation } from 'react-i18next';
 import { Main } from '../../Typography';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import Button from '../../Form/Button';
 import Input, { getInputErrors } from '../../Form/Input';
 
@@ -24,16 +24,19 @@ const PureTaskNotes = ({
     register,
     setValue,
     getValues,
+    control,
     formState: { errors, isValid },
-  } = useForm({});
+  } = useForm({
+    defaultValues: {
+      task_notes: persistedFormData?.task_notes,
+    },
+  });
 
   useHookFormPersist(persistedPaths, getValues);
   const TASK_NOTES = 'task_notes';
   register(TASK_NOTES, { required: false });
   let task_notes = watch(TASK_NOTES);
-
   const task = persistedFormData.task_type.toUpperCase();
-  console.log(task);
 
   return (
     <>
@@ -64,18 +67,25 @@ const PureTaskNotes = ({
             t('ADD_TASK.TASK')}
         </Main>
 
-        <Input
-          toolTipContent={t('ADD_TASK.SEARCH_INFOI')}
-          label={t('LOG_COMMON.NOTES')}
-          optional={true}
-          style={{ paddingTop: '20px' }}
-          hookFormRegister={register(TASK_NOTES, {
-            required: false,
-            valueAsNumber: false,
-          })}
-          errors={getInputErrors(errors, TASK_NOTES)}
-          name={TASK_NOTES}
-          hookFormSetValue-={setValue}
+        <Controller
+          control={control}
+          name={'task_notes'}
+          render={({ field }) => (
+            <Input
+              toolTipContent={t('ADD_TASK.SEARCH_INFOI')}
+              label={t('LOG_COMMON.NOTES')}
+              optional={true}
+              style={{ paddingTop: '20px' }}
+              hookFormRegister={register(TASK_NOTES, {
+                required: false,
+                valueAsNumber: false,
+              })}
+              errors={getInputErrors(errors, TASK_NOTES)}
+              name={TASK_NOTES}
+              hookFormSetValue-={setValue}
+              {...field}
+            />
+          )}
         />
       </Form>
     </>

e7a6e52d8f95e74d75f67c0b63d6c3c1a7414bdc
audreyylu <audrey_lu@outlook.com>
2021-08-23 10:40:02 -0700

    trying to create custom task type 3

--- packages/webapp/src/containers/AddTask/AddCustomTask/index.js
+++ packages/webapp/src/containers/AddTask/AddCustomTask/index.js
@@ -1,19 +1,20 @@
 import { HookFormPersistProvider } from '../../hooks/useHookFormPersist/HookFormPersistProvider';
 import PureAddCustomTask from '../../../components/AddTask/PureAddCustomTask';
 import { addCustomTask } from '../../Task/saga';
-import { addTaskTypeSaga } from '../../Task/saga';
+import { useDispatch } from 'react-redux';
 
 function AddCustomTask({ history, match }) {
   const onGoBackPath = '/add_task/manage_custom_tasks';
   const persistedPaths = [onGoBackPath];
+
+  const dispatch = useDispatch();
 
   const handleGoBack = () => {
     history.push(onGoBackPath);
   };
 
   const onSave = (payload) => {
-    addTaskTypeSaga(payload);
-    addCustomTask(payload);
+    dispatch(addCustomTask(payload));
     console.log(payload);
     history.push(onGoBackPath);
   };

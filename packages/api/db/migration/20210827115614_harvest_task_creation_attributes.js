7bddc0e70270e20c229604650501020c25e68a92
Shang Heng <shanghengmak@gmail.com>
2021-08-27 12:13:00 -0700

    Add migrations

---
+++ packages/api/db/migration/20210827115614_harvest_task_creation_attributes.js
@@ -0,0 +1,18 @@
+
+exports.up = async function(knex) {
+  await knex.schema.alterTable('harvest_task', function(table) {
+    table.dropColumn('quantity_kg');
+    table.decimal('quantity', 36, 12);
+    table.enu('quantity_unit', ['kg', 'mt', 'lb', 't']).defaultTo('kg');
+    table.boolean('harvest_everything').defaultTo(false);
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.alterTable('harvest_task', function(table) {
+    table.float('quantity_kg');
+    table.dropColumn('quantity_unit');
+    table.dropColumn('harvest_everything');
+    table.decimal('quantity_kg', 36, 12);
+  });
+};

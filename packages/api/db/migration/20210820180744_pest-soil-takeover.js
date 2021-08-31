b535957736a7a5f4048d1a6c0595801db86a54c6
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-08-30 23:54:17 -0300

    fixing soil and products migration to avoid data loss

--- packages/api/db/migration/20210820180744_pest-soil-takeover.js
+++ packages/api/db/migration/20210820180744_pest-soil-takeover.js
@@ -1,14 +1,22 @@
 const { formatAlterTableEnumSql } = require('../util');
 exports.up = async function (knex) {
-  const fertilizers = await knex('fertilizer').where({ farm_id: null });
-  const pesticide = await knex('pesticide').where({ farm_id: null });
-  await Promise.all(fertilizers.map(({ fertilizer_type }) => {
+  const fertilizers = await knex('fertilizer').whereNull('farm_id');
+  const pesticide = await knex('pesticide').whereNull('farm_id');
+  const soilTasks = await knex('soil_amendment_task').whereNotNull('historic_product_id');
+  const pestTasks = await knex('pest_control_task').whereNotNull('historic_product_id');
+  await Promise.all(fertilizers.map(async  ({ fertilizer_type, fertilizer_id }) => {
     const insert = { name: fertilizer_type, deleted: true, type: 'soil_amendment_task' };
-    return knex('product').insert(insert);
+    const [ { product_id }] = await knex('product').insert(insert).returning('*');
+    return Promise.all(soilTasks.filter(({ historic_product_id }) => historic_product_id === fertilizer_id).map(({ task_id }) => {
+      return knex('soil_amendment_task').update({ product_id }).where({ task_id })
+    }));
   }));
-  await Promise.all(pesticide.map(({ pesticide_name }) => {
+  await Promise.all(pesticide.map(async ({ pesticide_name, pesticide_id }) => {
     const insert = { name: pesticide_name, deleted: true, type: 'pest_control_task' };
-    return knex('product').insert(insert);
+    const [ { product_id }] = await knex('product').insert(insert).returning('*');
+    return Promise.all(pestTasks.filter(({ historic_product_id }) => historic_product_id === pesticide_id).map(({ task_id }) => {
+      return knex('pest_control_task').update({ product_id }).where({ task_id })
+    }));
   }));
   await knex.raw('ALTER TABLE pest_control_task DROP CONSTRAINT pest_control_task_type_check');
   await knex('pest_control_task').where({ control_method: 'handPick' }).update({ control_method: 'handWeeding' });
@@ -16,8 +24,13 @@ exports.up = async function (knex) {
     .update({ control_method: 'heatTreatment' });
   await knex.raw(formatAlterTableEnumSql('pest_control_task', 'control_method', [ 'systemicSpray',
     'foliarSpray', 'handWeeding', 'biologicalControl', 'flameWeeding', 'soilFumigation', 'heatTreatment', 'other',
-  ]))
-
+  ]));
+  await knex.schema.alterTable('soil_amendment_task', (t) => {
+    t.dropColumn('historic_product_id');
+  });
+  await knex.schema.alterTable('pest_control_task', (t) => {
+    t.dropColumn('historic_product_id');
+  })
 };
 
 exports.down = async function (knex) {

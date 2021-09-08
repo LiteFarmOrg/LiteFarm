3ed83b6023eea0e786eebd0fc88cf89f2d772792
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-09-08 01:56:21 -0700

    refactor PurePlantingMethod.js

--- packages/webapp/src/components/Crop/PlantingMethod/PureManagementPlanPlantingMethod.js
+++ packages/webapp/src/components/Crop/PlantingMethod/PureManagementPlanPlantingMethod.js
@@ -1,5 +1,5 @@
 import Button from '../../Form/Button';
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { Main } from '../../Typography';
@@ -7,47 +7,13 @@ import Form from '../../Form';
 import { useForm } from 'react-hook-form';
 import MultiStepPageTitle from '../../PageTitle/MultiStepPageTitle';
 import RadioGroup from '../../Form/RadioGroup';
-import styles from './styles.module.scss';
-import { ReactComponent as Individual } from '../../../assets/images/plantingMethod/Individual.svg';
-import { ReactComponent as Rows } from '../../../assets/images/plantingMethod/Rows.svg';
-
-import { ReactComponent as Beds } from '../../../assets/images/plantingMethod/Beds.svg';
-import { ReactComponent as Monocrop } from '../../../assets/images/plantingMethod/Monocrop.svg';
-import { DO_CDN_URL } from '../../../util/constants';
-import ImageModal from '../../Modals/ImageModal';
 import { cloneObject } from '../../../util';
 import Unit from '../../Form/Unit';
 import { seedYield } from '../../../util/unit';
 import { getPlantingMethodPaths } from '../getAddManagementPlanPath';
+import { PurePlantingMethod } from './PurePlantingMethod';
 
-const BROADCAST = 'BROADCAST_METHOD';
-const CONTAINER = 'CONTAINER_METHOD';
-const BEDS = 'BED_METHOD';
-const ROWS = 'ROW_METHOD';
-const images = {
-  [BROADCAST]: [
-    `${DO_CDN_URL}/planting_method/Broadcast_1.webp`,
-    `${DO_CDN_URL}/planting_method/Broadcast_2.webp`,
-    `${DO_CDN_URL}/planting_method/Broadcast_3.webp`,
-  ],
-  [CONTAINER]: [
-    `${DO_CDN_URL}/planting_method/Individual_1.webp`,
-    `${DO_CDN_URL}/planting_method/Individual_2.webp`,
-    `${DO_CDN_URL}/planting_method/Individual_3.webp`,
-  ],
-  [BEDS]: [
-    `${DO_CDN_URL}/planting_method/Bed_1.webp`,
-    `${DO_CDN_URL}/planting_method/Bed_2.webp`,
-    `${DO_CDN_URL}/planting_method/Bed_3.webp`,
-  ],
-  [ROWS]: [
-    `${DO_CDN_URL}/planting_method/Rows_1.webp`,
-    `${DO_CDN_URL}/planting_method/Rows_2.webp`,
-    `${DO_CDN_URL}/planting_method/Rows_3.webp`,
-  ],
-};
-
-export default function PurePlantingMethod({
+export default function PureManagementPlanPlantingMethod({
   useHookFormPersist,
   persistedFormData,
   match,
@@ -95,10 +61,12 @@ export default function PurePlantingMethod({
     shouldUnregister: false,
     defaultValues: cloneObject(persistedFormData),
   });
-  const plantingMethodPrefix = isFinalPlantingMethod ? 'final' : 'initial';
-  const ESTIMATED_YIELD = `crop_management_plan.planting_management_plans.${plantingMethodPrefix}.estimated_yield`;
-  const ESTIMATED_YIELD_UNIT = `crop_management_plan.planting_management_plans.${plantingMethodPrefix}.estimated_yield_unit`;
-  const PLANTING_METHOD = `crop_management_plan.planting_management_plans.${plantingMethodPrefix}.planting_method`;
+  const plantingMethodPrefix = `crop_management_plan.planting_management_plans.${
+    isFinalPlantingMethod ? 'final' : 'initial'
+  }`;
+  const ESTIMATED_YIELD = `${plantingMethodPrefix}.estimated_yield`;
+  const ESTIMATED_YIELD_UNIT = `${plantingMethodPrefix}.estimated_yield_unit`;
+  const PLANTING_METHOD = `${plantingMethodPrefix}.planting_method`;
   const planting_method = watch(PLANTING_METHOD);
   const IS_PLANTING_METHOD_KNOWN = `crop_management_plan.planting_management_plans.${plantingMethodPrefix}.is_planting_method_known`;
   const is_planting_method_known = watch(IS_PLANTING_METHOD_KNOWN);
@@ -139,9 +107,6 @@ export default function PurePlantingMethod({
     );
 
   const disabled = !isValid;
-  const [{ imageModalSrc, imageModalAlt }, setSelectedImage] = useState({});
-  const onImageSelect = (src, alt) => setSelectedImage({ imageModalSrc: src, imageModalAlt: alt });
-  const dismissModal = () => setSelectedImage({});
 
   return (
     <Form
@@ -169,58 +134,17 @@ export default function PurePlantingMethod({
         </>
       )}
       {(!showIsPlantingMethodKnown || is_planting_method_known) && (
-        <>
-          <Main
-            style={{ marginBottom: '24px' }}
-            tooltipContent={t('MANAGEMENT_PLAN.PLANTING_METHOD_TOOLTIP')}
-          >
-            {showIsPlantingMethodKnown
+        <PurePlantingMethod
+          planting_method={planting_method}
+          PLANTING_METHOD={PLANTING_METHOD}
+          title={
+            showIsPlantingMethodKnown
               ? t('MANAGEMENT_PLAN.WHAT_WAS_PLANTING_METHOD')
-              : t('MANAGEMENT_PLAN.PLANTING_METHOD')}
-          </Main>
-          <div className={styles.radioGroupContainer}>
-            <RadioGroup
-              hookFormControl={control}
-              name={PLANTING_METHOD}
-              radios={[
-                {
-                  label: t('MANAGEMENT_PLAN.ROWS'),
-                  value: ROWS,
-                },
-                { label: t('MANAGEMENT_PLAN.BEDS'), value: BEDS },
-                {
-                  label: t('MANAGEMENT_PLAN.INDIVIDUAL_CONTAINER'),
-                  value: CONTAINER,
-                },
-                ...(showBroadcast
-                  ? [{ label: t('MANAGEMENT_PLAN.BROADCAST'), value: BROADCAST }]
-                  : []),
-              ]}
-              required
-            />
-            <div className={styles.radioIconsContainer}>
-              <Rows />
-              <Beds />
-              <Individual />
-              {showBroadcast && <Monocrop />}
-            </div>
-          </div>
-          {planting_method && (
-            <div className={styles.imageGrid}>
-              {images[planting_method].map((url, index) => (
-                <img
-                  key={index}
-                  src={url}
-                  alt={`${planting_method}${index}`}
-                  onClick={() => onImageSelect(url, planting_method)}
-                />
-              ))}
-            </div>
-          )}
-          {imageModalSrc && (
-            <ImageModal src={imageModalSrc} alt={imageModalAlt} dismissModal={dismissModal} />
-          )}
-        </>
+              : t('MANAGEMENT_PLAN.PLANTING_METHOD')
+          }
+          control={control}
+          showBroadcast={showBroadcast}
+        />
       )}
       {is_planting_method_known === false && showIsPlantingMethodKnown && isFinalPlantingMethod && (
         <Unit
@@ -241,7 +165,7 @@ export default function PurePlantingMethod({
   );
 }
 
-PurePlantingMethod.prototype = {
+PureManagementPlanPlantingMethod.prototype = {
   history: PropTypes.object,
   match: PropTypes.object,
   onSubmit: PropTypes.func,

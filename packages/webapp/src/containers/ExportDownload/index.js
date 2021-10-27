dc183100ea94b2d28f0ac98a4d569f3c185871d6
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-10-27 15:02:16 -0300

    Modifying export to be in saga instead of function. Fixes multiple download issue.

--- packages/webapp/src/containers/ExportDownload/index.js
+++ packages/webapp/src/containers/ExportDownload/index.js
@@ -1,54 +1,19 @@
 import { useEffect } from 'react';
-import { getUserFarms } from '../ChooseFarm/saga';
 import { useDispatch, useSelector } from 'react-redux';
-import { userFarmSelector } from '../userFarmSlice';
+import { downloadExport } from './saga';
 
 export default function DownloadExport({ match }) {
-  const exportSource = match.params.id;
-  let fileSrc = atob(exportSource);
+  const { id, from, to } = match.params;
+  let fileSrc = atob(id);
   const dispatch = useDispatch();
-  const farm = useSelector(userFarmSelector);
-  fileSrc = `https://${process.env.REACT_APP_DO_BUCKET_NAME}.nyc3.digitaloceanspaces.com/${fileSrc}.zip`;
 
   useEffect(() => {
-    dispatch(getUserFarms());
+    dispatch(downloadExport({
+      file: `https://${process.env.REACT_APP_DO_BUCKET_NAME}.nyc3.digitaloceanspaces.com/${fileSrc}.zip`,
+      from,
+      to
+  }));
   }, [])
 
-  useEffect(() => {
-    const fileName = farm?.farm_name ? `${farm.farm_name}.zip` : undefined;
-    const config = {
-      headers: {
-        Authorization: 'Bearer ' + localStorage.getItem('farm_token'),
-      },
-      responseType: 'arraybuffer',
-      method: 'GET',
-    };
-    const url = new URL(fileSrc);
-    url.hostname = 'images.litefarm.workers.dev';
-    getResponse(url, config);
-
-    async function getResponse(url, config) {
-      const response = await fetch(url, config);
-      const newBlob = await response.blob();
-      downloadBlob(newBlob, fileName);
-    }
-  }, [farm]);
-
   return <>Loading...</>;
-}
-
-function downloadBlob(blob, name = 'export.zip') {
-  const blobUrl = URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  link.href = blobUrl;
-  link.download = name;
-  document.body.appendChild(link);
-  link.dispatchEvent(
-    new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-      view: window,
-    }),
-  );
-  document.body.removeChild(link);
 }

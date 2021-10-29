b131491de87a333fd1f9c0869362a16910a489e0
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2021-10-29 19:11:16 -0300

    Adding language preference and spanish translations for the certification export email

--- packages/api/src/jobs/certification/email.js
+++ packages/api/src/jobs/certification/email.js
@@ -1,11 +1,11 @@
 const { sendEmail, emails } = require('./../../templates/sendEmailTemplate')
 module.exports = (job) => {
   if(!job.data?.fail) {
-    const { first_name, email, file, farm_id, farm_name, from_date, to_date } = job.data;
+    const { first_name, email, file, farm_id, farm_name, from_date, to_date, language_preference } = job.data;
     const fileWithoutBucket = `${farm_id}${file.split(farm_id)[1]}`;
     const exportLink = `${Buffer.from(fileWithoutBucket).toString('base64')}`
     const buttonLink = `/export/${exportLink}/from/${from_date}/to/${to_date}`;
-    return sendEmail(emails.EXPORT_EMAIL, { first_name, farm_name }, email, {
+    return sendEmail(emails.EXPORT_EMAIL, { first_name, farm_name, locale: language_preference }, email, {
       buttonLink,
     })
   } else {

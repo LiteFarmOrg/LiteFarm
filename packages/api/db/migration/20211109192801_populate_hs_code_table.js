81503490bf1807cbd28c70b47e1dbdb1e7d25376
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-11-09 18:58:18 -0800

    add french crop translation

--- packages/api/db/migration/20211109192801_populate_hs_code_table.js
+++ packages/api/db/migration/20211109192801_populate_hs_code_table.js
@@ -1,17 +1,8 @@
 require('dotenv').config();
-const crops = require('../seeds/seedData/cropHscodeV2');
+const hscodes = require('../seeds/seedData/hscode.json');
 exports.up = function(knex) {
   if (process.env.NODE_ENV !== 'test') {
-    const hsCodesMap = Object.keys(crops).reduce((hsCodesMap, crop_id) => {
-      const hsCode = Number(crops[crop_id]['HS Code']);
-      hsCode && (hsCodesMap[Number(crops[crop_id]['HS Code'])] = crops[crop_id]['HS Code Product']);
-      return hsCodesMap;
-    }, {});
-    const hsCodes = Object.keys(hsCodesMap).map(hs_code => ({
-      hs_code_id: Number(hs_code),
-      description: hsCodesMap[hs_code],
-    }));
-    return knex.batchInsert('hs_code', hsCodes);
+    return knex.batchInsert('hs_code', hscodes);
   }
 };
 

49f174f4cd5d5512cf8290f4080ac54e5db4e85d
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-11-17 14:12:00 -0800

    skip country id population if not on beta or production

--- packages/api/db/migration/20211005205552_populate_farm_country_id.js
+++ packages/api/db/migration/20211005205552_populate_farm_country_id.js
@@ -1,7 +1,8 @@
 const { mapFarmsToCountryId } = require('../../src/jobs/station_sync/countrySync');
 exports.up = function(knex) {
-  if (['production', 'integration'].includes(process.env.NODE_ENV)) return mapFarmsToCountryId(knex);
-  return mapFarmsToCountryId(knex);
+  if (['production', 'integration'].includes(process.env.NODE_ENV)) {
+    return mapFarmsToCountryId(knex);
+  }
 };
 
 exports.down = function(knex) {

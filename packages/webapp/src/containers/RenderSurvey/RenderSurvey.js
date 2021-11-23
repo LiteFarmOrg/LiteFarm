a41fc985ec92eff7751ae292015818fb38855aa8
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-11-23 14:53:36 -0800

    survey stack export stying (#1651)
    * survey stack export stying
    
    * fix survey export style

--- packages/webapp/src/containers/RenderSurvey/RenderSurvey.js
+++ packages/webapp/src/containers/RenderSurvey/RenderSurvey.js
@@ -4,33 +4,34 @@ import { getCurrentDateLong } from '../../util/moment';
 
 export default function RenderSurvey() {
   const { t } = useTranslation();
-  const data = window.data;
-  const organicCertifierSurvey = data.organicCertifierSurvey;
-  const certification = data.certification
-    ? t(`certifications:${data.certification.certification_translation_key}`)
-    : organicCertifierSurvey.requested_certification;
-  const certifier = data.certifier
-    ? data.certifier.certifier_name
-    : organicCertifierSurvey.requested_certifier;
-  return (
+  const data = window?.data;
+  const organicCertifierSurvey = data?.organicCertifierSurvey;
+  const certification = data?.certification
+    ? t(`certifications:${data?.certification.certification_translation_key}`)
+    : organicCertifierSurvey?.requested_certification;
+  const certifier = data?.certifier
+    ? data?.certifier.certifier_name
+    : organicCertifierSurvey?.requested_certifier;
+  return data?.questionAnswerMap ? (
     <div
       style={{
         backgroundColor: 'white',
         transform: 'translateY(-76px)',
         width: '100%',
         zIndex: 9999,
+        padding: '48px 96px',
       }}
     >
       <h1 className={styles.title}>{t('SURVEY_STACK.TITLE', { certification, certifier })}</h1>
       <p className={styles.date}>
         {t('SURVEY_STACK.PRODUCED')}: {getCurrentDateLong()}
       </p>
-      {Object.keys(data.questionAnswerMap).map((label) => (
-        <>
+      {Object.keys(data.questionAnswerMap).map((label, index) => (
+        <div key={index}>
           <h3 className={styles.question}>{label}</h3>
           <p className={styles.answer}>{data.questionAnswerMap[label]}</p>
-        </>
+        </div>
       ))}
     </div>
-  );
+  ) : <></>;
 }

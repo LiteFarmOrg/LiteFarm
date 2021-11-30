596a7018a3aa6d2fccd8169a4305ae2f1c8381c3
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-11-30 10:42:13 -0800

    organicHistoryCheckOnPut organicHistoryCheckOnPost

--- packages/api/src/middleware/validation/organicHistoryLocationCheck.js
+++ packages/api/src/middleware/validation/organicHistoryLocationCheck.js
@@ -13,4 +13,25 @@ async function organicHistoryLocationCheck(req, res, next) {
 
 }
 
-module.exports = organicHistoryLocationCheck;
+async function organicHistoryCheckOnPut(req, res, next) {
+  const location = await locationModel.query().findById(req.params.location_id).withGraphJoined('[figure, field, garden, greenhouse]');
+  if (location && organicHistoryLocations.includes(location.figure.type)) {
+    req.body[location.figure.type].organic_history =
+      req.body[location.figure.type].organic_status === location[location.figure.type].organic_status ?
+        undefined :
+        {
+          organic_status: req.body[location.figure.type].organic_status,
+          effective_date: req.body[location.figure.type].organic_history.effective_date,
+        };
+  }
+  return next();
+}
+
+async function organicHistoryCheckOnPost(req, res, next) {
+  if (organicHistoryLocations.includes(req.body.figure.type)) {
+    req.body[req.body.figure.type].organic_history.organic_status = req.body[req.body.figure.type].organic_status;
+  }
+  return next();
+}
+
+module.exports = { organicHistoryLocationCheck, organicHistoryCheckOnPut, organicHistoryCheckOnPost };

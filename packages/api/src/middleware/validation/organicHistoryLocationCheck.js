6631e6a824bef443c19f9d286fa08e3ff5957703
Steve Mattingly <smattingly@litefarm.org>
2021-12-01 17:39:05 -0500

    [LF-2066] now with GET (#1693)
    * Fix deprecation warnings
    
    * Add table and POST routes
    
    * Fix tests
    
    * Populate data in migration
    
    * Change POST route to be generic location
    
    * Add permissions checks
    
    * Tweak `organic_history` table
    
    * POST organic history on field add, edit
    
    * add organic history on location edit and creation
    
    * fix test
    
    * organicHistoryCheckOnPut organicHistoryCheckOnPost
    
    * Add GET route for organic_history
    
    * Missed file from last commit
    
    * add 404 on get organic history by location id
    
    Co-authored-by: Jimmy Rowland <jimmyrowland@protonmail.com>

--- packages/api/src/middleware/validation/organicHistoryLocationCheck.js
+++ packages/api/src/middleware/validation/organicHistoryLocationCheck.js
@@ -4,14 +4,12 @@ const organicHistoryLocations = [
   'field', 'garden', 'greenhouse',
 ];
 
-async function organicHistoryLocationCheck(req, res, next) {
+async function organicHistoryLocationCheckOnPost(req, res, next) {
   const location = await locationModel.query().join('figure', 'figure.location_id', 'location.location_id')
     .where('location.location_id', req.body.location_id)
     .whereIn('type', organicHistoryLocations);
   if (!location.length) return res.status(400).send('Location must be crop enabled.');
   return next();
-
-
 }
 
 async function organicHistoryCheckOnPut(req, res, next) {
@@ -41,4 +39,4 @@ async function organicHistoryCheckOnPost(req, res, next) {
   return next();
 }
 
-module.exports = { organicHistoryLocationCheck, organicHistoryCheckOnPut, organicHistoryCheckOnPost };
+module.exports = { organicHistoryLocationCheckOnPost, organicHistoryCheckOnPut, organicHistoryCheckOnPost };

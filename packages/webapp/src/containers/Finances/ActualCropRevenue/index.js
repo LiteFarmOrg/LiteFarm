e40600441b400bd344f0e6d412e45d54697121ad
Hernan Carvajal <hernan-david.carvajal-bastidas@inria.fr>
2021-12-16 17:48:13 +0100

    Added the name of the crop as complement to the Crop variety name in the Actual revenue page

--- packages/webapp/src/containers/Finances/ActualCropRevenue/index.js
+++ packages/webapp/src/containers/Finances/ActualCropRevenue/index.js
@@ -3,21 +3,27 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import FinanceGroup from '../../../components/Finances/FinanceGroup';
 import { getManagementPlanCardDate } from '../../../util/moment';
-import { cropVarietyEntitiesSelector } from '../../cropVarietySlice';
+import { cropVarietiesSelector } from '../../cropVarietySlice';
 import { setSelectedSale } from '../actions';
 import { convertFromMetric, roundToTwoDecimal } from '../../../util';
+import { useTranslation } from 'react-i18next';
 
 const ActualCropRevenue = ({ sale, history, ...props }) => {
   const { sale_id, sale_date, customer_name, crop_variety_sale } = sale;
 
   const dispatch = useDispatch();
+  const { t } = useTranslation();
 
   // TODO: optimize this - put in parent component or seek by id
-  const cropVarietyEntities = useSelector(cropVarietyEntitiesSelector);
+  const cropVarietyEntities = {};
+  const cropVarieties= useSelector(cropVarietiesSelector);
 
   let total = 0;
   for (const cvs of crop_variety_sale) {
     total += cvs.sale_value;
+    cropVarietyEntities[cvs.crop_variety_id] = cropVarieties.filter(
+      (c) => c.crop_variety_id === cvs.crop_variety_id
+    )[0]
   }
 
   const onClickForward = () => {
@@ -35,6 +41,11 @@ const ActualCropRevenue = ({ sale, history, ...props }) => {
         const convertedQuantity = roundToTwoDecimal(
           convertFromMetric(cvs.quantity.toString(), cvs.quantity_unit, 'kg').toString(),
         );
+        const cropVariety =  cropVarietyEntities[cvs.crop_variety_id];
+        const title = cropVariety.crop_variety_name
+          ? `${cropVariety.crop_variety_name}, ${t(`crop:${cropVariety.crop.crop_translation_key}`)}`
+          : t(`crop:${cropVariety.crop.crop_translation_key}`);
+
         return {
           title: cropVarietyEntities[cvs.crop_variety_id].crop_variety_name,
           subtitle: `${convertedQuantity} ${cvs.quantity_unit}`,

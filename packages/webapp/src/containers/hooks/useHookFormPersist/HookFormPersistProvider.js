0cf45a9038ee45b14f2fe5ca472806304aad7c40
Jimmy Rowland <jimmyrowland@protonmail.com>
2021-12-29 20:06:44 -0800

    animated HookFormPersistProvider

--- packages/webapp/src/containers/hooks/useHookFormPersist/HookFormPersistProvider.js
+++ packages/webapp/src/containers/hooks/useHookFormPersist/HookFormPersistProvider.js
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { hookFormPersistSelector } from './hookFormPersistSlice';
-import Spinner from '../../../components/Spinner';
 import useHookFormPersist from './index';
+import { motion, useAnimation } from 'framer-motion';
+import history from '../../../history';
 
 export function HookFormPersistProvider({ children }) {
   const persistedFormData = useSelector(hookFormPersistSelector);
-  const [hookFormPersistUnMountResolved, setHookFormPersistUnMountResolved] = useState();
+  const [hookFormPersistUnMountResolved, setHookFormPersistUnMountResolved] = useState(false);
   useEffect(() => {
     setHookFormPersistUnMountResolved(true);
   }, []);
-  return hookFormPersistUnMountResolved ? (
-    React.cloneElement(children, { persistedFormData, useHookFormPersist })
-  ) : (
-    <Spinner />
-  );
+  const controls = useAnimation();
+  useEffect(() => {
+    if (hookFormPersistUnMountResolved) {
+      controls.start({
+        opacity: 1,
+        x: '0',
+        transition: {
+          duration: 0.3,
+          ease: 'easeInOut',
+        },
+      });
+    }
+  }, [hookFormPersistUnMountResolved]);
+
+
+  const initial = { opacity: 0, x: history.action === 'POP' ? '-100%' : '100%' };
+  return <motion.div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }} initial={initial}
+                     animate={controls}>
+    {hookFormPersistUnMountResolved && React.cloneElement(children, { persistedFormData, useHookFormPersist })}
+  </motion.div>;
 }

9a18f36cc3fd95036c6fcf17cb59cdaba20f4bb9
Jimmy Rowland <jimmyrowland@protonmail.com>
2022-01-12 10:12:50 -0800

    add migration to remove planned_time

---
+++ packages/api/db/migration/20220112180626_remove_planned_time_from_task_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.alterTable('task', function (table) {
+    table.dropColumn('planned_time');
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.alterTable('task', function (table) {
+    table.dateTime('planned_time');
+  });
+  const tasks = await knex('task');
+  for (const { task_id, due_date } of tasks) {
+    await knex('task').where('task_id', task_id).update({
+      planned_time: due_date,
+    });
+  }
+};

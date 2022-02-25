4c79d04a6fda31e8db681fe6b2d67adecc70124f
Jimmy Rowland <jimmyrowland@protonmail.com>
2022-02-25 02:16:07 -0800

    LF-2252/add-namespace-separator

--- packages/api/src/jobs/locales/i18n.js
+++ packages/api/src/jobs/locales/i18n.js
@@ -1,19 +1,21 @@
 const i18n = require('i18next');
 const Backend = require('i18next-fs-backend');
 
-i18n
-  .use(Backend)
-  .init({
+i18n.use(Backend).init(
+  {
     fallbackLng: 'en',
     preload: ['en', 'es', 'pt'],
     ns: ['translation', 'crop'],
     defaultNS: 'translation',
+    nsSeparator: ':',
     backend: {
       loadPath: 'src/jobs/locales/{{lng}}/{{ns}}.json',
     },
-  }, (err, t) => {
-    if (err) return console.error(err)
+  },
+  (err) => {
+    if (err) return console.error(err);
     console.log('i18next is ready...');
-  });
+  },
+);
 
 module.exports = i18n;

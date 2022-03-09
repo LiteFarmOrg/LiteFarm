a09f1e6a5de88e65e87444bd451cf47424e1b8c0
Steve Mattingly <mattingly@fastmail.com>
2022-03-08 21:35:21 -0500

    Fix country matching; run sync migration

---
+++ packages/api/db/migration/20220309022932_update_country_id.js
@@ -0,0 +1,9 @@
+const { mapFarmsToCountryId } = require('../../src/jobs/station_sync/countrySync');
+
+exports.up = function (knex) {
+  if (['integration', 'production'].includes(process.env.NODE_ENV)) {
+    return mapFarmsToCountryId(knex);
+  }
+};
+
+exports.down = function () {};

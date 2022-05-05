eb0472aea95f4e543ae0cb7d241b33672ebd134d
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-05-05 20:26:36 +0200

    Cypress tests for LF-1902

--- packages/webapp/cypress/integration/LF1902.spec.js
+++ packages/webapp/cypress/integration/LF1902.spec.js
@@ -1,10 +1,10 @@
 describe.only('Task card', () => {
 
-//Unassign tasks : Farm workers should be able to Assign the task to themselves 
+//Unassigned tasks : Farm workers should be able to assign the task to themselves 
 //(the farm worker and “Unassigned” should be the only quick assign options)
 
 
-//tasks assigned to the farm worker: Farm workers should be able to Unassign the task
+//Tasks assigned to the farm worker: Farm workers should be able to Unassign the task
 // (the farm worker and “Unassigned” should be the only quick assign options)
 
 //Tasks assigned to other individuals on the farm: None! Task card should be read-only

76c7071376277cfde61ee777de120d07c6c503c4
AbdusShaikh <abdus.shaikh123@gmail.com>
2022-05-13 13:46:51 -0400

    Numbers, text, date

--- packages/api/src/jobs/certification/survey_record.js
+++ packages/api/src/jobs/certification/survey_record.js
@@ -27,9 +27,26 @@ module.exports = async (submission, exportId) => {
     survey.revisions[survey.revisions.length - 1].controls,
   );
 
-  const defaultStyles = {
-    verticalAlignment: 'center',
-    fontFamily: 'Calibri',
+  /**
+   * Writes the question in bold and answer without bolding to Excel sheet at the position specified by col and row
+   */
+  const simpleQuestionProcess = (sheet, col, row, data) => {
+    sheet.cell(`${col}${row}`).value(data['Question']).style({ fontFamily: 'Calibri', bold: true });
+
+    sheet
+      .cell(`${col}${(row += 1)}`)
+      .value(data['Answer'])
+      .style({ fontFamily: 'Calibri' });
+  };
+
+  const typeToFuncMap = {
+    string: simpleQuestionProcess, // Text
+    number: simpleQuestionProcess,
+    //   'date': func3,
+    location: simpleQuestionProcess,
+    //   'selectSingle': func4, // Multiple choice
+    //   'ontology': func5, //Dropdown
+    //   'matrix': func6
   };
 
   console.log('THIS IS THE QUESITON & ANSWERS', questionAnswerMap);
@@ -37,16 +54,19 @@ module.exports = async (submission, exportId) => {
   return XlsxPopulate.fromBlankAsync().then((workbook) => {
     // Populate the workbook.
     const sheet1 = workbook.sheet(0);
+    var currentCol = 'A';
     var currentRow = 1;
     for (const qa of questionAnswerMap) {
-      sheet1
-        .cell(`A${currentRow++}`)
-        .value(qa['Question'])
-        .style(defaultStyles);
-      sheet1
-        .cell(`A${currentRow++}`)
-        .value(qa['Answer'])
-        .style(defaultStyles);
+      //   sheet1
+      //     .cell(`${currentCol}${currentRow++}`)
+      //     .value(qa['Question'])
+      //     .style(defaultStyles);
+      //   sheet1
+      //     .cell(`${currentCol}${currentRow++}`)
+      //     .value(qa['Answer'])
+      //     .style(defaultStyles);
+      typeToFuncMap[qa['Type']](sheet1, currentCol, currentRow, qa);
+      currentRow += 2;
     }
     // Write to file.
     return workbook.toFileAsync(`${process.env.EXPORT_WD}/temp/${exportId}/Survey Record.xlsx`);

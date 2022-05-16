f87b832ff3dbd3ccef670d3bf3b574641df32731
AbdusShaikh <abdus.shaikh123@gmail.com>
2022-05-16 10:38:04 -0400

    Majority of multiple choice/checkboxes is working

--- packages/api/src/jobs/certification/survey_record.js
+++ packages/api/src/jobs/certification/survey_record.js
@@ -19,26 +19,26 @@ module.exports = async (submission, exportId) => {
     survey.revisions[survey.revisions.length - 1].controls,
   );
 
-  const getQuestionsInfo = (questionAnswerList) => {
+  const getQuestionInfo = (questionAnswerList) => {
     return questionAnswerList
-      .map(({ label, name, type, hint, options }) => ({
+      .map(({ label, name, type, hint, options, moreInfo }) => ({
         Question: label,
         Answer: submissionData.data[name].value,
         Type: type,
         Hint: hint,
-        Instructions: type == 'instructions' ? options.source.replace(/<p>|<\/p>/g, '') : null,
+        Instructions: type == 'instructions' ? options.source.replace(/<p>|<\/p>/g, '') : null, // Replace the <p> and </p> tags from the string
+        Options: ['selectSingle', 'selectMultiple'].includes(type) ? options.source : null,
+        MoreInfo: moreInfo,
       }))
       .filter((entry) => !['geoJSON', 'script', 'farmOsField'].includes(entry['Type']));
   };
 
-  const questionAnswerMap = getQuestionsInfo(
-    survey.revisions[survey.revisions.length - 1].controls,
-  );
+  const questionAnswerMap = getQuestionInfo(survey.revisions[survey.revisions.length - 1].controls);
 
   /**
    * Writes the question in bold and answer without bolding to Excel sheet at the position specified by col and row
    */
-  const simpleQuestionProcess = (sheet, col, row, data) => {
+  const writeSimpleQs = (sheet, col, row, data) => {
     sheet.cell(`${col}${row}`).value(data['Question']).style({ fontFamily: 'Calibri', bold: true });
 
     sheet
@@ -54,18 +54,47 @@ module.exports = async (submission, exportId) => {
       .style({ fontFamily: 'Calibri', italic: true });
   };
 
+  const writeMultiOptionQs = (sheet, col, row, data) => {
+    sheet.cell(`${col}${row}`).value(data['Question']).style({ fontFamily: 'Calibri', bold: true });
+    if (data.Hint != null) {
+      sheet
+        .cell(`${col}${(row += 1)}`)
+        .value(data['Hint'])
+        .style({ fontFamily: 'Calibri', italic: true });
+    }
+    if (data.MoreInfo != null) {
+      sheet
+        .cell(`${col}${(row += 1)}`)
+        .value(data['MoreInfo'])
+        .style({ fontFamily: 'Calibri', italic: true });
+    }
+    for (const option of data['Options']) {
+      sheet
+        .cell(`${col}${(row += 1)}`)
+        .value(option['value'])
+        .style({ fontFamily: 'Calibri' });
+      if (data['Answer'].includes(option['value'])) {
+        sheet
+          .cell(`${String.fromCharCode(col.charCodeAt(0) + 1)}${row}`)
+          .value('X')
+          .style({ fontFamily: 'Calibri' }); // Write 'X' to the right column
+      }
+    }
+  };
+
   const typeToFuncMap = {
-    string: simpleQuestionProcess, // Text
-    number: simpleQuestionProcess,
+    string: writeSimpleQs, // Text
+    number: writeSimpleQs,
     //   'date': func3,
-    location: simpleQuestionProcess,
-    //   'selectSingle': func4, // Multiple choice
-    ontology: simpleQuestionProcess, //Dropdown
+    location: writeSimpleQs,
+    selectSingle: writeMultiOptionQs, // Multiple choice
+    selectMultiple: writeMultiOptionQs, // Multiple choice
+    ontology: writeSimpleQs, //Dropdown
     //   'matrix': func6
     instructions: writeInstructions,
   };
 
-  console.log('THIS IS THE QUESITON & ANSWERS', questionAnswerMap);
+  console.log('THIS IS THE QUESITON & ANSWERS', questionAnswerMap[0]);
 
   return XlsxPopulate.fromBlankAsync().then((workbook) => {
     // Populate the workbook.
@@ -73,16 +102,8 @@ module.exports = async (submission, exportId) => {
     var currentCol = 'A';
     var currentRow = 1;
     for (const qa of questionAnswerMap) {
-      //   sheet1
-      //     .cell(`${currentCol}${currentRow++}`)
-      //     .value(qa['Question'])
-      //     .style(defaultStyles);
-      //   sheet1
-      //     .cell(`${currentCol}${currentRow++}`)
-      //     .value(qa['Answer'])
-      //     .style(defaultStyles);
       typeToFuncMap[qa['Type']](sheet1, currentCol, currentRow, qa);
-      currentRow += 2;
+      currentRow += 2; // Have each function return a new current postion
     }
     // Write to file.
     return workbook.toFileAsync(`${process.env.EXPORT_WD}/temp/${exportId}/Survey Record.xlsx`);

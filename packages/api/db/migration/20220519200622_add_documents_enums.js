feb9d71b01a7e1cfe65ffe340850acc559f66169
Anant Awasthy <anant.awasthy29@gmail.com>
2022-05-19 13:30:42 -0700

    added receipts and invoices to the documents types in database migration and objection model of document

---
+++ packages/api/db/migration/20220519200622_add_documents_enums.js
@@ -0,0 +1,25 @@
+exports.up = async function (knex) {
+  return knex.schema.raw(`
+    ALTER TABLE "document"
+    DROP CONSTRAINT "document_type_check",
+    ADD CONSTRAINT "document_type_check"
+    CHECK (type IN (
+      'CLEANING_PRODUCT', 'CROP_COMPLIANCE', 'FERTILIZING_PRODUCT',
+      'PEST_CONTROL_PRODUCT', 'SOIL_AMENDMENT', 'SOIL_SAMPLE_RESULTS', 
+      'INVOICES', 'RECEIPTS', 'OTHER'
+    ))
+  `);
+};
+
+exports.down = async function (knex) {
+  return knex.schema.raw(`
+    ALTER TABLE "document"
+    DROP CONSTRAINT "document_type_check",
+    ADD CONSTRAINT "document_type_check"
+    CHECK (type IN (
+      'CLEANING_PRODUCT', 'CROP_COMPLIANCE', 'FERTILIZING_PRODUCT',
+      'PEST_CONTROL_PRODUCT', 'SOIL_AMENDMENT', 'SOIL_SAMPLE_RESULTS', 
+      'OTHER'
+    ))
+  `);
+};

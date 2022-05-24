a40515dd5fe9e4da6ac80fac0dedf30b90aa8004
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-05-24 20:20:07 +0200

    cypress test skeleton for LF-2301

---
+++ packages/webapp/cypress/integration/inviteUserFlow.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress"/>
+
+describe.only('Invite user tests', () => {
+  before(() => {});
+  it('Invite a user with a different chosen langauge ', () => {
+    //Test for LF-2301
+    //after running happy path test
+    //create a user account with language set to Spanish
+    //login to the default farm created by the happy path tests
+    //invite the spanish user to the default farm
+    //Spanish user should receive an invitation email in spanish
+    //repeat for all languages
+  });
+});

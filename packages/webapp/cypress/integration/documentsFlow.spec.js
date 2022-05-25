09303c0d1c9b23cfa0d6016f743f39047725af9c
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-05-25 18:58:09 +0200

    cypress test skeleton for LF-2346

--- packages/webapp/cypress/integration/documentsFlow.spec.js
+++ packages/webapp/cypress/integration/documentsFlow.spec.js
@@ -14,4 +14,26 @@ describe.only('Documents flow tests', () => {
     //Click the “Cancel” button to dismiss the modal without making any changes
     //Click “Unarchive” to change the status of this document to “Valid”
   });
+
+  it('Upload receipt', () => {
+    //Test for LF-2346
+    //Navigate to documents view page
+    //click add a document
+    //upload a sample receipt
+    //select receipt on the type dropdown
+    //type a document name
+    //save the document
+    //ensure document is uploaded correctly and card details are correct
+  });
+
+  it('Upload invoice', () => {
+    //Test for LF-2346
+    //Navigate to documents view page
+    //click add a document
+    //upload a sample invoice
+    //select receipt on the type dropdown
+    //type a document name
+    //save the document
+    //ensure document is uploaded correctly and card details are correct
+  });
 });

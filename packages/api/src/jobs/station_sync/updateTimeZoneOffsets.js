44b79011d4ba4236b9356dfc70577bd734974dc0
Cali0707 <calumramurray@gmail.com>
2022-05-26 11:04:38 -0400

    Updated error console log, used same date for full migration

--- packages/api/src/jobs/station_sync/updateTimeZoneOffsets.js
+++ packages/api/src/jobs/station_sync/updateTimeZoneOffsets.js
@@ -18,23 +18,24 @@ const { Client } = require('@googlemaps/google-maps-services-js');
 const client = new Client({});
 
 async function mapTimeZoneOffsetsToFarms(knex) {
+  const date = new Date(Date.now());
   const farmsWithLatLng = await knex('farm').select('*').whereNotNull('grid_points');
   for (const farm of farmsWithLatLng) {
     try {
       const timeZone = await client.timezone({
         params: {
           location: farm.grid_points,
-          timestamp: new Date(Date.now()),
+          timestamp: date,
           key: process.env.GOOGLE_MAPS_API_KEY,
         },
       });
       await knex('farm')
-        .update('utc_offset', Math.round(timeZone.data.rawOffset / 60))
+        .update('utc_offset', timeZone.data.rawOffset)
         .where('farm_id', farm.farm_id);
     } catch (e) {
       switch (e.response?.data?.status) {
         case 'OVER_QUERY_LIMIT':
-          console.log('Hit query limit for API: waiting for query limit to reset');
+          console.log('Hit query limit for timezones API: waiting for query limit to reset');
           await new Promise((resolve) => setTimeout(resolve, 60000)); // Rate limit is on a per-minute basis
           try {
             const timeZone = await client.timezone({
@@ -53,7 +54,7 @@ async function mapTimeZoneOffsetsToFarms(knex) {
           break;
         case 'OVER_DAILY_LIMIT':
           console.log(
-            'Over the daily limit. Please double check credentials are valid and try again tomorrow',
+            'Over the daily limit fot the timezones API. Please double check credentials are valid and try again tomorrow',
           );
           throw new Error('OVER_DAILY_LIMIT');
         default:

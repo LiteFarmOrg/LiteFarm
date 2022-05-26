0905fd9c1334e8fbae3c7270ef73172d1218cdeb
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-05-26 23:18:47 +0200

    cypress test for LF-2366

--- packages/webapp/cypress/integration/inviteUserFlow.spec.js
+++ packages/webapp/cypress/integration/inviteUserFlow.spec.js
@@ -11,4 +11,29 @@ describe.only('Invite user tests', () => {
     //Spanish user should receive an invitation email in spanish
     //repeat for all languages
   });
+
+  it.only('Invite a user with a different chosen langauge ', () => {
+    //Test for LF-2366
+
+    const userName = 'New User';
+    const userEmail = 'newuser@example.com';
+    //after running happy path test
+    cy.visit('/');
+    //login as an admin user
+    cy.loginFarmOwner();
+    //navigate to the people view
+    cy.visit('/people');
+    //Click invite a user
+    cy.contains('Invite User').click();
+    //Input a name, select a role and an invitation language and click send invitation
+    cy.get('[data-cy=invite-fullName]').should('exist').type(userName);
+    cy.contains('Choose Role').should('exist').click({ force: true });
+    cy.contains('Farm Worker').should('exist').click();
+    cy.get('[data-cy=invite-email]').should('exist').type(userEmail);
+    cy.contains('English').should('exist').click({ force: true });
+    cy.contains('Spanish').should('exist').click();
+    cy.intercept('POST', '/user/invite').as('invite');
+    cy.get('[data-cy=invite-submit]').should('exist').and('not.be.disabled').click();
+    //Ensure invitation is in correct language
+  });
 });

9e8aba28e1893a681a385baa2cb3f56cba922c73
fsayyid <fsayyid@litefarm.org>
2022-05-31 10:55:41 -0400

    fix: adding one day later flag to timed notification endpoints

--- packages/api/src/jobs/notifications/index.js
+++ packages/api/src/jobs/notifications/index.js
@@ -84,7 +84,7 @@ const sendOnSchedule = (queueConfig) => {
           for (const farmId of res.data) {
             // ... send daily 6am notifications ...
             apiQueue.add(
-              { farmId, type: 'Daily', reqConfig },
+              { farmId, type: 'Daily', reqConfig, is_day_later_than_utc: timeZone >= 7 },
               { jobId: `${farmId}-Daily-${utcDay}-${utcHour}` },
             );
 
@@ -94,7 +94,7 @@ const sendOnSchedule = (queueConfig) => {
               (utcDay === WEEKLY_NOTIFICATION_DAY_EARLIER_ZONES && timeZone >= 7)
             ) {
               apiQueue.add(
-                { farmId, type: 'Weekly', reqConfig },
+                { farmId, type: 'Weekly', reqConfig, is_day_later_than_utc: timeZone >= 7 },
                 { jobId: `${farmId}-Weekly-${utcDay}-${utcHour}` },
               );
             }
@@ -115,22 +115,24 @@ const sendOnSchedule = (queueConfig) => {
 
   // Process a job for each API request.
   apiQueue.process((job, done) => {
-    const { type, farmId, reqConfig } = job.data;
+    const { type, farmId, reqConfig, is_day_later_than_utc } = job.data;
     const urls = {
       Daily: 'time_notification/daily_due_today_tasks',
       Weekly: 'time_notification/weekly_unassigned_tasks',
     };
-    axios.post(`${apiUrl}/${urls[type]}/${farmId}`, {}, reqConfig).then(function (res) {
-      if ([200, 201].includes(res.status)) {
-        console.log(
-          `  ${type} notifications for farm ${farmId}: status ${res.status}, ${res.data}`,
-        );
-        done();
-      } else {
-        console.log(`Retrying job ${job.jobId}`);
-        job.retry();
-      }
-    });
+    axios
+      .post(`${apiUrl}/${urls[type]}/${farmId}`, { is_day_later_than_utc }, reqConfig)
+      .then(function (res) {
+        if ([200, 201].includes(res.status)) {
+          console.log(
+            `  ${type} notifications for farm ${farmId}: status ${res.status}, ${res.data}`,
+          );
+          done();
+        } else {
+          console.log(`Retrying job ${job.jobId}`);
+          job.retry();
+        }
+      });
   });
 };
 

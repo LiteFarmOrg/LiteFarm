d41d88e07afc58e8bce17d887beb9cccb5752572
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-06-02 17:38:59 +0200

    Cypress test for LF-2386

--- packages/webapp/cypress/integration/notificationsFlow.spec.js
+++ packages/webapp/cypress/integration/notificationsFlow.spec.js
@@ -34,30 +34,65 @@ describe.only('Notifications flow tests', () => {
     cy.url().should('include', '/notifications');
   });
 
-  it('Weekly scheduled notifications', () => {
+  it.only('Weekly scheduled notifications', () => {
     //Test for LF-2386
     //login as farm manager
     cy.visit('/');
     cy.loginFarmOwner();
     //Create unassigned tasks due this week
     cy.visit('/tasks');
-    cy.createTask(); //sets the task due date to today
-    //set the clock to monday this week
+    cy.createUnassignedTaskThisWeek(); //creates a task due date to today assigned to the logged in user
 
-    const date = new Date();
-    let day = date.getDay();
-    date.setDate(date.getDate() - (day - 1));
-    const alertDateTime = date.setHours(6, 0, 0);
-    cy.log(alertDateTime);
-    cy.clock(alertDateTime);
-    cy.wait(3 * 1000);
+    //post request to the api to generate notifications
 
-    //check notifications are generated for all unassigned tasks due this week on this farm
+    let id;
+    let authorization;
+
+    cy.window()
+      .its('store')
+      .invoke('getState')
+      .its('entitiesReducer.userFarmReducer.farm_id')
+      .then((farm_id) => {
+        id = farm_id;
+        authorization =
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXF1ZXN0VGltZWROb3RpZmljYXRpb25zIjp0cnVlfQ.iadEd66S9ICLLEzZODAN3-gdoA2frUFra-DRGIu2gIc';
+        cy.log(authorization);
+        cy.request({
+          method: 'POST',
+          url: `http://localhost:5001/time_notification/weekly_unassigned_tasks/${id}`,
+          headers: {
+            Authorization: `Bearer ${authorization}`,
+          },
+        })
+          .then((response) => {
+            return response;
+          })
+          .its('status')
+          .should('eq', 201);
+      });
+
     cy.visit('/notifications');
-    cy.url().should('include', '/notifications');
+    cy.get(':nth-child(3) > :nth-child(2) > ._semibold_prr07_51')
+      .contains('Unassigned tasks')
+      .should('exist');
+
+    cy.get(':nth-child(3) > :nth-child(2) > ._text_prr07_119')
+      .contains('You have unassigned tasks due this week.')
+      .should('exist');
+
+    cy.get('._container_ik1f8_1 > :nth-child(3)').click();
+    cy.get('._btn_104r1_28').click();
+
+    const today = new Date();
+    const day = today.getDay();
+    const Monday = today.setDate(today.getDate() - (day - 1));
+    const dispMonday = getDateInputFormat(Monday);
+    cy.get('._pillContainer_70or9_15 > :nth-child(2)')
+      .contains(`From: ${dispMonday}`)
+      .should('exist');
   });
 
-  it.only('Daily scheduled notifications', () => {
+  it('Daily scheduled notifications', () => {
     //Test for LF-2387 run after happyPath
     //login as farm manager
     cy.visit('/');

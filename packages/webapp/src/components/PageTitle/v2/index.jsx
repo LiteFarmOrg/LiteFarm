bcd4bdfba12f1893d061916f68a7a57014f09089
sein <44532313+seinnlee@users.noreply.github.com>
2022-06-02 08:49:50 -0700

    location long title overflow fix

--- packages/webapp/src/components/PageTitle/v2/index.jsx
+++ packages/webapp/src/components/PageTitle/v2/index.jsx
@@ -5,23 +5,18 @@ import { BsChevronLeft } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 import { CancelButton } from '../CancelButton';
 
-function PageTitle({
-  title,
-  onGoBack,
-  onCancel,
-  style,
-  cancelModalTitle,
-  label,
-}) {
+function PageTitle({ title, onGoBack, onCancel, style, cancelModalTitle, label }) {
   return (
     <div className={styles.container} style={style}>
-      <div className={styles.leftContainer}>
+      <div className={styles.leftContainer} style={{ overflow: 'hidden', wordBreak: 'break-word' }}>
         {onGoBack && (
           <button type={'button'} className={styles.buttonContainer} onClick={onGoBack}>
             <BsChevronLeft style={{ fontSize: '20px' }} />
           </button>
         )}
-        <Title style={{ marginBottom: 0 }}>{title}</Title>
+        <Title style={{ marginBottom: 0 }}>
+          {title.length > 77 ? `${title.substring(0, 77).trim()}...` : title}
+        </Title>
       </div>
       {!!onCancel && <CancelButton onCancel={onCancel} cancelModalTitle={cancelModalTitle} />}
       {label}

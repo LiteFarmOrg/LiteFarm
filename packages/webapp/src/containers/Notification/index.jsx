6ba8f31e4addee42cb1c0240b35646b709a648ea
Jag <jagdhillon077@gmail.com>
2022-06-06 14:35:58 -0700

    Notifications are based on time rather than alert status

--- packages/webapp/src/containers/Notification/index.jsx
+++ packages/webapp/src/containers/Notification/index.jsx
@@ -53,7 +53,7 @@ export default function NotificationPage() {
       {notifications.length > 0 ? (
         notifications
           .sort((a, b) => {
-            if (a.alert !== b.alert) return a.alert ? -1 : 1;
+            // if (a.alert !== b.alert) return a.alert ? -1 : 1;
             return new Date(a.created_at) - new Date(b.created_at);
           })
           .map((notification) => {

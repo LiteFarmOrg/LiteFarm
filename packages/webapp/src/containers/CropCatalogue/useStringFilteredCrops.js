87a931a851c629a845479f9565b5516cd4248486
sein <44532313+seinnlee@users.noreply.github.com>
2022-06-07 11:20:16 -0700

    underscore ignored from search

--- packages/webapp/src/containers/CropCatalogue/useStringFilteredCrops.js
+++ packages/webapp/src/containers/CropCatalogue/useStringFilteredCrops.js
@@ -10,6 +10,7 @@ export default function useStringFilteredCrops(crops, filterString) {
         .normalize('NFD')
         .replace(/\p{Diacritic}/gu, '')
         .replace(/\W/g, '')
+        .replace(/_/g, '')
         .trim() || '';
     const check = (names) => {
       for (const name of names) {
@@ -19,6 +20,7 @@ export default function useStringFilteredCrops(crops, filterString) {
             .normalize('NFD')
             .replace(/\p{Diacritic}/gu, '')
             .replace(/\W/g, '')
+            .replace(/_/g, '')
             .trim()
             .includes(lowerCaseFilter)
         )

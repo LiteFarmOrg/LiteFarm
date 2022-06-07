19c0caa4263bf1dcb33cacf6514f4bc7f1a5f5e2
AbdusShaikh <abdus.shaikh123@gmail.com>
2022-06-07 11:07:43 -0400

    Alert showing when value <= 0

--- packages/webapp/src/containers/Finances/NewExpense/AddExpense/index.jsx
+++ packages/webapp/src/containers/Finances/NewExpense/AddExpense/index.jsx
@@ -109,6 +109,8 @@ class AddExpense extends Component {
 
     if (data.length < 1) {
       alert(this.props.t('EXPENSE.ADD_EXPENSE.REQUIRED_ERROR'));
+    } else if (data.filter((d) => d.value <= 0).length > 0) {
+      alert(this.props.t('EXPENSE.ADD_EXPENSE.MIN_ERROR') + '0');
     } else {
       this.props.dispatch(addExpenses(data));
       history.push('/finances');

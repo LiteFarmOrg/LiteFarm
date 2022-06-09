143ef1fbc4615b6ed789385c8bf2b125d6c2d81c
fsayyid <fsayyid@litefarm.org>
2022-06-09 17:12:54 -0400

    fix: fetch notifications on page load

--- packages/webapp/src/containers/Notification/NotificationReadOnly/index.jsx
+++ packages/webapp/src/containers/Notification/NotificationReadOnly/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import PureNotificationReadOnly from '../../../components/Notifications';
 import { notificationSelector, relatedNotificationSelector } from '../../notificationSlice';
+import { getNotification } from '../saga';
 
 export default function NotificationReadOnly({ history, match }) {
+  const dispatch = useDispatch();
   const { notification_id } = match.params;
   const notification = useSelector(notificationSelector(notification_id));
   const relatedNotifications = useSelector(relatedNotificationSelector(notification.ref?.entity));
+
+  useEffect(() => {
+    dispatch(getNotification());
+  }, []);
+
   const onGoBack = () => {
     history.push('/notifications');
   };

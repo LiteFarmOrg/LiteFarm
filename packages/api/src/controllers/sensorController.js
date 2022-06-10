035ade96c623ff444b21ad588461c8f8dd1c6ff8
Cali0707 <calumramurray@gmail.com>
2022-06-10 15:13:38 -0400

    Added validation to csv parseing

--- packages/api/src/controllers/sensorController.js
+++ packages/api/src/controllers/sensorController.js
@@ -16,9 +16,18 @@
 const SensorController = {
   async bulkUploadSensors(req, res) {
     const { data, errors } = parseCsvString(req.file.buffer.toString(), {
-      Test1: 'firstValue',
-      test2: 'secondValue',
-      test3: 'thirdValue',
+      Test1: {
+        key: 'firstValue',
+        validator: () => true,
+      },
+      test2: {
+        key: 'secondValue',
+        validator: () => true,
+      },
+      test3: {
+        key: 'thirdValue',
+        validator: () => true,
+      },
     });
     if (errors.length > 0) {
       res.status(400).send({ errors });
@@ -32,27 +41,31 @@ const SensorController = {
 /**
  * Parses the csv string into an array of objects and an array of any lines that experienced errors.
  * @param {String} csvString
- * @param {Object} headerToKeyMap
+ * @param {Object} mapping - a mapping from csv column headers to object keys, as well as the validators for the data in the columns
  * @param {String} delimiter
  * @returns {Object<data: Array<Object>, errors: Array<Object>>}
  */
 
-const parseCsvString = (csvString, headerToKeyMap, delimiter = ',') => {
+const parseCsvString = (csvString, mapping, delimiter = ',') => {
   // regex checks for delimiters that are not contained within quotation marks
   const regex = new RegExp(`(?!\\B"[^"]*)${delimiter}(?![^"]*"\\B)`);
   const headers = csvString.substring(0, csvString.indexOf('\n')).split(regex);
-  const allowedHeaders = Object.keys(headerToKeyMap);
+  const allowedHeaders = Object.keys(mapping);
   const { data, errors } = csvString
     .substring(csvString.indexOf('\n') + 1)
     .split('\n')
     .reduce(
-      (previous, row) => {
+      (previous, row, rowIndex) => {
         const values = row.split(regex);
-        const parsedRow = headers.reduce((previous, current, index) => {
+        const parsedRow = headers.reduce((previousObj, current, index) => {
           if (allowedHeaders.includes(current)) {
-            previous[headerToKeyMap[current]] = values[index];
+            if (mapping[current].validator(values[index])) {
+              previousObj[mapping[current].key] = values[index];
+            } else {
+              previous.errors.push({ line: rowIndex + 2, errorMessage: 'Something went wrong!' });
+            }
           }
-          return previous;
+          return previousObj;
         }, {});
         previous.data.push(parsedRow);
         return previous;

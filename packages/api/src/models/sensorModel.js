3d4b297a9c68751585c9eb8bba1df588402414ca
AbdusShaikh <abdus.shaikh123@gmail.com>
2022-06-10 10:44:35 -0400

    Create sensorModel.js

---
+++ packages/api/src/models/sensorModel.js
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ *  This file (farmModel.js) is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+const Model = require('objection').Model;
+
+class Sensor extends Model {
+  static get tableName() {
+    return 'sensors';
+  }
+
+  static get idColumn() {
+    return 'sensor_id';
+  }
+
+  // Optional JSON schema. This is not the database schema! Nothing is generated
+  // based on this. This is only used for validation. Whenever a model instance
+  // is created it is checked against this schema. http://json-schema.org/.
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['sensor_id', 'farm_id', 'name', 'latitude', 'longitude', 'type'],
+
+      properties: {
+        sensor_id: { type: 'string' },
+        farm_id: { type: 'string', minLength: 1, maxLength: 255 },
+        name: { type: 'string', minLength: 1, maxLength: 255 },
+        latitude: { type: 'float' },
+        longitude: { type: 'float' },
+        type: { type: 'string' },
+        depth: { type: 'float' },
+        elevation: { type: 'float' },
+      },
+      additionalProperties: false,
+    };
+  }
+}
+
+module.exports = Sensor;

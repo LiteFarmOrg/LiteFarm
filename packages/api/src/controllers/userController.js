e6e06b8e25f36513f16970d8b6fa5e55964fed56
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-06-14 20:52:14 +0200

    added farm invite and confirmation emails to happyPath test

--- packages/api/src/controllers/userController.js
+++ packages/api/src/controllers/userController.js
@@ -135,7 +135,7 @@ const userController = {
       return res.status(400).send(errorMessage);
     }
 
-    const validEmailRegex = RegExp(/^$|^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+    const validEmailRegex = RegExp(/^$|^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i);
     if (!validEmailRegex.test(email)) {
       return res.status(400).send('Invalid email');
     }

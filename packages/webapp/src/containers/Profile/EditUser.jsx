d60cf2dce1a908ba603fa9914c246bff1673a9b3
Cali0707 <calumramurray@gmail.com>
2022-07-14 16:26:09 -0400

    Fixed update user

--- packages/webapp/src/containers/Profile/EditUser.jsx
+++ packages/webapp/src/containers/Profile/EditUser.jsx
@@ -11,23 +11,23 @@ export default function EditUser({ history, match }) {
   const { user_id } = match.params;
   const userFarm = userFarmsEntities[farm_id]?.[user_id];
 
-  const getReqBody = data => {
-    const role_id = Number(data.role_id?.value);
+  const getReqBody = (data) => {
+    const role_id = data.role_id ? parseInt(data.role_id?.value) : null;
     const reqBody = {
       ...data,
       user_id,
       role_id,
       wage: { amount: data.wage.amount, type: userFarm.wage?.type || 'hourly' },
     };
-    if (role_id === userFarm.role_id) delete reqBody.role_id;
+    if (role_id === userFarm.role_id || !role_id) delete reqBody.role_id;
     if (data.wage?.amount === userFarm.wage?.amount) delete reqBody.wage;
     return reqBody;
   };
 
-  const onUpdate = data => {
+  const onUpdate = (data) => {
     dispatch(updateUserFarm(getReqBody(data)));
   };
-  const onInvite = data => {
+  const onInvite = (data) => {
     dispatch(invitePseudoUser(getReqBody(data)));
   };
   const onRevoke = () => {
@@ -36,6 +36,15 @@ export default function EditUser({ history, match }) {
   const onActivate = () => {
     dispatch(reactivateUser(user_id));
   };
-  return <PureEditUser onActivate={onActivate} userFarm={userFarm} isAdmin={isAdmin} onUpdate={onUpdate}
-                       onRevoke={onRevoke} history={history} onInvite={onInvite} />;
+  return (
+    <PureEditUser
+      onActivate={onActivate}
+      userFarm={userFarm}
+      isAdmin={isAdmin}
+      onUpdate={onUpdate}
+      onRevoke={onRevoke}
+      history={history}
+      onInvite={onInvite}
+    />
+  );
 }

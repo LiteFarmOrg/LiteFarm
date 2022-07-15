ae8c3802b7f4802ae8507a29879089309e17de86
fsayyid <fsayyid@litefarm.org>
2022-07-15 15:24:17 -0400

    migration for sensors as standard location

---
+++ packages/api/db/migration/20220715165156_sensor_as_standard_location.js
@@ -0,0 +1,89 @@
+exports.up = async function (knex) {
+  // delete sensor table data
+  const sensorLocationIds = await knex('sensor').select('location_id');
+
+  if (sensorLocationIds.length > 0) {
+    await knex.raw(`
+            BEGIN TRANSACTION;
+            DELETE FROM point p WHERE p.figure_id IN
+            (SELECT f.figure_id FROM figure f WHERE f.type = 'sensor');
+            DELETE FROM figure f WHERE f.type = 'sensor';
+            DELETE FROM sensor_reading;
+            DELETE FROM sensor_reading_type;
+            DELETE FROM sensor;
+            COMMIT;
+        `);
+    await knex('location').whereIn('location_id', sensorLocationIds).del();
+  }
+
+  await knex.raw(`
+        DROP TABLE sensor_reading;
+        DROP TABLE sensor_reading_type;
+        DROP TABLE sensor;
+    `);
+
+  await knex.schema.createTable('sensor', function (table) {
+    table
+      .uuid('location_id')
+      .primary()
+      .notNullable()
+      .references('location_id')
+      .inTable('location')
+      .onDelete('CASCADE');
+    table.uuid('farm_id').notNullable();
+    table.string('name').notNullable();
+    table
+      .integer('partner_id')
+      .references('partner_id')
+      .inTable('integrating_partner')
+      .notNullable();
+    table.string('external_id').notNullable();
+    table.float('depth');
+    table.float('elevation');
+  });
+
+  await knex.schema.createTable('sensor_reading', function (table) {
+    table.uuid('reading_id').primary().notNullable().defaultTo(knex.raw('uuid_generate_v1()'));
+    table
+      .string('location_id')
+      .notNullable()
+      .references('location_id')
+      .inTable('sensor')
+      .onDelete('CASCADE');
+    table.timestamp('read_time').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.string('reading_type').notNullable();
+    table.float('value').notNullable();
+    table.string('unit').notNullable();
+    table.boolean('valid').notNullable().defaultTo(true);
+  });
+
+  await knex.schema.createTable('sensor_reading_type', function (table) {
+    table
+      .uuid('sensor_reading_type_id')
+      .primary()
+      .notNullable()
+      .defaultTo(knex.raw('uuid_generate_v1()'));
+    table
+      .uuid('partner_reading_type_id')
+      .references('partner_reading_type_id')
+      .inTable('partner_reading_type');
+    table.uuid('location_id').references('location_id').inTable('sensor').onDelete('CASCADE');
+  });
+};
+
+exports.down = function (knex) {
+  // delete sensor table data
+  knex.raw(`
+  BEGIN TRANSACTION;
+  DELETE FROM point p WHERE p.figure_id IN
+  (SELECT f.figure_id FROM figure f WHERE f.type = 'sensor');
+  DELETE FROM figure f WHERE WHERE f.type = 'sensor';
+  DELETE FROM location l WHERE l.location_id IN
+  (SELECT s.location_id FROM sensor);
+  DROP TABLE sensor_reading;
+  DROP TABLE sensor_reading_type;
+  DROP TABLE sensor;
+  COMMIT;
+`);
+};

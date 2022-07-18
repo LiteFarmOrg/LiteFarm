9e157922b7f4829a23dcfc45485b83c93fb01dae
Cali0707 <calumramurray@gmail.com>
2022-07-18 15:04:25 -0400

    Made cherry pick

--- packages/webapp/src/containers/Profile/EditUser.jsx
+++ packages/webapp/src/containers/Profile/EditUser.jsx
@@ -2,17 +2,22 @@ import PureEditUser from '../../components/Profile/EditUser';
 import { useDispatch, useSelector } from 'react-redux';
 import { isAdminSelector, userFarmEntitiesSelector, userFarmSelector } from '../userFarmSlice';
 import { deactivateUser, invitePseudoUser, reactivateUser, updateUserFarm } from './People/saga';
+import { useMemo } from 'react';
 
 export default function EditUser({ history, match }) {
-  const { farm_id } = useSelector(userFarmSelector);
+  const { farm_id, user_id: currentUserId } = useSelector(userFarmSelector);
   const isAdmin = useSelector(isAdminSelector);
   const dispatch = useDispatch();
   const userFarmsEntities = useSelector(userFarmEntitiesSelector);
   const { user_id } = match.params;
   const userFarm = userFarmsEntities[farm_id]?.[user_id];
 
-  const getReqBody = data => {
-    const role_id = Number(data.role_id?.value);
+  const isCurrentUser = useMemo(() => {
+    return user_id === currentUserId;
+  }, [user_id, currentUserId]);
+
+  const getReqBody = (data) => {
+    const role_id = parseInt(data.role_id?.value);
     const reqBody = {
       ...data,
       user_id,
@@ -24,10 +29,10 @@ export default function EditUser({ history, match }) {
     return reqBody;
   };
 
-  const onUpdate = data => {
+  const onUpdate = (data) => {
     dispatch(updateUserFarm(getReqBody(data)));
   };
-  const onInvite = data => {
+  const onInvite = (data) => {
     dispatch(invitePseudoUser(getReqBody(data)));
   };
   const onRevoke = () => {
@@ -36,6 +41,16 @@ export default function EditUser({ history, match }) {
   const onActivate = () => {
     dispatch(reactivateUser(user_id));
   };
-  return <PureEditUser onActivate={onActivate} userFarm={userFarm} isAdmin={isAdmin} onUpdate={onUpdate}
-                       onRevoke={onRevoke} history={history} onInvite={onInvite} />;
+  return (
+    <PureEditUser
+      onActivate={onActivate}
+      userFarm={userFarm}
+      isAdmin={isAdmin}
+      onUpdate={onUpdate}
+      onRevoke={onRevoke}
+      history={history}
+      onInvite={onInvite}
+      isCurrentUser={isCurrentUser}
+    />
+  );
 }

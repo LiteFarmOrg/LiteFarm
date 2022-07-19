b88512e3f5fad61300bfe2d27124cb68cdfaebd8
fsayyid <fsayyid@litefarm.org>
2022-07-19 10:51:32 -0400

    fix: tasks that do not have ON DELETE CASCADE constraint

--- packages/api/db/deleteSensorData.js
+++ packages/api/db/deleteSensorData.js
@@ -15,31 +15,45 @@ async function deleteSensorData(knex) {
     );
 
     const tasksWithSensorLocations = tasksWithSensorLocationsObjs.rows.map((t) => t.task_id);
-    await knex.transaction(async (trx) => {
-      try {
-        await knex.raw(
-          `
-                  DELETE FROM point p WHERE p.figure_id IN
-                  (SELECT f.figure_id FROM figure f WHERE f.type = 'sensor');
-                  DELETE FROM figure f WHERE f.type = 'sensor';
-                  DELETE FROM sensor_reading;
-                  DELETE FROM sensor_reading_type;
-                  DELETE FROM sensor;
-                `,
-        );
-        await knex.raw(`DELETE FROM task WHERE task_id = ANY(?);`, [tasksWithSensorLocations]);
-        await knex.raw(`DELETE FROM location WHERE location_id = ANY(?);`, [sensorLocationIds]);
-        await knex.raw(
-          `
-                  DROP TABLE sensor_reading;
-                  DROP TABLE sensor_reading_type;
-                  DROP TABLE sensor;
-                `,
-        );
-        trx.commit();
-      } catch (e) {
-        trx.rollback();
-      }
+    await knex.transaction(async () => {
+      await knex.raw(
+        `
+            DELETE FROM point p WHERE p.figure_id IN
+            (SELECT f.figure_id FROM figure f WHERE f.type = 'sensor');
+            DELETE FROM figure f WHERE f.type = 'sensor';
+            DELETE FROM sensor_reading;
+            DELETE FROM sensor_reading_type;
+            DELETE FROM sensor;
+          `,
+      );
+      await knex.raw(`DELETE FROM cleaning_task WHERE task_id = ANY(?);`, [
+        tasksWithSensorLocations,
+      ]);
+      await knex.raw(`DELETE FROM maintenance_task WHERE task_id = ANY(?);`, [
+        tasksWithSensorLocations,
+      ]);
+      await knex.raw(`DELETE FROM sale_task WHERE task_id = ANY(?);`, [tasksWithSensorLocations]);
+      await knex.raw(`DELETE FROM social_task WHERE task_id = ANY(?);`, [tasksWithSensorLocations]);
+      await knex.raw(`DELETE FROM sale_task WHERE task_id = ANY(?);`, [tasksWithSensorLocations]);
+      await knex.raw(`DELETE FROM transplant_task WHERE task_id = ANY(?);`, [
+        tasksWithSensorLocations,
+      ]);
+      await knex.raw(`DELETE FROM sale_task WHERE task_id = ANY(?);`, [tasksWithSensorLocations]);
+      await knex.raw(`DELETE FROM transport_task WHERE task_id = ANY(?);`, [
+        tasksWithSensorLocations,
+      ]);
+      await knex.raw(`DELETE FROM wash_and_pack_task WHERE task_id = ANY(?);`, [
+        tasksWithSensorLocations,
+      ]);
+      await knex.raw(`DELETE FROM task WHERE task_id = ANY(?);`, [tasksWithSensorLocations]);
+      await knex.raw(`DELETE FROM location WHERE location_id = ANY(?);`, [sensorLocationIds]);
+      await knex.raw(
+        `
+                DROP TABLE sensor_reading;
+                DROP TABLE sensor_reading_type;
+                DROP TABLE sensor;
+            `,
+      );
     });
   } else {
     await knex.raw(

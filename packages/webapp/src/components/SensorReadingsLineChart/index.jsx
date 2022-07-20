e9efe19344d3ebf45aeaf0516d5637a89245c65e
Anant Awasthy <anant.awasthy29@gmail.com>
2022-07-20 01:27:21 -0700

    Finished the color toggle and checkbox fuctionality on legends

--- packages/webapp/src/components/SensorReadingsLineChart/index.jsx
+++ packages/webapp/src/components/SensorReadingsLineChart/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
 import {
   LineChart,
@@ -9,11 +9,12 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  Surface,
+  Symbols,
 } from 'recharts';
 import { Label, Semibold } from '../Typography';
 import PropTypes from 'prop-types';
 import AxisLabel from './AxisLabel';
-import { NO_DATA } from '../../containers/SensorReadings/constants';
 
 const PureSensorReadingsLineChart = ({
   title,
@@ -25,12 +26,68 @@ const PureSensorReadingsLineChart = ({
   yAxisLabel,
   chartData,
 }) => {
-  const [selectedLine, setSelectedLine] = useState(null);
+  const [disabledLegends, setDisabledLegends] = useState({});
+  const [activeLegends, setActiveLegends] = useState({});
+  const [isActive, setIsActive] = useState(false);
+  const [legendsList, setLegendsList] = useState(() =>
+    yAxisDataKeys.reduce((acc, cv, idx) => {
+      acc[cv] = {
+        id: cv,
+        value: cv,
+        type: 'square',
+        color: lineColors[idx % lineColors.length],
+      };
+      return acc;
+    }, {}),
+  );
 
-  const selectLine = (event) => {
-    if (event.dataKey.includes(NO_DATA)) return;
-    let sl = selectedLine === event.dataKey ? null : event.dataKey.trim();
-    setSelectedLine(sl);
+  useEffect(() => {
+    const activeLegends = Object.keys(legendsList).reduce((acc, cv) => {
+      if (!Object.keys(disabledLegends).includes(cv)) {
+        acc[cv] = legendsList[cv];
+      }
+      return acc;
+    }, {});
+    setActiveLegends(activeLegends);
+  }, [disabledLegends, legendsList, isActive]);
+
+  const handleLabelClick = (entry) => {
+    setDisabledLegends((dis) => {
+      if (dis.hasOwnProperty(entry.value)) delete dis[entry.value];
+      else dis[entry.value] = entry;
+      return dis;
+    });
+    setIsActive((isA) => !isA);
+  };
+
+  const renderCusomizedLegend = ({ payload }) => {
+    return (
+      <div className={styles.legendWrapper}>
+        {Object.values(payload).map((entry, idx) => {
+          const { value = '', color = '' } = entry;
+          const active = Object.keys(disabledLegends).includes(value);
+          const style = {
+            color: active ? '#AAA' : '#000',
+            display: 'flex',
+            alignItems: 'center',
+          };
+          return (
+            <div
+              style={style}
+              key={idx}
+              onClick={() => handleLabelClick(entry)}
+              className={styles.legendContainer}
+            >
+              <Surface style={{ marginTop: '2px' }} width={10} height={10}>
+                <Symbols cx={5} cy={5} type="circle" size={50} fill={color} />
+                {active && <Symbols cx={5} cy={5} type="circle" size={25} fill={'#FFF'} />}
+              </Surface>
+              <span style={{ marginLeft: '4px' }}>{value}</span>
+            </div>
+          );
+        })}
+      </div>
+    );
   };
 
   return (
@@ -69,19 +126,19 @@ const PureSensorReadingsLineChart = ({
               verticalAlign="top"
               align="center"
               wrapperStyle={{ top: 10, left: 50 }}
-              onClick={selectLine}
+              payload={Object.values(legendsList)}
+              content={renderCusomizedLegend}
             />
           )}
           {yAxisDataKeys.length &&
-            yAxisDataKeys.map((attribute, idx) => (
+            Object.values(activeLegends).map((attribute, idx) => (
               <Line
                 key={idx}
                 strokeWidth={2}
-                dataKey={
-                  selectedLine === null || selectedLine === attribute ? attribute : `${attribute} `
-                }
-                stroke={lineColors[idx % lineColors.length]}
+                dataKey={attribute.value}
+                stroke={attribute.color}
                 activeDot={{ r: 6 }}
+                isAnimationActive={false}
               />
             ))}
         </LineChart>

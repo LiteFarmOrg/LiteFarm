26c5ede28976c932a8c9c27c5c30fca7ab39ac21
Anant Awasthy <anant.awasthy29@gmail.com>
2022-07-21 11:36:35 -0700

    Fixed the temperature unit change

--- packages/webapp/src/containers/SensorReadings/index.jsx
+++ packages/webapp/src/containers/SensorReadings/index.jsx
@@ -6,10 +6,10 @@ import { CURRENT_DATE_TIME } from './constants';
 import PageTitle from '../../components/PageTitle/v2';
 import RouterTab from '../../components/RouterTab';
 import { bulkSensorsReadingsSliceSelector } from '../bulkSensorReadingsSlice';
-import { weatherSelector } from '../WeatherBoard/weatherSlice';
 import { sensorsSelector } from '../sensorSlice';
 import utils from '../WeatherBoard/utils';
 import { TEMPERATURE } from './constants';
+import { measurementSelector } from '../../containers/userFarmSlice';
 
 function SensorReadings({ history, match }) {
   const { t } = useTranslation();
@@ -19,8 +19,8 @@ function SensorReadings({ history, match }) {
   const { latestMinTemperature = '', latestMaxTemperature = '' } = useSelector(
     bulkSensorsReadingsSliceSelector,
   );
-  const { measurement } = useSelector(weatherSelector);
-  const { tempUnit } = utils.getUnits(measurement);
+  const measurementUnit = useSelector(measurementSelector);
+  const { tempUnit } = utils.getUnits(measurementUnit);
 
   return (
     <div style={{ padding: '24px 16px', height: '100%' }}>

8e1168f8ffcf8eb8283957096e5f47ae25ce835d
Steve Mattingly <mattingly@fastmail.com>
2022-07-25 15:40:59 -0400

    Add env vars to set choice of buckets

--- packages/api/src/jobs/certification/do_retrieve.js
+++ packages/api/src/jobs/certification/do_retrieve.js
@@ -1,11 +1,7 @@
-const { spawn }= require('child_process')
+const { spawn } = require('child_process');
 const fs = require('fs').promises;
 const path = require('path');
-const bucketNames = {
-  production: 'litefarm-app-secret',
-  integration: 'litefarm-beta-secret',
-  development: 'litefarm-dev-secret',
-}
+const { getPrivateS3BucketName } = require('../../util/digitalOceanSpaces');
 
 module.exports = (nextQueue, emailQueue) => (job, done) => {
   console.log('JOB DATA', JSON.stringify(job.data));
@@ -14,43 +10,47 @@ module.exports = (nextQueue, emailQueue) => (job, done) => {
   const args = [
     's3', // command
     'cp', //sub command
-    `s3://${getS3BucketName()}/${farm_id}/document`, // location
+    `s3://${getPrivateS3BucketName()}/${farm_id}/document`, // location
     `temp/${exportId}`, // destination
     '--recursive',
     '--endpoint=https://nyc3.digitaloceanspaces.com',
     '--exclude=*',
-  ].concat(files.map(({ url }) => `--include=${url.split('/').pop()}`))
+  ].concat(files.map(({ url }) => `--include=${url.split('/').pop()}`));
   const awsCopyProcess = spawn('aws', args, { cwd: process.env.EXPORT_WD });
-  awsCopyProcess.on('exit', childProcessExitCheck(() => {
-    const fileNames = files.map(({ url, file_name }) => ({
-      oldName: url.split('/').pop(),
-      newName: file_name,
-    }));
-    Promise.all(fileNames.map(({ oldName, newName }) => {
-      const directory = path.join(process.env.EXPORT_WD, 'temp', exportId);
-      return fs.stat(path.join(directory, oldName)).then(() =>
-        fs.rename(path.join(directory, oldName), path.join(directory, newName)),
-      ).catch(e => console.log('Could not find file', newName));
-    })).then(() => {
-      done();
-      nextQueue.add(job.data, { removeOnComplete: true });
-    })
-  }, ()=> {
-    done();
-    emailQueue.add({ fail: true, email }, { removeOnComplete: true });
-  }));
-}
+  awsCopyProcess.on(
+    'exit',
+    childProcessExitCheck(
+      () => {
+        const fileNames = files.map(({ url, file_name }) => ({
+          oldName: url.split('/').pop(),
+          newName: file_name,
+        }));
+        Promise.all(
+          fileNames.map(({ oldName, newName }) => {
+            const directory = path.join(process.env.EXPORT_WD, 'temp', exportId);
+            return fs
+              .stat(path.join(directory, oldName))
+              .then(() => fs.rename(path.join(directory, oldName), path.join(directory, newName)))
+              .catch(() => console.log('Could not find file', newName));
+          }),
+        ).then(() => {
+          done();
+          nextQueue.add(job.data, { removeOnComplete: true });
+        });
+      },
+      () => {
+        done();
+        emailQueue.add({ fail: true, email }, { removeOnComplete: true });
+      },
+    ),
+  );
+};
 
 function childProcessExitCheck(successFn, failFn) {
   return (exitCode) => {
     if (exitCode !== 0) {
       return failFn();
     }
-    successFn()
-  }
-}
-
-function getS3BucketName() {
-  const node_env = process.env.NODE_ENV || 'development';
-  return bucketNames[node_env];
+    successFn();
+  };
 }
--- packages/api/src/util/digitalOceanSpaces.js
+++ packages/api/src/util/digitalOceanSpaces.js
@@ -5,6 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 
 function getPrivateS3BucketName() {
+  if (process.env.PRIVATE_BUCKET_NAME) return process.env.PRIVATE_BUCKET_NAME;
   const node_env = process.env.NODE_ENV;
   if (node_env === 'production') return 'litefarm-app-secret';
   if (node_env === 'integration') return 'litefarm-beta-secret';
@@ -12,16 +13,17 @@ function getPrivateS3BucketName() {
 }
 
 function getPublicS3BucketName() {
+  if (process.env.PUBLIC_BUCKET_NAME) return process.env.PUBLIC_BUCKET_NAME;
   const node_env = process.env.NODE_ENV;
   if (node_env === 'production') return 'litefarmapp';
   if (node_env === 'integration') return 'litefarmbeta';
   return 'litefarm';
 }
 
-function getImaginaryUrl({ url, type = 'webp', width, ...imaginaryQueries } = {}, {
-  endpoint = 'thumbnail',
-  serverUrl = 'https://image.litefarm.org',
-} = {}) {
+function getImaginaryUrl(
+  { url, type = 'webp', width, ...imaginaryQueries } = {},
+  { endpoint = 'thumbnail', serverUrl = 'https://image.litefarm.org' } = {},
+) {
   const reqUrl = new URL(`${serverUrl}/${endpoint}`);
   width && reqUrl.searchParams.append('width', width);
   reqUrl.searchParams.append('type', type);
@@ -40,41 +42,55 @@ const s3 = new S3({
   secretAccessKey: process.env.DO_SPACES_SECRET_ACCESS_KEY,
 });
 
-async function imaginaryPost({ buffer, originalname }, {
-  width,
-  type,
-  ...imaginaryQueries
-} = {}, { endpoint = 'thumbnail', serverUrl = 'https://image.litefarm.org' } = {}) {
+async function imaginaryPost(
+  { buffer, originalname },
+  { width, type, ...imaginaryQueries } = {},
+  { endpoint = 'thumbnail', serverUrl = 'https://image.litefarm.org' } = {},
+) {
   const form = new FormData();
   form.append('file', buffer, { filename: originalname });
-  return axios.post(getImaginaryUrl({
-    width,
-    type,
-    ...imaginaryQueries,
-  }, { endpoint, serverUrl }), form, {
-    headers: {
-      'API-Key': process.env.IMAGINARY_TOKEN,
-      ...form.getHeaders(),
+  return axios.post(
+    getImaginaryUrl(
+      {
+        width,
+        type,
+        ...imaginaryQueries,
+      },
+      { endpoint, serverUrl },
+    ),
+    form,
+    {
+      headers: {
+        'API-Key': process.env.IMAGINARY_TOKEN,
+        ...form.getHeaders(),
+      },
+      responseType: 'arraybuffer',
     },
-    responseType: 'arraybuffer',
-  });
+  );
 }
 
-async function imaginaryGet(url, { width, type, ...imaginaryQueries } = {}, {
-  endpoint = 'thumbnail',
-  serverUrl = 'https://image.litefarm.org',
-} = {}) {
-  return axios.post(getImaginaryUrl({
-    width,
-    type,
-    url,
-    ...imaginaryQueries,
-  }, { endpoint, serverUrl }), {
-    headers: {
-      'API-Key': process.env.IMAGINARY_TOKEN,
+async function imaginaryGet(
+  url,
+  { width, type, ...imaginaryQueries } = {},
+  { endpoint = 'thumbnail', serverUrl = 'https://image.litefarm.org' } = {},
+) {
+  return axios.post(
+    getImaginaryUrl(
+      {
+        width,
+        type,
+        url,
+        ...imaginaryQueries,
+      },
+      { endpoint, serverUrl },
+    ),
+    {
+      headers: {
+        'API-Key': process.env.IMAGINARY_TOKEN,
+      },
+      responseType: 'arraybuffer',
     },
-    responseType: 'arraybuffer',
-  });
+  );
 }
 
 function getRandomFileName(file) {

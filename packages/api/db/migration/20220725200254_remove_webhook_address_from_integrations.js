2580a9d09279976ea9c17c9b389124d9526eacaf
fsayyid <104768598+fsayyid@users.noreply.github.com>
2022-07-29 15:27:12 -0400

    Update packages/api/db/migration/20220725200254_remove_webhook_address_from_integrations.js
    Co-authored-by: Calum Murray <calumramurray@gmail.com>

--- packages/api/db/migration/20220725200254_remove_webhook_address_from_integrations.js
+++ packages/api/db/migration/20220725200254_remove_webhook_address_from_integrations.js
@@ -6,6 +6,6 @@ exports.up = async function (knex) {
 
 exports.down = async function (knex) {
   return await knex.schema.alterTable('farm_external_integration', (table) => {
-    table.integer('webhook_address');
+    table.string('webhook_address');
   });
 };

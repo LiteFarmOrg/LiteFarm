7ab5a8322e832a72fe814c5a461c2c0b6289ad60
AbdusShaikh <abdus.shaikh123@gmail.com>
2022-08-03 11:22:10 -0400

    Update index.jsx

--- packages/webapp/src/components/LocationDetailLayout/PointDetails/Sensor/index.jsx
+++ packages/webapp/src/components/LocationDetailLayout/PointDetails/Sensor/index.jsx
@@ -62,6 +62,8 @@ export default function PureSensorDetail({ history, isAdmin, system, sensorInfo,
     shouldUnregister: false,
     mode: 'onChange',
   });
+  const DEPTH = 'depth';
+  const DEPTH_UNIT = 'depth_unit';
 
   return (
     <div style={{ padding: '24px 16px 24px 24px' }}>

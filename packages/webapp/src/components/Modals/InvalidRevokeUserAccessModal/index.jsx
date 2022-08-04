0249768a7cc33be87b94de13ae926fbb299057b2
sein <44532313+seinnlee@users.noreply.github.com>
2022-08-04 13:53:39 -0700

    revoke access modal to help request link

--- packages/webapp/src/components/Modals/InvalidRevokeUserAccessModal/index.jsx
+++ packages/webapp/src/components/Modals/InvalidRevokeUserAccessModal/index.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import ModalComponent from '../ModalComponent/v2';
-import { useTranslation } from 'react-i18next';
-import Button from '../../Form/Button';
+import { Trans, useTranslation } from 'react-i18next';
+import styles from '../../../../src/components/Typography/typography.module.scss';
 
 export default function InvalidRevokeUserAccessModal({ dismissModal }) {
   const { t } = useTranslation();
   return (
     <ModalComponent
       title={t(`PROFILE.PEOPLE.INVALID_REVOKE_ACCESS`)}
-      contents={[t('PROFILE.PEOPLE.LAST_ADMIN_ERROR')]}
       dismissModal={dismissModal}
-      buttonGroup={
-        <>
-          <Button onClick={dismissModal} color={'secondary'} sm>
-            {t('common:NO')}
-          </Button>
-        </>
-      }
       warning
-    />
+    >
+      <div className={styles.info}>
+        <Trans i18nKey="PROFILE.PEOPLE.LAST_ADMIN_ERROR">
+          text <a href="/help">link</a> text
+        </Trans>
+      </div>
+    </ModalComponent>
   );
 }

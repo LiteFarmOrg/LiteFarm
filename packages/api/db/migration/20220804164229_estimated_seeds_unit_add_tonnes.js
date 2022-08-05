9fe5ad15102ce415fa00db2760bb43f07802e39c
fsayyid <fsayyid@litefarm.org>
2022-08-05 13:58:53 -0400

    fix: down function for estimated seeds migration

--- packages/api/db/migration/20220804164229_estimated_seeds_unit_add_tonnes.js
+++ packages/api/db/migration/20220804164229_estimated_seeds_unit_add_tonnes.js
@@ -1,14 +1,16 @@
 exports.up = function (knex) {
   return knex.schema.raw(`
-        ALTER TABLE planting_management_plan
-        DROP CONSTRAINT planting_management_plan_estimated_seeds_unit_check,
-        ADD CONSTRAINT planting_management_plan_estimated_seeds_unit_check
-        CHECK (estimated_seeds_unit = ANY (ARRAY['g'::text, 'kg'::text, 'mt'::text, 'oz'::text, 'lb'::text, 't'::text]));
-    `);
+          ALTER TABLE planting_management_plan
+          DROP CONSTRAINT planting_management_plan_estimated_seeds_unit_check,
+          ADD CONSTRAINT planting_management_plan_estimated_seeds_unit_check
+          CHECK (estimated_seeds_unit = ANY (ARRAY['g'::text, 'kg'::text, 'mt'::text, 'oz'::text, 'lb'::text, 't'::text]));
+      `);
 };
 
 exports.down = function (knex) {
   return knex.schema.raw(`
+        UPDATE planting_management_plan SET estimated_seeds_unit = 'lb' WHERE estimated_seeds_unit = 't';
+        UPDATE planting_management_plan SET estimated_seeds_unit = 'kg' WHERE estimated_seeds_unit = 'mt';
         ALTER TABLE planting_management_plan
         DROP CONSTRAINT planting_management_plan_estimated_seeds_unit_check,
         ADD CONSTRAINT planting_management_plan_estimated_seeds_unit_check

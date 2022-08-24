53419bf27de2fffba5e16c573fe724a83c214098
fsayyid <fsayyid@litefarm.org>
2022-08-24 15:50:39 -0400

    fix: invite user form name length validation

--- packages/webapp/src/components/Form/Input/utils.js
+++ packages/webapp/src/components/Form/Input/utils.js
@@ -5,3 +5,20 @@ export const isNotInFuture = (data) => {
     ? true
     : i18n.t('MANAGEMENT_PLAN.COMPLETE_PLAN.FUTURE_DATE_INVALID');
 };
+
+export const isValidName = (name) => {
+  const nameSplit = name.split(/ (.+)/);
+  const firstName = nameSplit.length > 0 ? nameSplit[0] : '';
+  const lastName = nameSplit.length > 1 ? nameSplit[1] : '';
+  if (firstName.length > 255 && lastName.length > 255) {
+    return (
+      i18n.t('PROFILE.ERROR.FIRST_NAME_LENGTH') + ', ' + i18n.t('PROFILE.ERROR.LAST_NAME_LENGTH')
+    );
+  } else if (lastName.length > 255) {
+    return i18n.t('PROFILE.ERROR.LAST_NAME_LENGTH');
+  } else if (firstName.length > 255) {
+    return i18n.t('PROFILE.ERROR.FIRST_NAME_LENGTH');
+  } else {
+    return true;
+  }
+};

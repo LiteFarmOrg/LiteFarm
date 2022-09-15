803420474aee5c3dc7c6e7f4a53edd92e15096b2
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-09-15 19:13:10 +0200

    basic script

--- packages/webapp/cypress/e2e/test.spec.js
+++ packages/webapp/cypress/e2e/test.spec.js
@@ -1,5 +1,40 @@
 describe.only('LiteFarm end to end test', () => {
-  it.only('Test', () => {
+  let userEmail;
+  let userPassword;
+
+  before(() => {
+    cy.getEmail().then((email) => {
+      userEmail = email;
+    });
+
+    cy.getPassword().then((password) => {
+      userPassword = password;
+    });
+  });
+
+  it.only('Happy path test', { defaultCommandTimeout: 7000 }, () => {
     cy.visit('/');
+    cy.get('[data-cy=email]').should('exist');
+    cy.get('[data-cy=continue]').should('exist');
+    cy.get('[data-cy=continue]').should('be.disabled');
+    cy.get('[data-cy=continueGoogle]').should('exist');
+
+    //create test data
+    const emailOwner = userEmail;
+    const usrname = emailOwner.indexOf('@');
+    const emailWorker = emailOwner.slice(0, usrname) + '+1' + emailOwner.slice(usrname);
+    const gender = 'Male';
+    const fullName = 'Test Farmer';
+    const password = `${userPassword}+1@`;
+    const farmName = 'UBC FARM';
+    const location = '49.250833,-123.2410777';
+    const fieldName = 'Test Field';
+    const workerName = 'Test Worker';
+    const testCrop = 'New Crop';
+    const role = 'Manager';
+    const lang = 'English';
+
+    //Login as a new user
+    cy.newUserLogin(emailOwner);
   });
 });

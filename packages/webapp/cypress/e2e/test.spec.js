ad1bc27574a6bb56a80c1b2c011c2c3ee37959b6
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-09-15 19:35:20 +0200

    check if api call works

--- packages/webapp/cypress/e2e/test.spec.js
+++ packages/webapp/cypress/e2e/test.spec.js
@@ -36,5 +36,8 @@ describe.only('LiteFarm end to end test', () => {
 
     //Login as a new user
     cy.newUserLogin(emailOwner);
+
+    //create account
+    cy.createAccount(emailOwner, fullName, gender, null, null, password);
   });
 });

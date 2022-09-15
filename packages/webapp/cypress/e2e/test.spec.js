56e17457e64baec21fd7f312873b210883c2a0eb
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-09-15 19:27:15 +0200

    testing

--- packages/webapp/cypress/e2e/test.spec.js
+++ packages/webapp/cypress/e2e/test.spec.js
@@ -1,4 +1,17 @@
 describe.only('LiteFarm end to end test', () => {
+  let userEmail;
+  let userPassword;
+
+  before(() => {
+    cy.getEmail().then((email) => {
+      userEmail = email;
+    });
+
+    cy.getPassword().then((password) => {
+      userPassword = password;
+    });
+  });
+
   it.only('Happy path test', { defaultCommandTimeout: 7000 }, () => {
     cy.visit('/');
     cy.get('[data-cy=email]').should('exist');
@@ -7,12 +20,12 @@ describe.only('LiteFarm end to end test', () => {
     cy.get('[data-cy=continueGoogle]').should('exist');
 
     //create test data
-    const emailOwner = 'mbolokonya@litefarm.org';
+    const emailOwner = userEmail;
     const usrname = emailOwner.indexOf('@');
     const emailWorker = emailOwner.slice(0, usrname) + '+1' + emailOwner.slice(usrname);
     const gender = 'Male';
     const fullName = 'Test Farmer';
-    const password = `${'P@ssword123'}+1@`;
+    const password = `${userPassword}+1@`;
     const farmName = 'UBC FARM';
     const location = '49.250833,-123.2410777';
     const fieldName = 'Test Field';

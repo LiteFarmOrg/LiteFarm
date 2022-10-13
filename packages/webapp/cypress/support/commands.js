b236c8dbc7336aa75444f8ed92e9b5979fcca1f1
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-10-14 00:02:48 +0200

    added browser location permissions

--- packages/webapp/cypress/support/commands.js
+++ packages/webapp/cypress/support/commands.js
@@ -523,7 +523,7 @@ Cypress.Commands.add('addFarm', (farmName, location) => {
   cy.waitForGoogleApi().then(() => {
     cy.wait(3000);
     cy.get('[data-cy=addFarm-farmName]').should('exist').type(farmName);
-    cy.wait('@loadMap').then(() => {
+    cy.wait('@loadMap', { timeout: 15000 }).then(() => {
       cy.get('[data-cy=addFarm-location]').should('exist').type(location).wait(1000);
 
       cy.get('.pac-item').should('exist').click({ force: true });

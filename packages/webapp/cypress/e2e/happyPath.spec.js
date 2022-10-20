085370ef95518f0aed39b4373950395afae38180
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-10-20 17:28:00 +0200

    wait for map to load on farm map page

--- packages/webapp/cypress/e2e/happyPath.spec.js
+++ packages/webapp/cypress/e2e/happyPath.spec.js
@@ -126,7 +126,15 @@ describe.only('LiteFarm end to end test', () => {
       .and('not.be.disabled')
       .click();
     cy.get('[data-cy=map-addFeature]').should('exist').and('not.be.disabled').click();
+
+    cy.get('[data-cy="map-selection"]').should('be.visible');
+
     cy.get('[data-cy=map-drawer]').contains('Field').should('exist').and('not.be.disabled').click();
+    cy.get('[data-cy=map-mapContainer]')
+      .find('button')
+      .eq(2)
+      .should('exist')
+      .and('be.visible', { timeout: 30 * 1000 });
     cy.get('[data-cy=mapTutorial-continue]')
       .contains('Got it')
       .should('exist')

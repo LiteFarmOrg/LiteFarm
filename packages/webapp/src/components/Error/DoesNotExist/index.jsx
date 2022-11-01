9a196c696bc9d22ae1f199138b527f7eee98782b
Mr-BrainSpace <duncan@brainenterprise.ca>
2022-11-01 18:00:29 -0400

    Add jsdoc comments

--- packages/webapp/src/components/Error/DoesNotExist/index.jsx
+++ packages/webapp/src/components/Error/DoesNotExist/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PageTitle from '../../PageTitle/v2';
 
+/**
+ * @function DoesNotExist
+ * @callback onGoBack
+ * @param {object} props Component props
+ * @param {string} props.message - String that describes issue
+ * @param {onGoBack} props.onGoBack - Callback function for 'Go Back' arrow
+ * @returns {object} DoesNotExist
+ * @description Error Display For Does Not Exist
+ * @todo Make pretty
+ */
 export default function DoesNotExist({message, onGoBack}) {
     return (
     <>

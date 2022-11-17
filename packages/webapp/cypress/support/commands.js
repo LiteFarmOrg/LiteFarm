a737ac7489a3b6a7625435fc5e7b85ce96e5c487
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-11-17 21:34:47 +0200

    wait longer for map to display

--- packages/webapp/cypress/support/commands.js
+++ packages/webapp/cypress/support/commands.js
@@ -570,7 +570,7 @@ Cypress.Commands.add('addFarm', (farmName, location) => {
     .type(location);
   cy.wait(5 * 1000);
   cy.get('.pac-item').should('exist').click({ force: true });
-  cy.get('.gm-tilt', { timeout: 60 * 1000 }).should('be.visible');
+  cy.get('.gm-tilt', { timeout: 100 * 1000 }).should('be.visible');
   cy.get('[data-cy=addFarm-farmName]').should('exist').type(farmName);
   //   });
   cy.intercept('POST', '/farm').as('addFarm');

cd740edd5b5068d633341312b39a76927364cb14
Richard wasswa <wasswarichard55@gmail.com>
2022-11-22 18:19:47 +0300

    Fix failing tests

--- packages/api/tests/mock.factories.js
+++ packages/api/tests/mock.factories.js
@@ -1613,6 +1613,14 @@ function fakeIrrigationTask(defaultData = {}) {
   return {
     type: faker.helpers.arrayElement(['sprinkler', 'drip', 'subsurface', 'flood']),
     estimated_duration: faker.datatype.number(10),
+    estimated_duration_unit: faker.random.word(),
+    // estimated_duration_unit: faker.helpers.arrayElement(['h', 'min']),
+    estimated_flow_rate: faker.datatype.number(10),
+    // estimated_flow_rate_unit: faker.helpers.arrayElement(['l/h', 'l/min']),
+    estimated_water_usage: faker.datatype.number(10),
+    // estimated_water_usage_unit: faker.helpers.arrayElement(['ml', 'l']),
+    application_depth: faker.datatype.number(10),
+    // application_depth_unit: faker.helpers.arrayElement(['mm']),
     ...defaultData,
   };
 }

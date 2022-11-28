dd6a426814a0b6e88226397b5dbb85429a7531de
Richard wasswa <wasswarichard55@gmail.com>
2022-11-28 17:13:50 +0300

    LF-2803 Post Irrigation data https://lite-farm.atlassian.net/browse/LF-2803

--- packages/api/db/migration/20221128095616_fix_irrigation_task_creation_errors.js
+++ packages/api/db/migration/20221128095616_fix_irrigation_task_creation_errors.js
@@ -17,9 +17,8 @@ export const up = async function (knex) {
   ];
 
   // add new enum values
-  await knex.schema
-    .raw(`ALTER TABLE irrigation_task DROP CONSTRAINT IF EXISTS irrigationLog_type_check;
-                          ALTER TABLE irrigation_task ADD CONSTRAINT irrigationLog_type_check 
+  await knex.schema.raw(`ALTER TABLE irrigation_task DROP CONSTRAINT "irrigationLog_type_check";
+                          ALTER TABLE irrigation_task ADD CONSTRAINT "irrigationLog_type_check" 
                            CHECK (type = ANY (ARRAY['${irrigation_type_enum.join(
                              `'::text,'`,
                            )}'::text]))`);
@@ -30,9 +29,8 @@ export const down = async function (knex) {
     table.dropColumn('default_measuring_type');
   });
   const irrigation_type_enum = ['sprinkler', 'drip', 'subsurface', 'flood'];
-  await knex.schema
-    .raw(`ALTER TABLE irrigation_task DROP CONSTRAINT IF EXISTS irrigationLog_type_check;
-                          ALTER TABLE irrigation_task ADD CONSTRAINT irrigationLog_type_check 
+  await knex.schema.raw(`ALTER TABLE irrigation_task DROP CONSTRAINT "irrigationLog_type_check";
+                          ALTER TABLE irrigation_task ADD CONSTRAINT "irrigationLog_type_check" 
                            CHECK (type = ANY (ARRAY['${irrigation_type_enum.join(
                              `'::text,'`,
                            )}'::text]))`);

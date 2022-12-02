6441a805ce3a3b38489de35d9e413a3b0f2e1195
Mwayanjana Bolokonya <mwayab@gmail.com>
2022-12-02 23:32:23 +0200

    increased timeout

--- packages/webapp/cypress.config.ts
+++ packages/webapp/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: '45vid6',
-  defaultCommandTimeout: 60 * 1000,
+  defaultCommandTimeout: 90 * 1000,
   env: {
     'cypress-react-selector': {
       root: '#root',

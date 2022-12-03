bff875af822245f507d655e3e4e8fa2e7589026a
Jimmy Rowland <jimmyrowland@protonmail.com>
2022-12-02 21:59:50 -0800

    fix cypress plugin imports

--- packages/webapp/cypress.config.ts
+++ packages/webapp/cypress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return import('./cypress/plugins/index.js').then((cypress) => cypress(on, config));
+      return import('./cypress/plugins/index.js').then((cypress) => cypress.default(on, config));
     },
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',

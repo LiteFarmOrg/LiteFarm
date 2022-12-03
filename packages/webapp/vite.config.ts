2b97e774fcab4ec0d52b528c028ad8a94d814333
Jimmy Rowland <jimmyrowland@protonmail.com>
2022-12-02 21:23:36 -0800

    Add vite pwa

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 import svgrPlugin from 'vite-plugin-svgr';
 import IstanbulPlugin from 'vite-plugin-istanbul';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,9 @@ export default defineConfig({
       extension: ['.js', '.ts', '.jsx'],
       requireEnv: true,
       cypress: true,
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
     }),
   ],
   build: {

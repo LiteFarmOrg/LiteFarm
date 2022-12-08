b1b0b882d6a70af35afc73fdfe51ab993ef7df89
Richard wasswa <wasswarichard55@gmail.com>
2022-12-08 09:39:21 +0300

    Read only view

--- packages/api/db/migration/20221207131355_add_extra_columns_irrigation_table.js
+++ packages/api/db/migration/20221207131355_add_extra_columns_irrigation_table.js
@@ -5,10 +5,10 @@ export const up = async function (knex) {
     table.string('location_size_unit');
     table.float('percentage_location_irrigated');
     table.string('percentage_location_irrigated_unit');
-    table.boolean('default_location_flow_rate').defaultTo(false);
     table.boolean('default_location_application_depth').defaultTo(false);
     table.boolean('default_irrigation_task_type_location').defaultTo(false);
     table.boolean('default_irrigation_task_type_measurement').defaultTo(false);
+    table.boolean('default_location_flow_rate').defaultTo(false);
   });
 };
 
@@ -20,8 +20,8 @@ export const down = async function (knex) {
     table.dropColumn('percentage_location_irrigated');
     table.dropColumn('percentage_location_irrigated_unit');
     table.dropColumn('default_location_flow_rate');
-    table.dropColumn('default_location_application_depth');
     table.dropColumn('default_irrigation_task_type_location');
     table.dropColumn('default_irrigation_task_type_measurement');
+    table.dropColumn('default_location_application_depth');
   });
 };

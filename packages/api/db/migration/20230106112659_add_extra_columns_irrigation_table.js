fec8b2a3cc5a8450319040d375c5a03a4efef2ff
Mac <mac@MBP-de-Hajer.lan>
2023-01-10 22:32:54 +0300

    LF-2804 Irrigation Task read only

--- packages/api/db/migration/20230106112659_add_extra_columns_irrigation_table.js
+++ packages/api/db/migration/20230106112659_add_extra_columns_irrigation_table.js
@@ -1,9 +1,9 @@
 export const up = async function (knex) {
   await knex.schema.alterTable('irrigation_task', (table) => {
-    table.boolean('default_location_flow_rate').defaultTo(false);
-    table.boolean('default_location_application_depth').defaultTo(false);
-    table.boolean('default_irrigation_task_type_location').defaultTo(false);
-    table.boolean('default_irrigation_task_type_measurement').defaultTo(false);
+    table.boolean('default_location_flow_rate').defaultTo(false).notNullable();
+    table.boolean('default_location_application_depth').defaultTo(false).notNullable();
+    table.boolean('default_irrigation_task_type_location').defaultTo(false).notNullable();
+    table.boolean('default_irrigation_task_type_measurement').defaultTo(false).notNullable();
   });
   const irrigation_task_type = await knex('irrigation_type').select(
     'irrigation_type_id',

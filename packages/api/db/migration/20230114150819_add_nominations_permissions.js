ab4aedf8ab709ce768f81611ed9a76ff9f7ceb2c
Mr-BrainSpace <duncan@brainenterprise.ca>
2023-01-17 14:50:50 -0500

    Do not love this but its fine -- using other migrations as template to manually add permission id vs using serial

--- packages/api/db/migration/20230114150819_add_nominations_permissions.js
+++ packages/api/db/migration/20230114150819_add_nominations_permissions.js
@@ -1,35 +1,29 @@
 export const up = async function (knex) {
-  //repair the permissions auto increment
-  await knex.raw(
-    "BEGIN; LOCK TABLE permissions IN EXCLUSIVE MODE; SELECT setval('permissions_permission_id_seq', (SELECT COALESCE((SELECT MAX(permission_id)+1 FROM permissions), 1)), false); COMMIT;",
-  );
-  const newPermissions = await knex('permissions')
+  await knex('permissions')
     .insert([
-      { name: 'add:nomination', description: 'Nominate an piece of data' },
-      { name: 'edit:nomination', description: 'Edit a nomination' },
-      { name: 'delete:nomination', description: 'Delete a nomination' },
+      { permission_id: 135, name: 'add:nomination', description: 'Nominate an piece of data' },
+      { permission_id: 136, name: 'edit:nomination', description: 'Edit a nomination' },
+      { permission_id: 137, name: 'delete:nomination', description: 'Delete a nomination' },
     ])
     .returning('permission_id');
   //Copied role permissions for add, edit delete crop
-  const allowedRoles = ['Owner', 'Manager', 'Extension Officer'];
-  const roleIds = await knex('role').select('role_id').whereIn('role', allowedRoles);
-  for (const roleId of roleIds) {
-    for (const permissionId of newPermissions) {
-      await knex('rolePermissions').insert([
-        { role_id: roleId.role_id, permission_id: permissionId },
-      ]);
-    }
-  }
+  await knex('rolePermissions').insert([
+    { role_id: 1, permission_id: 135 },
+    { role_id: 2, permission_id: 135 },
+    { role_id: 5, permission_id: 135 },
+    { role_id: 1, permission_id: 136 },
+    { role_id: 2, permission_id: 136 },
+    { role_id: 5, permission_id: 136 },
+    { role_id: 1, permission_id: 137 },
+    { role_id: 2, permission_id: 137 },
+    { role_id: 5, permission_id: 137 },
+  ]);
 };
 
 export const down = function (knex) {
-  const permissions = ['add:nomination', 'edit:nomination', 'delete:nomination'];
-  const permissionIds = knex('permissions').select('permission_id').whereIn('name', permissions);
+  const permissionIds = [135, 136, 137];
   return Promise.all([
     knex('rolePermissions').whereIn('permission_id', permissionIds).del(),
     knex('permissions').whereIn('permission_id', permissionIds).del(),
-    knex.raw(
-      "BEGIN; LOCK TABLE permissions IN EXCLUSIVE MODE; SELECT setval('permissions_permission_id_seq', 84, false); COMMIT;",
-    ),
   ]);
 };

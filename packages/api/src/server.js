d46ea1791551b716e31fbc325ad1e841856f7bde
Mr-BrainSpace <duncan@brainenterprise.ca>
2023-02-02 10:18:54 -0500

    move out of other declarations

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -88,7 +88,8 @@ import promiseRouter from 'express-promise-router';
 import { Model } from 'objection';
 import checkJwt from './middleware/acl/checkJwt.js';
 import cors from 'cors';
-import { rateLimiterUsingThirdParty } from './middleware/rateLimiter.js';
+//import { rateLimiterUsingThirdParty } from './middleware/rateLimiter.js';
+import rateLimit from 'express-rate-limit';
 
 // initialize knex
 import knex from './util/knex.js';
@@ -197,10 +198,20 @@ app.set('json replacer', (key, value) => {
   return value;
 });
 
-// apply rate limiter to all requests
-if (['production', 'integration'].includes(environment)) {
-  app.use(rateLimiterUsingThirdParty);
-}
+// // apply rate limiter to all requests
+// if (['production', 'integration'].includes(environment)) {
+//   app.use(rateLimiterUsingThirdParty);
+// }
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app
   .use(bodyParser.json())

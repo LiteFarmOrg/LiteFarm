e34f9e96d8ce93a23acacbc2737e39f5d2e9fbb5
Mr-BrainSpace <duncan@brainenterprise.ca>
2023-02-02 10:28:38 -0500

    remove extra params

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -206,8 +206,6 @@ app.set('json replacer', (key, value) => {
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 // Apply the rate limiting middleware to all requests

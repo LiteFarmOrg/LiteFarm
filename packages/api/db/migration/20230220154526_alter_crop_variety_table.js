9c9bb67273a3c6742663d325e8f053710dc1e045
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-03-02 15:04:16 +0200

    removed unnecessary defaultTo

--- packages/api/db/migration/20230220154526_alter_crop_variety_table.js
+++ packages/api/db/migration/20230220154526_alter_crop_variety_table.js
@@ -1,7 +1,22 @@
+/*
+ *  Copyright 2019, 2020, 2021, 2022, 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
 export const up = async function (knex) {
   await knex.schema.alterTable('crop_variety', (t) => {
-    t.string('crop_varietal').nullable().defaultTo(null);
-    t.string('crop_cultivar').nullable().defaultTo(null);
+    t.string('crop_varietal').nullable();
+    t.string('crop_cultivar').nullable();
   });
 };
 

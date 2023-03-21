ed78577592c0ab966f28fba8f69d3b10f160b3f9
Duncan-Brain <dbrain@litefarm.org>
2023-03-21 10:07:55 -0400

    remove pwa config in vite config

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 import svgrPlugin from 'vite-plugin-svgr';
 import IstanbulPlugin from 'vite-plugin-istanbul';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,9 +20,6 @@ export default defineConfig({
       extension: ['.js', '.ts', '.jsx'],
       requireEnv: true,
       cypress: true,
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
     }),
   ],
   build: {

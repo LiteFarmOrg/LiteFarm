01e5109fb54b956f4e476d10404ff96162d6d1be
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-03-30 11:26:45 -0700

    add tests for complete HarvestTaskQuantity

--- packages/webapp/src/stories/Pages/Tasks/HarvestTaskQuantity.stories.jsx
+++ packages/webapp/src/stories/Pages/Tasks/HarvestTaskQuantity.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PureHarvestCompleteQuantity from '../../../components/Task/TaskComplete/HarvestComplete/Quantity';
 import decorator from '../config/Decorators';
+import UnitTest from '../../../test-utils/storybook/unit';
 
 export default {
   title: 'Form/Crop/Tasks/HarvestCompleteQuantity',
@@ -8,18 +9,46 @@ export default {
   decorators: decorator,
 };
 
-const Template = (args) => <PureHarvestCompleteQuantity {...args} />;
-
-export const HarvestCompleteQuantity = Template.bind({});
-HarvestCompleteQuantity.args = {
+const args = {
   onCancel: () => {},
   onGoBack: () => {},
-  system: 'metric',
   useHookFormPersist: () => ({}),
   persistedFormData: {},
+};
+
+const Template = (args) => <PureHarvestCompleteQuantity {...args} />;
+export const MetricHarvestCompleteQuantity = Template.bind({});
+MetricHarvestCompleteQuantity.args = {
+  ...args,
+  system: 'metric',
   task: {
     harvest_task: {
       projected_quantity: 1,
+      projected_quantity_unit: 'kg',
     },
   },
+};
+MetricHarvestCompleteQuantity.play = async ({ canvasElement }) => {
+  const test = new UnitTest(canvasElement, 'unit');
+  await test.testVisibleValue(1);
+  await test.testHiddenValue(1);
+  await test.testSelectedUnit('kg');
+};
+
+export const ImperialHarvestCompleteQuantity = Template.bind({});
+ImperialHarvestCompleteQuantity.args = {
+  ...args,
+  system: 'imperial',
+  task: {
+    harvest_task: {
+      projected_quantity: 14.968536,
+      projected_quantity_unit: 'lb',
+    },
+  },
+};
+ImperialHarvestCompleteQuantity.play = async ({ canvasElement }) => {
+  const test = new UnitTest(canvasElement, 'unit');
+  await test.testVisibleValue(33);
+  await test.testHiddenValue(14.968536);
+  await test.testSelectedUnit('lb');
 };

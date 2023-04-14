6b8476ced7f964172a7b63834fa25d5de8cd8dc9
Javier Antonio Hernandez de Paz <javiercito53@gmail.com>
2023-04-14 13:48:18 -0600

    added redirection to UnknownRecord for sensors

--- packages/webapp/src/containers/SensorReadings/index.jsx
+++ packages/webapp/src/containers/SensorReadings/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import SensorReadingsLineChart from '../SensorReadingsLineChart';
@@ -28,50 +28,59 @@ function SensorReadings({ history, match }) {
   } = useSelector(bulkSensorsReadingsSliceSelector);
   const measurementUnit = useSelector(measurementSelector);
   const { tempUnit, soilWaterPotentialUnit } = utils.getUnits(measurementUnit);
-  const { reading_types = [] } = useSelector(sensorReadingTypesByLocationSelector(location_id));
+  const [readingTypes, setReadingTypes] = useState([]);
+  const reading_types = useSelector(sensorReadingTypesByLocationSelector(location_id));
+  const [sensorVisualizationPropList, setSensorVisualizationPropList] = useState({});
 
-  const sensorVisualizationPropList = {
-    [TEMPERATURE]: {
-      title: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.TITLE'),
-      subTitle: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.SUBTITLE', {
-        high: latestMaxTemperature,
-        low: latestMinTemperature,
-        tempUnit: tempUnit ?? 'C',
-      }),
-      weatherStationName: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.WEATHER_STATION', {
-        weatherStationLocation: nearestStationName,
-      }),
+  useEffect(() => {
+    if (sensorInfo === undefined || reading_types === undefined) {
+      history.replace('/unknown_record');
+    } else {
+      setReadingTypes(reading_types.reading_types);
+      setSensorVisualizationPropList({
+        [TEMPERATURE]: {
+          title: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.TITLE'),
+          subTitle: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.SUBTITLE', {
+            high: latestMaxTemperature,
+            low: latestMinTemperature,
+            tempUnit: tempUnit ?? 'C',
+          }),
+          weatherStationName: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.WEATHER_STATION', {
+            weatherStationLocation: nearestStationName,
+          }),
 
-      xAxisDataKey: CURRENT_DATE_TIME,
-      yAxisLabel: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.Y_AXIS_LABEL', {
-        tempUnit: tempUnit ?? 'C',
-      }),
-      noDataText: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.NO_DATA'),
-      ambientTempFor: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.AMBIENT_TEMPERATURE_FOR'),
-      predictedXAxisLabel: predictedXAxisLabel,
-      activeReadingTypes: reading_types,
-    },
-    [SOIL_WATER_POTENTIAL]: {
-      title: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.TITLE'),
-      subTitle: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.SUBTITLE', {
-        high: latestMaxTemperature,
-        low: latestMinTemperature,
-        soilWaterPotentialUnit: soilWaterPotentialUnit ?? 'kPa',
-      }),
-      xAxisDataKey: CURRENT_DATE_TIME,
-      yAxisLabel: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.Y_AXIS_LABEL', {
-        soilWaterPotentialUnit: soilWaterPotentialUnit ?? 'kPa',
-      }),
-      noDataText: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.NO_DATA'),
-      predictedXAxisLabel: predictedXAxisLabel,
-      activeReadingTypes: reading_types,
-    },
-  };
+          xAxisDataKey: CURRENT_DATE_TIME,
+          yAxisLabel: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.Y_AXIS_LABEL', {
+            tempUnit: tempUnit ?? 'C',
+          }),
+          noDataText: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.NO_DATA'),
+          ambientTempFor: t('SENSOR.TEMPERATURE_READINGS_OF_SENSOR.AMBIENT_TEMPERATURE_FOR'),
+          predictedXAxisLabel: predictedXAxisLabel,
+          activeReadingTypes: reading_types,
+        },
+        [SOIL_WATER_POTENTIAL]: {
+          title: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.TITLE'),
+          subTitle: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.SUBTITLE', {
+            high: latestMaxTemperature,
+            low: latestMinTemperature,
+            soilWaterPotentialUnit: soilWaterPotentialUnit ?? 'kPa',
+          }),
+          xAxisDataKey: CURRENT_DATE_TIME,
+          yAxisLabel: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.Y_AXIS_LABEL', {
+            soilWaterPotentialUnit: soilWaterPotentialUnit ?? 'kPa',
+          }),
+          noDataText: t('SENSOR.SOIL_WATER_POTENTIAL_READINGS_OF_SENSOR.NO_DATA'),
+          predictedXAxisLabel: predictedXAxisLabel,
+          activeReadingTypes: reading_types,
+        },
+      });
+    }
+  }, [sensorInfo, reading_types, history]);
 
   return (
     <div style={{ padding: '24px 16px 24px 24px', height: '100%' }}>
       <PageTitle
-        title={sensorInfo.name}
+        title={sensorInfo?.name || ''}
         onGoBack={() => history.push('/map')}
         style={{ marginBottom: '24px' }}
       />
@@ -93,15 +102,14 @@ function SensorReadings({ history, match }) {
           },
         ]}
       />
-      {reading_types?.length > 0
-        ? [...reading_types]
+      {readingTypes?.length > 0
+        ? [...readingTypes]
             ?.sort()
             ?.reverse()
             ?.map((type, index) => {
               if (!sensorVisualizationPropList[type]) {
                 return null;
               }
-
               const {
                 title,
                 subTitle,
@@ -141,9 +149,9 @@ function SensorReadings({ history, match }) {
               );
             })
         : null}
-      {reading_types.length > 0 && (
+      {readingTypes?.length > 0 && (
         <>
-          {reading_types.reduce((acc, cv, i) => {
+          {readingTypes.reduce((acc, cv, i) => {
             if (cv === TEMPERATURE || cv === SOIL_WATER_POTENTIAL) return acc;
             acc.push(
               <div key={i}>

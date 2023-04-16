8cad12f1a6b6bbed2e5a70447744d9f6aea73afe
Duncan-Brain <dbrain@litefarm.org>
2023-04-16 17:00:09 -0400

    check vars before query

--- packages/api/src/controllers/cropController.js
+++ packages/api/src/controllers/cropController.js
@@ -69,18 +69,20 @@ const cropController = {
       const trx = await transaction.start(Model.knex());
       try {
         const { crop, variety, farm_id } = req.body;
-        const duplicateCrop = await CropModel.query().findOne({
-          farm_id,
-          crop_common_name: crop.crop_common_name,
-          crop_genus: crop.crop_genus || null, // Use null if the value is undefined
-          crop_specie: crop.crop_specie || null, // Use null if the value is undefined
-          deleted: false,
-        });
-        if (duplicateCrop) {
-          await trx.rollback();
-          return res.status(400).json({
-            error: 'This crop already exists, please edit your crop name, genus or species',
+        if ((farm_id, crop.crop_common_name)) {
+          const duplicateCrop = await CropModel.query().findOne({
+            farm_id,
+            crop_common_name: crop.crop_common_name,
+            crop_genus: crop.crop_genus || null, // Use null if the value is undefined
+            crop_specie: crop.crop_specie || null, // Use null if the value is undefined
+            deleted: false,
           });
+          if (duplicateCrop) {
+            await trx.rollback();
+            return res.status(400).json({
+              error: 'This crop already exists, please edit your crop name, genus or species',
+            });
+          }
         }
         crop.user_added = true;
         crop.crop_translation_key = crop.crop_common_name;

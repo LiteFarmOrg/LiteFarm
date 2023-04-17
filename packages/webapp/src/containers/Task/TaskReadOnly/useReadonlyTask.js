b51846db4d63c289da1dd7265bd2763566e1245e
Javier Antonio Hernandez de Paz <javiercito53@gmail.com>
2023-04-16 22:57:52 -0600

    fix the order of hooks issue

--- packages/webapp/src/containers/Task/TaskReadOnly/useReadonlyTask.js
+++ packages/webapp/src/containers/Task/TaskReadOnly/useReadonlyTask.js
@@ -29,10 +29,10 @@ const getManagementPlanTile = (managementPlan, tasksByManagementPlanId) =>
 
 export const useReadonlyTask = (task_id) => {
   const task = useSelector(taskWithProductSelector(task_id));
-  if (task === undefined) return undefined;
   const tasksByManagementPlanId = useSelector(taskEntitiesByManagementPlanIdSelector);
 
   const getTransplantTask = (task) => {
+    if (task === undefined) return undefined;
     const managementPlan = task.managementPlans[0];
     // prev_planting_management_plan will default to planting_management_plan if undefined
     const { planting_management_plan, prev_planting_management_plan = planting_management_plan } =
@@ -55,6 +55,7 @@ export const useReadonlyTask = (task_id) => {
     task.selectedLocationIds = [planting_management_plan.location_id];
   };
   const getTask = (task) => {
+    if (task === undefined) return undefined;
     task.pinCoordinates = [];
     task.managementPlansByPinCoordinate = task.managementPlans.reduce(
       (managementPlansByLocation, managementPlan) => {
@@ -85,7 +86,7 @@ export const useReadonlyTask = (task_id) => {
   };
 
   return useMemo(() => {
-    return produce(task, task.transplant_task ? getTransplantTask : getTask);
+    return produce(task, task?.transplant_task ? getTransplantTask : getTask);
   }, [task]);
 };
 

6f58e72d06f681eb8949610edbac0967c1a9f29a
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-05-25 19:37:20 +0200

    removed commented code

--- packages/webapp/cypress/support/commands.js
+++ packages/webapp/cypress/support/commands.js
@@ -638,7 +638,6 @@ Cypress.Commands.add('giveConsent', () => {
   cy.get('[data-cy=consentPage-content]').should('exist');
   cy.get('[data-cy=consent-continue]').should('exist').and('be.disabled');
   cy.get('[data-cy=checkbox-component]').should('exist').click();
-  //cy.get('[data-cy=consent-agree]').should('exist').check({ force: true });
   cy.get('[data-cy=consent-continue]').should('not.be.disabled').click();
 });
 

59b54b007ae59265ddd4e73462eb05a9130df90a
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-06-07 17:28:00 +0200

    added field work type table delete to cleanup script

--- packages/api/tests/testEnvironment.js
+++ packages/api/tests/testEnvironment.js
@@ -51,6 +51,7 @@ async function tableCleanup(knex) {
     DELETE FROM "showedSpotlight";
     DELETE FROM "userLog";
     DELETE FROM "location_tasks";
+    DELETE FROM "field_work_type";
     DELETE FROM "field_work_task";
     DELETE FROM "harvest_use";
     DELETE FROM "harvest_task";

c692ad82012e43af4943ec1c21ae294a4ab0358f
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2023-06-14 15:24:19 -0700

    LF-3081 Add debugging and use own image with correct Node version

--- packages/api/dev.export.sh
+++ packages/api/dev.export.sh
@@ -3,4 +3,9 @@ set -eu
 
 npm install
 
-npm run scheduler
+cp ../webapp/public/locales/en/crop.json src/jobs/locales/en
+cp ../webapp/public/locales/es/crop.json src/jobs/locales/es
+cp ../webapp/public/locales/pt/crop.json src/jobs/locales/pt
+cp ../webapp/public/locales/fr/crop.json src/jobs/locales/fr
+
+npx nodemon --inspect=0.0.0.0:9229 src/jobs/index.js

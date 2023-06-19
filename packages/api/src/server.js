3ed463e8e0ec1b3b3d6202a423ff0d1db20037ab
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-06-19 19:07:56 +0200

    entire dsn url to env variable

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -27,6 +27,29 @@ const app = express();
 import expressOasGenerator from 'express-oas-generator';
 const environment = process.env.NODE_ENV || 'development';
 
+Sentry.init({
+  dsn: process.env.UPDATE_OAS_FILES,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context, so that all
+// transactions/spans/breadcrumbs are isolated across requests
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 /*
  expressOasGenerator (EOG) automatically creates API documentation.
  Endpoint documentation is generated from route definitions.

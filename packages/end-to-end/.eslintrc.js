4c77020142fc7ad8bdad3c182008af760444883e
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-06-27 08:15:56 +0200

    testing linting

--- packages/end-to-end/.eslintrc.js
+++ packages/end-to-end/.eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
-    env: {
-      browser: true,
-      node: true,
-    },
-    extends: ["eslint:recommended", 'plugin:cypress/recommended'],
-    plugins: ['cypress'],
-    rules: {
-      // Add any specific linting rules for your Cypress tests
-    },
-  };
-  
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:cypress/recommended'],
+  plugins: ['cypress'],
+  rules: {
+    // Add any specific linting rules for your Cypress tests
+  },
+};

57e16005dda685dd71a2da7a8e5e62f518a2ba9a
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-06-27 20:09:33 -0700

    LF-1239 create PhoneNumberInput component

---
+++ packages/webapp/src/components/Form/PhoneNumberInput/CountrySelect.jsx
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2019, 2020, 2021, 2022, 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import React, { Suspense } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { getCountryCallingCode } from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import ReactSelect from '../ReactSelect';
+import { styles as reactSelectDefaultStyles } from '../ReactSelect';
+import styles from './styles.module.scss';
+
+const getStyles = (menuWidth) => ({
+  ...reactSelectDefaultStyles,
+  control: (provided, state) => ({
+    ...reactSelectDefaultStyles.control(null, state),
+    borderRadius: '4px 0 0 4px',
+    borderRight: 'none',
+    height: '48px',
+    width: '72px',
+    '&:hover': {
+      borderColor: 'none',
+    },
+  }),
+  valueContainer: (provided, state) => ({
+    ...reactSelectDefaultStyles.valueContainer(null, state),
+    maxHeight: '48px',
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    padding: 0,
+    paddingLeft: '9px',
+  }),
+  singleValue: (provided, state) => ({
+    ...reactSelectDefaultStyles.singleValue(null, state),
+    width: '100%',
+    height: '21px',
+  }),
+  menu: (provided, state) => ({
+    ...reactSelectDefaultStyles.menu(null, state),
+    width: menuWidth,
+    marginTop: 0,
+    border: 'solid 1px var(--inputActive)',
+    borderTop: 'none',
+  }),
+  dropdownIndicator: (provided, state) => ({
+    ...reactSelectDefaultStyles.dropdownIndicator(null, state),
+    paddingRight: '7px',
+    color: 'var(--grey600)',
+  }),
+});
+
+const CountrySelect = ({
+  classNames,
+  country,
+  labels,
+  iconComponent: Icon,
+  onChange,
+  menuWidth,
+  options,
+  onFocus,
+  onBlur,
+}) => {
+  return (
+    <Suspense fallback={<div className={styles.emptyDiv} />}>
+      <ReactSelect
+        classNames={classNames}
+        styles={getStyles(menuWidth)}
+        value={country}
+        options={options}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        formatOptionLabel={({ value }, meta) => {
+          if (value && meta.context === 'menu') {
+            return (
+              <div className={styles.option}>
+                <Icon country={value} label={value} />
+                <span className={styles.countryName}>{labels[value]}</span>
+                <span className={clsx(styles.code)}>+{value && getCountryCallingCode(value)}</span>
+              </div>
+            );
+          }
+          return <Icon country={value} label={value} />;
+        }}
+      />
+    </Suspense>
+  );
+};
+
+CountrySelect.propTypes = {
+  classNames: PropTypes.object,
+  country: PropTypes.shape({ value: PropTypes.string, label: PropTypes.string }),
+  labels: PropTypes.objectOf(PropTypes.string).isRequired,
+  iconComponent: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape({ value: PropTypes.string, label: PropTypes.string }))
+    .isRequired,
+  onFocus: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  menuWidth: PropTypes.number,
+};
+
+export default CountrySelect;
---
+++ packages/webapp/src/components/Form/PhoneNumberInput/index.jsx
@@ -0,0 +1,110 @@
+/*
+ *  Copyright 2019, 2020, 2021, 2022, 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import React, { useRef, useState, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import CountrySelect from './CountrySelect';
+import { colors } from '../../../assets/theme';
+import styles from './styles.module.scss';
+
+const CONTROL_CLASSNAME = 'phone-number-select-control';
+
+const PhoneNumberInput = ({ defaultCountry, labels, options }) => {
+  const [phone, setPhone] = useState(null);
+  const [country, setCountry] = useState({ value: defaultCountry, label: defaultCountry });
+  const [wrapperWidth, setWrapperWidth] = useState(null);
+
+  const inputRef = useRef(null);
+  const wrapperRef = useRef(null);
+
+  const setReactSelectBorderColor = useCallback((color) => {
+    document.body.querySelector(`.${CONTROL_CLASSNAME}`).style.borderColor = color;
+  }, []);
+
+  const setBorderColor = (color) => {
+    setReactSelectBorderColor(color);
+    inputRef.current.style.borderColor = color;
+  };
+
+  const setWidth = () => {
+    if (!wrapperRef.current) {
+      return;
+    }
+    const width = wrapperRef.current.offsetWidth;
+    setWrapperWidth(width);
+  };
+
+  useEffect(() => {
+    setWidth();
+    window.addEventListener('resize', () => setWidth());
+
+    return () => setWidth();
+  }, []);
+
+  const onPhoneNumberChange = (phone) => {
+    setPhone(phone);
+    // delay setting the color so that the change is reflected
+    setTimeout(() => {
+      setReactSelectBorderColor(colors.teal500);
+    }, 0);
+  };
+
+  // work around of setting state when onFocus since it affects react-select's behaviour
+  const onFocus = () => setBorderColor(colors.teal500);
+  const onBlur = () => setBorderColor(colors.grey400);
+
+  return (
+    <div ref={wrapperRef} className={styles.wrapper}>
+      <PhoneInput
+        ref={inputRef}
+        className={styles.phoneNumberInput}
+        international
+        defaultCountry={defaultCountry}
+        value={phone}
+        onChange={onPhoneNumberChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        countrySelectComponent={(props) => (
+          <CountrySelect
+            {...props}
+            classNames={{
+              control: () => CONTROL_CLASSNAME,
+            }}
+            country={country}
+            labels={labels}
+            menuWidth={wrapperWidth}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            options={options}
+            onChange={(country) => {
+              setCountry(country || undefined);
+              props.onChange(country?.value || undefined);
+            }}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+PhoneNumberInput.propTypes = {
+  defaultCountry: PropTypes.string,
+  labels: PropTypes.objectOf(PropTypes.string).isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape({ value: PropTypes.string, label: PropTypes.string }))
+    .isRequired,
+};
+
+export default PhoneNumberInput;

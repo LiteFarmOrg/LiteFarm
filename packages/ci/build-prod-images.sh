256b3e67d21b4c8b458ee7447cf555d5167f8bb8
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-06-30 15:25:43 +0200

    LF-3400 refactored backend image build script to build for linux/amd64 due to failure related to some dependencies on arm64

--- packages/ci/build-prod-images.sh
+++ packages/ci/build-prod-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building backend docker image..."
-docker build -t litefarm/backend:latest -f ./packages/api/prod.Dockerfile ./packages/
+docker build --platform linux/amd64 -t litefarm/backend:latest -f ./packages/api/prod.Dockerfile ./packages/
 
 echo "Building frontend docker image..."
 docker build -t litefarm/frontend:latest -f ./packages/webapp/prod.Dockerfile ./packages/

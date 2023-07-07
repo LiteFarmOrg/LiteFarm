5eb762bf838b44b02d68862f3957fd1e2c8a2b2b
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-07-06 22:26:03 -0700

    LF-3371 create useElementHeight hook

---
+++ packages/webapp/src/components/hooks/useElementHeight.jsx
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2019, 2020, 2021, 2022, 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import { useMemo } from 'react';
+
+const useElementHeight = (ref) => {
+  const elementHeight = useMemo(() => {
+    if (!ref?.current) {
+      return;
+    }
+
+    return ref.current.offsetHeight;
+  }, [ref.current]);
+
+  return { elementHeight };
+};
+
+export default useElementHeight;

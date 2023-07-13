f37bc283e78a70aba9577c62aa9de781c27b981d
Cali0707 <calumramurray@gmail.com>
2023-07-12 20:51:25 -0400

    Updated isNotInFuture check to handle timezones better

--- packages/webapp/src/components/Form/Input/utils.js
+++ packages/webapp/src/components/Form/Input/utils.js
@@ -1,7 +1,22 @@
 import i18n from '../../../locales/i18n';
 
+/* @param {string} dateString - a date string to parse to local time
+ * @returns {Date}
+ */
+const parseISOStringToLocalDate = (dateString) => {
+  if (dateString.endsWith('z') || dateString.endsWith('Z')) {
+    // this string should be parsed in UTC and then converted to local time
+    return new Intl.DateTimeFormat().format(new Date(dateString));
+  }
+  // this string does not specify to parse in UTC, so we should parse properly ourselves
+  // note: by default, new Date(string) and Date.parse(String) WILL parse in UTC
+  const split = dateString.replace(/[zZ]$/).split(/\D/);
+  return new Date(split[0], split[1] - 1, ...split.slice(2));
+};
+
 export const isNotInFuture = (data) => {
-  return new Date(data) <= new Date()
+  // new Date() is in local time, so we need to ensure that we parse the date in local time
+  return parseISOStringToLocalDate(data) <= new Date()
     ? true
     : i18n.t('MANAGEMENT_PLAN.COMPLETE_PLAN.FUTURE_DATE_INVALID');
 };

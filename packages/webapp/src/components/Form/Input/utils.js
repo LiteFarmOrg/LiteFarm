e7be3fa83861d8d8ae1057b425ac11c4ff125ef3
Cali0707 <calumramurray@gmail.com>
2023-07-12 21:15:10 -0400

    Handled case where timezone is specified on the ISO string

--- packages/webapp/src/components/Form/Input/utils.js
+++ packages/webapp/src/components/Form/Input/utils.js
@@ -4,13 +4,14 @@ import i18n from '../../../locales/i18n';
  * @returns {Date}
  */
 const parseISOStringToLocalDate = (dateString) => {
-  if (dateString.endsWith('z') || dateString.endsWith('Z')) {
-    // this string should be parsed in UTC and then converted to local time
+  const offsetPattern = /([+-]\d{2}|Z):?(\d{2})?\s*$/;
+  if (offsetPattern.test(dateString.slice(19))) {
+    // this string can be parsed normally as it has a specified offset, and then formatted into local time
     return new Intl.DateTimeFormat().format(new Date(dateString));
   }
-  // this string does not specify to parse in UTC, so we should parse properly ourselves
+  // this string does not have a specified offset, so we should treat it as if it is in local time
   // note: by default, new Date(string) and Date.parse(String) WILL parse in UTC
-  const split = dateString.replace(/[zZ]$/).split(/\D/);
+  const split = dateString.replace(/[zZ]$/, '').split(/\D/);
   return new Date(split[0], split[1] - 1, ...split.slice(2));
 };
 

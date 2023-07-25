b038fa2ab564391d65d311442f13920b3fc8cb78
Orangel Marquez (Kike) <kike_marquez8@hotmail.com>
2023-07-25 13:14:49 -0300

    Fixing issue where 403 caused infinite loop
    This was caused because the first condition to check the 403 error was not met
    so the "else" branch just went home, which triggered another "getAlerts" request which returned 403 which went home and so on.

--- packages/webapp/src/containers/ErrorHandler/Forbidden/Forbidden.jsx
+++ packages/webapp/src/containers/ErrorHandler/Forbidden/Forbidden.jsx
@@ -4,15 +4,18 @@ import { useDispatch } from 'react-redux';
 import { handle403 } from '../saga';
 
 export default function Forbidden({ history }) {
- const dispatch = useDispatch();
- useEffect(() => {
-  if (history?.location?.state?.error?.response?.status === 403) {
-   dispatch(handle403());
-  } else if (history.length) {
-   history.back();
-  } else {
-   history.replace('/');
-  }
- }, []);
- return <Spinner />;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (
+      history?.location?.state?.error?.response?.status === 403 ||
+      history?.location?.state?.error?.message?.includes('403')
+    ) {
+      dispatch(handle403());
+    } else if (history.length) {
+      history.back();
+    } else {
+      history.replace('/');
+    }
+  }, []);
+  return <Spinner />;
 }

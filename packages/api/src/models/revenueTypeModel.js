fb4affd3b291ad7e4fd40066508d6bcea0a9d884
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2023-08-28 22:42:14 -0700

    LF-3581 Create revenue type model

---
+++ packages/api/src/models/revenueTypeModel.js
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import baseModel from './baseModel.js';
+
+class RevenueType extends baseModel {
+  static get tableName() {
+    return 'revenue_type';
+  }
+
+  static get idColumn() {
+    return 'revenue_type_id';
+  }
+
+  // Optional JSON schema. This is not the database schema! Nothing is generated
+  // based on this. This is only used for validation. Whenever a model instance
+  // is created it is checked against this schema. http://json-schema.org/.
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['revenue_name', 'farm_id'],
+
+      properties: {
+        revenue_type_id: { type: 'integer' },
+        revenue_name: { type: 'string', minLength: 1, maxLength: 255 },
+        farm_id: { type: 'string' },
+        revenue_translation_key: { type: 'string' },
+        ...this.baseProperties,
+      },
+      additionalProperties: false,
+    };
+  }
+}
+
+export default RevenueType;

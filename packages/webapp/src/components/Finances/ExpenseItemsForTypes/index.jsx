089d003f4a26ee1ab0ac07ef0d799d2b61bdb59d
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-08-30 17:18:32 -0700

    LF-3582 create ExpenseItemsForTypes component

---
+++ packages/webapp/src/components/Finances/ExpenseItemsForTypes/index.jsx
@@ -0,0 +1,67 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import ExpenseItemsForType from './ExpenseItemsForType';
+import { NOTE, VALUE } from './constants';
+
+export default function ExpenseItemsForTypes({ types, setIsValid, ...props }) {
+  const {
+    register,
+    control,
+    setValue,
+    getValues,
+    formState: { isValid, errors },
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {},
+  });
+
+  const setExpenseDetail = () => {
+    props.setExpenseDetail(getValues());
+  };
+
+  useEffect(() => {
+    types.forEach(({ id }) => {
+      setValue(id, [{ [NOTE]: '', [VALUE]: null }]);
+    });
+    setExpenseDetail();
+  }, []);
+
+  useEffect(() => {
+    setIsValid(isValid);
+  }, [isValid]);
+
+  return types.map((type) => {
+    return (
+      <ExpenseItemsForType
+        key={type.id}
+        type={type}
+        register={register}
+        control={control}
+        setValue={setValue}
+        setExpenseDetail={setExpenseDetail}
+        errors={errors}
+      />
+    );
+  });
+}
+
+ExpenseItemsForTypes.propTypes = {
+  types: PropTypes.array,
+  setExpenseDetail: PropTypes.func,
+  setIsValid: PropTypes.func,
+};

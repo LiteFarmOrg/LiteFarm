3f3a2c99ce264555b17d7c96002283ae96724ea1
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-08-30 17:13:09 -0700

    LF-3582 create ExpenseItemInputs component

---
+++ packages/webapp/src/components/Finances/ExpenseItemsForTypes/ExpenseItemInputs.jsx
@@ -0,0 +1,83 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import { IconLink } from '../../Typography';
+import { ReactComponent as TrashIcon } from '../../../assets/images/document/trash.svg';
+import Input, { numberOnKeyDown } from '../../Form/Input';
+import styles from './styles.module.scss';
+
+export default function ExpenseItemInputs({
+  register,
+  itemNameFieldName,
+  valueFieldName,
+  onRemove,
+  onChange,
+  getErrors,
+}) {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.expenseItem}>
+      <Input
+        style={{ marginBottom: '24px' }}
+        label={t('EXPENSE.ITEM_NAME')}
+        onChange={(e) => onChange(e, itemNameFieldName)}
+        errors={getErrors(itemNameFieldName)}
+        hookFormRegister={register(itemNameFieldName, {
+          required: true,
+          maxLength: {
+            value: 100,
+            message: t('common:CHAR_LIMIT_ERROR', { value: 100 }),
+          },
+        })}
+      />
+      <Input
+        style={{ marginBottom: '24px' }}
+        type="number"
+        label={t('EXPENSE.VALUE')}
+        onKeyDown={numberOnKeyDown}
+        min="0.01"
+        step="0.01"
+        onChange={(e) => onChange(e, valueFieldName)}
+        errors={getErrors(valueFieldName)}
+        hookFormRegister={register(valueFieldName, {
+          required: true,
+          valueAsNumber: true,
+          min: { value: 0.01 },
+        })}
+      />
+      <IconLink
+        className={styles.iconLink}
+        icon={<TrashIcon className={styles.icon} />}
+        onClick={onRemove}
+        isIconClickable
+        underlined={false}
+      >
+        {t('common:REMOVE_ITEM')}
+      </IconLink>
+    </div>
+  );
+}
+
+ExpenseItemInputs.propTypes = {
+  register: PropTypes.func,
+  itemNameFieldName: PropTypes.string,
+  valueFieldName: PropTypes.string,
+  onRemove: PropTypes.func,
+  onChange: PropTypes.func,
+  getErrors: PropTypes.func,
+};

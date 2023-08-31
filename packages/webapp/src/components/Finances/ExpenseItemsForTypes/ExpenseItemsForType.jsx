0da5801cb2f3c47cfa4cefd35dc7c82e7ec4808e
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-08-30 17:17:52 -0700

    LF-3582 create ExpenseItemsForType component

---
+++ packages/webapp/src/components/Finances/ExpenseItemsForTypes/ExpenseItemsForType.jsx
@@ -0,0 +1,78 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import { useFieldArray } from 'react-hook-form';
+import { AddLink, Main } from '../../Typography';
+import ExpenseItemInputs from './ExpenseItemInputs';
+import { getInputErrors } from '../../Form/Input';
+import { NOTE, VALUE } from './constants';
+import styles from './styles.module.scss';
+
+export default function ExpenseItemsForType({
+  type,
+  register,
+  control,
+  setValue,
+  setExpenseDetail,
+  errors,
+}) {
+  const { t } = useTranslation();
+  const { fields, append, remove } = useFieldArray({ control, name: type.id });
+
+  return (
+    <div className={styles.expenseItemsForType}>
+      <Main className={styles.type}>{type.name}</Main>
+      {fields && fields.length ? (
+        <div className={styles.box}>
+          {fields.map((field, index) => {
+            return (
+              <ExpenseItemInputs
+                key={field.id}
+                itemNameFieldName={NOTE}
+                valueFieldName={VALUE}
+                onRemove={() => {
+                  remove(index);
+                  setExpenseDetail();
+                }}
+                register={(fieldName, options) =>
+                  register(`${type.id}.${index}.${fieldName}`, options)
+                }
+                onChange={(e, fieldName) => {
+                  setValue(`${type.id}.${index}.${fieldName}`, e.target.value);
+                  setExpenseDetail();
+                }}
+                getErrors={(fieldName) =>
+                  getInputErrors(errors, `${type.id}.${index}.${fieldName}`)
+                }
+              />
+            );
+          })}
+        </div>
+      ) : null}
+      <AddLink onClick={append}>{t('common:ADD_ANOTHER_ITEM')}</AddLink>
+    </div>
+  );
+}
+
+ExpenseItemsForType.propTypes = {
+  type: PropTypes.string,
+  register: PropTypes.func,
+  control: PropTypes.func,
+  setValue: PropTypes.func,
+  setExpenseDetail: PropTypes.func,
+  errors: PropTypes.object,
+};

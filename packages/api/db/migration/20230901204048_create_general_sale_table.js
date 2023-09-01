f39541217fcbb2662e4731c8d088dff29684c7b2
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-09-01 14:38:51 -0700

    LF-3592 create migration for general_sale table

---
+++ packages/api/db/migration/20230901204048_create_general_sale_table.js
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async function (knex) {
+  await knex.schema.createTable('general_sale', (table) => {
+    table.integer('sale_id').references('sale_id').inTable('sale');
+    table.integer('sale_value');
+    table.string('notes');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async function (knex) {
+  await knex.schema.dropTable('general_sale');
+};

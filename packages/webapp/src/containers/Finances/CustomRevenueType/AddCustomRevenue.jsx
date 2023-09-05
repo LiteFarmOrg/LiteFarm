a7b2e607301bd63f79a49790c10eeefeb4c1eb32
Duncan-Brain <dbrain@litefarm.org>
2023-09-05 15:24:09 -0400

    LF-3588 - Add custom revenue added

---
+++ packages/webapp/src/containers/Finances/CustomRevenueType/AddCustomRevenue.jsx
@@ -0,0 +1,63 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import PureSimpleCustomType from '../../../components/Forms/SimpleCustomType';
+import { HookFormPersistProvider } from '../../hooks/useHookFormPersist/HookFormPersistProvider';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCustomRevenueType } from '../saga';
+import { revenueTypesSelector } from '../../revenueTypeSlice';
+import { CUSTOM_REVENUE_NAME } from './constants';
+
+function AddCustomRevenue({ history }) {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const revenueTypes = useSelector(revenueTypesSelector);
+
+  const handleGoBack = () => {
+    history.back();
+  };
+
+  const onSubmit = (payload) => {
+    dispatch(addCustomRevenueType(payload));
+  };
+
+  const validateUniqueTypeName = (value) => {
+    const revenueNameExists = revenueTypes.some((type) => {
+      return type.revenue_name === value;
+    });
+
+    if (revenueNameExists) {
+      return t('REVENUE.ADD_REVENUE.DUPLICATE_NAME');
+    }
+    return true;
+  };
+
+  return (
+    <HookFormPersistProvider>
+      <PureSimpleCustomType
+        handleGoBack={handleGoBack}
+        onSubmit={onSubmit}
+        view="add"
+        buttonText={t('common:SAVE')}
+        pageTitle={t('REVENUE.ADD_REVENUE.ADD_CUSTOM_REVENUE')}
+        inputLabel={t('REVENUE.ADD_REVENUE.CUSTOM_REVENUE_NAME')}
+        customTypeRegister={CUSTOM_REVENUE_NAME}
+        validateInput={validateUniqueTypeName}
+      />
+    </HookFormPersistProvider>
+  );
+}
+
+export default AddCustomRevenue;
---
+++ packages/webapp/src/containers/Finances/CustomRevenueType/constants.js
@@ -0,0 +1,1 @@
+export const CUSTOM_REVENUE_NAME = 'custom_revenue_name';

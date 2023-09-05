95a773f219d31994a780dbcfa24b63ad451ba9ea
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-09-05 15:45:01 -0700

    LF-3592 create AddGeneralTypeIncome component

---
+++ packages/webapp/src/components/Finances/AddIncome/AddGeneralTypeIncome.jsx
@@ -0,0 +1,98 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import Form from '../../Form';
+import Button from '../../Form/Button';
+import Input, { getInputErrors } from '../../Form/Input';
+import InputAutoSize from '../../Form/InputAutoSize';
+import PageTitle from '../../PageTitle/v2';
+// TODO: replace the function in https://github.com/LiteFarmOrg/LiteFarm/pull/2858
+import { getDateInputFormat } from '../../../util/moment';
+// import { getLocalDateInYYYYDDMM } from '../../../util/date';
+
+const SALE_DATE = 'sale_date';
+const SALE_CUSTOMER = 'customer_name';
+const GENERAL_SALE = 'general_sale';
+const SALE_VALUE = `${GENERAL_SALE}.sale_value`;
+const NOTES = `${GENERAL_SALE}.notes`;
+
+const AddGeneralTypeIncome = ({ onSubmit, title, dateLabel, customerLabel, currency, sale }) => {
+  const { t } = useTranslation();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      [SALE_DATE]: sale?.[SALE_DATE] ? getDateInputFormat(sale[SALE_DATE]) : undefined,
+      [SALE_CUSTOMER]: sale?.[SALE_CUSTOMER] || '',
+      [GENERAL_SALE]: sale?.[GENERAL_SALE] || {
+        sale_value: null,
+        notes: '',
+      },
+    },
+  });
+
+  return (
+    <Form
+      onSubmit={handleSubmit(onSubmit)}
+      buttonGroup={
+        <Button disabled={!isValid} fullLength type={'submit'}>
+          {t('common:SAVE')}
+        </Button>
+      }
+    >
+      <PageTitle title={title} onGoBack={() => history.back()} style={{ marginBottom: '24px' }} />
+      <Input
+        label={customerLabel}
+        hookFormRegister={register(SALE_CUSTOMER, { required: true })}
+        style={{ marginBottom: '40px' }}
+        errors={getInputErrors(errors, SALE_CUSTOMER)}
+        type={'text'}
+      />
+      <Input
+        label={dateLabel}
+        hookFormRegister={register(SALE_DATE, { required: true })}
+        style={{ marginBottom: '40px' }}
+        type={'date'}
+        errors={getInputErrors(errors, SALE_DATE)}
+      />
+      <Input
+        label={t('SALE.DETAIL.VALUE')}
+        type="number"
+        hookFormRegister={register(SALE_VALUE, { required: true, valueAsNumber: true })}
+        currency={currency}
+        style={{ marginBottom: '40px' }}
+        errors={getInputErrors(errors, SALE_VALUE)}
+      />
+      <InputAutoSize
+        style={{}}
+        label={t('LOG_COMMON.NOTES')}
+        optional={true}
+        hookFormRegister={register(NOTES, {
+          maxLength: { value: 10000, message: t('ADD_TASK.TASK_NOTES_CHAR_LIMIT') },
+        })}
+        name={NOTES}
+        errors={errors[NOTES]?.message}
+      />
+    </Form>
+  );
+};
+
+export default AddGeneralTypeIncome;

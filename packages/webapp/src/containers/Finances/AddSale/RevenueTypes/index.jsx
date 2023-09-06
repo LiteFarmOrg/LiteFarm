e538f02a4dc8b049dba5b823ca141dc48407259f
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-09-06 12:09:27 -0700

    LF-3583 create RevenueTypes container

---
+++ packages/webapp/src/containers/Finances/AddSale/RevenueTypes/index.jsx
@@ -0,0 +1,72 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import { HookFormPersistProvider } from '../../../hooks/useHookFormPersist/HookFormPersistProvider';
+import { setFormData } from '../../../hooks/useHookFormPersist/hookFormPersistSlice';
+import { ReactComponent as CropSaleIcon } from '../../../../assets/images/log/crop_sale.svg';
+import { ReactComponent as CustomTypeIcon } from '../../../../assets/images/log/custom_revenue.svg';
+import PureFinanceTypeSelection from '../../../../components/Finances/PureFinanceTypeSelection';
+import useSortedRevenueTypes from './useSortedRevenueTypes';
+import styles from '../../NewExpense/ExpenseCategories/styles.module.scss';
+
+export const icons = {
+  CROP_SALE: <CropSaleIcon />,
+  CUSTOM: <CustomTypeIcon />,
+};
+
+export default function RevenueTypes({ useHookFormPersist, history }) {
+  const { t } = useTranslation(['translation', 'revenue']);
+  const dispatch = useDispatch();
+  const revenueTypes = useSortedRevenueTypes();
+
+  const onTileClick = (typeId) => {
+    dispatch(setFormData({ revenue_type_id: typeId }));
+    history.push('/add_sale');
+  };
+
+  return (
+    <HookFormPersistProvider>
+      <PureFinanceTypeSelection
+        title={t('SALE.ADD_SALE.ADD_REVENUE')}
+        leadText={t('SALE.ADD_SALE.WHICH_TYPE_TO_RECORD')}
+        cancelTitle={t('SALE.ADD_SALE.FLOW')}
+        types={revenueTypes}
+        onGoBack={history.back}
+        progressValue={33}
+        onGoToManageCustomType={() => history.push('/manage_custom_revenues')}
+        formatTileData={(data) => {
+          const { farm_id, revenue_translation_key, revenue_type_id, revenue_name } = data;
+
+          return {
+            key: revenue_type_id,
+            tileKey: revenue_type_id,
+            icon: icons[farm_id ? 'CUSTOM' : revenue_translation_key],
+            label: farm_id ? revenue_name : t(`revenue:${revenue_translation_key}`),
+            onClick: () => onTileClick(revenue_type_id),
+            className: styles.labelIcon,
+          };
+        }}
+        useHookFormPersist={useHookFormPersist}
+      />
+    </HookFormPersistProvider>
+  );
+}
+
+RevenueTypes.propTypes = {
+  useHookFormPersist: PropTypes.func,
+  history: PropTypes.object,
+};

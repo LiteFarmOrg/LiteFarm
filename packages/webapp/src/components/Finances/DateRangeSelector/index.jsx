a4accf2bcf3a37af3e5d6161af3dfd18efecc904
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2023-09-07 13:43:19 -0700

    LF-3615 Convert DateRangeSelector component

--- packages/webapp/src/components/Finances/DateRangeSelector/index.jsx
+++ packages/webapp/src/components/Finances/DateRangeSelector/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './styles.module.scss';
 import { FromToDateContainer } from '../../../components/Inputs/DateContainer';
 import { setDateRange } from '../../../containers/Finances/actions';
@@ -7,100 +7,79 @@ import moment from 'moment';
 import InfoBoxComponent from '../../InfoBoxComponent';
 import { dateRangeSelector } from '../../../containers/Finances/selectors';
 import { Semibold } from '../../Typography';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-class DateRangeSelector extends Component {
-  constructor(props) {
-    super(props);
-    let startDate, endDate, validRange;
-    const { dateRange } = this.props;
-    if (dateRange && dateRange.startDate && dateRange.endDate) {
-      startDate = moment(dateRange.startDate);
-      endDate = moment(dateRange.endDate);
-    } else {
-      startDate = moment().startOf('year');
-      endDate = moment().endOf('year');
-    }
-    startDate <= endDate ? (validRange = true) : (validRange = false);
+const DateRangeSelector = ({ changeDateMethod, hideTooltip }) => {
+  const dateRange = useSelector((state) => dateRangeSelector(state));
+  const dispatch = useDispatch();
 
-    this.state = {
-      startDate,
-      endDate,
-      validRange,
-    };
-    this.changeStartDate.bind(this);
-    this.changeEndDate.bind(this);
-  }
+  const { t } = useTranslation();
 
-  changeStartDate = (date) => {
-    if (date > this.state.endDate) {
-      this.setState({ validRange: false });
+  const [startDate, setStartDate] = useState(
+    dateRange && dateRange.startDate ? moment(dateRange.startDate) : moment().startOf('year'),
+  );
+  const [endDate, setEndDate] = useState(
+    dateRange && dateRange.endDate ? moment(dateRange.endDate) : moment().endOf('year'),
+  );
+  const [validRange, setValidRange] = useState(startDate <= endDate);
+
+  useEffect(() => {
+    setStartDate(
+      dateRange && dateRange.startDate ? moment(dateRange.startDate) : moment().startOf('year'),
+    );
+    setEndDate(dateRange && dateRange.endDate ? moment(dateRange.endDate) : moment().endOf('year'));
+  }, [dateRange]);
+
+  const changeStartDate = (date) => {
+    if (date > endDate) {
+      setValidRange(false);
       return;
     }
-    this.setState({ validRange: true });
-    this.setState({ startDate: date });
-    const endDate = this.state.endDate;
-    this.props.dispatch(setDateRange({ startDate: date, endDate }));
-    this.props.changeDateMethod('start', date);
+    setValidRange(true);
+    setStartDate(date);
+    dispatch(setDateRange({ startDate: date, endDate }));
+    changeDateMethod('start', date);
   };
 
-  changeEndDate = (date) => {
-    if (date < this.state.startDate) {
-      this.setState({ validRange: false });
+  const changeEndDate = (date) => {
+    if (date < startDate) {
+      setValidRange(false);
       return;
     }
-    this.setState({ validRange: true });
-    this.setState({ endDate: date });
-    const startDate = this.state.startDate;
-    this.props.dispatch(setDateRange({ startDate, endDate: date }));
-    this.props.changeDateMethod('end', date);
+    setValidRange(true);
+    setEndDate(date);
+    dispatch(setDateRange({ startDate, endDate: date }));
+    changeDateMethod('end', date);
   };
 
-  render() {
-    const { hideTooltip } = this.props;
-    const changeDateToParent = this.props.changeDateMethod;
-
-    return (
-      <div className={styles.rangeContainer}>
-        <div className={styles.titleContainer}>
-          <Semibold style={{ textAlign: 'left', marginBottom: '20px' }}>
-            {this.props.t('DATE_RANGE.TITLE')}
-          </Semibold>
-          {!hideTooltip && (
-            <InfoBoxComponent
-              customStyle={{ float: 'right' }}
-              title={this.props.t('DATE_RANGE.HELP_TITLE')}
-              body={this.props.t('DATE_RANGE.HELP_BODY')}
-            />
-          )}
-        </div>
-
-        <FromToDateContainer
-          onStartDateChange={this.changeStartDate}
-          onEndDateChange={this.changeEndDate}
-          endDate={this.state.endDate}
-          startDate={this.state.startDate}
-        />
-        {!this.state.validRange && (
-          <Semibold style={{ textAlign: 'center', color: 'red' }}>
-            {this.props.t('DATE_RANGE.INVALID_RANGE_MESSAGE')}
-          </Semibold>
+  return (
+    <div className={styles.rangeContainer}>
+      <div className={styles.titleContainer}>
+        <Semibold style={{ textAlign: 'left', marginBottom: '20px' }}>
+          {t('DATE_RANGE.TITLE')}
+        </Semibold>
+        {!hideTooltip && (
+          <InfoBoxComponent
+            customStyle={{ float: 'right' }}
+            title={t('DATE_RANGE.HELP_TITLE')}
+            body={t('DATE_RANGE.HELP_BODY')}
+          />
         )}
       </div>
-    );
-  }
-}
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    dispatch,
-  };
+      <FromToDateContainer
+        onStartDateChange={changeStartDate}
+        onEndDateChange={changeEndDate}
+        endDate={endDate}
+        startDate={startDate}
+      />
+      {!validRange && (
+        <Semibold style={{ textAlign: 'center', color: 'red' }}>
+          {t('DATE_RANGE.INVALID_RANGE_MESSAGE')}
+        </Semibold>
+      )}
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    dateRange: dateRangeSelector(state),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(withTranslation()(DateRangeSelector));
+export default DateRangeSelector;

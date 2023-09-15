865838756d55e6dea4d59275162213f5a9d7a3b8
Mika Saryan <msaryan@formic.co>
2023-09-15 11:23:12 -0700

    Add code coverage reporting

--- packages/end-to-end/cypress.config.js
+++ packages/end-to-end/cypress.config.js
@@ -11,5 +11,13 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
   },
 });

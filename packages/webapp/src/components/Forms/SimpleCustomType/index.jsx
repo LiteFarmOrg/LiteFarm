1b1746bd0934c560cc18f0b216ba802d89013c18
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2023-09-22 14:26:49 -0700

    LF-3595 Add RadioGroups to PureSimpleCustomType

--- packages/webapp/src/components/Forms/SimpleCustomType/index.jsx
+++ packages/webapp/src/components/Forms/SimpleCustomType/index.jsx
@@ -21,10 +21,11 @@ import PageTitle from '../../PageTitle/v2';
 import Input, { getInputErrors } from '../../Form/Input';
 import Button from '../../Form/Button';
 import PropTypes from 'prop-types';
-import { IconLink } from '../../Typography';
+import { IconLink, Main } from '../../Typography';
 import { MdOutlineInventory2 } from 'react-icons/md';
 import DeleteBox from '../../Task/TaskReadOnly/DeleteBox';
 import { hookFormMaxCharsValidation } from '../../Form/hookformValidationUtils';
+import RadioGroup from '../../Form/RadioGroup';
 
 /**
  * React component for the addition of custom type with just a name field this form has add,
@@ -66,6 +67,8 @@ const PureSimpleCustomType = ({
   inputMaxChars = 100,
   validateInput,
   useHookFormPersist = () => ({}),
+  agricultureRadio,
+  cropGeneratedRadio,
 }) => {
   const { t } = useTranslation();
   const [isDeleting, setIsDeleting] = useState(false);
@@ -73,10 +76,16 @@ const PureSimpleCustomType = ({
   const {
     handleSubmit,
     register,
+    control,
+    getValues,
     formState: { errors, isValid, isDirty },
   } = useForm({
     mode: 'onChange',
-    defaultValues: { [customTypeRegister]: defaultValue || undefined },
+    defaultValues: {
+      [customTypeRegister]: defaultValue || undefined,
+      [agricultureRadio?.name]: agricultureRadio?.defaultValue,
+      [cropGeneratedRadio?.name]: cropGeneratedRadio?.defaultValue,
+    },
   });
   const readonly = view === 'read-only';
   const disabledInput = readonly;
@@ -112,6 +121,62 @@ const PureSimpleCustomType = ({
         optional={false}
         disabled={disabledInput}
       />
+      {agricultureRadio && (
+        <>
+          <Main
+            style={
+              view !== 'add'
+                ? { paddingBlock: '12px', color: 'var(--grey600)' }
+                : { paddingBlock: '12px' }
+            }
+          >
+            Is the revenue associated with agricultural activities?
+          </Main>
+
+          <RadioGroup
+            hookFormControl={control}
+            name={agricultureRadio.name}
+            radios={[
+              {
+                label: 'Yes',
+                value: true,
+              },
+              { label: 'No', value: false },
+            ]}
+            required
+            style={{ paddingBottom: '16px' }}
+            disabled={view !== 'add'}
+          />
+
+          {getValues(agricultureRadio.name) && (
+            <>
+              <Main
+                style={
+                  view !== 'add'
+                    ? { paddingBottom: '12px', color: 'var(--grey600)' }
+                    : { paddingBottom: '12px' }
+                }
+              >
+                Is this revenue generated from crops?
+              </Main>
+              <RadioGroup
+                hookFormControl={control}
+                name={cropGeneratedRadio.name}
+                radios={[
+                  {
+                    label: 'Yes',
+                    value: true,
+                  },
+                  { label: 'No', value: false },
+                ]}
+                required={getValues(agricultureRadio.name)}
+                style={{ paddingBottom: '16px' }}
+                disabled={view !== 'add'}
+              />
+            </>
+          )}
+        </>
+      )}
       <div style={{ marginTop: 'auto' }}>
         {readonly && !isDeleting && (
           <IconLink

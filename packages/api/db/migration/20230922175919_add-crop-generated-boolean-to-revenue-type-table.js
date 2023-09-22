4d1f4dec535d008fb66fa36b6dfc99c13ab1cbb7
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2023-09-22 11:06:17 -0700

    LF-3595 Add revenue_type table migration

---
+++ packages/api/db/migration/20230922175919_add-crop-generated-boolean-to-revenue-type-table.js
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (c) 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async function (knex) {
+  await knex.schema.alterTable('revenue_type', (table) => {
+    table.boolean('crop_generated').defaultTo(false);
+  });
+
+  await knex('revenue_type').where('revenue_name', 'Crop Sale').update({
+    crop_generated: true,
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async function (knex) {
+  await knex.schema.alterTable('revenue_type', (table) => {
+    table.dropColumn('crop_generated');
+  });
+};

d1508f2d776f7954b15a28b7ee7e2a0e3e9acf3f
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-09-26 13:06:42 -0700

    LF-3637 create constants for DateRangeSelector

---
+++ packages/webapp/src/components/DateRangeSelector/constants.jsx
@@ -0,0 +1,28 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+export const DATE_RANGE = 'date_range';
+
+export const dateRangeOptions = {
+  THIS_YEAR: 'this_year',
+  LAST_7_DAYS: 'last_seven_days',
+  LAST_14_DAYS: 'last_fourteen_days',
+  LAST_30_DAYS: 'last_thirty_days',
+  THIS_WEEK: 'this_week',
+  LAST_WEEK: 'last_week',
+  THIS_MONTH: 'this_month',
+  LAST_MONTH: 'last_month',
+  CUSTOM: 'custom_range',
+};

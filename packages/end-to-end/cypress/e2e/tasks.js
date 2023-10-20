ca95d12c88bed81af3cd010ef6c788f25d6ee6d9
Mika Saryan <msaryan@formic.co>
2023-10-20 12:55:18 -0700

    Confirm that location exists before opening tasks

--- packages/end-to-end/cypress/e2e/tasks.js
+++ packages/end-to-end/cypress/e2e/tasks.js
@@ -50,6 +50,18 @@ describe('Tasks', () => {
   };
 
   it('CheckTasksNavigation', () => {
+    // Confirm that location exists
+    cy.get('[data-cy=home-farmButton]')
+      .should('exist')
+      .and('not.be.disabled')
+      .click({ force: true });
+    cy.get('[data-cy=navbar-option]')
+      .eq(1)
+      .should('exist')
+      .and('not.be.disabled')
+      .click({ force: true });
+    cy.contains('First Field').should('be.visible');
+
     // Add a crop variety
     cy.get('[data-cy=navbar-hamburger]').should('exist').click();
     cy.contains(translation['SLIDE_MENU']['TASKS']).should('exist').click();
@@ -63,6 +75,18 @@ describe('Tasks', () => {
   });
 
   it('CreateCleanTask', () => {
+    // Confirm that location exists
+    cy.get('[data-cy=home-farmButton]')
+      .should('exist')
+      .and('not.be.disabled')
+      .click({ force: true });
+    cy.get('[data-cy=navbar-option]')
+      .eq(1)
+      .should('exist')
+      .and('not.be.disabled')
+      .click({ force: true });
+    cy.contains('First Field').should('be.visible');
+
     cy.get('[data-cy=navbar-hamburger]').should('exist').click();
     cy.contains(translation['SLIDE_MENU']['TASKS']).should('exist').click();
     cy.waitForReact();

ec7857ad813cdd66104c8925e131767b23e402d9
Mika Saryan <msaryan@formic.co>
2023-10-20 16:28:11 -0700

    Use get call to read location

--- packages/end-to-end/cypress/e2e/tasks.js
+++ packages/end-to-end/cypress/e2e/tasks.js
@@ -50,6 +50,35 @@ describe('Tasks', () => {
             translation['FARM_MAP']['MAP_FILTER']['FIELD'],
           );
 
+          // Get location data from API
+          cy.window()
+            .its('localStorage')
+            .invoke('getItem', 'id_token')
+            .then((token) => {
+              cy.window()
+                .its('store')
+                .invoke('getState')
+                .its('entitiesReducer')
+                .its('userFarmReducer')
+                .its('farm_id')
+                .then((farm_id) => {
+                  // Do something with farm_id
+                  console.log(farm_id);
+
+                  cy.request({
+                    method: 'GET',
+                    url: 'http://localhost:5001/location/farm/' + farm_id,
+                    headers: {
+                      Authorization: 'Bearer ' + token,
+                      farm_id: farm_id,
+                    },
+                  }).then((response) => {
+                    console.log(response); // Log the entire response
+                    console.log(response.body); // Log just the response body if preferred
+                  });
+                });
+            });
+
           const endTime = new Date().getTime() + 180000; // Set the end time to 3 min seconds from now
           checkReduxState(endTime);
         },

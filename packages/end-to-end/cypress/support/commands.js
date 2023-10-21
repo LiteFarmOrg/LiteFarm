299dec7a3790891068bd94df7597617922bc6903
Mika Saryan <msaryan@formic.co>
2023-10-20 17:05:19 -0700

    Add location data back from API after it was sent

--- packages/end-to-end/cypress/support/commands.js
+++ packages/end-to-end/cypress/support/commands.js
@@ -208,6 +208,35 @@ Cypress.Commands.add('createFirstLocation', (fieldString) => {
     .its('fieldReducer')
     .its('ids')
     .should('not.be.empty');
+
+  // Get location data from API
+  cy.window()
+    .its('localStorage')
+    .invoke('getItem', 'id_token')
+    .then((token) => {
+      cy.window()
+        .its('store')
+        .invoke('getState')
+        .its('entitiesReducer')
+        .its('userFarmReducer')
+        .its('farm_id')
+        .then((farm_id) => {
+          // Do something with farm_id
+          console.log(farm_id);
+
+          cy.request({
+            method: 'GET',
+            url: 'http://localhost:5001/location/farm/' + farm_id,
+            headers: {
+              Authorization: 'Bearer ' + token,
+              farm_id: farm_id,
+            },
+          }).then((response) => {
+            console.log(response); // Log the entire response
+            console.log(response.body); // Log just the response body if preferred
+          });
+        });
+    });
 });
 
 Cypress.Commands.add('acceptSlideMenuSpotlights', (crop_menu_name) => {

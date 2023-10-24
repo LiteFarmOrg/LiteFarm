355a7f943aaeca1c54ed23ffa84dbed2558b214d
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2023-10-24 10:52:17 -0700

    LF-3757 Add data migration

---
+++ packages/api/db/migration/20231024164318_null_agricuture_associated.js
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async function (knex) {
+  await knex('revenue_type').update({
+    agriculture_associated: null,
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async function (knex) {
+  /* Down function will set agricuture_associated to its default value, but any previous data in this column cannot be restored -- i.e. the data migration is irreversible */
+
+  await knex('revenue_type').update({
+    agriculture_associated: true,
+  });
+};

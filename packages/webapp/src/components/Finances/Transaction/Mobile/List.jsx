39b0a6a16997a1b74f809677ec2dc9b1d86c92f6
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-10-30 14:28:23 -0700

    LF-3745 convert generateRows into component in TransactionList

--- packages/webapp/src/components/Finances/Transaction/Mobile/List.jsx
+++ packages/webapp/src/components/Finances/Transaction/Mobile/List.jsx
@@ -35,7 +35,7 @@ export const MainContent = ({ t, note, typeLabel, amount, icon, currencySymbol }
   );
 };
 
-const generateRows = (t, data, currencySymbol) => {
+const Rows = ({ t, data, currencySymbol }) => {
   const { expandedIds, toggleExpanded } = useExpandable({ isSingleExpandable: true });
   const rows = [];
 
@@ -61,7 +61,7 @@ const generateRows = (t, data, currencySymbol) => {
       </div>,
     );
   });
-  return rows;
+  return <div>{rows}</div>;
 };
 
 export default function TransactionList({ data, minRows = 10 }) {
@@ -76,7 +76,7 @@ export default function TransactionList({ data, minRows = 10 }) {
 
   return (
     <div className={styles.transactionList}>
-      <div>{generateRows(t, data.slice(0, visibleRows), currencySymbol)}</div>
+      <Rows t={t} data={data.slice(0, visibleRows)} currencySymbol={currencySymbol} />
       {data.length > visibleRows && (
         <div className={styles.buttonWrapper}>
           <Button

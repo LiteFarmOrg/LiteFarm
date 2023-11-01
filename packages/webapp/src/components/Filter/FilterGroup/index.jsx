dc3acdc0520d01225199288f3c281ad595218f54
Antonella Sgarlatta <antsgar@gmail.com>
2023-11-01 15:15:00 -0300

    LF-3752 add prop types and license

--- packages/webapp/src/components/Filter/FilterGroup/index.jsx
+++ packages/webapp/src/components/Filter/FilterGroup/index.jsx
@@ -1,4 +1,18 @@
-import React from 'react';
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://wwwl.gnu.org/licenses/>.
+ */
+
 import PropTypes from 'prop-types';
 import FilterPillSelect from '../FilterPillSelect';
 import { DATE, DATE_RANGE, PILL_SELECT, SEARCHABLE_MULTI_SELECT } from '../filterTypes';
@@ -50,6 +64,27 @@ const FilterGroup = ({ filters, filterRef, onChange, shouldReset }) => {
       return <FilterDate {...filter} key={filter.subject} filterRef={filterRef} />;
     }
   });
+};
+
+FilterGroup.propTypes = {
+  filters: PropTypes.arrayOf(
+    PropTypes.shape({
+      subject: PropTypes.string,
+      filterKey: PropTypes.string.isRequired,
+      options: PropTypes.arrayOf(
+        PropTypes.shape({
+          value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+          default: PropTypes.bool,
+          label: PropTypes.string.isRequired,
+        }),
+      ),
+    }),
+  ).isRequired,
+  filterRef: PropTypes.shape({
+    current: PropTypes.shape({ active: PropTypes.bool, label: PropTypes.string }),
+  }).isRequired,
+  onChange: PropTypes.func,
+  shouldReset: PropTypes.bool,
 };
 
 export default FilterGroup;
--- packages/webapp/src/containers/Filter/Transactions/index.jsx
+++ packages/webapp/src/containers/Filter/Transactions/index.jsx
@@ -1,4 +1,19 @@
-import React from 'react';
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { EXPENSE_TYPE, REVENUE_TYPE } from '../constants';
@@ -44,6 +59,14 @@ const TransactionFilterContent = ({ transactionsFilter, filterRef, onChange }) =
       <FilterGroup filters={filters} filterRef={filterRef} onChange={onChange} />
     </>
   );
+};
+
+const filterShape = { active: PropTypes.bool, label: PropTypes.string };
+
+TransactionFilterContent.propTypes = {
+  transactionsFilter: PropTypes.objectOf(PropTypes.shape(filterShape)).isRequired,
+  filterRef: PropTypes.shape({ current: PropTypes.shape(filterShape) }).isRequired,
+  onChange: PropTypes.func,
 };
 
 export default TransactionFilterContent;

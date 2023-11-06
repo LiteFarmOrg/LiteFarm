61ba7fa21aebe1457a42799b894441fce7c6b55a
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-11-05 23:37:36 -0800

    LF-3753 update DropdownButton custom menu styles

--- packages/webapp/src/stories/Button/DropdownButton.stories.jsx
+++ packages/webapp/src/stories/Button/DropdownButton.stories.jsx
@@ -2,6 +2,7 @@ import { forwardRef } from 'react';
 import DropdownButton from '../../components/Form/DropDownButton';
 import FloatingMenu from '../../components/Menu/FloatingButtonMenu/FloatingMenu';
 import { componentDecorators } from '../Pages/config/Decorators';
+import customizedMenuStyle from '../../components/Finances/AddTransactionButton/styles.module.scss';
 
 export default {
   title: 'Components/DropdownButton',
@@ -27,12 +28,7 @@ export const WithoutArrowIcon = {
 
 const Menu = forwardRef((props, ref) => (
   <FloatingMenu
-    classes={
-      {
-        // TODO: set width
-        // menuList:
-      }
-    }
+    classes={{ menuList: customizedMenuStyle.menuList }}
     options={[
       { label: '+ Add revenue', onClick: () => console.log('Add revenue') },
       { label: '+ Add expense', onClick: () => console.log('Add Eepense') },
@@ -47,10 +43,7 @@ export const WithCustomizedMenu = {
     children: '+ Add transaction',
     type: 'v2',
     noIcon: true,
-    classes: {
-      // TODO: set button size
-      // button:
-    },
+    classes: { button: customizedMenuStyle.button },
     Menu,
   },
 };

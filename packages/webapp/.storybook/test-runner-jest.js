be3ef9203204d0d6ae5ba713c08db890878cc6aa
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-12-19 16:22:15 -0800

    create test-runner-jest.js

---
+++ packages/webapp/.storybook/test-runner-jest.js
@@ -0,0 +1,13 @@
+import { getJestConfig } from '@storybook/test-runner';
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+export default {
+  // The default configuration comes from @storybook/test-runner
+  ...getJestConfig(),
+  /** Add your own overrides below
+   * @see https://jestjs.io/docs/configuration
+   */
+  testTimeout: 30000,
+};

7416b4a8d07705b264ac0ac6c8531c7ffd614453
Antonella Sgarlatta <antsgar@gmail.com>
2024-01-23 15:00:21 -0300

    LF-3977 remove unknown

--- packages/api/db/migration/20240115182150_create_animal_breed_tables.js
+++ packages/api/db/migration/20240115182150_create_animal_breed_tables.js
@@ -63,19 +63,19 @@ export const up = async function (knex) {
   const defaultBreedKeys = [
     {
       typeKey: 'CATTLE',
-      breedKeys: ['ANGUS', 'HEREFORD', 'CHAROLAIS', 'UNKNOWN'],
+      breedKeys: ['ANGUS', 'HEREFORD', 'CHAROLAIS'],
     },
     {
       typeKey: 'PIGS',
-      breedKeys: ['YORKSHIRE_LARGE_WHITE', 'LANDRACE', 'DUROC', 'UNKNOWN'],
+      breedKeys: ['YORKSHIRE_LARGE_WHITE', 'LANDRACE', 'DUROC'],
     },
     {
       typeKey: 'CHICKEN_BROILERS',
-      breedKeys: ['CORNISH_CROSS', 'ROSS_308', 'COBB_500', 'UNKNOWN'],
+      breedKeys: ['CORNISH_CROSS', 'ROSS_308', 'COBB_500'],
     },
     {
       typeKey: 'CHICKEN_LAYERS',
-      breedKeys: ['LEGHORN', 'RHODE_ISLAND_RED', 'PLYMOUTH_ROCK', 'UNKNOWN'],
+      breedKeys: ['LEGHORN', 'RHODE_ISLAND_RED', 'PLYMOUTH_ROCK'],
     },
   ];
 

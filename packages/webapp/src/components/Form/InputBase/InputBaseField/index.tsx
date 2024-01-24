2a6fef80f9bf8d1b9602c87b805acf2e70522a65
Navdeep <nsdhammu@gmail.com>
2024-01-24 16:52:02 -0500

    prevent blur event from firing when clicking inside wrapper div that contains input element

--- packages/webapp/src/components/Form/InputBase/InputBaseField/index.tsx
+++ packages/webapp/src/components/Form/InputBase/InputBaseField/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ReactElement } from 'react';
+import { ComponentPropsWithoutRef, ReactElement, useRef } from 'react';
 import { type InputBaseProps } from '../';
 import styles from './styles.module.scss';
 import clsx from 'clsx';
@@ -9,15 +9,19 @@ function InputBaseField(
     inputProps: ComponentPropsWithoutRef<'input'>;
   },
 ) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const showCross = !!props.crossIcon;
   return (
-    <div className={clsx(styles.input, showCross && styles.inputError)}>
+    <div
+      onMouseDown={(e) => document.activeElement === inputRef.current && e.preventDefault()}
+      className={clsx(styles.input, showCross && styles.inputError)}
+    >
       {props.leftSection && (
         <div className={styles.inputSection} data-section="left">
           {props.leftSection}
         </div>
       )}
-      <input {...props.inputProps} />
+      <input {...props.inputProps} ref={inputRef} />
       {(showCross || props.rightSection) && (
         <div className={styles.inputSection} data-section="right">
           {props.rightSection}

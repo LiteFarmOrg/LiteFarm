da050e5b40f4198ce2c53a8768ac3eabe2846052
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-02-06 12:56:10 -0800

    LF-4079 Create HoverPill component

---
+++ packages/webapp/src/components/HoverPill/index.tsx
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { useTranslation } from 'react-i18next';
+import styles from './styles.module.scss';
+import { Semibold, Main } from '../Typography';
+
+interface HoverPillProps {
+  items: string[];
+}
+
+export const HoverPill = ({ items }: HoverPillProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.pill}>
+        <Semibold className={styles.pillText}>
+          {t('HOVERPILL.PLUS_OTHERS_COUNT', { count: items.length })}
+        </Semibold>
+      </div>
+      <div className={styles.hoverDetails}>
+        {items.map((item, index) => (
+          <Main key={index} className={styles.detailText}>
+            {item}
+          </Main>
+        ))}
+      </div>
+    </div>
+  );
+};

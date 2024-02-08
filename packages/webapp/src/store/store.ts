10d8bbfc89e8126886888ae9fe325b988fc35a2c
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-02-08 12:42:15 -0800

    LF-4082 Convert Redux store to TypeScript

--- packages/webapp/src/store/store.ts
+++ packages/webapp/src/store/store.ts
@@ -1,16 +1,21 @@
 import { persistReducer, persistStore } from 'redux-persist';
-import { configureStore } from '@reduxjs/toolkit';
+import { AnyAction, configureStore } from '@reduxjs/toolkit';
 import { sagaMiddleware } from './sagaMiddleware';
 import rootReducer from './reducer';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
+
+export type RootReducer = ReturnType<typeof rootReducer>;
 
 const persistConfig = {
   key: 'root',
   storage,
   stateReconciler: autoMergeLevel2,
 };
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// https://github.com/rt2zz/redux-persist/issues/1140#issuecomment-1208523633
+const persistedReducer = persistReducer<RootReducer, AnyAction>(persistConfig, rootReducer);
+
 const middlewares = [sagaMiddleware];
 export const store = configureStore({
   reducer: persistedReducer,
@@ -28,4 +33,9 @@ export const store = configureStore({
 export const purgeState = () => {
   persistor.purge();
 };
+
 export const persistor = persistStore(store);
+
+// https://redux-toolkit.js.org/tutorials/typescript#define-root-state-and-dispatch-types
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

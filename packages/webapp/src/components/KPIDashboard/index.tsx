32334d3317fa509f11595c2655ee5c8723f3299d
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-02-12 19:39:28 -0800

    LF-4089 WIP -- Collapsing '+ more' functionality on resize

--- packages/webapp/src/components/KPIDashboard/index.tsx
+++ packages/webapp/src/components/KPIDashboard/index.tsx
@@ -13,8 +13,8 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 
-import { ReactNode } from 'react';
-import { useTranslation, Trans } from 'react-i18next';
+import { ReactNode, useState, useEffect, useLayoutEffect, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useTheme } from '@mui/styles';
 import { useMediaQuery } from '@mui/material';
 import clsx from 'clsx';
@@ -22,6 +22,13 @@ import styles from './styles.module.scss';
 import { Title } from '../Typography';
 import { SummaryTiles } from './SummaryTiles';
 import { KPITile } from './KPITile';
+import { ReactComponent as ChevronDown } from '../../assets/images/animals/chevron-down.svg';
+import TextButton from '../Form/Button/TextButton';
+import {
+  useComponentHeight,
+  useComponentWidth,
+} from '../../containers/hooks/useComponentWidthHeight';
+import Button from '../Form/Button';
 
 export interface KPI {
   label: string;
@@ -49,66 +56,97 @@ export const PureKPIDashboard = ({
   const totalCount = KPIs.reduce((sum, element) => sum + element.count, 0);
   const categoryCount = KPIs.length;
 
+  const [KPIBreakpoint, setKPIBreakpoint] = useState(0);
+  const visibleKPIs = KPIs.slice(0, KPIs.length - KPIBreakpoint);
+  const hiddenKPIs = KPIs.slice(KPIs.length - KPIBreakpoint);
+
+  const { ref, height } = useComponentHeight() as unknown as {
+    ref: React.RefObject<HTMLDivElement>;
+    height: number;
+  }; // ugh. Fix the hook instead
+  const { ref: widthRef, width } = useComponentWidth() as unknown as {
+    ref: React.RefObject<HTMLDivElement>;
+    width: number;
+  };
+  console.log({ height, width, isDesktop });
+
+  useLayoutEffect(() => {
+    setKPIBreakpoint(isDesktop ? 0 : KPIs.length);
+  }, [isDesktop]);
+
+  const adjustKPIBreakpoint = useCallback(() => {
+    const IDEAL_HEIGHT = isDesktop ? 71 : 100; // 1 row : 2 rows
+    console.log({ IDEAL_HEIGHT });
+    if (height && height > IDEAL_HEIGHT && KPIBreakpoint < KPIs.length) {
+      console.log('incrementing breakpoint');
+      setKPIBreakpoint((prevBreakpoint) => prevBreakpoint + 1);
+    } else if (height && height < IDEAL_HEIGHT && KPIBreakpoint > 0) {
+      console.log('decrementing breakpoint');
+      setKPIBreakpoint((prevBreakpoint) => prevBreakpoint - 1);
+    }
+  }, [isDesktop, height, width]);
+
+  useLayoutEffect(() => {
+    adjustKPIBreakpoint();
+  }, [height, width]);
+
+  useEffect(() => {
+    setTimeout(adjustKPIBreakpoint, 100);
+  }, []);
+
+  console.log({ KPIBreakpoint });
+
   return (
-    <div className={styles.container}>
-      <Title className={styles.title}>{dashboardTitle}</Title>
-      <div className={styles.mainContent}>
-        <SummaryTiles
-          totalCount={totalCount}
-          categoryCount={categoryCount}
-          categoryLabel={categoryLabel}
-        />
-        <div className={styles.gridContainer}>
-          {KPIs.map((item, index) => (
-            <div key={index} className={clsx(styles.gridItem, isDesktop ? styles.fixed : '')}>
+    <>
+      <div className={styles.container} ref={widthRef}>
+        <Title className={styles.title}>{dashboardTitle}</Title>
+        <div className={styles.mainContent} ref={ref}>
+          <SummaryTiles
+            totalCount={totalCount}
+            categoryCount={categoryCount}
+            categoryLabel={categoryLabel}
+          />
+          <div className={styles.gridContainer}>
+            {visibleKPIs.map((item, index) => (
+              <div key={index} className={clsx(styles.gridItem)}>
+                <KPITile key={index} {...item} />
+              </div>
+            ))}
+            {hiddenKPIs.length ? <MoreComponent moreKPIs={hiddenKPIs} width={width} /> : <></>}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+interface MoreComponentProps {
+  moreKPIs: KPI[];
+  width: number;
+}
+
+const MoreComponent = ({ moreKPIs, width }: MoreComponentProps) => {
+  const { t } = useTranslation();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={clsx(styles.moreContainer, styles.gridItem, width && width > 900 && styles.fixed)}
+    >
+      <TextButton className={clsx(styles.moreButton)} onClick={() => setIsOpen((prev) => !prev)}>
+        <span>{t('TABLE.NUMBER_MORE', { number: moreKPIs.length })} </span>
+        <ChevronDown />
+      </TextButton>
+      {isOpen && ( // TODO: add click away
+        <div className={styles.moreContent}>
+          {moreKPIs.map((item, index) => (
+            <div key={index} className={clsx(styles.contentItem)}>
               <KPITile key={index} {...item} />
             </div>
           ))}
         </div>
-      </div>
+      )}
     </div>
   );
 };
-
-// export const PureKPIDashboard = ({
-//   KPIs,
-//   dashboardTitle,
-//   categoryLabel,
-// }: PureKPIDashboardProps) => {
-//   const { t } = useTranslation();
-
-//   const totalCount = KPIs.reduce((sum, element) => {
-//     return sum + element.count;
-//   }, 0);
-
-//   const categoryCount = KPIs.length;
-
-//   return (
-//     <div className={styles.container}>
-//       <Title className={styles.title}>{dashboardTitle}</Title>
-//       <div className={styles.tileContainer}>
-//         <div className={styles.summaryTiles}>
-//           <div className={styles.totalCount}>
-//             <Main className={styles.countLabel}>total</Main>
-//             <Main className={styles.countText}>{totalCount}</Main>
-//           </div>
-//           <div className={styles.typeCount}>
-//             <Main className={styles.countLabel}>{categoryLabel}</Main>
-//             <Main className={styles.countText}>{categoryCount}</Main>
-//           </div>
-//         </div>
-//         <div className={styles.KPIcontainer}>
-//           {KPIs.map((item, index) => (
-//             <TextButton className={styles.tile} key={index} onClick={item.onClick}>
-//               <div className={styles.icon}>{item.icon}</div>
-//               <div className={styles.tileText}>
-//                 <Main className={styles.tileLabel}>{item.label}</Main>
-//                 <Main className={styles.tileCountLabel}>{item.count}</Main>
-//               </div>
-//             </TextButton>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
--- packages/webapp/src/containers/hooks/useComponentWidthHeight.js
+++ packages/webapp/src/containers/hooks/useComponentWidthHeight.js
@@ -17,3 +17,21 @@ export function useComponentWidth() {
   }, [ref, ref.current]);
   return { ref, width };
 }
+
+export function useComponentHeight() {
+  const ref = useRef();
+  const [height, setHeight] = useState();
+  const handleResize = () => {
+    if (ref && ref.current && ref.current.offsetHeight !== height) {
+      setHeight(ref.current.offsetHeight);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return (_) => window.removeEventListener('resize', handleResize);
+  });
+  useEffect(() => {
+    handleResize();
+  }, [ref, ref.current]);
+  return { ref, height };
+}

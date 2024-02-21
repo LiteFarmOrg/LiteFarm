747f76be208ab40fd8bdb42390b0368b583b1fad
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-02-21 10:04:13 -0800

    LF-4081 format SQL file

--- packages/api/db/sql/20240207214919_animal_union_batch_id_view.sql
+++ packages/api/db/sql/20240207214919_animal_union_batch_id_view.sql
@@ -1,25 +1,25 @@
 CREATE VIEW animal_union_batch_id_view AS
 SELECT
-    *,
-    ROW_NUMBER() OVER (PARTITION BY farm_id ORDER BY created_at)::INTEGER AS internal_identifier
+  *,
+  ROW_NUMBER() OVER (PARTITION BY farm_id ORDER BY created_at)::INTEGER AS internal_identifier
 FROM (
-    SELECT
-        id,
-        farm_id,
-        FALSE AS batch,
-        created_at
-    FROM
-        animal a
+  SELECT
+    id,
+    farm_id,
+    FALSE AS batch,
+    created_at
+  FROM
+    animal a
 
-    UNION ALL
+  UNION ALL
 
-    SELECT
-        id,
-        farm_id,
-        TRUE AS batch,
-        created_at
-    FROM
-        animal_batch ab
+  SELECT
+    id,
+    farm_id,
+    TRUE AS batch,
+    created_at
+  FROM
+    animal_batch ab
 ) animal_union_batch_id_view
 ORDER BY
-    created_at;
+  created_at;

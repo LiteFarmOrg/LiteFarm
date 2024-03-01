a4bd2be279962995c316848f281d539e1c4f0bc7
Duncan-Brain <dbrain@litefarm.org>
2024-03-01 09:31:11 -0500

    LF-4103 -fix tests

--- packages/api/tests/animal_batch.test.js
+++ packages/api/tests/animal_batch.test.js
@@ -177,8 +177,12 @@ describe('Animal Batch Tests', () => {
           expect(animalBatch.farm_id).toBe(mainFarm.farm_id);
           expect(animalBatch.internal_identifier).toBeGreaterThan(0);
         });
-        expect({ ...firstAnimalBatch, internal_identifier: 1 }).toMatchObject(res.body[0]);
-        expect({ ...secondAnimalBatch, internal_identifier: 2 }).toMatchObject(res.body[1]);
+        expect({ ...firstAnimalBatch, internal_identifier: 1, group_ids: [] }).toMatchObject(
+          res.body[0],
+        );
+        expect({ ...secondAnimalBatch, internal_identifier: 2, group_ids: [] }).toMatchObject(
+          res.body[1],
+        );
       }
     });
 

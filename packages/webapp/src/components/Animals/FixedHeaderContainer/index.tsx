f84537de5f2fa983ebd9ea607aaaba6719bace1f
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-03-07 16:56:19 -0800

    LF-4142 create FixedHeaderContainer component

---
+++ packages/webapp/src/components/Animals/FixedHeaderContainer/index.tsx
@@ -0,0 +1,84 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import React, { ReactElement, ReactNode, useEffect, useMemo, useRef, useState } from 'react';
+import { Paper, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/styles';
+import { sumObjectValues } from '../../../util';
+import styles from './styles.module.scss';
+
+const heights = {
+  mobile: { globalNavbar: 64 },
+  desktop: { globalNavbar: 55, paperMargin: 32 },
+};
+
+const FixedHeaderContainer = ({ header, children }: { header: ReactNode; children: ReactNode }) => {
+  const [headerHeight, setHeaderHeight] = useState<number | null>(null);
+  const [paperHeightInPx, setPaperHeightInPx] = useState<number | null>(null);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const headerRef = useRef<HTMLDivElement>(null);
+  const paperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const setNewPaperHeight = () => setHeaderHeight(headerRef.current?.offsetHeight || null);
+    const setPaperHeight = () => setPaperHeightInPx(paperRef.current?.offsetHeight || null);
+
+    setNewPaperHeight();
+    setPaperHeight();
+
+    window.addEventListener('resize', () => {
+      setNewPaperHeight();
+      setPaperHeight();
+    });
+
+    return () => {
+      window.removeEventListener('resize', () => {
+        setNewPaperHeight();
+        setPaperHeight();
+      });
+    };
+  }, []);
+
+  const paperHeightCSS = useMemo<string | undefined>(() => {
+    const usedPx = sumObjectValues(heights[isMobile ? 'mobile' : 'desktop']) + (headerHeight || 0);
+    return `calc(100vh - ${usedPx}px)`;
+  }, [isMobile, headerHeight]);
+
+  const childrenWithProps = useMemo(() => {
+    // Provide the 'containerHeight' prop to children components,
+    // allowing them to adjust their layout based on the height of the container.
+    return React.Children.map(children, (child) => {
+      if (React.isValidElement(child)) {
+        return React.cloneElement(child as ReactElement<any>, {
+          containerHeight: paperHeightInPx,
+        });
+      }
+      return child;
+    });
+  }, [children, paperHeightInPx]);
+
+  return (
+    <>
+      <div ref={headerRef}>{header}</div>
+      <Paper ref={paperRef} className={styles.paper} sx={{ height: paperHeightCSS }}>
+        {childrenWithProps}
+      </Paper>
+    </>
+  );
+};
+
+export default FixedHeaderContainer;

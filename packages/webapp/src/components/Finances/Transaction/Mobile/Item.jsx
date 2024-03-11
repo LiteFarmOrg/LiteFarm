5af0c334da6cab7f31d2c9031cc9b69bdf0ca2da
Duncan-Brain <dbrain@litefarm.org>
2024-03-11 15:01:12 -0400

    bugfix-icons - use circle icon with transactions

--- packages/webapp/src/components/Finances/Transaction/Mobile/Item.jsx
+++ packages/webapp/src/components/Finances/Transaction/Mobile/Item.jsx
@@ -13,46 +13,47 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 import clsx from 'clsx';
-import { ReactComponent as CropSaleIcon } from '../../../../assets/images/finance/transaction/Crop-sale-icn.svg';
-import { ReactComponent as CustomTypeIcon } from '../../../../assets/images/finance/transaction/Custom-revenue.svg';
-import { ReactComponent as EquipIcon } from '../../../../assets/images/finance/transaction/Equipment-icn.svg';
-import { ReactComponent as SoilAmendmentIcon } from '../../../../assets/images/finance/transaction/Soil-amendment-icn.svg';
-import { ReactComponent as PestIcon } from '../../../../assets/images/finance/transaction/Pest-icn.svg';
-import { ReactComponent as FuelIcon } from '../../../../assets/images/finance/transaction/Fuel-icn.svg';
-import { ReactComponent as MachineIcon } from '../../../../assets/images/finance/transaction/Machinery-icn.svg';
-import { ReactComponent as SeedIcon } from '../../../../assets/images/finance/transaction/Seeds-icn.svg';
-import { ReactComponent as OtherIcon } from '../../../../assets/images/finance/transaction/Custom-expense.svg';
-import { ReactComponent as LandIcon } from '../../../../assets/images/finance/transaction/Land-icn.svg';
-import { ReactComponent as MiscellaneousIcon } from '../../../../assets/images/finance/transaction/Miscellaneous-icn.svg';
-import { ReactComponent as UtilitiesIcon } from '../../../../assets/images/finance/transaction/Utilities-icn.svg';
-import { ReactComponent as LabourIcon } from '../../../../assets/images/finance/transaction/Labour-icn.svg';
-import { ReactComponent as InfrastructureIcon } from '../../../../assets/images/finance/transaction/Infrastructure-icn.svg';
-import { ReactComponent as TransportationIcon } from '../../../../assets/images/finance/transaction/Transportation-icn.svg';
-import { ReactComponent as ServicesIcon } from '../../../../assets/images/finance/transaction/Services-icn.svg';
+import { ReactComponent as CropSaleIcon } from '../../../../assets/images/finance/n/crops.svg';
+import { ReactComponent as CustomTypeIcon } from '../../../../assets/images/finance/n/Custom-revenue.svg';
+import { ReactComponent as EquipIcon } from '../../../../assets/images/finance/n/Equipment-icn.svg';
+import { ReactComponent as SoilAmendmentIcon } from '../../../../assets/images/finance/n/Soil-amendment-icn.svg';
+import { ReactComponent as PestIcon } from '../../../../assets/images/finance/n/Pest-icn.svg';
+import { ReactComponent as FuelIcon } from '../../../../assets/images/finance/n/Fuel-icn.svg';
+import { ReactComponent as MachineIcon } from '../../../../assets/images/finance/n/Machinery-icn.svg';
+import { ReactComponent as SeedIcon } from '../../../../assets/images/finance/n/Seeds-icn.svg';
+import { ReactComponent as OtherIcon } from '../../../../assets/images/finance/n/Custom-expense.svg';
+import { ReactComponent as LandIcon } from '../../../../assets/images/finance/n/Land-icn.svg';
+import { ReactComponent as MiscellaneousIcon } from '../../../../assets/images/finance/n/Miscellaneous-icn.svg';
+import { ReactComponent as UtilitiesIcon } from '../../../../assets/images/finance/n/Utilities-icn.svg';
+import { ReactComponent as LabourIcon } from '../../../../assets/images/finance/n/Labour-icn.svg';
+import { ReactComponent as InfrastructureIcon } from '../../../../assets/images/finance/n/Infrastructure-icn.svg';
+import { ReactComponent as TransportationIcon } from '../../../../assets/images/finance/n/Transportation-icn.svg';
+import { ReactComponent as ServicesIcon } from '../../../../assets/images/finance/n/Services-icn.svg';
 import { formatAmount } from '../../../../containers/Finances/util';
 import commonStyles from '../styles.module.scss';
 import styles from './styles.module.scss';
+import { FramedIcon } from '../../../Icons';
 
 const revenueTypeIcons = {
-  CROP_SALE: <CropSaleIcon />,
-  CUSTOM: <CustomTypeIcon />,
+  CROP_SALE: CropSaleIcon,
+  CUSTOM: CustomTypeIcon,
 };
 
 const expenseTypeIcons = {
-  EQUIPMENT: <EquipIcon />,
-  SOIL_AMENDMENT: <SoilAmendmentIcon />,
-  PEST_CONTROL: <PestIcon />,
-  FUEL: <FuelIcon />,
-  MACHINERY: <MachineIcon />,
-  SEEDS_AND_PLANTS: <SeedIcon />,
-  OTHER: <OtherIcon />,
-  LAND: <LandIcon />,
-  MISCELLANEOUS: <MiscellaneousIcon />,
-  UTILITIES: <UtilitiesIcon />,
-  LABOUR: <LabourIcon />,
-  INFRASTRUCTURE: <InfrastructureIcon />,
-  TRANSPORTATION: <TransportationIcon />,
-  SERVICES: <ServicesIcon />,
+  EQUIPMENT: EquipIcon,
+  SOIL_AMENDMENT: SoilAmendmentIcon,
+  PEST_CONTROL: PestIcon,
+  FUEL: FuelIcon,
+  MACHINERY: MachineIcon,
+  SEEDS_AND_PLANTS: SeedIcon,
+  OTHER: OtherIcon,
+  LAND: LandIcon,
+  MISCELLANEOUS: MiscellaneousIcon,
+  UTILITIES: UtilitiesIcon,
+  LABOUR: LabourIcon,
+  INFRASTRUCTURE: InfrastructureIcon,
+  TRANSPORTATION: TransportationIcon,
+  SERVICES: ServicesIcon,
 };
 
 const icons = { ...revenueTypeIcons, ...expenseTypeIcons };
@@ -61,7 +62,7 @@ export default function TransactionItem({ iconKey, transaction, type, amount, cu
   return (
     <div className={styles.mainContent}>
       <div className={styles.mainContentLeft}>
-        <div>{icons[iconKey]}</div>
+        <FramedIcon kind="circle" icon={icons[iconKey]} className={styles.expandableItemIcon} />
         <div className={styles.mainContentText}>
           <div className={styles.mainContentTitle}>{transaction}</div>
           <div className={styles.mainContentInfo}>{type}</div>

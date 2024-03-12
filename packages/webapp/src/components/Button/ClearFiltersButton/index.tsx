7eb7a3c50f3ae7d9ee28ff9aa6171544262c39a5
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-03-12 15:38:43 -0700

    LF-4095 create ClearFiltersButton component and stories

---
+++ packages/webapp/src/components/Button/ClearFiltersButton/index.tsx
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { useTranslation } from 'react-i18next';
+import TextButton from '../../Form/Button/TextButton';
+import { ReactComponent as ClearFilterIcon } from '../../../assets/images/clear-filters.svg';
+import styles from './styles.module.scss';
+
+export enum ClearFiltersButtonType {
+  ICON = 'icon',
+  TEXT = 'text',
+}
+
+export type ClearFiltersButtonProps = {
+  type: ClearFiltersButtonType;
+  onClick: () => void;
+  isFiltered: boolean;
+};
+
+const ClearFiltersButton = ({ type, onClick, isFiltered }: ClearFiltersButtonProps) => {
+  const { t } = useTranslation();
+  const isIconType = type === ClearFiltersButtonType.ICON;
+
+  if (!isFiltered) {
+    return null;
+  }
+
+  return (
+    <TextButton onClick={onClick}>
+      <span className={isIconType ? styles.iconButton : styles.textButton}>
+        <ClearFilterIcon />
+        <span className={styles.text}>
+          {t(isIconType ? 'common:CLEAR' : 'common:CLEAR_ACTIVE_FILTERS')}
+        </span>
+      </span>
+    </TextButton>
+  );
+};
+
+export default ClearFiltersButton;

ad7e4c78f0952af82cc47f29317a90c474358c9c
Duncan-Brain <dbrain@litefarm.org>
2024-03-13 18:21:27 -0400

    Revert "LF-4092 - rename patch on test"
    This reverts commit 89111865000c1611b8b2f6badc6151236f6458de.

--- packages/api/tests/animal_group.test.js
+++ packages/api/tests/animal_group.test.js
@@ -172,24 +172,17 @@ describe('Animal Group Tests', () => {
         expect(res.status).toBe(200);
         // Should return first two animal groups
         expect(res.body.length).toBe(2);
-
         // Sometimes response is returned in different order than created
-        // TODO: Find a better solution to this:
-        const orderedMockGroupIds = res.body.map((group) =>
-          groups.findIndex(({ name }) => name === group.name),
-        );
-
+        const idMap = res.body.map((group) => groups.findIndex(({ name }) => name === group.name));
         res.body.forEach((group, index) => {
           expect(group.farm_id).toBe(mainFarm.farm_id);
-          expect(group.name).toBe(groups[orderedMockGroupIds[index]].name);
+          expect(group.name).toBe(groups[idMap[index]].name);
           // Match relationships in any order
           expect(group.related_animal_ids.sort()).toEqual(
-            animalRelationships[orderedMockGroupIds[index]]
-              .map((relationship) => relationship.animal_id)
-              .sort(),
+            animalRelationships[idMap[index]].map((relationship) => relationship.animal_id).sort(),
           );
           expect(group.related_batch_ids.sort()).toEqual(
-            batchRelationships[orderedMockGroupIds[index]]
+            batchRelationships[idMap[index]]
               .map((relationship) => relationship.animal_batch_id)
               .sort(),
           );

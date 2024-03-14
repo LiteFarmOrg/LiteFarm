6d1cb53431b53f412e3802a8020e176f30860b20
Duncan-Brain <dbrain@litefarm.org>
2024-03-14 09:57:09 -0400

    bugfix-icons - delete dupes and fix stories

--- packages/webapp/src/components/Icons/icons.jsx
+++ packages/webapp/src/components/Icons/icons.jsx
@@ -4,24 +4,24 @@ import { ReactComponent as CropIcon } from '../../assets/images/finance/Crop-icn
 import { ReactComponent as ProfitLossIcon } from '../../assets/images/finance/Profit-loss-icn.svg';
 
 // Revenue types
-import { ReactComponent as CropSaleIcon } from '../../assets/images/finance/n/crops.svg';
-import { ReactComponent as CustomTypeIcon } from '../../assets/images/finance/n/Custom-revenue.svg';
+import { ReactComponent as CropSaleIcon } from '../../assets/images/finance/Crop-Sale-icn.svg';
+import { ReactComponent as CustomTypeIcon } from '../../assets/images/finance/Custom-revenue.svg';
 
 // Expense types
-import { ReactComponent as EquipIcon } from '../../assets/images/finance/n/Equipment-icn.svg';
-import { ReactComponent as SoilAmendmentIcon } from '../../assets/images/finance/n/Soil-amendment-icn.svg';
-import { ReactComponent as PestIcon } from '../../assets/images/finance/n/Pest-icn.svg';
-import { ReactComponent as FuelIcon } from '../../assets/images/finance/n/Fuel-icn.svg';
-import { ReactComponent as MachineIcon } from '../../assets/images/finance/n/Machinery-icn.svg';
-import { ReactComponent as SeedIcon } from '../../assets/images/finance/n/Seeds-icn.svg';
-import { ReactComponent as OtherIcon } from '../../assets/images/finance/n/Custom-expense.svg';
-import { ReactComponent as LandIcon } from '../../assets/images/finance/n/Land-icn.svg';
-import { ReactComponent as MiscellaneousIcon } from '../../assets/images/finance/n/Miscellaneous-icn.svg';
-import { ReactComponent as UtilitiesIcon } from '../../assets/images/finance/n/Utilities-icn.svg';
-import { ReactComponent as LabourIcon } from '../../assets/images/finance/n/Labour-icn.svg';
-import { ReactComponent as InfrastructureIcon } from '../../assets/images/finance/n/Infrastructure-icn.svg';
-import { ReactComponent as TransportationIcon } from '../../assets/images/finance/n/Transportation-icn.svg';
-import { ReactComponent as ServicesIcon } from '../../assets/images/finance/n/Services-icn.svg';
+import { ReactComponent as EquipIcon } from '../../assets/images/finance/Equipment-icn.svg';
+import { ReactComponent as SoilAmendmentIcon } from '../../assets/images/finance/Soil-amendment-icn.svg';
+import { ReactComponent as PestIcon } from '../../assets/images/finance/Pest-icn.svg';
+import { ReactComponent as FuelIcon } from '../../assets/images/finance/Fuel-icn.svg';
+import { ReactComponent as MachineIcon } from '../../assets/images/finance/Machinery-icn.svg';
+import { ReactComponent as SeedIcon } from '../../assets/images/finance/Seeds-icn.svg';
+import { ReactComponent as OtherIcon } from '../../assets/images/finance/Custom-expense.svg';
+import { ReactComponent as LandIcon } from '../../assets/images/finance/Land-icn.svg';
+import { ReactComponent as MiscellaneousIcon } from '../../assets/images/finance/Miscellaneous-icn.svg';
+import { ReactComponent as UtilitiesIcon } from '../../assets/images/finance/Utilities-icn.svg';
+import { ReactComponent as LabourIcon } from '../../assets/images/finance/Labour-icn.svg';
+import { ReactComponent as InfrastructureIcon } from '../../assets/images/finance/Infrastructure-icn.svg';
+import { ReactComponent as TransportationIcon } from '../../assets/images/finance/Transportation-icn.svg';
+import { ReactComponent as ServicesIcon } from '../../assets/images/finance/Services-icn.svg';
 
 // Animal Inventory
 import { ReactComponent as CattleIcon } from '../../assets/images/animals/cattle-icon.svg';

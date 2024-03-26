3401fc26812f258cce9c8c990eb982b627318648
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-03-26 12:02:42 -0700

    LF-4178 Update button styles and SCSS organization
    Refactored SCSS to use CSS variables to make it easier to define the now larger number of unique variants

--- packages/webapp/src/components/Form/Button/index.tsx
+++ packages/webapp/src/components/Form/Button/index.tsx
@@ -1,9 +1,24 @@
+/*
+ *  Copyright 2022-2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
 import React, { ReactNode } from 'react';
 import styles from './button.module.scss';
 import clsx from 'clsx';
 
 type ButtonProps = {
-  color?: 'primary' | 'secondary' | 'success' | 'error' | 'warning' | 'none';
+  color?: 'primary' | 'secondary' | 'secondary-2' | 'secondary-cta' | 'none';
   children?: ReactNode;
   sm?: boolean;
   disabled?: boolean;

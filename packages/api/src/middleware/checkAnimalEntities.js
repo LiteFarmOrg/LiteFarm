9a25d67c675921189c0e191556588f21c31a3d50
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-03-28 17:06:36 -0700

    LF-4152 create validateAnimalBatchCreationBody middleware

--- packages/api/src/middleware/checkAnimalEntities.js
+++ packages/api/src/middleware/checkAnimalEntities.js
@@ -1,6 +1,10 @@
 import { Model, transaction } from 'objection';
 import { handleObjectionError } from '../util/errorCodes.js';
 
+import CustomAnimalTypeModel from '../models/customAnimalTypeModel.js';
+import DefaultAnimalBreedModel from '../models/defaultAnimalBreedModel.js';
+import CustomAnimalBreedModel from '../models/customAnimalBreedModel.js';
+
 /**
  * Middleware function to check if the provided animal entities exist and belong to the farm. The IDs must be passed as a comma-separated query string.
  *
@@ -60,6 +64,166 @@ export function checkAnimalEntities(model) {
           message:
             'Some entities do not exist, are already deleted, or are not associated with the given farm.',
         });
+      }
+
+      await trx.commit();
+      next();
+    } catch (error) {
+      handleObjectionError(error, res, trx);
+    }
+  };
+}
+
+const hasOneValue = (values) => {
+  const nonNullValues = values.filter(Boolean);
+  return nonNullValues.length === 1;
+};
+
+const allFalsy = (values) => values.every((value) => !value);
+
+export function validateAnimalBatchCreationBody() {
+  return async (req, res, next) => {
+    const trx = await transaction.start(Model.knex());
+
+    try {
+      const { farm_id } = req.headers;
+      const newTypesSet = new Set();
+      const newBreedsSet = new Set();
+
+      for (const animalOrBatch of req.body) {
+        const {
+          default_type_id,
+          custom_type_id,
+          default_breed_id,
+          custom_breed_id,
+          type_name,
+          breed_name,
+        } = animalOrBatch;
+
+        if (custom_type_id) {
+          const customType = await CustomAnimalTypeModel.query().findById(custom_type_id);
+
+          if (customType && customType.farm_id !== farm_id) {
+            await trx.rollback();
+            return res.status(403).send('Forbidden custom type does not belong to this farm');
+          }
+        }
+
+        if (default_breed_id && default_type_id) {
+          const defaultBreed = await DefaultAnimalBreedModel.query().findById(default_breed_id);
+
+          if (defaultBreed && defaultBreed.default_type_id !== default_type_id) {
+            await trx.rollback();
+            return res.status(400).send('Breed does not match type');
+          }
+        }
+
+        if (default_breed_id && custom_type_id) {
+          await trx.rollback();
+          return res.status(400).send('Default breed does not use custom type');
+        }
+
+        if (custom_breed_id) {
+          const customBreed = await CustomAnimalBreedModel.query()
+            .whereNotDeleted()
+            .findById(custom_breed_id);
+
+          if (customBreed && customBreed.farm_id !== farm_id) {
+            await trx.rollback();
+            return res.status(403).send('Forbidden custom breed does not belong to this farm');
+          }
+
+          if (customBreed.default_type_id && customBreed.default_type_id !== default_type_id) {
+            await trx.rollback();
+            return res.status(400).send('Breed does not match type');
+          }
+
+          if (customBreed.custom_type_id && customBreed.custom_type_id !== custom_type_id) {
+            await trx.rollback();
+            return res.status(400).send('Breed does not match type');
+          }
+        }
+
+        // Skip the process if type_name and breed_name are not passed
+        if (!type_name && !breed_name) {
+          continue;
+        }
+
+        if (!hasOneValue([default_type_id, custom_type_id, type_name])) {
+          await trx.rollback();
+          return res
+            .status(400)
+            .send('Exactly one of default_type_id, custom_type_id, or type_name must be sent');
+        }
+
+        if (
+          !hasOneValue([default_breed_id, custom_breed_id, breed_name]) &&
+          !allFalsy([default_breed_id, custom_breed_id, breed_name])
+        ) {
+          await trx.rollback();
+          return res
+            .status(400)
+            .send('Exactly one of default_breed_id, custom_breed_id and breed_name must be sent');
+        }
+
+        if (type_name) {
+          if (default_breed_id || custom_breed_id) {
+            await trx.rollback();
+            return res
+              .status(400)
+              .send('Cannot create a new type associated with an existing breed');
+          }
+
+          newTypesSet.add(type_name);
+        }
+
+        if (!type_name && breed_name) {
+          let breedDetails = '';
+
+          if (custom_type_id) {
+            breedDetails = `custom_type_id/${custom_type_id}/${breed_name}`;
+          }
+          if (default_type_id) {
+            breedDetails = `default_type_id/${default_type_id}/${breed_name}`;
+          }
+
+          newBreedsSet.add(breedDetails);
+        }
+      }
+
+      if (newTypesSet.size) {
+        const record = await CustomAnimalTypeModel.getTypesByFarmAndTypes(farm_id, [
+          ...newTypesSet,
+        ]);
+
+        if (record.length) {
+          await trx.rollback();
+          return res.status(409).send('Animal type already exists');
+        }
+      }
+
+      if (newBreedsSet.size) {
+        const typeBreedPairs = [...newBreedsSet].map((breed) => breed.split('/'));
+        const [typeColumn, typeId] = typeBreedPairs;
+        if (typeColumn === 'custom_type_id') {
+          const customType = await CustomAnimalTypeModel.query()
+            .where('id', typeId)
+            .andWhere('farm_id', farm_id);
+          if (!customType) {
+            await trx.rollback();
+            return res.status(400).send('custom_type_id has invalid value');
+          }
+        }
+
+        const record = await CustomAnimalBreedModel.getBreedsByFarmAndTypeBreedPairs(
+          farm_id,
+          typeBreedPairs,
+        );
+
+        if (record.length) {
+          await trx.rollback();
+          return res.status(409).send('Animal breed already exists');
+        }
       }
 
       await trx.commit();

ebba156603d6cb74be5afae67f76bd9d7e5ed76e
Duncan-Brain <dbrain@litefarm.org>
2024-04-03 14:37:56 -0400

    LF-4187 - add copyright to icon file

--- packages/webapp/src/components/Icons/icons.jsx
+++ packages/webapp/src/components/Icons/icons.jsx
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <<https://www.gnu.org/licenses/>.>
+ */
+
 // Finances Carousel
 import { ReactComponent as ExpenseIcon } from '../../assets/images/finance/Expense-icn.svg';
 import { ReactComponent as CropIcon } from '../../assets/images/finance/Crop-icn.svg';

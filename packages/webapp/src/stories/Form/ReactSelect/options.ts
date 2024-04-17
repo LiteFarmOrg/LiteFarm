ef50a5bb72a7aab188ff26acc1720ca8cac64c03
Navdeep <nsdhammu@gmail.com>
2024-04-17 15:12:58 -0400

    LF-4195 update stories to TS; add story for Creatable select

---
+++ packages/webapp/src/stories/Form/ReactSelect/options.ts
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+export const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+  { label: 'Sand', value: 'sand' },
+  { label: 'Loamy Sand', value: 'loamySand' },
+  { label: 'Sandy Loam', value: 'sandyLoam' },
+  { label: 'Loam', value: 'loam' },
+  { label: 'Silt Loam', value: 'siltLoam' },
+  { label: 'Silt', value: 'silt' },
+  { label: 'Sandy Clay Loam', value: 'sandyClayLoam' },
+  { label: 'Clay Loam', value: 'clayLoam' },
+  { label: 'Silty Clay Loam', value: 'siltyClayLoam' },
+  { label: 'Sandy Clay', value: 'sandyClay' },
+  { label: 'Silty Clay', value: 'siltyClay' },
+  { label: 'Clay', value: 'clay' },
+  { label: '0-5cm', value: 5 },
+  { label: '0-10cm', value: 10 },
+  { label: '0-20cm', value: 20 },
+  { label: '21-30cm', value: 30 },
+  { label: '30-50cm', value: 50 },
+  { label: '51-100cm', value: 100 },
+];

6962ac24f45259fa88ceb67673c4e34492fa0564
Navdeep <nsdhammu@gmail.com>
2024-04-23 17:05:15 -0400

    LF-4156 change storybook config to detect story files anywhere inside src

--- packages/webapp/.storybook/main.cjs
+++ packages/webapp/.storybook/main.cjs
@@ -1,24 +1,29 @@
 module.exports = {
-  stories: ['../src/stories/**/*.stories.mdx', '../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", '@storybook/addon-a11y'],
-  "framework": {
-    name: "@storybook/react-vite",
-    options: {}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
   },
   async viteFinal(config) {
     return {
       ...config,
-      plugins: [...config.plugins.filter(plugin => !['@mdx-js/rollup'].includes(plugin.name))],
+      plugins: [...config.plugins.filter((plugin) => !['@mdx-js/rollup'].includes(plugin.name))],
       optimizeDeps: {
         ...config.optimizeDeps,
-        include: [...(config.optimizeDeps?.include ?? []), '@storybook/addon-docs/blocks']
-      }
+        include: [...(config.optimizeDeps?.include ?? []), '@storybook/addon-docs/blocks'],
+      },
     };
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };

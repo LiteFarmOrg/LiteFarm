cebc33c472c2ca49620643bbf6fb188370299165
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-05-09 20:53:51 +0530

    LF-4216 Migrate purpose data from enum to enum array

--- packages/api/db/migration/20240504185218_create_soil_amendement_task_products_table.js
+++ packages/api/db/migration/20240504185218_create_soil_amendement_task_products_table.js
@@ -17,12 +17,21 @@
 const productQuantityUnits = ['g', 'lb', 'kg', 't', 'mt', 'oz', 'l', 'gal', 'ml', 'fl-oz'];
 
 export const up = async function (knex) {
+  // Create new product table
   await knex.schema.createTable('soil_amendment_task_products', (table) => {
     table.increments('id').primary();
     table.integer('task_id').references('task_id').inTable('soil_amendment_task');
     table.integer('product_id').references('product_id').inTable('product');
     table.decimal('product_quantity', 36, 12);
     table.enu('product_quantity_unit', productQuantityUnits);
+    table.decimal('application_rate', 36, 12);
+    table.enu('application_rate_unit', ['kg/ha', 'lb/acre']);
+
+    table.check(
+      '?? IS NOT NULL AND ?? IS NULL OR ?? IS NULL AND ?? IS NOT NULL',
+      ['product_quantity', 'application_rate', 'product_quantity', 'application_rate'],
+      'quantity_or_rate_check',
+    );
   });
 
   // Migrate existng data to the new table (reversibly)
@@ -31,14 +40,61 @@ export const up = async function (knex) {
     SELECT task_id, product_id, product_quantity, product_quantity_unit FROM soil_amendment_task
   `);
 
+  await knex.schema.raw(
+    "CREATE TYPE purpose_enum AS ENUM ('structure', 'moisture_retention', 'nutrient_availability', 'ph', 'other')",
+  );
+
   await knex.schema.alterTable('soil_amendment_task', (table) => {
     table.dropColumn('product_id');
     table.dropColumn('product_quantity');
     table.dropColumn('product_quantity_unit');
+
+    // Migrate purpose. From https://github.com/knex/knex/issues/5038
+    table
+      .specificType('temp_purpose', 'purpose_enum[]')
+      .defaultTo(
+        knex.raw(
+          "ARRAY['structure', 'moisture_retention', 'nutrient_availability', 'ph', 'other']::purpose_enum[]",
+        ),
+      );
+  });
+
+  await knex.raw(`
+    UPDATE soil_amendment_task
+    SET temp_purpose = ARRAY[purpose::purpose_enum]
+  `);
+
+  await knex.schema.alterTable('soil_amendment_task', (table) => {
+    table.dropColumn('purpose');
+    table.renameColumn('temp_purpose', 'purpose');
   });
 };
 
 export const down = async function (knex) {
+  // Reverse migration of purpose
+  await knex.schema.alterTable('soil_amendment_task', (table) => {
+    table.enu('temp_purpose', [
+      'structure',
+      'moisture_retention',
+      'nutrient_availability',
+      'ph',
+      'other',
+    ]);
+  });
+
+  await knex.raw(`
+      UPDATE soil_amendment_task
+      SET temp_purpose = purpose[1]
+    `);
+
+  await knex.schema.alterTable('soil_amendment_task', (table) => {
+    table.dropColumn('purpose');
+    table.renameColumn('temp_purpose', 'purpose');
+  });
+
+  await knex.schema.raw('DROP TYPE purpose_enum');
+
+  // Reverse product data migration
   await knex.schema.alterTable('soil_amendment_task', (table) => {
     table.decimal('product_quantity', 36, 12);
     table.enu('product_quantity_unit', productQuantityUnits);

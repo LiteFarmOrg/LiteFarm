b16ed466f4107e8d577a945b23ee7dfe6785e172
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-05-14 14:55:08 -0700

    LF-4220 create SmallButton component

---
+++ packages/webapp/src/stories/Button/SmallButton.stories.tsx
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2023 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import { Meta, StoryObj } from '@storybook/react';
+import SmallButton from '../../components/Form/Button/SmallButton';
+import { componentDecorators } from '../Pages/config/Decorators';
+
+const meta: Meta<typeof SmallButton> = {
+  title: 'Components/Button/SmallButton',
+  component: SmallButton,
+  decorators: componentDecorators,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SmallButton>;
+
+export const Default: Story = {
+  args: {
+    children: 'button',
+    onClick: () => console.log('Clicked!'),
+  },
+};
+
+export const RemoveButton: Story = {
+  args: {
+    variant: 'remove',
+    onClick: () => console.log('Removed!'),
+  },
+};

0b03dc643a98d3e244890e62a5e01732e2548362
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-06-04 17:00:02 -0700

    LF-4240 remove unused import

--- packages/webapp/src/containers/Consent/index.jsx
+++ packages/webapp/src/containers/Consent/index.jsx
@@ -1,5 +1,4 @@
 import { useForm } from 'react-hook-form';
-import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PureConsent from '../../components/Consent';
 import { userFarmSelector } from '../userFarmSlice';

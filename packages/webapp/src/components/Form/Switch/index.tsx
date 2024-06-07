7aa4bedb72e2af7a3770c4eea09a5c7a74a3653e
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-06-07 16:46:50 -0700

    LF-4230 Add yes/no text to track

--- packages/webapp/src/components/Form/Switch/index.tsx
+++ packages/webapp/src/components/Form/Switch/index.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent } from 'react';
-import styles from './styles.module.scss';
 import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
+import styles from './styles.module.scss';
 import { Main } from '../../Typography';
 
 interface SwitchProps {
@@ -11,12 +12,17 @@ interface SwitchProps {
 }
 
 const Switch = ({ checked, onChange, label, leftLabel, ...props }: SwitchProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container} {...props}>
       {leftLabel && <Main>{leftLabel}</Main>}
       <label className={styles.switch}>
         <input onChange={onChange} checked={checked} type="checkbox" />
-        <span className={clsx(styles.track, styles.round)} />
+        <span className={clsx(styles.track, styles.round)}>
+          <span className={clsx(styles.innerText, styles.yes)}>{t('common:YES')}</span>
+          <span className={clsx(styles.innerText, styles.no)}>{t('common:NO')}</span>
+        </span>
       </label>
       {label && <Main>{label}</Main>}
     </div>

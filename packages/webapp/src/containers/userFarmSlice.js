9b0232f533b707db5d8613be31d870ba80490377
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-06-10 16:13:24 +0000

    Merge pull request #3222 from LiteFarmOrg/LF-4256-old-consent-version-is-shown-after-setting-has-consent-to-false
    LF-4256 Old consent version is shown after setting `has_consent` to false

--- packages/webapp/src/containers/userFarmSlice.js
+++ packages/webapp/src/containers/userFarmSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
+import { CONSENT_VERSION } from '../util/constants';
 
 export function onLoadingStart(state) {
   state.loading = true;
@@ -92,6 +93,10 @@ const userFarmSlice = createSlice({
         const prevUserFarms = state.byFarmIdUserId[farm_id] || {};
         state.byFarmIdUserId[farm_id] = prevUserFarms;
         state.byFarmIdUserId[farm_id][user_id] = prevUserFarms[user_id] || {};
+
+        const { has_consent, consent_version } = userFarm;
+        userFarm.has_consent = has_consent && consent_version === CONSENT_VERSION;
+
         Object.assign(state.byFarmIdUserId[farm_id][user_id], userFarm);
         delete state.byFarmIdUserId[farm_id][user_id].role;
       });

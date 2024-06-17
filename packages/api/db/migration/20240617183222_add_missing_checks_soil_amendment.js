98f6057c74389a7d7475b937116ffd2f9639f081
Duncan-Brain <dbrain@litefarm.org>
2024-06-17 14:51:01 -0400

    LF-4253 - migrate other purpose data and add check constraint

--- packages/api/db/migration/20240617183222_add_missing_checks_soil_amendment.js
+++ packages/api/db/migration/20240617183222_add_missing_checks_soil_amendment.js
@@ -17,10 +17,29 @@ export const up = async function (knex) {
   await knex.schema.alterTable('soil_amendment_product', (table) => {
     table.check('moisture_content_percent <= 100', [], 'moisture_percent_check');
   });
+
+  const soilAmendmentPurposes = await knex.select().table('soil_amendment_purpose');
+  const otherPurpose = soilAmendmentPurposes.find((pu) => pu.key === 'OTHER');
+
+  await knex('soil_amendment_task_products_purpose_relationship')
+    .whereNot({ purpose_id: otherPurpose.id })
+    .update({ other_purpose: null });
+
+  await knex.schema.alterTable('soil_amendment_task_products_purpose_relationship', (table) => {
+    table.check(
+      '(?? IS NOT NULL AND ?? = ??) OR (?? IS NULL)',
+      ['other_purpose', 'purpose_id', otherPurpose.id, 'other_purpose'],
+      'other_purpose_id_check',
+    );
+  });
 };
 
 export const down = async function (knex) {
   await knex.schema.alterTable('soil_amendment_product', (table) => {
     table.dropChecks(['moisture_percent_check']);
+  });
+
+  await knex.schema.alterTable('soil_amendment_task_products_purpose_relationship', (table) => {
+    table.dropChecks(['other_purpose_id_check']);
   });
 };

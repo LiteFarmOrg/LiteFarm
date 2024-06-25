869b23a292c6ea7882ba94329849848e3fcaeee6
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-06-25 14:13:35 -0700

    LF-3994, 3984 - add JSDoc to handleObjectionError function * taken from 3a874ac

--- packages/api/src/util/errorCodes.js
+++ packages/api/src/util/errorCodes.js
@@ -1,3 +1,34 @@
+/*
+ *  Copyright (C) 2024 Free Software Foundation, Inc. <https://fsf.org/>
+ *  This file (baseController.js) is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Handles objection.js errors and sends appropriate responses based on the error type.
+ *
+ * @param {Error} err - The objection.js error object.
+ * @param {import('express').Response} res - The Express response object.
+ * @param {import('objection').Transaction} trx - The objection.js transaction object.
+ * @returns {Promise<void>} A promise that resolves after handling the error and sending the response.
+ *
+ * @example
+ * try {
+ *   // Some objection.js operation that may throw an error
+ * } catch (error) {
+ *   await handleObjectionError(error, res, trx);
+ * }
+ */
+
 export async function handleObjectionError(err, res, trx) {
   switch (err.name) {
     case 'ValidationError': {

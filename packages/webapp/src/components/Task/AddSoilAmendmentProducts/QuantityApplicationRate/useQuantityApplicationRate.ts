f8ef4f1c0f337ecf05ebf781685bb26ad6abe93a
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-07-03 16:36:23 -0700

    LF-4249 Correct and add remaining useEffect logic

--- packages/webapp/src/components/Task/AddSoilAmendmentProducts/QuantityApplicationRate/useQuantityApplicationRate.ts
+++ packages/webapp/src/components/Task/AddSoilAmendmentProducts/QuantityApplicationRate/useQuantityApplicationRate.ts
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { UseFormWatch, UseFormSetValue } from 'react-hook-form';
 import { TASK_PRODUCT_FIELD_NAMES, TaskProductFormFields } from '../types';
 import { getUnitOptionMap } from '../../../../util/convert-units/getUnitOptionMap';
@@ -23,8 +23,7 @@ import { convert } from '../../../../util/convert-units/convert';
 
 interface UseQuantityApplicationRate {
   total_area: number;
-  total_area_unit: string;
-  //   percent_of_location: number;
+  total_area_unit: 'm2' | 'ha' | 'ft2' | 'ac'; // as defined in location_area
   system: 'metric' | 'imperial';
   isWeight: boolean;
   watch: UseFormWatch<TaskProductFormFields>;
@@ -34,7 +33,6 @@ interface UseQuantityApplicationRate {
 export const useQuantityApplicationRate = ({
   total_area,
   total_area_unit,
-  //   percent_of_location,
   system,
   isWeight,
   watch,
@@ -42,16 +40,15 @@ export const useQuantityApplicationRate = ({
 }: UseQuantityApplicationRate) => {
   const percent_of_location = watch(TASK_PRODUCT_FIELD_NAMES.PERCENT_OF_LOCATION);
   const application_area = watch(TASK_PRODUCT_FIELD_NAMES.APPLICATION_AREA);
+  const application_area_unit = watch(TASK_PRODUCT_FIELD_NAMES.APPLICATION_AREA_UNIT);
   const weight = watch(TASK_PRODUCT_FIELD_NAMES.WEIGHT);
   const weight_unit = watch(TASK_PRODUCT_FIELD_NAMES.WEIGHT_UNIT);
   const volume = watch(TASK_PRODUCT_FIELD_NAMES.VOLUME);
   const volume_unit = watch(TASK_PRODUCT_FIELD_NAMES.VOLUME_UNIT);
   const application_rate_weight = watch(TASK_PRODUCT_FIELD_NAMES.APPLICATION_RATE_WEIGHT);
   const application_rate_volume = watch(TASK_PRODUCT_FIELD_NAMES.APPLICATION_RATE_VOLUME);
-
-  console.log({ volume });
-  console.log({ application_area });
-  console.log({ application_rate_volume });
+  const application_rate_weight_unit = watch(TASK_PRODUCT_FIELD_NAMES.APPLICATION_RATE_WEIGHT_UNIT);
+  const application_rate_volume_unit = watch(TASK_PRODUCT_FIELD_NAMES.APPLICATION_RATE_VOLUME_UNIT);
 
   /* Set initial application area to area total area */
   useEffect(() => {
@@ -59,7 +56,7 @@ export const useQuantityApplicationRate = ({
       setValue(TASK_PRODUCT_FIELD_NAMES.APPLICATION_AREA, total_area);
 
       /* display user-selected unit initially */
-      setValue(TASK_PRODUCT_FIELD_NAMES.APPLICATION_AREA_UNIT, total_area_unit);
+      setValue(TASK_PRODUCT_FIELD_NAMES.APPLICATION_AREA_UNIT, getUnitOptionMap()[total_area_unit]);
     }
   }, []);
 
@@ -78,53 +75,72 @@ export const useQuantityApplicationRate = ({
     );
   }, [percent_of_location]);
 
-  /* For the preview string, always use the user-defined unit. This replicates the conversion the unit component would do if the value had been displayed in a <Unit /> rather than a straight string */
-  const previewStringTotalAreaValue =
-    total_area &&
-    roundToTwoDecimal(
-      convertFn(location_area[system], total_area, location_area.databaseUnit, total_area_unit),
-    );
-
-  // useEffect(() => {
-  //   let irrigatedArea = null;
-  //   if (percentage_location_irrigated) {
-  //     irrigatedArea = roundToTwoDecimal(
-  //       location.total_area * (percentage_location_irrigated / 100),
-  //     );
-  //   }
-  //   setValue(IRRIGATED_AREA, irrigatedArea);
-  //   setValue(
-  //     IRRIGATED_AREA_UNIT,
-  //     getUnitOptionMap()[getDefaultUnit(location_area, irrigatedArea, system).displayUnit],
-  //     { shouldValidate: true },
-  //   );
-  // }, [location, percentage_location_irrigated]);
-
-  // useEffect(() => {
-  //   if (irrigated_area && application_depth) {
-  //     setTotalWaterUsage(
-  //       convert(irrigated_area * application_depth)
-  //         .from('m3')
-  //         .to('l'),
-  //     );
-  //   } else {
-  //     setTotalWaterUsage('');
-  //   }
-  // }, [application_depth, irrigated_area]);
-
   /* Update application rate based on quantity */
   useEffect(() => {
-    if (weight && isWeight) {
+    if (weight && isWeight && weight_unit && application_area) {
       setValue(
         TASK_PRODUCT_FIELD_NAMES.APPLICATION_RATE_WEIGHT,
-        convert(weight / application_area!)
-          .from('kg/m2') // database units
-          .to('kg/ha'), // FIX -- should be system dependent
+        convert(weight / application_area)
+          .from('kg/m2') // database unit weight / database unit area
+          .to('kg/ha'), // database unit application_rate_weight
       );
-    } else if (volume && !isWeight) {
-      setValue(TASK_PRODUCT_FIELD_NAMES.APPLICATION_RATE_VOLUME, volume / application_area!);
+    } else if (volume && !isWeight && volume_unit && application_area) {
+      setValue(
+        TASK_PRODUCT_FIELD_NAMES.APPLICATION_RATE_VOLUME,
+        convert(volume / application_area)
+          .from('l/m2') // database unit volume / database unit area
+          .to('l/ha'), // database unit application_rate_volume
+      );
     }
-  }, [volume, weight, application_area]);
+  }, [weight, weight_unit, volume, volume_unit, application_area, isWeight]);
 
-  return { previewStringTotalAreaValue };
+  /* Update quantity based on application rate */
+  useEffect(() => {
+    if (application_rate_weight && isWeight && application_rate_weight_unit && application_area) {
+      setValue(
+        TASK_PRODUCT_FIELD_NAMES.WEIGHT,
+        convert(application_rate_weight).from('kg/ha').to('kg/m2') * application_area,
+      );
+    } else if (
+      application_rate_volume &&
+      !isWeight &&
+      application_rate_volume_unit &&
+      application_area
+    ) {
+      setValue(
+        TASK_PRODUCT_FIELD_NAMES.WEIGHT,
+        convert(application_rate_volume).from('l/ha').to('l/m2') * application_area,
+      );
+    }
+  }, [
+    application_rate_weight,
+    application_rate_weight_unit,
+    application_rate_volume,
+    application_rate_volume_unit,
+    isWeight,
+  ]);
+
+  /* For the preview string, replicate the conversion the unit component would do if the value had been displayed in a <Unit /> rather than a string. However, don't update upon changes to application_area_unit */
+  const [previewStringValue, setPreviewStringValue] = useState<number | null>(null);
+  const [previewStringUnit, setPreviewStringUnit] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (application_area_unit && previewStringValue === null) {
+      /* if the user-selected unit is in the wrong system (e.g. metric stored value but farm on imperial), use the <Unit /> converted unit */
+      const unit = location_area[system].units.includes(total_area_unit)
+        ? getUnitOptionMap()[total_area_unit]
+        : application_area_unit;
+
+      const value =
+        total_area &&
+        roundToTwoDecimal(
+          convertFn(location_area[system], total_area, location_area.databaseUnit, unit.value),
+        );
+
+      setPreviewStringValue(value);
+      setPreviewStringUnit(unit.label);
+    }
+  }, [location_area, application_area_unit]);
+
+  return { previewStringValue, previewStringUnit };
 };

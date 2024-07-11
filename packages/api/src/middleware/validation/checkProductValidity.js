44b5a90f659f74ea6ff2dd9607f19827fdd15f0c
Duncan-Brain <dbrain@litefarm.org>
2024-07-11 14:56:33 -0400

    LF-4260 - make reusability of middleware clear

--- packages/api/src/middleware/validation/checkProductValidity.js
+++ packages/api/src/middleware/validation/checkProductValidity.js
@@ -18,56 +18,68 @@ import { handleObjectionError } from '../../util/errorCodes.js';
 import ProductModel from '../../models/productModel.js';
 import { checkAndTrimString } from '../../util/util.js';
 
+const taskProductRelationMap = {
+  soil_amendment_task: 'soil_amendment_product',
+  // pest_control_task: 'pest_control_product',
+  // cleaning_task: 'cleaning_product'
+};
+const productCheckMap = {
+  soil_amendment_task: checkSoilAmendmentProduct,
+  // pest_control_task: checkPestControlProduct,
+  // cleaning_task: checkCleaningProduct
+};
+
+function checkSoilAmendmentProduct(res, sap) {
+  const elements = [
+    'n',
+    'p',
+    'k',
+    'calcium',
+    'magnesium',
+    'sulfur',
+    'copper',
+    'manganese',
+    'boron',
+  ];
+  const molecularCompounds = ['ammonium', 'nitrate'];
+  // Check that element values are all positive
+  if (!elements.every((element) => !sap[element] || sap[element] >= 0)) {
+    return res.status(400).send('element values must all be positive');
+  }
+
+  // Check that a unit has been provided along with element values
+  if (elements.some((element) => sap[element]) && !sap.elemental_unit) {
+    return res.status(400).send('elemental_unit is required');
+  }
+
+  // Check that element values do not exceed 100 if element_unit is percent
+  if (
+    sap.elemental_unit === 'percent' &&
+    elements.reduce((sum, element) => sum + (sap[element] || 0), 0) > 100
+  ) {
+    return res.status(400).send('percent elemental values must not exceed 100');
+  }
+
+  // Check that compound values are all positive
+  if (!molecularCompounds.every((compound) => !sap[compound] || sap[compound] >= 0)) {
+    return res.status(400).send('compounds values must all be positive');
+  }
+
+  if (sap.moisture_content_percent && sap.moisture_content_percent < 0) {
+    return res.status(400).send('moisture content value must be positive');
+  }
+}
+
 export function checkProductValidity() {
   return async (req, res, next) => {
     const { farm_id } = req.headers;
     const { product_id } = req.params;
-    const { soil_amendment_product: sap } = req.body;
+
     let { type, name } = req.body;
+    const { [taskProductRelationMap[type]]: productDetails } = req.body;
 
-    const elements = [
-      'n',
-      'p',
-      'k',
-      'calcium',
-      'magnesium',
-      'sulfur',
-      'copper',
-      'manganese',
-      'boron',
-    ];
-    const molecularCompounds = ['ammonium', 'nitrate'];
-    const taskTypeProductMap = {
-      soil_amendment_task: 'soil_amendment_product',
-    };
-
-    if (sap) {
-      // Check that element values are all positive
-      if (!elements.every((element) => !sap[element] || sap[element] >= 0)) {
-        return res.status(400).send('element values must all be positive');
-      }
-
-      // Check that a unit has been provided along with element values
-      if (elements.some((element) => sap[element]) && !sap.elemental_unit) {
-        return res.status(400).send('elemental_unit is required');
-      }
-
-      // Check that element values do not exceed 100 if element_unit is percent
-      if (
-        sap.elemental_unit === 'percent' &&
-        elements.reduce((sum, element) => sum + (sap[element] || 0), 0) > 100
-      ) {
-        return res.status(400).send('percent elemental values must not exceed 100');
-      }
-
-      // Check that compound values are all positive
-      if (!molecularCompounds.every((compound) => !sap[compound] || sap[compound] >= 0)) {
-        return res.status(400).send('compounds values must all be positive');
-      }
-
-      if (sap.moisture_content_percent && sap.moisture_content_percent < 0) {
-        return res.status(400).send('moisture content value must be positive');
-      }
+    if (productDetails) {
+      productCheckMap[type](res, productDetails);
     }
 
     // Null empty strings
@@ -99,14 +111,14 @@ export function checkProductValidity() {
         return res.status(400).send('new product must have type');
       }
 
-      const nonModifiableAssets = Object.values(taskTypeProductMap).filter((productType) => {
-        return productType !== taskTypeProductMap[type];
+      const nonModifiableAssets = Object.values(taskProductRelationMap).filter((productType) => {
+        return productType !== taskProductRelationMap[type];
       });
 
       if (
         !(req.method === 'PATCH') &&
-        taskTypeProductMap[type] &&
-        !req.body[taskTypeProductMap[type]]
+        taskProductRelationMap[type] &&
+        !req.body[taskProductRelationMap[type]]
       ) {
         return res.status(400).send('must have product details');
       }

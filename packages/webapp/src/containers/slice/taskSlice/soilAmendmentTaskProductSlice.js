931bbe073460c6f39bcc6052f601a867243d7b5d
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-07-12 13:36:58 -0700

    LF-4221 create soilAmendmentTaskProdcutSlice and add reducer to entitiesReducer

---
+++ packages/webapp/src/containers/slice/taskSlice/soilAmendmentTaskProductSlice.js
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
+
+const upsertManySoilAmendmentTaskProduct = (state, { payload: taskProducts }) => {
+  soilAmendmentTaskProductAdapter.upsertMany(state, taskProducts);
+};
+
+const soilAmendmentTaskProductAdapter = createEntityAdapter({
+  selectId: (soilAmendmentTaskProduct) => soilAmendmentTaskProduct.id,
+});
+
+const soilAmendmentTaskProductSlice = createSlice({
+  name: 'soilAmendmentTaskProductReducer',
+  initialState: soilAmendmentTaskProductAdapter.getInitialState({}),
+  reducers: {
+    getSoilAmendmentTaskProductsSuccess: upsertManySoilAmendmentTaskProduct,
+  },
+});
+
+export const { getSoilAmendmentTaskProductsSuccess } = soilAmendmentTaskProductSlice.actions;
+
+export default soilAmendmentTaskProductSlice.reducer;
+
+export const soilAmendmentTaskProductReducerSelector = (state) =>
+  state.entitiesReducer[soilAmendmentTaskProductSlice.name];
+
+const soilAmendmentTaskProductSelectors = soilAmendmentTaskProductAdapter.getSelectors(
+  (state) => state.entitiesReducer[soilAmendmentTaskProductSlice.name],
+);
+
+export const soilAmendmentTaskProductEntitiesSelector =
+  soilAmendmentTaskProductSelectors.selectEntities;
+
+export const soilAmendmentTaskProductStatusSelector = createSelector(
+  [soilAmendmentTaskProductReducerSelector],
+  ({ loading, error, loaded }) => {
+    return { loading, error, loaded };
+  },
+);

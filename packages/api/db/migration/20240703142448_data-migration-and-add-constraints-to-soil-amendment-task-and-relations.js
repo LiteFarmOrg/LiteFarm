163c1c5ea1c549f8892d778f3f26987ae0ee1ddc
Duncan-Brain <dbrain@litefarm.org>
2024-07-15 14:06:29 -0400

    LF-4286 - add uniqueness check and fix local beta data for task products

--- packages/api/db/migration/20240703142448_data-migration-and-add-constraints-to-soil-amendment-task-and-relations.js
+++ packages/api/db/migration/20240703142448_data-migration-and-add-constraints-to-soil-amendment-task-and-relations.js
@@ -118,6 +118,48 @@ export const up = async function (knex) {
       'cleaning_task',
     ]),
   );
+
+  /*----------------------------------------
+   Add uniqueness check for (task_id, product_id) composite - data deletion for local + beta only
+  ----------------------------------------*/
+  const taskProductsWithDuplicates = await knex
+    .select('task_id', 'product_id')
+    .count('*', { as: 'cnt' })
+    .from('soil_amendment_task_product')
+    .groupBy('task_id', 'product_id')
+    .havingRaw('COUNT(*) > 1')
+    .orderBy('task_id', 'asc');
+  for (const taskProduct of taskProductsWithDuplicates) {
+    const duplicates = await knex
+      .select('*')
+      .table('soil_amendment_task_product')
+      .where({ task_id: taskProduct.task_id, product_id: taskProduct.product_id })
+      .orderBy('created_at', 'asc');
+    const countOfNotDeleted = duplicates.filter((dupe) => !dupe.deleted);
+    let keep;
+    let hardDelete;
+    if (countOfNotDeleted.length === 0) {
+      keep = duplicates.pop();
+      await knex('soil_amendment_task_product').where('id', keep.id).update({ deleted: false });
+      hardDelete = duplicates;
+    } else if (countOfNotDeleted.length >= 1) {
+      //Choose only or latest task_product with deleted false
+      keep = countOfNotDeleted.pop();
+      hardDelete = duplicates.filter((dupe) => dupe.id !== keep.id);
+    }
+    for (const deleteable of hardDelete) {
+      await knex('soil_amendment_task_product_purpose_relationship')
+        .where('task_products_id', deleteable.id)
+        .del();
+      await knex('soil_amendment_task_product').where('id', deleteable.id).del();
+    }
+  }
+
+  await knex.schema.alterTable('soil_amendment_task_product', (table) => {
+    table.unique(['task_id', 'product_id'], {
+      indexName: 'task_product_uniqueness_composite',
+    });
+  });
 };
 
 export const down = async function (knex) {
@@ -195,4 +237,11 @@ export const down = async function (knex) {
       'cleaning_task',
     ]),
   );
+
+  /*----------------------------------------
+   Add uniqueness check for (task_id, product_id) composite
+  ----------------------------------------*/
+  await knex.schema.alterTable('soil_amendment_task_products', (table) => {
+    table.dropChecks(['task_product_uniqueness_composite']);
+  });
 };

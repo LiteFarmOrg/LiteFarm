9a48e4f4a4949b62f019f74354416f498f6b21cb
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-07-25 10:21:44 -0700

    Update VERSION_RELEASE_NOTES_LINK

--- packages/webapp/src/util/constants.js
+++ packages/webapp/src/util/constants.js
@@ -9,6 +9,7 @@ export const SUPPORT_EMAIL = 'support@litefarm.org';
 
 // Changing this forces logout and updates the new release card
 export const APP_VERSION = '3.6.5';
-export const VERSION_RELEASE_NOTES_LINK = 'https://www.litefarm.org/post/a-new-look-for-a-new-year';
+export const VERSION_RELEASE_NOTES_LINK =
+  'https://www.litefarm.org/post/exciting-improvements-enhanced-soil-amendment-task-management-in-litefarm';
 
 export const CONSENT_VERSION = '6.0';

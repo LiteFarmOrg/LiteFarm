dbe7a6a106286ff96ba8c3cb133018530e338a09
Navdeep <nsdhammu@gmail.com>
2024-07-31 17:37:15 -0400

    revert auto increment permission id

--- packages/api/db/migration/20240620131606_add_permissions_for_animal_identifier_type.js
+++ packages/api/db/migration/20240620131606_add_permissions_for_animal_identifier_type.js
@@ -21,22 +21,19 @@
 const PERMISSION_NAME = 'get:animal_identifier_types';
 
 export const up = async function (knex) {
-  // Synchronize the permissions_permission_id_seq sequence with the latest permission_id value.
-  await knex.raw(`
-        SELECT setval('permissions_permission_id_seq', 
-           COALESCE((SELECT MAX(permission_id)+1 FROM permissions), 1), 
-           false);
-`);
-
-  const [{ permission_id }] = await knex('permissions')
+  await knex('permissions')
     .insert({
+      permission_id: 171,
       name: PERMISSION_NAME,
       description: 'get animal identifier types',
     })
     .returning('*');
 
-  await knex('rolePermissions').insert([1, 2, 3, 5].map((role_id) => ({ role_id, permission_id })));
+  await knex('rolePermissions').insert(
+    [1, 2, 3, 5].map((role_id) => ({ role_id, permission_id: 171 })),
+  );
 };
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }

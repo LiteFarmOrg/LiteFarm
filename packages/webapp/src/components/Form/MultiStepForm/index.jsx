5e601146438cfbc0ccf53a251bafa7738927bc75
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-08-02 17:18:38 -0700

    LF-4166 Update MultiStepForm to handle different variants * window.confirm() when leaving the form * hide navigation buttons on summary page

--- packages/webapp/src/components/Form/MultiStepForm/index.jsx
+++ packages/webapp/src/components/Form/MultiStepForm/index.jsx
@@ -13,17 +13,38 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 
-import { useState, useMemo } from 'react';
-
+import { useState, useMemo, useEffect, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
 import MultiStepPageTitle from '../../PageTitle/MultiStepPageTitle';
 import Layout from '../../Layout';
 import { ClickAwayListener } from '@mui/material';
 import { FormProvider, useForm } from 'react-hook-form';
+import StepperProgressBar from '../../StepperProgressBar';
+import FloatingContainer from '../../FloatingContainer';
+import FormNavigationButtons from '../FormNavigationButtons';
 
-export const MultiStepForm = ({ history, getSteps, cancelModalTitle, defaultFormValues }) => {
+export const VARIANT = {
+  PAGE_TITLE: 'page_title',
+  STEPPER_PROGRESS_BAR: 'stepper_progress_bar',
+};
+
+export const MultiStepForm = ({
+  history,
+  getSteps,
+  cancelModalTitle,
+  defaultFormValues,
+  variant = VARIANT.PAGE_TITLE,
+  isCompactSideMenu,
+  hasSummaryWithinForm,
+  onSave,
+  stepperProgressBarConfig = {},
+}) => {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
   const [showConfirmCancelModal, setShowConfirmCancelModal] = useState(false);
   const [formData, setFormData] = useState();
+
+  const { t } = useTranslation();
 
   const form = useForm({
     mode: 'onBlur',
@@ -35,6 +56,36 @@ export const MultiStepForm = ({ history, getSteps, cancelModalTitle, defaultForm
     () => (100 / (steps.length + 1)) * (activeStepIndex + 1),
     [steps],
   );
+
+  const isSummaryPage =
+    variant === VARIANT.STEPPER_PROGRESS_BAR &&
+    hasSummaryWithinForm &&
+    activeStepIndex === steps.length - 1;
+
+  const isSummaryPageRef = useRef(isSummaryPage);
+
+  useEffect(() => {
+    isSummaryPageRef.current = isSummaryPage;
+  }, [isSummaryPage]);
+
+  // Block the page transition
+  // https://github.com/remix-run/history/blob/dev/docs/blocking-transitions.md
+  useEffect(() => {
+    if (variant !== VARIANT.STEPPER_PROGRESS_BAR || isSummaryPage) {
+      return;
+    }
+    const unblock = history.block((tx) => {
+      if (window.confirm(`TODO: ${t('CANCEL_FLOW_MODAL.BODY')}`)) {
+        // Unblock the navigation.
+        unblock();
+
+        // Retry the transition.
+        tx.retry();
+      }
+    });
+
+    return () => unblock();
+  }, [variant, isSummaryPage]);
 
   const storeFormData = () => {
     const values = form.getValues();
@@ -65,24 +116,73 @@ export const MultiStepForm = ({ history, getSteps, cancelModalTitle, defaultForm
 
   const activeStep = steps[activeStepIndex];
 
+  const isFinalStep =
+    (!hasSummaryWithinForm && activeStepIndex === steps.length - 1) ||
+    (hasSummaryWithinForm && activeStepIndex === steps.length - 2);
+
+  const shouldShowFormNavigationButtons =
+    variant === VARIANT.STEPPER_PROGRESS_BAR && !isSummaryPage;
+
+  const onContinue = () => {
+    if (isFinalStep) {
+      form.handleSubmit((data) => onSave(data, onGoForward))();
+      return;
+    }
+    onGoForward();
+  };
+
   return (
     <ClickAwayListener onClickAway={onClickAway} mouseEvent="onMouseDown" touchEvent="onTouchStart">
       <div>
-        <Layout>
-          <MultiStepPageTitle
-            title={activeStep.title}
-            onGoBack={onGoBack}
-            onCancel={onCancel}
-            cancelModalTitle={cancelModalTitle}
-            value={progressBarValue}
-            showConfirmCancelModal={showConfirmCancelModal}
-            setShowConfirmCancelModal={setShowConfirmCancelModal}
+        {variant === VARIANT.STEPPER_PROGRESS_BAR && (
+          <StepperProgressBar
+            {...stepperProgressBarConfig}
+            steps={steps.map(({ title }) => title)}
+            activeStep={activeStepIndex}
           />
+        )}
+        <Layout>
+          {variant === VARIANT.PAGE_TITLE && (
+            <MultiStepPageTitle
+              title={activeStep.title}
+              onGoBack={onGoBack}
+              onCancel={onCancel}
+              cancelModalTitle={cancelModalTitle}
+              value={progressBarValue}
+              showConfirmCancelModal={showConfirmCancelModal}
+              setShowConfirmCancelModal={setShowConfirmCancelModal}
+            />
+          )}
           <FormProvider {...form}>
             <activeStep.FormContent onGoForward={onGoForward} form={form} />
           </FormProvider>
         </Layout>
+        {shouldShowFormNavigationButtons && (
+          <FloatingContainer isCompactSideMenu={isCompactSideMenu}>
+            <FormNavigationButtons
+              onContinue={onContinue}
+              onCancel={onCancel}
+              onGoBack={onGoBack}
+              isFinalStep={isFinalStep}
+            />
+          </FloatingContainer>
+        )}
       </div>
     </ClickAwayListener>
   );
+};
+
+MultiStepForm.propTypes = {
+  history: PropTypes.object,
+  getSteps: PropTypes.func,
+  cancelModalTitle: PropTypes.string,
+  defaultFormValues: PropTypes.object,
+  variant: PropTypes.oneOf(Object.values(VARIANT)),
+  isCompactSideMenu: PropTypes.bool,
+  hasSummaryWithinForm: PropTypes.bool,
+  onSave: PropTypes.func,
+  stepperProgressBarConfig: PropTypes.shape({
+    isMobile: PropTypes.bool,
+    isDarkMode: PropTypes.bool,
+  }),
 };

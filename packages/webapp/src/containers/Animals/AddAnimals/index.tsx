f4171ffbd7abd7f9a7c907a774dde830e92b5177
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-08-09 13:56:11 -0700

    LF-4166 Pass cancelModalTitle to MultiStepForm in AddAnimals

--- packages/webapp/src/containers/Animals/AddAnimals/index.tsx
+++ packages/webapp/src/containers/Animals/AddAnimals/index.tsx
@@ -74,6 +74,7 @@ function AddAnimals({ isCompactSideMenu, history }: AddAnimalsProps) {
       history={history}
       getSteps={getFormSteps}
       defaultFormValues={defaultFormValues}
+      cancelModalTitle={t('ADD_ANIMAL.ADD_ANIMALS_FLOW')}
     />
   );
 }

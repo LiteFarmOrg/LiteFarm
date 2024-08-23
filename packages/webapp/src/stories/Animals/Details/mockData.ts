34486cba5661acbad7a2f15fd3ae991c211bf3d8
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-08-22 17:29:14 -0700

    Fix animal details image picker in stories

--- packages/webapp/src/stories/Animals/Details/mockData.ts
+++ packages/webapp/src/stories/Animals/Details/mockData.ts
@@ -13,10 +13,13 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 
+import { ChangeEvent, DragEvent } from 'react';
 import {
   DetailsFields,
   AnimalDetailsFormFields,
 } from '../../../containers/Animals/AddAnimals/types';
+import { FileEvent } from '../../../components/ImagePicker';
+import { GetOnFileUpload } from '../../../components/ImagePicker/useImagePickerUpload';
 
 export const sexOptions = [
   { value: 0, label: `I don't know` },
@@ -65,3 +68,18 @@ export const defaultValues: Partial<AnimalDetailsFormFields> = {
   [DetailsFields.TYPE]: { value: 'default_1', label: 'Cattle' },
   [DetailsFields.BREED]: { value: 'default_2', label: 'Angus' },
 };
+
+export const getOnFileUpload: GetOnFileUpload =
+  (targetRoute, onSelectImage) => async (e, setPreviewUrl, event) => {
+    const file =
+      event === FileEvent.CHANGE
+        ? (e as ChangeEvent<HTMLInputElement>)?.target?.files?.[0]
+        : (e as DragEvent).dataTransfer?.files?.[0];
+
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setPreviewUrl(url);
+      onSelectImage(url);
+      console.log(targetRoute);
+    }
+  };

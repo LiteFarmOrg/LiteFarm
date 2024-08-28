59f45cde310880b49f8af77a4d6819d86896cb67
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-08-28 12:30:28 -0700

    LF-4347 Add brought_in_date and birth_date to animal_batch table

---
+++ packages/api/db/migration/20240828191003_add_brought_in_date_and_birth_date_to_animal_batch_table.js
@@ -0,0 +1,28 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+export const up = async function (knex) {
+  await knex.schema.alterTable('animal_batch', (table) => {
+    table.datetime('brought_in_date');
+    table.datetime('birth_date');
+  });
+};
+
+export const down = async function (knex) {
+  await knex.schema.alterTable('animal_batch', (table) => {
+    table.dropColumn('brought_in_date');
+    table.dropColumn('birth_date');
+  });
+};

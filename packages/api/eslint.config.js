e76b724804d95559f5057464e767ac78377b53c6
Navdeep <nsdhammu@gmail.com>
2024-09-11 16:27:25 -0400

    LF-4395 fix linting of json files

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -2,20 +2,30 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import json from 'eslint-plugin-json';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        args: 'all',
-        argsIgnorePattern: '^_',
-        caughtErrors: 'all',
-        caughtErrorsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        ignoreRestSiblings: true,
-      },
-    ],
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    ...json.configs['recommended'],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
   },
-});
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+);

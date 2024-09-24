6e859c91aff65eba2b8e908d4497e6d2a0a9a1b6
Antonella Sgarlatta <antsgar@gmail.com>
2024-09-24 18:48:32 -0300

    LF-4436 add copyright

--- packages/webapp/src/polyfillDateTimeFormat.ts
+++ packages/webapp/src/polyfillDateTimeFormat.ts
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
 // We are polyfilling because Chrome desktop has incomplete support for some of the languages (even if in theory they are supported)
 // Once Chrome starts returning the right thing these imports (and the package) should be removed
 import '@formatjs/intl-datetimeformat/polyfill-force';

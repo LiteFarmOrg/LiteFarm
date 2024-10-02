8581feebc4bcd75d89a1be9d49f91f73eef51484
Duncan-Brain <dbrain@litefarm.org>
2024-10-02 08:16:41 -0400

    LF-4380 Replace code with utility function to be reused in edit, and change plain insert with insertGraph

--- packages/api/src/controllers/animalController.js
+++ packages/api/src/controllers/animalController.js
@@ -16,14 +16,12 @@
 import { Model, transaction } from 'objection';
 import AnimalModel from '../models/animalModel.js';
 import baseController from './baseController.js';
-import CustomAnimalBreedModel from '../models/customAnimalBreedModel.js';
-import CustomAnimalTypeModel from '../models/customAnimalTypeModel.js';
-import AnimalGroupModel from '../models/animalGroupModel.js';
-import AnimalGroupRelationshipModel from '../models/animalGroupRelationshipModel.js';
-import { assignInternalIdentifiers } from '../util/animal.js';
+import {
+  assignInternalIdentifiers,
+  checkAndAddGroup,
+  checkAndAddCustomTypeAndBreed,
+} from '../util/animal.js';
 import { handleObjectionError } from '../util/errorCodes.js';
-import { checkAndTrimString } from '../util/util.js';
-import AnimalUseRelationshipModel from '../models/animalUseRelationshipModel.js';
 import { uploadPublicImage } from '../util/imageUpload.js';
 
 const animalController = {
@@ -58,119 +56,43 @@ const animalController = {
   addAnimals() {
     return async (req, res) => {
       const trx = await transaction.start(Model.knex());
-
       try {
         const { farm_id } = req.headers;
         const result = [];
 
-        // avoid attempts to add an already created type or breed to the DB
-        // where multiple animals have the same type_name or breed_name
-        const typeIdsMap = {};
-        const typeBreedIdsMap = {};
+        // Create utility object used in type and breed
+        req.body.typeIdsMap = {};
+        req.body.typeBreedIdsMap = {};
 
         for (const animal of req.body) {
-          if (animal.type_name) {
-            let typeId = typeIdsMap[animal.type_name];
-
-            if (!typeId) {
-              const newType = await baseController.postWithResponse(
-                CustomAnimalTypeModel,
-                { type: animal.type_name, farm_id },
-                req,
-                { trx },
-              );
-              typeId = newType.id;
-              typeIdsMap[animal.type_name] = typeId;
-            }
-            animal.custom_type_id = typeId;
-            delete animal.type_name;
-          }
-
-          if (animal.breed_name) {
-            const typeColumn = animal.default_type_id ? 'default_type_id' : 'custom_type_id';
-            const typeId = animal.type_name
-              ? typeIdsMap[animal.type_name]
-              : animal.default_type_id || animal.custom_type_id;
-            const typeBreedKey = `${typeColumn}_${typeId}_${animal.breed_name}`;
-            let breedId = typeBreedIdsMap[typeBreedKey];
-
-            if (!breedId) {
-              const newBreed = await baseController.postWithResponse(
-                CustomAnimalBreedModel,
-                { farm_id, [typeColumn]: typeId, breed: animal.breed_name },
-                req,
-                { trx },
-              );
-              breedId = newBreed.id;
-              typeBreedIdsMap[typeBreedKey] = breedId;
-            }
-            animal.custom_breed_id = breedId;
-            delete animal.breed_name;
-          }
+          await checkAndAddCustomTypeAndBreed(req, animal, farm_id, trx);
+          await checkAndAddGroup(req, animal, farm_id, trx);
 
           // Remove farm_id if it happens to be set in animal object since it should be obtained from header
           delete animal.farm_id;
 
-          const groupName = checkAndTrimString(animal.group_name);
-          delete animal.group_name;
-
-          const individualAnimalResult = await baseController.postWithResponse(
+          const individualAnimalResult = await baseController.insertGraphWithResponse(
             AnimalModel,
             { ...animal, farm_id },
             req,
             { trx },
           );
-
-          const groupIds = [];
-          if (groupName) {
-            let group = await baseController.existsInTable(trx, AnimalGroupModel, {
-              name: groupName,
-              farm_id,
-              deleted: false,
-            });
-
-            if (!group) {
-              group = await baseController.postWithResponse(
-                AnimalGroupModel,
-                { name: groupName, farm_id },
-                req,
-                { trx },
-              );
-            }
-
-            groupIds.push(group.id);
-
-            // Insert into join table
-            await AnimalGroupRelationshipModel.query(trx).insert({
-              animal_id: individualAnimalResult.id,
-              animal_group_id: group.id,
-            });
-          }
-
-          individualAnimalResult.group_ids = groupIds;
-
-          const animalUseRelationships = [];
-          if (animal.animal_use_relationships?.length) {
-            for (const relationship of animal.animal_use_relationships) {
-              animalUseRelationships.push(
-                await baseController.postWithResponse(
-                  AnimalUseRelationshipModel,
-                  { ...relationship, animal_id: individualAnimalResult.id },
-                  req,
-                  { trx },
-                ),
-              );
-            }
-          }
-
-          individualAnimalResult.animal_use_relationships = animalUseRelationships;
+          // Format group_ids
+          const groupIdMap =
+            individualAnimalResult.group_ids?.map((group) => group.animal_group_id) || [];
+          individualAnimalResult.group_ids = groupIdMap;
 
           result.push(individualAnimalResult);
         }
 
+        // delete utility objects
+        delete req.body.typeIdsMap;
+        delete req.body.typeBreedIdsMap;
+
         await trx.commit();
 
         await assignInternalIdentifiers(result, 'animal');
+
         return res.status(201).send(result);
       } catch (error) {
         await handleObjectionError(error, res, trx);

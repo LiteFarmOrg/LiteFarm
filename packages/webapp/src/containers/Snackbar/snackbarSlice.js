afbefc898d223a553739bb53906e26b244351c9b
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-10-02 15:18:51 -0700

    LF-4390 Reimplement NotistackSnackbar using SnackbarProvider's Components prop * update enqueueSuccessSnackbar to pass variant * update enqueueErrorSnackbar to handle object payload for persist * remove hard-coded 'persist: true' from enqueueSnackbar arguments

--- packages/webapp/src/containers/Snackbar/snackbarSlice.js
+++ packages/webapp/src/containers/Snackbar/snackbarSlice.js
@@ -9,11 +9,34 @@ const snackbarSlice = createSlice({
   reducers: {
     enqueueSuccessSnackbar: (state, { payload: message }) => {
       const key = `success-${new Date().getTime()}`;
-      state.notifications = [...state.notifications, { message, key }];
+      state.notifications = [
+        ...state.notifications,
+        { message, key, options: { variant: 'common' } },
+      ];
     },
-    enqueueErrorSnackbar: (state, { payload: message }) => {
+    /**
+     * Accept a string message directly or an object containing the message and persist.
+     *
+     * Examples:
+     * - enqueueErrorSnackbar("Error message.")
+     * - enqueueErrorSnackbar({ message: "Error message.", persist: true })
+     *
+     * @param {string | { message: string, persist?: boolean }} payload
+     */
+    enqueueErrorSnackbar: (state, { payload }) => {
+      if (typeof payload === 'object' && !payload.message) {
+        console.error('Invalid payload: missing message property');
+        return;
+      }
+
+      const message = typeof payload === 'object' ? payload.message : payload;
+      const persist = typeof payload === 'object' && payload.persist ? payload.persist : false;
+
       const key = `error-${new Date().getTime()}`;
-      state.notifications = [...state.notifications, { message, key }];
+      state.notifications = [
+        ...state.notifications,
+        { message, key, options: { variant: 'common', persist } },
+      ];
     },
     closeSnackbar: (state, { payload: key }) => {
       state.notifications = state.notifications.map((notification) => ({

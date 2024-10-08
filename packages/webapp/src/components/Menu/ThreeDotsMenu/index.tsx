a9b9b9a8c08c8132c1d1221ec43ef2d22273db6b
Sayaka Ono <sayaka.ono.81@gmail.com>
2024-10-08 14:11:38 -0700

    LF-4381 Create ThreeDotsMenu component with story

---
+++ packages/webapp/src/components/Menu/ThreeDotsMenu/index.tsx
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import { forwardRef, ReactNode } from 'react';
+import { BsThreeDots } from 'react-icons/bs';
+import FloatingMenu from '../FloatingButtonMenu/FloatingMenu';
+import DropdownButton from '../../Form/DropDownButton';
+import styles from './styles.module.scss';
+
+export type ThreeDotsMenuProps = {
+  options: { label: ReactNode; onClick: () => void }[];
+};
+
+const ThreeDotsMenu = ({ options }: ThreeDotsMenuProps) => {
+  return (
+    <DropdownButton
+      type={'v2'}
+      noIcon
+      classes={{ button: styles.menuButton }}
+      Menu={forwardRef((menuProps, ref) => (
+        <FloatingMenu
+          ref={ref}
+          options={options}
+          classes={{ menuItem: styles.menuItem }}
+          {...menuProps}
+        />
+      ))}
+      menuPositionOffset={[0, 1]}
+    >
+      <BsThreeDots className={styles.menuIcon} />
+    </DropdownButton>
+  );
+};
+
+export default ThreeDotsMenu;

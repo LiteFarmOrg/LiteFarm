c32864a3495f4e69ccee5613353d85c19b291d8a
Duncan-Brain <dbrain@litefarm.org>
2024-10-16 18:54:17 -0400

    LF-4380 Remove unreachable test

--- packages/api/tests/animal_batch.test.js
+++ packages/api/tests/animal_batch.test.js
@@ -2044,20 +2044,6 @@ describe('Animal Batch Tests', () => {
             message: 'Brought in date must be used with brought in origin',
           },
         },
-        {
-          testName: 'Cannot create a new type associated with an existing breed',
-          getPatchBody: (batch) => [
-            {
-              id: batch.id,
-              defaultBreedId: animalBreed.id,
-              type_name: 'string',
-            },
-          ],
-          patchErr: {
-            code: 400,
-            message: 'Cannot create a new type associated with an existing breed',
-          },
-        },
       ],
     };
 

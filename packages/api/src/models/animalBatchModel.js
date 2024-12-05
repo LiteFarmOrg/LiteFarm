00fefb84be9266900f82bf635cb9aaf6b9aa3a83
Duncan-Brain <dbrain@litefarm.org>
2024-12-04 20:02:14 -0500

    Revert "LF-4528 Update animal models and controllers"
    This reverts commit 9f26f35d4b61bcf656df284c9f8cd8a5894acfec.

--- packages/api/src/models/animalBatchModel.js
+++ packages/api/src/models/animalBatchModel.js
@@ -176,7 +176,6 @@ class AnimalBatchModel extends baseModel {
           },
           to: 'task.task_id',
         },
-        modify: (query) => query.select('task.task_id').where('deleted', false),
       },
       default_type: {
         modelClass: DefaultAnimalTypeModel,

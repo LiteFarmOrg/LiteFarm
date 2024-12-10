29c8f2c2f5431277f4a6982d9f02bbfdfb24ee6f
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2024-12-10 12:46:34 -0800

    LF-4551 Fix placement of custom task conditional

--- packages/webapp/src/containers/Task/saga.js
+++ packages/webapp/src/containers/Task/saga.js
@@ -613,17 +613,13 @@ export function* createTaskSaga({ payload }) {
         task_translation_key === 'HARVEST_TASK' ? result.data[0] : result.data;
       yield call(getTasksSuccessSaga, { payload: isHarvest ? result.data : [result.data] });
       if (alreadyCompleted) {
+        const isCustomTaskWithAnimals =
+          isCustomTask && (result.data.animals?.length || result.data.animal_batches?.length);
+        yield call(onReqSuccessSaga, {
+          message: i18n.t('message:TASK.CREATE.SUCCESS'),
+          pathname: getTaskCompletePathname(task_id, task_translation_key, isCustomTaskWithAnimals),
+        });
         if (isCustomTask) {
-          const isCustomTaskWithAnimals =
-            isCustomTask && (result.data.animals?.length || result.data.animal_batches?.length);
-          yield call(onReqSuccessSaga, {
-            message: i18n.t('message:TASK.CREATE.SUCCESS'),
-            pathname: getTaskCompletePathname(
-              task_id,
-              task_translation_key,
-              isCustomTaskWithAnimals,
-            ),
-          });
           yield put(
             setPersistedPaths([`/tasks/${task_id}/complete`, `/tasks/${task_id}/before_complete`]),
           );

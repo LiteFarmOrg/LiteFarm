ac2715e95f348a3cfad5efbecf94e67692c4104d
Duncan-Brain <dbrain@litefarm.org>
2025-01-27 18:18:25 -0500

    LF-4694 Add scope check

--- packages/api/src/routes/farmAddonRoute.js
+++ packages/api/src/routes/farmAddonRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import checkScope from '../middleware/acl/checkScope.js';
 import farmAddonController from '../controllers/farmAddonController.js';
 
 const router = express.Router();
 
-router.get('/', farmAddonController.getFarmAddon());
+router.get('/', checkScope(['get:addon']), farmAddonController.getFarmAddon());
 
 export default router;

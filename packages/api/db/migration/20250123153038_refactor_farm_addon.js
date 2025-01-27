32b8db65d4625a8bfc6352ee6bf889788f2ab583
Duncan-Brain <dbrain@litefarm.org>
2025-01-27 14:43:23 -0500

    LF-4689 Rename tables

--- packages/api/db/migration/20250123153038_refactor_farm_addon.js
+++ packages/api/db/migration/20250123153038_refactor_farm_addon.js
@@ -14,15 +14,15 @@
  */
 
 export const up = async function (knex) {
-  await knex.schema.renameTable('integrating_partner', 'addon');
-  await knex.schema.alterTable('addon', (t) => {
+  await knex.schema.renameTable('integrating_partner', 'addon_partner');
+  await knex.schema.alterTable('addon_partner', (t) => {
     t.renameColumn('partner_id', 'id');
     t.renameColumn('partner_name', 'name');
   });
 
-  await knex.schema.renameTable('farm_external_integration', 'farm_addon');
-  await knex.schema.alterTable('farm_addon', (t) => {
-    t.renameColumn('partner_id', 'addon_id');
+  await knex.schema.renameTable('farm_external_integration', 'addon');
+  await knex.schema.alterTable('addon', (t) => {
+    t.renameColumn('partner_id', 'addon_partner_id');
     t.renameColumn('organization_uuid', 'org_uuid');
     t.dropColumn('webhook_id');
     t.integer('org_pk').nullable();
@@ -30,10 +30,10 @@ export const up = async function (knex) {
 
   await knex('permissions').insert([
     { permission_id: 172, name: 'get:sensors', description: 'get sensors' },
-    { permission_id: 173, name: 'get:farm_addon', description: 'get farm_addons' },
-    { permission_id: 174, name: 'add:farm_addon', description: 'add farm_addons' },
-    { permission_id: 175, name: 'edit:farm_addon', description: 'edit farm_addons' },
-    { permission_id: 176, name: 'delete:farm_addon', description: 'delete farm_addons' },
+    { permission_id: 173, name: 'get:addon', description: 'get addons' },
+    { permission_id: 174, name: 'add:addon', description: 'add addons' },
+    { permission_id: 175, name: 'edit:addon', description: 'edit addons' },
+    { permission_id: 176, name: 'delete:addon', description: 'delete addons' },
   ]);
 
   // Farm workers CAN get sensors
@@ -59,15 +59,15 @@ export const up = async function (knex) {
 };
 
 export const down = async function (knex) {
-  await knex.schema.renameTable('addon', 'integrating_partner');
+  await knex.schema.renameTable('addon_partner', 'integrating_partner');
   await knex.schema.alterTable('integrating_partner', (t) => {
     t.renameColumn('id', 'partner_id');
     t.renameColumn('name', 'partner_name');
   });
 
-  await knex.schema.renameTable('farm_addon', 'farm_external_integration');
+  await knex.schema.renameTable('addon', 'farm_external_integration');
   await knex.schema.alterTable('farm_external_integration', (t) => {
-    t.renameColumn('addon_id', 'partner_id');
+    t.renameColumn('addon_partner_id', 'partner_id');
     t.renameColumn('org_uuid', 'organization_uuid');
     t.dropColumn('org_pk');
     t.integer('webhook_id').nullable();

70bdada77945fe8c5fd7707027d98c91daa4b924
Duncan-Brain <dbrain@litefarm.org>
2025-02-03 20:02:15 -0500

    LF-4703 Add uniquess partial index to only allow 1 entry per farm and partner combination

--- packages/api/db/migration/20250203144322_add_base_properties_to_farm_addon.js
+++ packages/api/db/migration/20250203144322_add_base_properties_to_farm_addon.js
@@ -15,17 +15,39 @@
 
 export const up = async function (knex) {
   await knex.schema.alterTable('farm_addon', (t) => {
+    // Knex apparently does not rename indexes aliong with table names
+    t.dropPrimary('farm_external_integration_pkey');
+  });
+  await knex.schema.alterTable('farm_addon', (t) => {
+    t.increments('id').primary();
     t.boolean('deleted').notNullable().defaultTo(false);
     t.string('created_by_user_id').references('user_id').inTable('users');
     t.string('updated_by_user_id').references('user_id').inTable('users');
     t.dateTime('created_at').notNullable().defaultTo(new Date('2000/1/1').toISOString());
     t.dateTime('updated_at').notNullable().defaultTo(new Date('2000/1/1').toISOString());
   });
+
+  // Add the partial unique index using raw SQL
+  // Knex partial indexes not working correctly
+  // TODO: Delete this index when multiple organizations are allowed per partner id
+  await knex.raw(`
+    CREATE UNIQUE INDEX farm_addon_uniqueness_composite
+    ON farm_addon(farm_id, addon_partner_id)
+    WHERE deleted = false;
+  `);
 };
 
 export const down = async function (knex) {
   await knex.schema.alterTable('farm_addon', (t) => {
+    t.dropIndex(['farm_id', 'addon_partner_id'], 'farm_addon_uniqueness_composite');
+    t.dropPrimary();
+  });
+  await knex.schema.alterTable('farm_addon', (t) => {
+    t.primary(['farm_id', 'addon_partner_id'], {
+      constraintName: 'farm_external_integration_pkey',
+    });
     t.dropColumns([
+      'id',
       'deleted',
       'created_by_user_id',
       'updated_by_user_id',

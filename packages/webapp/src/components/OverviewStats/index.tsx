e04a648bf624397b7a9d42deeb65eaa5aae8715b
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-02-10 10:52:30 -0800

    LF-4696 Add OverviewStats component

---
+++ packages/webapp/src/components/OverviewStats/index.tsx
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
+import clsx from 'clsx';
+import styles from './styles.module.scss';
+
+export type OverviewStatsProps = {
+  stats: Record<string, number>;
+  translationMappings: { key: string; translationKey: string }[];
+  format?: (key: string, label: string) => ReactNode;
+  isCompact?: boolean;
+};
+
+const OverviewStats = ({ stats, translationMappings, format, isCompact }: OverviewStatsProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={clsx(styles.wrapper, isCompact ? styles.isCompact : '')}>
+      {translationMappings.map(({ key, translationKey }) => {
+        const label = t(translationKey);
+        const count = key in stats ? stats[key] : 0;
+
+        return (
+          <div key={key} className={styles.tile}>
+            <span className={styles.category}>{format ? format(key, label) : label}</span>
+            <span className={styles.count}>{count}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default OverviewStats;

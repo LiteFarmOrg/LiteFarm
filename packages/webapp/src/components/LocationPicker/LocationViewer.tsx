342873d80c99e392beaa076b11e8e96a111dd7b5
Duncan-Brain <dbrain@litefarm.org>
2025-02-27 20:27:58 -0500

    LF-4742 Add licence and expect error instead of undefined props

--- packages/webapp/src/components/LocationPicker/LocationViewer.tsx
+++ packages/webapp/src/components/LocationPicker/LocationViewer.tsx
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
 import { MutableRefObject } from 'react';
 import { Location, UserFarm } from '../../types';
 import PureMapHeader from '../Map/Header';
@@ -22,12 +37,9 @@ const LocationViewer = ({
   const { grid_points, farm_name } = userFarm;
   return (
     <>
-      <PureMapHeader
-        farmName={farm_name}
-        showClose={handleClose}
-        showVideo={undefined}
-        isAdmin={undefined}
-      />
+      {/* @ts-expect-error */}
+      <PureMapHeader farmName={farm_name} showClose={handleClose} />
+      {/* @ts-expect-error */}
       <LocationPicker
         onSelectLocation={() => {
           //  TODO: fix onSelectLocationRef in LocationPicker
@@ -39,13 +51,6 @@ const LocationViewer = ({
         maxZoomRef={maxZoomRef}
         getMaxZoom={getMaxZoom}
         className={styles.removeBottomMargin}
-        // Unused to quiet typescript
-        style={undefined}
-        maxZoom={undefined}
-        isPinMode={undefined}
-        setPinCoordinate={undefined}
-        pinCoordinate={undefined}
-        readOnlyPinCoordinates={undefined}
       />
     </>
   );

8b9561975676faade1c16c9b92c10ca1af4ffb1a
Duncan-Brain <dbrain@litefarm.org>
2025-02-28 20:19:06 -0500

    LF-4747 Customize gap and max columns

--- packages/webapp/src/components/Layout/BentoLayout.tsx
+++ packages/webapp/src/components/Layout/BentoLayout.tsx
@@ -1,10 +1,21 @@
 import { ReactNode } from 'react';
 import styles from './layout.module.scss';
 
-type BentoLayout = {
+export type BentoLayoutProps = {
   children: ReactNode;
+  layoutConfig: {
+    gapInPx: number;
+    maxColumns: number;
+  };
 };
 
-export default function BentoLayout({ children }: BentoLayout) {
-  return <div className={styles.bento}>{children}</div>;
+export default function BentoLayout({ children, layoutConfig }: BentoLayoutProps) {
+  const { gapInPx, maxColumns } = layoutConfig;
+  const style = { '--gap': `${gapInPx}px`, '--columns': maxColumns } as React.CSSProperties;
+
+  return (
+    <div className={styles.bento} style={style}>
+      {children}
+    </div>
+  );
 }
---
+++ packages/webapp/src/stories/Layout/Layout.stories.tsx
@@ -0,0 +1,66 @@
+/*
+ *  Copyright 2024 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Meta, StoryObj } from '@storybook/react';
+import BentoLayout, { BentoLayoutProps } from '../../components/Layout/BentoLayout';
+import { componentDecorators } from '../Pages/config/Decorators';
+import styles from './styles.module.scss';
+
+const Template = (args: BentoLayoutProps) => {
+  return (
+    <BentoLayout {...args}>
+      {new Array(args.layoutConfig.maxColumns + 2).fill('').map(() => {
+        return <div className={styles.divHeight} />;
+      })}
+    </BentoLayout>
+  );
+};
+
+// https://storybook.js.org/docs/writing-stories/typescript
+const meta: Meta<typeof BentoLayout> = {
+  title: 'Components/Layout/BentoLayout',
+  component: Template,
+  decorators: [...componentDecorators],
+};
+export default meta;
+
+type Story = StoryObj<typeof Template>;
+
+export const MaxThreeWidth: Story = {
+  args: {
+    layoutConfig: {
+      gapInPx: 8,
+      maxColumns: 3,
+    },
+  },
+};
+
+export const MaxFiveWidth: Story = {
+  args: {
+    layoutConfig: {
+      gapInPx: 8,
+      maxColumns: 5,
+    },
+  },
+};
+
+export const SixteenPxGap: Story = {
+  args: {
+    layoutConfig: {
+      gapInPx: 16,
+      maxColumns: 3,
+    },
+  },
+};

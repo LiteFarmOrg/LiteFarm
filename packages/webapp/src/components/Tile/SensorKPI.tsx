7c0eede1fa8656262cd6cb2ef15cc6ebb274d03c
Duncan-Brain <dbrain@litefarm.org>
2025-03-03 18:01:23 -0500

    LF-4747 Create sensor KPI tile

---
+++ packages/webapp/src/components/Tile/SensorKPI.tsx
@@ -0,0 +1,70 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import clsx from 'clsx';
+import Icon from '../Icons';
+import BentoLayout from '../Layout/BentoLayout';
+import { StatusIndicatorPill, StatusIndicatorPillProps } from '../StatusIndicatorPill';
+import styles from './sensorKPI.module.scss';
+import { TMeasurement } from './SensorReadingKPI';
+
+export type SensorKPIprops = {
+  sensor: {
+    id: string | number;
+    status: StatusIndicatorPillProps;
+  };
+  discriminator: TMeasurement;
+  measurements: TMeasurement[];
+};
+
+const Measurement = ({ measurement, value, unit }: TMeasurement) => (
+  <div className={styles.measurement}>
+    <div className={styles.measureText}>{measurement}</div>
+    <div className={styles.valueText}>
+      {value}
+      {unit}
+    </div>
+  </div>
+);
+
+export default function SensorKPI({ sensor, discriminator, measurements }: SensorKPIprops) {
+  const { measurement, value, unit } = discriminator;
+  const { status, id } = sensor;
+  return (
+    <div className={styles.sensorKpi}>
+      <div className={clsx(styles.opaqueLayer, styles.gap8px)}>
+        <div className={styles.topDetails}>
+          <div className={styles.sensor}>
+            <Icon iconName="SENSOR" className={styles.icon} />
+            {id}
+          </div>
+          <div className={styles.discriminator}>
+            <Icon iconName="RULER" className={styles.icon} />
+            <div className={styles.discriminatorText}>
+              {value}
+              {unit}
+            </div>
+          </div>
+          <StatusIndicatorPill {...status} />
+        </div>
+        <BentoLayout>
+          {measurements.map((measurement) => (
+            <Measurement {...measurement} />
+          ))}
+        </BentoLayout>
+      </div>
+    </div>
+  );
+}

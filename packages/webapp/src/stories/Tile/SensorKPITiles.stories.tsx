809a617e3770f84d214f688537b432234ab9164f
Duncan-Brain <dbrain@litefarm.org>
2025-03-03 18:02:13 -0500

    LF-4747 Add stories for sensor KPI tile

--- packages/webapp/src/stories/Tile/SensorKPITiles.stories.tsx
+++ packages/webapp/src/stories/Tile/SensorKPITiles.stories.tsx
@@ -15,25 +15,26 @@
 
 import { Meta, StoryObj } from '@storybook/react';
 import BentoLayout, { BentoLayoutProps } from '../../components/Layout/BentoLayout';
-import SensorReadingKPI, { SensorReadingKPIprops } from '../../components/Tile/SensorReadingKPI';
+import SensorReadingKPI from '../../components/Tile/SensorReadingKPI';
+import SensorKPI from '../../components/Tile/SensorKPI';
 import { componentDecorators } from '../Pages/config/Decorators';
+import { Status } from '../../components/StatusIndicatorPill';
 
-type PropTypes = SensorReadingKPIprops;
+// type PropTypes = SensorReadingKPIprops | SensorKPIprops;
 
 type TemplateProps = Partial<BentoLayoutProps> & {
-  TileComponent: (props: PropTypes) => JSX.Element;
-  tileComponentProps: PropTypes;
+  TileComponent: () => JSX.Element;
 };
 
-const Template = ({ TileComponent, tileComponentProps, layoutConfig }: TemplateProps) => {
+const Template = ({ TileComponent, layoutConfig }: TemplateProps) => {
   return layoutConfig ? (
     <BentoLayout layoutConfig={layoutConfig}>
       {new Array(layoutConfig.maxColumns + 2).fill('').map(() => {
-        return TileComponent(tileComponentProps);
+        return <TileComponent />;
       })}
     </BentoLayout>
   ) : (
-    TileComponent(tileComponentProps)
+    <TileComponent />
   );
 };
 
@@ -47,14 +48,13 @@ export default meta;
 
 type Story = StoryObj<typeof Template>;
 
+const SensorReadingKPITile = () => {
+  return <SensorReadingKPI measurement={'Soil Water Potential'} value={50} unit={'°F'} />;
+};
+
 export const SensorReadingKPIBento: Story = {
   args: {
-    TileComponent: SensorReadingKPI,
-    tileComponentProps: {
-      measurement: 'Soil Water Potential',
-      value: 50,
-      unit: '°F',
-    },
+    TileComponent: SensorReadingKPITile,
     layoutConfig: {
       gapInPx: 8,
       maxColumns: 3,
@@ -64,11 +64,54 @@ export const SensorReadingKPIBento: Story = {
 
 export const SensorReadingKPIOne: Story = {
   args: {
-    TileComponent: SensorReadingKPI,
-    tileComponentProps: {
-      measurement: 'Soil Water Potential',
-      value: 50,
-      unit: '°F',
+    TileComponent: SensorReadingKPITile,
+  },
+};
+
+const SensorKPITile = () => {
+  const props = {
+    sensor: {
+      id: 'AS4TG5',
+      status: {
+        status: Status.ONLINE,
+        pillText: 'Online',
+        tooltipText: 'Device has sent data in the last 12 hours',
+      },
     },
+    discriminator: {
+      measurement: 'depth_elevation',
+      value: 50,
+      unit: 'cm',
+    },
+    measurements: [
+      {
+        measurement: 'Temperature',
+        value: 50,
+        unit: '°F',
+      },
+      {
+        measurement: 'Soil Water Potential',
+        value: 124,
+        unit: 'kPa',
+      },
+    ],
+  };
+
+  return <SensorKPI {...props} />;
+};
+
+export const SensorKPIBento: Story = {
+  args: {
+    TileComponent: SensorKPITile,
+    layoutConfig: {
+      gapInPx: 8,
+      maxColumns: 3,
+    },
+  },
+};
+
+export const SensorKPIOne: Story = {
+  args: {
+    TileComponent: SensorKPITile,
   },
 };

4777f43655703b82363000ff5d15d66f75b3da26
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-03-07 15:43:45 -0800

    LF-4746 Pull out the state-independent logic from FinancesDateRangeSelector and useFinancesDateRange

---
+++ packages/webapp/src/components/DateRangeSelector/SmartDateRangeSelector.tsx
@@ -0,0 +1,111 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import clsx from 'clsx';
+import moment, { Moment } from 'moment';
+import DateRange, { SUNDAY } from '../../util/dateRange';
+import DateRangeSelector from '.';
+import { DateRangeOptions } from './types';
+import { FROM_DATE, TO_DATE } from '../Form/DateRangePicker';
+import { DateRangeSelection } from './types';
+import styles from './styles.module.scss';
+
+/**
+ * A wrapper component around DateRangeSelector that converts a semantic range (such as "Year to Date" or "Last 7 Days") into date values using the DateRange utility class.
+ *
+ * Pass either local (component) state or Redux-based state management
+ */
+
+interface SmartDateRangeSelectorProps {
+  dateRange: DateRangeSelection;
+
+  /**
+   * When using Redux, ensure your reducer handles this action by merging the new values into the current state, for example:
+   *
+   *   updateDateRange(state, action: PayloadAction<Partial<DateRangeSelection>>) {
+   *     state.date_range = { ...state.date_range, ...action.payload };
+   *   }
+   *
+   * For local state (e.g., using useState), merge as follows
+   *
+   *   setDateRange((prev) => ({ ...prev, ...newDateRange }));
+   */
+  updateDateRange: (newDateRange: Partial<DateRangeSelection>) => void;
+
+  defaultValue: DateRangeOptions;
+  onValidityChange?: (valid: boolean) => void;
+  className?: string;
+}
+
+const isDateValid = (date: string | Moment | undefined): boolean => {
+  return date ? moment(date).isValid() : false;
+};
+
+const SmartDateRangeSelector = ({
+  dateRange,
+  updateDateRange,
+  onValidityChange,
+  className,
+  defaultValue = DateRangeOptions.YEAR_TO_DATE,
+}: SmartDateRangeSelectorProps) => {
+  const { option, customRange = {} } = dateRange;
+  const initialOption = option || defaultValue;
+  const dateRangeUtil = new DateRange(new Date(), SUNDAY);
+
+  const initialStartDate = customRange.startDate ? moment(customRange.startDate) : undefined;
+  const initialEndDate = customRange.endDate ? moment(customRange.endDate) : undefined;
+
+  const changeDate = (type: 'start' | 'end', date: Moment) => {
+    const startDate = type === 'start' ? date : customRange.startDate;
+    const endDate = type === 'end' ? date : customRange.endDate;
+    const newDateRange: DateRangeSelection = { customRange: { startDate, endDate } };
+
+    // If both dates are valid, update the dates.
+    if ([startDate, endDate].every(isDateValid)) {
+      newDateRange.startDate = startDate;
+      newDateRange.endDate = endDate;
+    }
+
+    updateDateRange(newDateRange);
+  };
+
+  const onChangeDateRangeOption = (value: DateRangeOptions) => {
+    let newDateRange: DateRangeSelection = { option: value };
+    if (value !== DateRangeOptions.CUSTOM) {
+      newDateRange = { ...newDateRange, ...dateRangeUtil.getDates(value) };
+    } else if (
+      Object.keys(customRange).length === 2 &&
+      Object.values(customRange).every((date) => date && isDateValid(date))
+    ) {
+      newDateRange = { ...newDateRange, ...customRange };
+    }
+
+    updateDateRange(newDateRange);
+  };
+
+  return (
+    <div className={clsx(styles.rangeContainer, className)}>
+      <DateRangeSelector
+        defaultDateRangeOptionValue={initialOption}
+        defaultCustomDateRange={{ [FROM_DATE]: initialStartDate, [TO_DATE]: initialEndDate }}
+        onChangeDateRangeOption={onChangeDateRangeOption}
+        changeDateMethod={changeDate}
+        onValidityChange={onValidityChange}
+      />
+    </div>
+  );
+};
+
+export default SmartDateRangeSelector;
--- packages/webapp/src/components/Finances/DateRangeSelector/useFinancesDateRange.ts
+++ packages/webapp/src/components/Finances/DateRangeSelector/useFinancesDateRange.ts
@@ -13,41 +13,31 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 import { Moment } from 'moment';
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
 import { dateRangeDataSelector } from '../../../containers/Finances/selectors';
 import { setDateRange } from '../../../containers/Finances/actions';
-import DateRange, { MONDAY, SUNDAY } from '../../../util/dateRange';
+import { MONDAY, SUNDAY } from '../../../util/dateRange';
 import { DateRangeOptions, DateRangeSelection } from '../../DateRangeSelector/types';
+import useSmartDateRange from '../../DateRangeSelector/useSmartDateRange';
 
+/* Finance-specific wrapper for useSmartDateRange for use with the Finances Redux store slice */
 interface UseFinancesDateRangeProps {
   weekStartDate?: typeof SUNDAY | typeof MONDAY;
 }
 
-/**
- * Returns startDate and endDate in the store.
- * If they are not defined, set the default option and dates.
- */
-export default function useFinancesDateRange({
-  weekStartDate = SUNDAY,
-}: UseFinancesDateRangeProps): { startDate?: string | Moment; endDate?: string | Moment } {
-  const dateRange: DateRangeSelection = useSelector(dateRangeDataSelector);
+export default function useFinancesDateRange({ weekStartDate }: UseFinancesDateRangeProps): {
+  startDate?: string | Moment;
+  endDate?: string | Moment;
+} {
   const dispatch = useDispatch();
 
-  const dateRangeUtil = new DateRange(new Date(), weekStartDate);
-  const option = dateRange.option || DateRangeOptions.YEAR_TO_DATE;
+  const dateRange: DateRangeSelection = useSelector(dateRangeDataSelector);
 
-  useEffect(() => {
-    if ((!dateRange.startDate || !dateRange.endDate) && option !== DateRangeOptions.CUSTOM) {
-      const { startDate, endDate } = dateRangeUtil.getDates(option);
-      dispatch(setDateRange({ option, startDate, endDate }));
-    }
-  }, []);
-
-  return { startDate: dateRange.startDate, endDate: dateRange.endDate };
+  return useSmartDateRange({
+    weekStartDate,
+    dateRange,
+    updateDateRange: (newDateRange: Partial<DateRangeSelection>) =>
+      dispatch(setDateRange(newDateRange)),
+    defaultOption: DateRangeOptions.YEAR_TO_DATE,
+  });
 }
-
-useFinancesDateRange.propTypes = {
-  weekStartDate: PropTypes.oneOf([SUNDAY, MONDAY]),
-};

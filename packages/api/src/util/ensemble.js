8eb2bc10429e8873e0c000e67bce80ee2a4d3cd2
Duncan-Brain <dbrain@litefarm.org>
2025-03-17 14:41:02 -0400

    LF-4702 Fix api endpoint to esid instead of non-existent external_id property from esci

--- packages/api/src/util/ensemble.js
+++ packages/api/src/util/ensemble.js
@@ -114,7 +114,7 @@ const getEnsembleSensors = async (farm_id) => {
           sensorArrayMap[device.profile_id] = [];
         }
         sensorArrayMap[device.profile_id].push({
-          external_id: device.external_id,
+          external_id: device.esid,
           latest_position: device.latest_position,
         });
       }

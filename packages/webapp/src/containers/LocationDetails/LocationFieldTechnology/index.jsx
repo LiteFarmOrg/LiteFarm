00d375cc154c878e08b6f4472383ad86aa218bf2
Duncan-Brain <dbrain@litefarm.org>
2025-03-21 18:53:43 -0400

    LF-4702 Use enum to check type

--- packages/webapp/src/containers/LocationDetails/LocationFieldTechnology/index.jsx
+++ packages/webapp/src/containers/LocationDetails/LocationFieldTechnology/index.jsx
@@ -3,7 +3,7 @@ import { locationByIdSelector } from '../../locationSlice';
 
 import PureLocationFieldTechnology from '../../../components/LocationFieldTechnology';
 import { sensorSelector } from '../../sensorSlice';
-import useGroupedSensors from '../../SensorList/useGroupedSensors';
+import useGroupedSensors, { SensorType } from '../../SensorList/useGroupedSensors';
 import { getPointLocationsWithinPolygon } from '../../../util/geoUtils';
 
 function LocationFieldTechnology({ history, match }) {
@@ -19,14 +19,16 @@ function LocationFieldTechnology({ history, match }) {
     if (sensors) {
       fieldTechnology.sensors = getPointLocationsWithinPolygon(sensors, location.grid_points);
     }
-    const externalSensors = groupedSensors.filter((sensor) => !sensor.isSensorArray);
+    const externalSensors = groupedSensors.filter((sensor) => sensor.type == SensorType.SENSOR);
     if (externalSensors.length) {
       fieldTechnology.externalSensors = getPointLocationsWithinPolygon(
         externalSensors,
         location.grid_points,
       );
     }
-    const externalSensorArrays = groupedSensors.filter((sensor) => sensor.isSensorArray);
+    const externalSensorArrays = groupedSensors.filter(
+      (sensor) => sensor.type == SensorType.SENSOR_ARRAY,
+    );
     if (externalSensorArrays.length) {
       fieldTechnology.externalSensorArrays = getPointLocationsWithinPolygon(
         externalSensorArrays,

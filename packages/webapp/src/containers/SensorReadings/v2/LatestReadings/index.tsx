00bde3ed447815fa56b790b0ccf229df35b283ec
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-03-21 15:57:00 -0700

    LF-4699 [WIP]Add LatestReadings component

---
+++ packages/webapp/src/containers/SensorReadings/v2/LatestReadings/index.tsx
@@ -0,0 +1,96 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { GrUpdate } from 'react-icons/gr';
+import TextButton from '../../../../components/Form/Button/TextButton';
+import { measurementSelector } from '../../../userFarmSlice';
+import useLatestReadings from './useLatestReadings';
+import { timeDifference } from '../../utils';
+import {
+  formatReadingsToSensorKPIProps,
+  formatReadingsToSensorReadingKPIProps,
+  SensorKPIprops,
+  SensorReadingKPIprops,
+} from './utils';
+import { Sensor } from '../../../../store/api/types';
+import styles from '../styles.module.scss';
+
+function LatestReadings({ sensors, isSensorArray }: { sensors: Sensor[]; isSensorArray: boolean }) {
+  const { t } = useTranslation();
+  const system = useSelector(measurementSelector);
+
+  const { isLoading, latestReadings, latestReadingTime, update } = useLatestReadings(sensors);
+
+  const lastSeenString = latestReadingTime
+    ? timeDifference(new Date(), new Date(latestReadingTime))
+    : '-';
+
+  const props = isSensorArray
+    ? formatReadingsToSensorKPIProps(sensors, latestReadings, system, t)
+    : formatReadingsToSensorReadingKPIProps(sensors[0], latestReadings, system, t);
+
+  return (
+    <>
+      <div className={styles.contentTop}>
+        <dl>
+          <dt>{t('SENSOR.READING.LATEST_READING')}</dt>
+          <dd>{lastSeenString}</dd>
+        </dl>
+        <TextButton onClick={update} className={styles.updateButton}>
+          <GrUpdate />
+          {t('SENSOR.READING.UPDATE_READINGS')}
+        </TextButton>
+      </div>
+      <div className={styles.kpi}>
+        {/* TODO: Use KPI components */}
+        {isSensorArray &&
+          (props as SensorKPIprops[])?.map(({ sensor, measurements, discriminator }) => {
+            return (
+              <div key={sensor.id}>
+                <div>{sensor.id}</div>
+                <div>{sensor.status.status}</div>
+                <div>{discriminator.value}</div>
+                <div>
+                  {measurements.map((measurement) => {
+                    return (
+                      <div key={measurement.measurement}>
+                        {measurement.measurement}
+                        {measurement.value}
+                        {measurement.unit}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        {!isSensorArray &&
+          (props as SensorReadingKPIprops[])?.map(({ measurement, value, unit }) => {
+            return (
+              <div key={measurement}>
+                <div>{measurement}</div>
+                <div>{value}</div>
+                <div>{unit}</div>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+}
+
+export default LatestReadings;

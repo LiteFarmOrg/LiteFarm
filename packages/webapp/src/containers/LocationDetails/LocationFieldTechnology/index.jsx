2e6548903c0ea5d7ff62b241a26b167ecd609f7a
Duncan-Brain <dbrain@litefarm.org>
2025-03-25 10:27:16 -0400

    LF-4702 Replace field tech selectors with reusable hook and cleanup file

--- packages/webapp/src/containers/LocationDetails/LocationFieldTechnology/index.jsx
+++ packages/webapp/src/containers/LocationDetails/LocationFieldTechnology/index.jsx
@@ -1,41 +1,15 @@
 import { useSelector } from 'react-redux';
 import { locationByIdSelector } from '../../locationSlice';
-
 import PureLocationFieldTechnology from '../../../components/LocationFieldTechnology';
-import { sensorSelector } from '../../sensorSlice';
-import useGroupedSensors, { SensorType } from '../../SensorList/useGroupedSensors';
-import { getPointLocationsWithinPolygon } from '../../../util/geoUtils';
+import useFieldTechnology from './useFieldTechnology';
+import useLocationRouterTabs from '../useLocationRouterTabs';
 
 function LocationFieldTechnology({ history, match }) {
   const { location_id } = match.params;
   const location = useSelector(locationByIdSelector(location_id));
 
-  const sensors = useSelector(sensorSelector);
-  const { groupedSensors } = useGroupedSensors();
-
-  let fieldTechnology = {};
-
-  if (location && location.grid_points) {
-    if (sensors) {
-      fieldTechnology.sensors = getPointLocationsWithinPolygon(sensors, location.grid_points);
-    }
-    const externalSensors = groupedSensors.filter((sensor) => sensor.type == SensorType.SENSOR);
-    if (externalSensors.length) {
-      fieldTechnology.externalSensors = getPointLocationsWithinPolygon(
-        externalSensors,
-        location.grid_points,
-      );
-    }
-    const externalSensorArrays = groupedSensors.filter(
-      (sensor) => sensor.type == SensorType.SENSOR_ARRAY,
-    );
-    if (externalSensorArrays.length) {
-      fieldTechnology.externalSensorArrays = getPointLocationsWithinPolygon(
-        externalSensorArrays,
-        location.grid_points,
-      );
-    }
-  }
+  const fieldTechnology = useFieldTechnology(location);
+  const routerTabs = useLocationRouterTabs(location, match);
 
   return (
     <PureLocationFieldTechnology
@@ -43,6 +17,7 @@ function LocationFieldTechnology({ history, match }) {
       history={history}
       match={match}
       location={location}
+      routerTabs={routerTabs}
     />
   );
 }

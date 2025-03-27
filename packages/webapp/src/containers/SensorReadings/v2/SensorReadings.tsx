cc740cd9bc85ccbd96fda65896326d23773b4105
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-03-27 16:27:44 -0700

    LF-4699 Organize standalone sensor colors

--- packages/webapp/src/containers/SensorReadings/v2/SensorReadings.tsx
+++ packages/webapp/src/containers/SensorReadings/v2/SensorReadings.tsx
@@ -15,35 +15,18 @@
 
 import { useTranslation } from 'react-i18next';
 import { Paper } from '@mui/material';
-import { colors } from '../../../assets/theme';
 import PageTitle from '../../../components/PageTitle/v2';
 import SensorsDateRangeSelector from '../../../components/Sensor/v2/SensorsDateRange';
 import SensorCharts from './Charts/SensorCharts';
 import useSensorsDateRange from '../../../components/Sensor/v2/SensorsDateRange/useSensorsDateRange';
 import { CustomRouteComponentProps } from '../../../types';
 import { useGetSensorsQuery } from '../../../store/api/apiSlice';
-import { SensorReadingTypes, SensorTypes } from '../../../store/api/types';
 import LatestReadings from './LatestReadings';
 import styles from './styles.module.scss';
 
 interface RouteParams {
   id: string;
 }
-
-export const STANDALONE_SENSOR_COLORS: Partial<
-  Record<SensorTypes, Partial<Record<SensorReadingTypes, string>>>
-> = {
-  'Weather station': {
-    temperature: colors.chartRed,
-    relative_humidity: colors.chartBlue,
-    rainfall_rate: colors.chartBlue,
-    cumulative_rainfall: colors.chartGreen,
-  },
-  'Soil Water Potential Sensor': {
-    temperature: colors.chartBlue,
-    soil_water_potential: colors.chartRed,
-  },
-};
 
 function SensorReadings({ match, history }: CustomRouteComponentProps<RouteParams>) {
   const { t } = useTranslation();

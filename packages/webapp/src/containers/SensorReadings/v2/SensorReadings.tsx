355fa4ab7fe8d5ec23bf71099b411bea71f6274d
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-03-28 09:40:11 -0700

    LF-4699 Add sensor info to SensorReadings

--- packages/webapp/src/containers/SensorReadings/v2/SensorReadings.tsx
+++ packages/webapp/src/containers/SensorReadings/v2/SensorReadings.tsx
@@ -15,10 +15,14 @@
 
 import { useTranslation } from 'react-i18next';
 import { Paper } from '@mui/material';
+import i18n from '../../../locales/i18n';
 import PageTitle from '../../../components/PageTitle/v2';
 import SensorsDateRangeSelector from '../../../components/Sensor/v2/SensorsDateRange';
+import { Status, StatusIndicatorPill } from '../../../components/StatusIndicatorPill';
 import SensorCharts from './Charts/SensorCharts';
+import { ReactComponent as SensorIcon } from '../../../assets/images/map/signal-01.svg';
 import useSensorsDateRange from '../../../components/Sensor/v2/SensorsDateRange/useSensorsDateRange';
+import { toTranslationKey } from '../../../util';
 import { CustomRouteComponentProps } from '../../../types';
 import { useGetSensorsQuery } from '../../../store/api/apiSlice';
 import LatestReadings from './LatestReadings';
@@ -42,7 +46,6 @@ function SensorReadings({ match, history }: CustomRouteComponentProps<RouteParam
       };
     },
   });
-  console.log({ sensor });
 
   if (!sensorFetching && !sensor) {
     history.replace('/unknown_record');
@@ -55,10 +58,23 @@ function SensorReadings({ match, history }: CustomRouteComponentProps<RouteParam
         onGoBack={history.back}
         classNames={{ wrapper: styles.title }}
       />
-
-      <div className={styles.content}>
-        {sensor && (
-          <>
+      {sensor && (
+        <>
+          <div className={styles.sensorInfo}>
+            <div className={styles.sensorIdAndType}>
+              <div className={styles.sensorId}>
+                <SensorIcon />
+                {sensor.external_id}
+              </div>
+              <div className={styles.deviceType}>
+                {i18n.exists(`SENSOR.DEVICE_TYPES.${toTranslationKey(sensor.name)}`)
+                  ? t(`SENSOR.DEVICE_TYPES.${toTranslationKey(sensor.name)}`)
+                  : sensor.name}
+              </div>
+            </div>
+            <StatusIndicatorPill status={Status.ONLINE} pillText={t('STATUS.ONLINE')} />
+          </div>
+          <div className={styles.content}>
             <LatestReadings sensors={[sensor]} isSensorArray={false} />
             <div className={styles.mainData}>
               <SensorsDateRangeSelector
@@ -76,9 +92,9 @@ function SensorReadings({ match, history }: CustomRouteComponentProps<RouteParam
                 />
               )}
             </div>
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
       <div>Manage ESci link</div>
     </Paper>
   );

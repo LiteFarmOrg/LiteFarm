16b0bacea6e746a94ceb2223fca723e407b62410
Duncan-Brain <dbrain@litefarm.org>
2025-03-31 19:14:24 -0400

    LF-4702 Early return addon sensor tabs and use location enum

--- packages/webapp/src/containers/LocationDetails/useLocationRouterTabs.js
+++ packages/webapp/src/containers/LocationDetails/useLocationRouterTabs.js
@@ -2,9 +2,14 @@ import { useTranslation } from 'react-i18next';
 import useFieldTechnology from './LocationFieldTechnology/useFieldTechnology';
 import { locationEnum } from '../Map/constants';
 
-const cropLocations = ['field', 'garden', 'greenhouse', 'buffer_zone'];
-const fieldTechnologyLocations = ['field', 'garden', 'greenhouse'];
-const readingsLocations = ['sensor', 'sensor_array'];
+const cropLocations = [
+  locationEnum.field,
+  locationEnum.garden,
+  locationEnum.greenhouse,
+  locationEnum.buffer_zone,
+];
+const fieldTechnologyLocations = [locationEnum.field, locationEnum.garden, locationEnum.greenhouse];
+const readingsLocations = [locationEnum.sensor, locationEnum.sensor_array];
 
 export default function useLocationRouterTabs(location, match) {
   const { t } = useTranslation();
@@ -24,7 +29,7 @@ export default function useLocationRouterTabs(location, match) {
     currentTab = 'readings';
   }
 
-  if ([locationEnum.sensor, locationEnum.sensor_array].includes(type) && !location.farm_id) {
+  if (location.isAddonSensor && readingsLocations.includes(type)) {
     // External sensors do not have any other tabs
     return [
       {
@@ -32,45 +37,39 @@ export default function useLocationRouterTabs(location, match) {
         path: match.url.replace(currentTab, 'readings'),
       },
     ];
-  }
+  } else {
+    const fieldTechnology = useFieldTechnology(location);
+    const locationFieldTechnology = Object.keys(fieldTechnology).some(
+      (key) => !!fieldTechnology[key].length,
+    )
+      ? true
+      : false;
 
-  const fieldTechnology = useFieldTechnology(location);
-  const locationFieldTechnology = Object.keys(fieldTechnology).some(
-    (key) => !!fieldTechnology[key].length,
-  )
-    ? true
-    : false;
+    const routerTabs = [
+      {
+        label: t('FARM_MAP.TAB.DETAILS'),
+        path: match.url.replace(currentTab, 'details'),
+      },
+      {
+        label: t('FARM_MAP.TAB.TASKS'),
+        path: match.url.replace(currentTab, 'tasks'),
+      },
+    ];
 
-  const routerTabs = [
-    {
-      label: t('FARM_MAP.TAB.DETAILS'),
-      path: match.url.replace(currentTab, 'details'),
-    },
-    {
-      label: t('FARM_MAP.TAB.TASKS'),
-      path: match.url.replace(currentTab, 'tasks'),
-    },
-  ];
+    // Order reflects tab order
+    if (cropLocations.includes(type)) {
+      routerTabs.push({
+        label: t('FARM_MAP.TAB.CROPS'),
+        path: match.url.replace(currentTab, 'crops'),
+      });
+    }
+    if (locationFieldTechnology && fieldTechnologyLocations.includes(type)) {
+      routerTabs.push({
+        label: t('FARM_MAP.TAB.FIELD_TECHNOLOGY'),
+        path: match.url.replace(currentTab, 'field_technology'),
+      });
+    }
 
-  // Order reflects tab order
-  if (cropLocations.includes(type)) {
-    routerTabs.push({
-      label: t('FARM_MAP.TAB.CROPS'),
-      path: match.url.replace(currentTab, 'crops'),
-    });
+    return routerTabs;
   }
-  if (locationFieldTechnology && fieldTechnologyLocations.includes(type)) {
-    routerTabs.push({
-      label: t('FARM_MAP.TAB.FIELD_TECHNOLOGY'),
-      path: match.url.replace(currentTab, 'field_technology'),
-    });
-  }
-  if (readingsLocations.includes(type)) {
-    routerTabs.push({
-      label: t('SENSOR.VIEW_HEADER.READINGS'),
-      path: match.url.replace(currentTab, 'readings'),
-    });
-  }
-
-  return routerTabs;
 }

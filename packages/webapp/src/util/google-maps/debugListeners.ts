04aebd7c6ae09f7ad6237b6bbd0ba6110baa50ed
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-04-17 11:30:25 -0700

    LF-4797 Add debugging functions

---
+++ packages/webapp/src/util/google-maps/debugListeners.ts
@@ -0,0 +1,105 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Geometry, instanceTypes } from './cleanupListeners';
+
+const userEvents = ['click', 'mouseover', 'mouseout'];
+const mapEvents = [
+  'set_at',
+  'insert_at',
+  'polygoncomplete',
+  'polylinecomplete',
+  'overlaycomplete',
+  'zoom_changed',
+  'center_changed',
+  'bounds_changed',
+];
+
+const eventTypes = [...userEvents, ...mapEvents];
+
+/*---------------------------------
+ Use the helper tools below to evaluate the listeners on your Google Map components
+
+ These were written for <LocationPicker /> and subsequently extended for <Map /> to decide which objects needed to be cleared
+
+ E.g. in <LocationPicker />
+
+      if (gMaps && geometriesRef.current) {
+        logGeometryListeners(geometriesRef.current, gMaps);
+      }
+      if (gMaps && markerClusterRef.current) {
+        logInstanceListeners(markerClusterRef.current, gMaps, 'markerCluster');
+      }
+      if (gMaps && pinMarkerRef.current) {
+        logInstanceListeners(pinMarkerRef.current, gMaps, 'pinMarker');
+      }
+
+ ---------------------------------------------*/
+
+/**
+ * Logs the event listeners on each Geometry object contained within the provided collection.
+ *
+ * @param {{ [key: string]: Geometry }} geometries - An object mapping keys to Geometry objects.
+ * @param {typeof google.maps} gMaps - The Google Maps API reference.
+ */
+
+export function logGeometryListeners(
+  geometries: Record<string, Geometry | Geometry[]>,
+  gMaps: typeof google.maps,
+) {
+  Object.values(geometries).forEach((entry) => {
+    const items = Array.isArray(entry) ? entry : [entry];
+
+    items.forEach((geometry) => {
+      const name = geometry.location?.name || geometry.location_name;
+      const id = geometry.location?.location_id || geometry.location_id;
+      const label = name || id;
+
+      console.group(`\nEvaluating geometry "${label}"`);
+
+      instanceTypes.forEach((instanceKey) => {
+        if (geometry[instanceKey]) {
+          console.log(`- Instance: ${instanceKey}`);
+
+          userEvents.forEach((eventName) => {
+            const hasListener = gMaps.event.hasListeners(geometry[instanceKey]!, eventName);
+            console.log(`   - ${eventName}: ${hasListener}`);
+          });
+        }
+      });
+      console.groupEnd();
+    });
+  });
+}
+
+/**
+ * Logs the event listeners attached to a specific instance object
+ *
+ * @param {google.maps.MVCObject} mapInstance - The map instance for which to log listeners.
+ * @param {typeof google.maps} gMaps - The Google Maps API reference.
+ * @param {string} [label='map instance'] - An optional label for identifying the instance.
+ */
+export function logInstanceListeners(
+  mapInstance: google.maps.MVCObject,
+  gMaps: typeof google.maps,
+  label: string = 'map instance',
+) {
+  console.log(`\nEvaluating ${label} listeners:`);
+
+  eventTypes.forEach((eventName) => {
+    const hasListener = gMaps.event.hasListeners(mapInstance, eventName);
+    console.log(`- ${eventName}: ${hasListener}`);
+  });
+}

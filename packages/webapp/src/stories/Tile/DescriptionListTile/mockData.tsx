0f5ccf4de62e63a8d5b19708c6ee25b2ec1e6baa
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-04-23 14:04:31 -0700

    LF-4790 Cleanup stories

--- packages/webapp/src/stories/Tile/DescriptionListTile/mockData.tsx
+++ packages/webapp/src/stories/Tile/DescriptionListTile/mockData.tsx
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 
-import { ReactElement } from 'react';
+import { IconAndText } from '../../../components/IPDetailKPI/util';
 import { ReactComponent as WindIcon } from '../../../assets/images/weather/wind.svg';
 import { ReactComponent as RainfallIcon } from '../../../assets/images/weather/droplets.svg';
 import { ReactComponent as ThemometerWarmIcon } from '../../../assets/images/weather/thermometer-warm.svg';
@@ -21,6 +21,7 @@ import WeatherIcon from '../../../components/WeatherBoard/WeatherIcon';
 import PivotIcon from '../../../assets/images/irrigation/pivot-icon.svg';
 import ClockIcon from '../../../assets/images/clock-stopwatch.svg';
 import styles from './styles.module.scss';
+import ipKPIStyle from '../../../components/IPDetailKPI/styles.module.scss';
 
 export const mockTextData = [
   { label: 'Temperature', data: '23°C' },
@@ -32,20 +33,11 @@ export const mockTextData = [
   { label: 'Rainfall rate', data: '2mm/h' },
 ];
 
-const IconAndData = ({ Icon, data }: { Icon: ReactElement; data: string }) => {
-  return (
-    <span className={styles.iconAndData}>
-      {Icon}
-      {data}
-    </span>
-  );
-};
-
 export const dataWithIconData = {
   label: 'Temperature',
   data: (
-    <div className={styles.dataWithIcon}>
-      <IconAndData Icon={<ThemometerWarmIcon />} data="23°C" />
+    <div className={ipKPIStyle.dataWithIcon}>
+      <IconAndText Icon={<ThemometerWarmIcon />} text="23°C" />
     </div>
   ),
 };
@@ -56,23 +48,23 @@ export const iPData = [
   {
     label: 'Temperature',
     data: (
-      <span className={styles.temperatureData}>
-        <IconAndData Icon={<ThemometerWarmIcon />} data="23°C" />
+      <span className={ipKPIStyle.temperatureData}>
+        <IconAndText Icon={<ThemometerWarmIcon />} text="23°C" />
         <WeatherIcon name="wi-day-sunny" />
       </span>
     ),
   },
   {
     label: 'Wind speed',
-    data: <IconAndData Icon={<WindIcon />} data="8 km/h" />,
+    data: <IconAndText Icon={<WindIcon />} text="8 km/h" />,
   },
   {
     label: 'Cumulative rainfall',
-    data: <IconAndData Icon={<RainfallIcon />} data="20mm" />,
+    data: <IconAndText Icon={<RainfallIcon />} text="20mm" />,
   },
   {
     label: 'ET Rate',
-    data: <IconAndData Icon={<WindIcon />} data="5mm / day" />,
+    data: <IconAndText Icon={<WindIcon />} text="5mm / day" />,
   },
   { label: 'Estimated time', data: '14h', iconURL: ClockIcon },
   {

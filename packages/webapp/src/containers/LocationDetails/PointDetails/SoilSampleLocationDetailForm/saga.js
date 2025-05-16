8edf9ba7611ffb7c3252982d0bc59520bf13673a
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-05-16 13:04:48 -0700

    LF-4831 Add routes, container, and saga for view/edit/delete

--- packages/webapp/src/containers/LocationDetails/PointDetails/SoilSampleLocationDetailForm/saga.js
+++ packages/webapp/src/containers/LocationDetails/PointDetails/SoilSampleLocationDetailForm/saga.js
@@ -20,6 +20,8 @@ import { axios, getHeader } from '../../../saga';
 import apiConfig from '../../../../apiConfig';
 import { loginSelector } from '../../../userFarmSlice';
 import {
+  deleteSoilSampleLocationSuccess,
+  editSoilSampleLocationSuccess,
   getLocationObjectFromSoilSampleLocation,
   postSoilSampleLocationSuccess,
 } from '../../../soilSampleLocationSlice';
@@ -70,6 +72,91 @@ export function* postSoilSampleLocationLocationSaga({ payload: data }) {
   }
 }
 
+export const editSoilSampleLocationLocation = createAction(`editSoilSampleLocationLocationSaga`);
+
+export function* editSoilSampleLocationLocationSaga({ payload: data }) {
+  const { formData, location_id, figure_id } = data;
+  const { locationURL } = apiConfig;
+  let { user_id, farm_id } = yield select(loginSelector);
+  formData.farm_id = farm_id;
+  const header = getHeader(user_id, farm_id);
+  const locationObject = getLocationObjectFromSoilSampleLocation({
+    ...formData,
+    location_id,
+    figure_id,
+  });
+
+  try {
+    const result = yield call(
+      axios.put,
+      `${locationURL}/${locationObject.figure.type}/${location_id}`,
+      locationObject,
+      header,
+    );
+    yield put(editSoilSampleLocationSuccess(result.data));
+
+    yield put(
+      setSuccessMessage([
+        i18n.t('FARM_MAP.MAP_FILTER.SOIL_SAMPLE_LOCATION'),
+        i18n.t('message:MAP.SUCCESS_PATCH'),
+      ]),
+    );
+    yield put(canShowSuccessHeader(true));
+    history.push({ pathname: '/map' });
+  } catch (e) {
+    history.push(
+      {
+        pathname: history.location.pathname,
+      },
+      {
+        error: `${i18n.t('message:MAP.FAIL_PATCH')} ${i18n
+          .t('FARM_MAP.MAP_FILTER.SOIL_SAMPLE_LOCATION')
+          .toLowerCase()}`,
+      },
+    );
+    console.log(e);
+  }
+}
+
+export const deleteSoilSampleLocationLocation = createAction(
+  `deleteSoilSampleLocationLocationSaga`,
+);
+
+export function* deleteSoilSampleLocationLocationSaga({ payload: data }) {
+  const { location_id } = data;
+  const { locationURL } = apiConfig;
+  let { user_id, farm_id } = yield select(loginSelector);
+  const header = getHeader(user_id, farm_id);
+
+  try {
+    yield call(axios.delete, `${locationURL}/${location_id}`, header);
+    yield put(setMapCache({ maxZoom: undefined, farm_id }));
+    yield put(deleteSoilSampleLocationSuccess(location_id));
+    yield put(
+      setSuccessMessage([
+        i18n.t('FARM_MAP.MAP_FILTER.SOIL_SAMPLE_LOCATION'),
+        i18n.t('message:MAP.SUCCESS_DELETE'),
+      ]),
+    );
+    yield put(canShowSuccessHeader(true));
+    history.push({ pathname: '/map' });
+  } catch (e) {
+    history.push(
+      {
+        pathname: history.location.pathname,
+      },
+      {
+        error: {
+          retire: true,
+        },
+      },
+    );
+    console.log(e);
+  }
+}
+
 export default function* soilSampleLocationLocationSaga() {
   yield takeLeading(postSoilSampleLocationLocation.type, postSoilSampleLocationLocationSaga);
+  yield takeLeading(editSoilSampleLocationLocation.type, editSoilSampleLocationLocationSaga);
+  yield takeLeading(deleteSoilSampleLocationLocation.type, deleteSoilSampleLocationLocationSaga);
 }

61a578ca1831c9bb07ec203696c742f8aeb000b9
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-05-21 11:01:42 -0700

    LF-4709 Remove old sensors from container's saga and locationSlice * adjust taskEntitiesSelector and getLocationsSuccessSaga logic not to crash

--- packages/webapp/src/containers/saga.js
+++ packages/webapp/src/containers/saga.js
@@ -124,7 +124,6 @@ import {
   onLoadingRowMethodFail,
   onLoadingRowMethodStart,
 } from './rowMethodSlice';
-import { getSensorSuccess, onLoadingSensorFail, onLoadingSensorStart } from './sensorSlice';
 import {
   getSurfaceWatersSuccess,
   onLoadingSurfaceWaterFail,
@@ -339,7 +338,6 @@ export function* onLoadingLocationStartSaga() {
   yield put(onLoadingFenceStart());
   yield put(onLoadingGateStart());
   yield put(onLoadingWaterValveStart());
-  yield put(onLoadingSensorStart());
 }
 
 export const getLocations = createAction('getLocationsSaga');
@@ -364,7 +362,8 @@ export function* getLocationsSuccessSaga({ payload: locations }) {
     {},
   );
   for (const location of locations) {
-    locations_by_figure_type[location.figure.type].push(location);
+    // TODO: Remove "?" once sensors are no longer included in the backend response
+    locations_by_figure_type[location.figure.type]?.push(location);
   }
   for (const figure_type in figureTypeActionMap) {
     try {
@@ -395,7 +394,6 @@ const figureTypeActionMap = {
     success: getSoilSampleLocationsSuccess,
     fail: onLoadingSoilSampleLocationFail,
   },
-  sensor: { success: getSensorSuccess, fail: onLoadingSensorFail },
 };
 
 export function* onLoadingManagementPlanAndPlantingMethodStartSaga() {

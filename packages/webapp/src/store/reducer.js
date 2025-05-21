e9d8c5094237b279743ded7f5a303060d7e39a8f
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-05-21 09:46:36 -0700

    LF-4709 Remove bulkSensorsReadingsSlice

--- packages/webapp/src/store/reducer.js
+++ packages/webapp/src/store/reducer.js
@@ -66,7 +66,6 @@ import mapCacheReducer from '../containers/Map/mapCacheSlice';
 import mapSensorReducer from '../containers/Map/mapSensorSlice';
 import sensorReadingTypesReducer from '../containers/sensorReadingTypesSlice';
 import showedSpotlightReducer from '../containers/showedSpotlightSlice';
-import bulkSensorsReadingsReducer from '../containers/bulkSensorReadingsSlice';
 import hookFormPersistReducer from '../containers/hooks/useHookFormPersist/hookFormPersistSlice';
 import offlineDetectorReducer from '../containers/hooks/useOfflineDetector/offlineDetectorSlice';
 import filterReducer from '../containers/filterSlice';
@@ -181,7 +180,6 @@ const entitiesReducer = combineReducers({
   waterValveReducer,
   sensorReducer,
   showedSpotlightReducer,
-  bulkSensorsReadingsReducer,
   managementPlanReducer,
   cropManagementPlanReducer,
   plantingManagementPlanReducer,

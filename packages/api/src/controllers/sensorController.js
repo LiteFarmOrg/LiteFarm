9b0fc73b81f6a96dd3b620ed8ed9aa999c493728
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-05-23 10:47:56 -0700

    LF-4709 Remove sensorController.addReading and associated constant

--- packages/api/src/controllers/sensorController.js
+++ packages/api/src/controllers/sensorController.js
@@ -13,15 +13,7 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 
-import SensorModel from '../models/sensorModel.js';
-import PartnerReadingTypeModel from '../models/PartnerReadingTypeModel.js';
-import {
-  ENSEMBLE_UNITS_MAPPING_WEBHOOK,
-  getEnsembleSensors,
-  getEnsembleSensorReadings,
-} from '../util/ensemble.js';
-import { databaseUnit } from '../util/unit.js';
-import knex from '../util/knex.js';
+import { getEnsembleSensors, getEnsembleSensorReadings } from '../util/ensemble.js';
 
 const sensorController = {
   async getSensors(req, res) {
@@ -60,93 +52,6 @@ const sensorController = {
 
       return res.status(status).json({
         error: error.message || error,
-      });
-    }
-  },
-
-  // Note : API is called at the ensemble backend. the ensemble backend sends the same status code when we add sensors to the farm (register sensor API).
-  // when we register some sensors, the add readings API is called and the same status code is passed in the response of register sensors (i.e 200).
-  // For example, if we make that as 400 then the registered sensor API of the ensemble will send back 400 and the add sensor API will fail.
-  // therefore, The error status code should be 200.
-  async addReading(req, res) {
-    if (!Object.keys(req.body).length) {
-      return res.status(200).send('No sensor readings posted');
-    }
-    try {
-      const infoBody = [];
-      const partnerId = parseInt(req.params.partner_id);
-      const farmId = req.params.farm_id || '';
-      if (!farmId.length) return res.status(400).send('farm id not found');
-      const { rows: partnerSensorReadingTypes = [] } =
-        await PartnerReadingTypeModel.getPartnerReadingTypeByPartnerId(partnerId);
-      if (!partnerSensorReadingTypes.length)
-        return res.status(400).send('partner not registered with the Litefarm');
-      const readingTypeValidation = [];
-      for (const sensor of Object.keys(req.body)) {
-        const sensorData = req.body[sensor].data;
-        let { rows: corresponding_sensor = [] } = await SensorModel.getLocationIdForSensorReadings(
-          sensor,
-          partnerId,
-          farmId,
-        );
-        if (!corresponding_sensor.length) return res.status(400).send('sensor id not found');
-        corresponding_sensor = corresponding_sensor[0];
-        for (const sensorInfo of sensorData) {
-          const readingType = sensorInfo.parameter_category.toLowerCase().replaceAll(' ', '_');
-          const isPartnerReadingTypePresent = partnerSensorReadingTypes.some(
-            (partnerSensorReadingType) => partnerSensorReadingType.readable_value === readingType,
-          );
-          if (!isPartnerReadingTypePresent) {
-            readingTypeValidation.push(
-              `Invalid reading type ${sensorInfo.parameter_category} of ${sensor} sensor.`,
-            );
-            continue;
-          }
-          // Reconcile incoming units with stored as units and conversion function keys
-          const system = ENSEMBLE_UNITS_MAPPING_WEBHOOK[sensorInfo.unit]?.system;
-          const unit = ENSEMBLE_UNITS_MAPPING_WEBHOOK[sensorInfo.unit]?.conversionKey;
-          const readingTypeStoredAsUnit = databaseUnit[readingType] ?? undefined;
-          const isStoredAsUnit = unit == readingTypeStoredAsUnit;
-
-          if (sensorInfo.values.length < sensorInfo.timestamps.length) {
-            return res.status(400).send('sensor values and timestamps are not in sync');
-          }
-          if (!system || !unit || !readingTypeStoredAsUnit || !isStoredAsUnit) {
-            return res.status(400).send('provided units are not supported');
-          }
-          for (let k = 0; k < sensorInfo.values.length; ++k) {
-            infoBody.push({
-              read_time: sensorInfo.timestamps[k] || '',
-              location_id: corresponding_sensor.location_id,
-              value: sensorInfo.values[k],
-              reading_type: readingType,
-              valid: sensorInfo.validated[k] || false,
-              unit,
-            });
-          }
-        }
-      }
-
-      if (infoBody.length === 0) {
-        return res.status(200).json({
-          error: 'No records of sensor readings added to the Litefarm.',
-          readingTypeValidationError: readingTypeValidation.length
-            ? readingTypeValidation
-            : undefined,
-        });
-      } else {
-        const chunkSize = 999;
-        const result = await knex.batchInsert('sensor_reading', infoBody, chunkSize).returning('*');
-        return res.status(200).json({
-          sensorsAdded: result,
-          readingTypeValidationError: readingTypeValidation.length
-            ? readingTypeValidation
-            : undefined,
-        });
-      }
-    } catch (error) {
-      return res.status(200).json({
-        error,
       });
     }
   },

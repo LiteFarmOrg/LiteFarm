63f0eaef4db4502847dd760645b98e1cff3c08f9
Duncan-Brain <dbrain@litefarm.org>
2025-05-26 14:02:35 -0400

    LF-4840 Remove unuse style and add licence

--- packages/webapp/src/components/Modals/NoSoilSampleLocationsModal/index.jsx
+++ packages/webapp/src/components/Modals/NoSoilSampleLocationsModal/index.jsx
@@ -1,7 +1,21 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <<https://www.gnu.org/licenses/>.>
+ */
+
 import ModalComponent from '../ModalComponent/v2';
 import { useTranslation } from 'react-i18next';
 import Button from '../../Form/Button';
-import styles from './styles.module.scss';
 
 export function NoSoilSampleLocationsModal({ dismissModal, goToMap }) {
   const { t } = useTranslation();
@@ -15,7 +29,6 @@ export function NoSoilSampleLocationsModal({ dismissModal, goToMap }) {
         <>
           <Button
             data-cy="tasks-noSoilSampleLocationCancel"
-            className={styles.button}
             onClick={dismissModal}
             color={'secondary'}
             type={'button'}

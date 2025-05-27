c4bf47c69afd3e0213b0a7b4daa2a34f663b1337
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-05-27 16:24:08 -0700

    LF-4788 Copy relevant parts of frontend geoUtils.ts to backend

---
+++ packages/api/src/util/geoUtils.ts
@@ -0,0 +1,65 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { polygon } from '@turf/helpers';
+import { centroid } from '@turf/centroid';
+
+export interface Point {
+  lat: number;
+  lng: number;
+}
+
+// turf expects [lng, lat] for all points and polygon coordinates
+type TurfPoint = [lng: number, lat: number];
+
+export const getCentroidOfPolygon = (gridPoints: Point[]): Point | null => {
+  const coordinates = gridPoints.map((p: Point): TurfPoint => [p.lng, p.lat]);
+  const areaPolygon = safeCreatePolygon(coordinates);
+
+  if (!areaPolygon) {
+    return areaPolygon;
+  }
+
+  try {
+    const centroidFeature = centroid(areaPolygon);
+    const [lng, lat] = centroidFeature.geometry.coordinates;
+    return { lat, lng };
+  } catch (err) {
+    console.error('Error computing centroid:', err);
+    return null;
+  }
+};
+
+// turf requires that all polygons be closed (first and last point equivalent)
+const closePolygon = (coordinates: TurfPoint[]): TurfPoint[] => {
+  const first = coordinates[0];
+  const last = coordinates[coordinates.length - 1];
+
+  const isClosed = first[0] === last[0] && first[1] === last[1];
+
+  return isClosed ? coordinates : [...coordinates, first];
+};
+
+const safeCreatePolygon = (coordinates: TurfPoint[]): ReturnType<typeof polygon> | null => {
+  try {
+    // for necessity of wrapping coordinates in an array, see
+    // https://github.com/Turfjs/turf/issues/1583
+    return polygon([closePolygon(coordinates)]);
+  } catch (error) {
+    // turf will throw an error if the polygon is invalid
+    console.error(error);
+    return null;
+  }
+};

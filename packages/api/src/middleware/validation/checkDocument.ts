4cdbd3a9a81df88f96c11e352ed92ab7ccc00478
Duncan-Brain <dbrain@litefarm.org>
2025-06-03 12:10:03 -0400

    LF-4836 Extract document middleware for reuse

---
+++ packages/api/src/middleware/validation/checkDocument.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from 'express';
+import { customError, LiteFarmCustomError } from '../../util/customErrors.js';
+import { DocumentWithFiles } from '../../models/types.js';
+
+export function validateFilesLength(document: DocumentWithFiles) {
+  if (!Array.isArray(document.files) || document.files?.length <= 0) {
+    throw customError('Can not create document without file links');
+  }
+}
+
+export function checkCreateDocument() {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const document = req.body;
+      if (!document) {
+        throw customError('Document not found');
+      }
+      validateFilesLength(document);
+      next();
+    } catch (error: unknown) {
+      console.error(error);
+      if (error instanceof LiteFarmCustomError) {
+        return error.body
+          ? res.status(error.code).json({ ...error.body, message: error.message })
+          : res.status(error.code).send(error.message);
+      }
+      return res.status(500).json({
+        error,
+      });
+    }
+  };
+}
+
+export function checkUpdateDocument() {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const document = req.body;
+      if (!document) {
+        throw customError('Document not found');
+      }
+      validateFilesLength(document);
+      next();
+    } catch (error: unknown) {
+      console.error(error);
+      if (error instanceof LiteFarmCustomError) {
+        return error.body
+          ? res.status(error.code).json({ ...error.body, message: error.message })
+          : res.status(error.code).send(error.message);
+      }
+      return res.status(500).json({
+        error,
+      });
+    }
+  };
+}

a81ae60f7053dd8ce3e63fe7f575dbd645484d86
Duncan-Brain <dbrain@litefarm.org>
2025-06-06 18:56:33 -0400

    LF-4836 Fix migration

--- packages/api/db/migration/20250523133726_create_task_document_join_table.js
+++ packages/api/db/migration/20250523133726_create_task_document_join_table.js
@@ -19,9 +19,9 @@
  */
 export const up = async function (knex) {
   await knex.schema.createTable('task_document', (table) => {
-    table.integer('task_id').primary().references('task_id').inTable('task');
-    table.uuid('document_id').references('document_id').inTable('document');
-    table.unique(['task_id', 'document_id']);
+    table.integer('task_id').references('task_id').inTable('task').notNullable();
+    table.uuid('document_id').references('document_id').inTable('document').notNullable();
+    table.unique(['task_id', 'document_id']).primary();
   });
 };
 

abd79fea8938e93fda4447fcad22908457812aa5
Duncan-Brain <dbrain@litefarm.org>
2025-06-05 20:44:10 -0400

    Fix migration for testing database

--- packages/api/db/migration/20250527173006_cleanup_previous_sensors_implementation.js
+++ packages/api/db/migration/20250527173006_cleanup_previous_sensors_implementation.js
@@ -180,9 +180,14 @@ export const up = async (knex) => {
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
   });
 
-  await knex('migration_deletion_logs').insert(
-    rowsToLog.map((row) => ({ migration_name: 'cleanup_previous_sensors_implementation', ...row })),
-  );
+  if (rowsToLog.length) {
+    await knex('migration_deletion_logs').insert(
+      rowsToLog.map((row) => ({
+        migration_name: 'cleanup_previous_sensors_implementation',
+        ...row,
+      })),
+    );
+  }
 
   // Remove notifications for sensors
   const sensorNotifications = await knex('notification').whereRaw(

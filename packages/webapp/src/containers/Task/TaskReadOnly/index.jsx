d25d32e77c8e80b1de5bd28cbda0b7b7ddf44c78
Duncan-Brain <dbrain@litefarm.org>
2025-06-06 20:46:56 -0400

    LF-4836 Add structured clone to use task documents in files

--- packages/webapp/src/containers/Task/TaskReadOnly/index.jsx
+++ packages/webapp/src/containers/Task/TaskReadOnly/index.jsx
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details, see <<https://www.gnu.org/licenses/>.>
  */
 
-import { React, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PureTaskReadOnly from '../../../components/Task/TaskReadOnly';
 import {
@@ -128,11 +128,10 @@ function TaskReadOnly({ history, match, location }) {
     files.push({ url: externalIrrigationPrescription.prescription.vriData.file_url });
   }
   if (task.documents?.length) {
-    files.push(
-      ...task.documents.reduce((acc, cv) => {
-        return [...acc, ...cv.files];
-      }, []),
-    );
+    const documentFiles = structuredClone(task.documents).reduce((acc, cv) => {
+      return [...acc, ...cv.files];
+    }, []);
+    files.push(...documentFiles);
   }
 
   const users = useSelector(userFarmsByFarmSelector);

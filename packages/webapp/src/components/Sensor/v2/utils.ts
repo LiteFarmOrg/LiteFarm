edd7a46f548801867df3e24ed32823c01b3ab26c
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-06-10 14:37:19 -0700

    LF-4872 Create helper function to format display names

---
+++ packages/webapp/src/components/Sensor/v2/utils.ts
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+export function createSensorsDisplayName({
+  system,
+  label,
+  fallback,
+}: {
+  system?: string;
+  label?: string;
+  fallback?: string;
+}) {
+  return [system, label].filter(Boolean).join(' | ') || fallback;
+}

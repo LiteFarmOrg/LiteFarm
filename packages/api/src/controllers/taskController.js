34f3a02682f34a9afda96bbdc4adfbfddc671aa1
Duncan-Brain <dbrain@litefarm.org>
2025-06-10 13:57:58 -0400

    LF-4836 Finish removing taskDocuments relation from model

--- packages/api/src/controllers/taskController.js
+++ packages/api/src/controllers/taskController.js
@@ -218,7 +218,7 @@ async function updateTaskWithCompletedData(
 
     case 'soil_sample_task': {
       const noInsert = [
-        ...nonModifiable.filter((asset) => !['documents', 'taskDocuments'].includes(asset)),
+        ...nonModifiable.filter((asset) => !['documents'].includes(asset)),
         'soil_sample_task',
       ];
       const task = await TaskModel.query(trx)
@@ -1074,14 +1074,9 @@ const taskController = {
 
 function getNonModifiable(asset) {
   const nonModifiableAssets = typesOfTask.filter((a) => a !== asset);
-  return [
-    'createdByUser',
-    'updatedByUser',
-    'location',
-    'management_plan',
-    'documents',
-    'taskDocuments',
-  ].concat(nonModifiableAssets);
+  return ['createdByUser', 'updatedByUser', 'location', 'management_plan', 'documents'].concat(
+    nonModifiableAssets,
+  );
 }
 
 function removeNullTypes(task) {

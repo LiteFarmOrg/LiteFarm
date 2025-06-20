5463da79ed0f89f8ed3b4b5e5a52f3918bb10d1d
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-06-20 15:52:09 -0700

    LF-4788 Clean up types

--- packages/webapp/src/components/IrrigationPrescription/types.ts
+++ packages/webapp/src/components/IrrigationPrescription/types.ts
@@ -14,7 +14,6 @@
  */
 
 import { Point } from '../../util/geoUtils';
-import { SensorReadingTypeUnits } from '../../store/api/types';
 
 export interface UriPrescriptionData {
   soil_moisture_deficit: number;
@@ -28,51 +27,4 @@ export type VriPrescriptionData = UriPrescriptionData & {
 
 export type IrrigationPrescriptionTableInfo = UriPrescriptionData & {
   id: number;
-};
-
-// TODO LF-4788: This is the backend-received data structure and belongs in the RTK Query slice types once that endpoint is implemented
-export type IrrigationPrescription = {
-  id: number;
-
-  location_id: string;
-  management_plan_id: number | null;
-  recommended_start_datetime: string; // ISO string
-
-  pivot: {
-    center: { lat: number; lng: number };
-    radius: number; // in meters
-  };
-
-  metadata: {
-    // metadata = external sources of information used to generate the irrigation prescription
-    weather_forecast: {
-      temperature: number;
-      temperature_unit: SensorReadingTypeUnits;
-      wind_speed: number;
-      wind_speed_unit: SensorReadingTypeUnits;
-      cumulative_rainfall: number;
-      cumulative_rainfall_unit: SensorReadingTypeUnits;
-      et_rate: number;
-      et_rate_unit: string;
-      weather_icon_code: string; // '02d', '50n', OpenWeatherMap icon code if available
-    };
-  };
-
-  estimated_time: number;
-  estimated_time_unit: string;
-
-  // calculated by the backend
-  estimated_water_consumption: number;
-  estimated_water_consumption_unit: string;
-
-  // TODO: confirm with product if we are indeed getting only URI or VRI data per prescription
-  prescription:
-    | { uriData: UriPrescriptionData; vriData?: never }
-    | {
-        vriData: {
-          zones: VriPrescriptionData[];
-          file_url: string;
-        };
-        uriData?: never;
-      };
 };

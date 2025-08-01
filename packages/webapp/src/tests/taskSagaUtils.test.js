a3691531e033a45835e0661a756ca339e2625386
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-08-01 12:52:15 -0700

    LF-4228 Add tests for postTaskRequestHasNewProduct

---
+++ packages/webapp/src/tests/taskSagaUtils.test.js
@@ -0,0 +1,51 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { expect, describe, test } from 'vitest';
+import { postTaskRequestHasNewProduct } from '../containers/Task/sagaUtils';
+
+const createFakeProduct = (productId, name) => {
+  return { product: { product_id: productId, name } };
+};
+
+describe('Task saga utils test', () => {
+  describe('postTaskRequestHasNewProduct', () => {
+    test('returns true when product has a name but no productId', () => {
+      expect(
+        postTaskRequestHasNewProduct(
+          { cleaning_task: createFakeProduct(undefined, 'New product') },
+          'CLEANING_TASK',
+        ),
+      ).toBe(true);
+
+      expect(
+        postTaskRequestHasNewProduct(
+          { pest_control_task: createFakeProduct(undefined, 'New product') },
+          'PEST_CONTROL_TASK',
+        ),
+      ).toBe(true);
+    });
+
+    test('returns false when product has a productId', () => {
+      expect(
+        postTaskRequestHasNewProduct({ cleaning_task: createFakeProduct('xxxx') }, 'CLEANING_TASK'),
+      ).toBe(false);
+    });
+
+    test('returns false when the task has no product', () => {
+      expect(postTaskRequestHasNewProduct({ irrigation_task: {} }, 'IRRIGATION_TASK')).toBe(false);
+    });
+  });
+});

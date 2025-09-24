ca074135601e2625c4085c62239cd9058b382972
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-09-24 11:30:02 -0700

    LF-4962 Add constants file

---
+++ packages/webapp/src/containers/ProductInventory/ProductForm/constants.ts
@@ -0,0 +1,73 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+import SoilAmendmentProductForm from './SoilAmendmentProductForm';
+import { TASK_TYPES } from '../../Task/constants';
+import { PRODUCT_FIELD_NAMES } from '../../../components/Task/AddSoilAmendmentProducts/types';
+import { ElementalUnit, MolecularCompoundsUnit } from '../../../store/api/types';
+
+export const productFormMap = {
+  [TASK_TYPES.SOIL_AMENDMENT]: SoilAmendmentProductForm,
+};
+
+const {
+  PRODUCT_ID,
+  NAME,
+  FERTILISER_TYPE_ID,
+  MOISTURE_CONTENT,
+  DRY_MATTER_CONTENT,
+  SUPPLIER,
+  PERMITTED,
+  COMPOSITION,
+  ELEMENTAL_UNIT,
+  N,
+  P,
+  K,
+  CA,
+  MG,
+  S,
+  CU,
+  MN,
+  B,
+  AMMONIUM,
+  NITRATE,
+  MOLECULAR_COMPOUNDS_UNIT,
+} = PRODUCT_FIELD_NAMES;
+
+export const productDefaultValues = {
+  [TASK_TYPES.SOIL_AMENDMENT]: {
+    [PRODUCT_ID]: '',
+    [NAME]: '',
+    [SUPPLIER]: '',
+    [PERMITTED]: undefined,
+    [FERTILISER_TYPE_ID]: undefined,
+    [MOISTURE_CONTENT]: NaN,
+    [DRY_MATTER_CONTENT]: undefined,
+    [COMPOSITION]: {
+      [ELEMENTAL_UNIT]: ElementalUnit.RATIO,
+      [N]: NaN,
+      [P]: NaN,
+      [K]: NaN,
+      [CA]: NaN,
+      [MG]: NaN,
+      [S]: NaN,
+      [CU]: NaN,
+      [MN]: NaN,
+      [B]: NaN,
+    },
+    [AMMONIUM]: NaN,
+    [NITRATE]: NaN,
+    [MOLECULAR_COMPOUNDS_UNIT]: MolecularCompoundsUnit.PPM,
+  },
+};

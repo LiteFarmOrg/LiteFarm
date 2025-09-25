d64abc276a46c9bd333620ffd3ffb8a71f079ce1
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-09-25 13:25:38 -0700

    LF-4969 Fix flattened spelling

--- packages/api/src/controllers/productController.js
+++ packages/api/src/controllers/productController.js
@@ -55,7 +55,7 @@ const productController = {
             product_farm: [{ farm_id, supplier, on_permitted_substances_list }],
           });
 
-        const flattenenedResult = await ProductModel.query(trx)
+        const flattenedResult = await ProductModel.query(trx)
           .findById(inserted.product_id)
           .joinRelated('product_farm')
           .where('product_farm.farm_id', farm_id)
@@ -64,7 +64,7 @@ const productController = {
           .first();
 
         await trx.commit();
-        res.status(201).send(flattenenedResult);
+        res.status(201).send(flattenedResult);
       } catch (error) {
         await handleObjectionError(error, res, trx);
       }

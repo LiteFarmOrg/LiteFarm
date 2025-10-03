75b529eac470a63911c7d3c61c90c4e5f25989a3
mrestivill <mrestivill@protonmail.com>
2025-10-03 15:18:41 +0200

    fix: local language translation

--- packages/webapp/src/hooks/useLanguageOptions.ts
+++ packages/webapp/src/hooks/useLanguageOptions.ts
@@ -19,7 +19,7 @@ const supportedLanguages = [
   ['de', 'Deutsch'],
   ['fr', 'Français'],
   ['pt', 'Português'],
-  ['ca', 'Catalan'],
+  ['ca', 'Català'],
   ['hi', 'हिंदी'],
   ['ml', 'മലയാളം'],
   ['pa', 'ਪੰਜਾਬੀ'],

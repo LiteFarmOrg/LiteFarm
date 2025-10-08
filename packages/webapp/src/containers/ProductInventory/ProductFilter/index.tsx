334247fd058722b9e47d1447f2da79ad697715d5
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-10-08 12:58:38 -0700

    LF-4967 Extract a common FilterDrawerContainer from the Animal and Inventory filters

--- packages/webapp/src/containers/ProductInventory/ProductFilter/index.tsx
+++ packages/webapp/src/containers/ProductInventory/ProductFilter/index.tsx
@@ -13,70 +13,31 @@
  *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
  */
 
-import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
-import styles from './styles.module.scss';
-import Drawer from '../../../components/Drawer';
-import FilterButton from '../../../components/Filter/FilterButton';
-import Button from '../../../components/Form/Button';
 import {
   setInventoryFilter,
   inventoryFilterSelector,
   initialInventoryFilter,
 } from '../../filterSlice';
+import FilterDrawerContainer from '../../Filter/FilterDrawerContainer';
 import ProductInventoryFilterContent from '../../Filter/ProductInventory/';
 import type { InventoryFilterKeys } from '../../Filter/ProductInventory/types';
-import type { ReduxFilterEntity } from '../../Filter/types';
 
 const ProductInventoryFilter = ({ isFilterActive }: { isFilterActive: boolean }) => {
-  const { t } = useTranslation();
-  const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const [isDirty, setIsDirty] = useState(false);
-  const [tempProductInventoryFilter, setTempProductInventoryFilter] =
-    useState<ReduxFilterEntity<InventoryFilterKeys>>(initialInventoryFilter);
-
-  const productInventoryFilter = useSelector(inventoryFilterSelector);
-  const dispatch = useDispatch();
-
-  const handleApply = () => {
-    dispatch(setInventoryFilter(tempProductInventoryFilter));
-    setIsFilterOpen(false);
-    setIsDirty(false);
-  };
-
-  useEffect(() => {
-    setTempProductInventoryFilter(productInventoryFilter);
-  }, [productInventoryFilter]);
+  const { t } = useTranslation(['filter']);
 
   return (
-    <div className={styles.filterButton}>
-      <FilterButton onClick={() => setIsFilterOpen(true)} isFilterActive={isFilterActive} />
-      <Drawer
-        isOpen={isFilterOpen}
-        title={t('filter:INVENTORY.TITLE')}
-        onClose={() => setIsFilterOpen(false)}
-        buttonGroup={
-          <Button fullLength onClick={handleApply} color={'primary'} disabled={!isDirty}>
-            {t('common:APPLY')}
-          </Button>
-        }
-        classes={{
-          drawerBackdrop: styles.drawerBackdrop,
-        }}
-      >
-        <ProductInventoryFilterContent
-          inventoryFilter={productInventoryFilter}
-          onChange={(filterKey, filterState) => {
-            !isDirty && setIsDirty(true);
-            setTempProductInventoryFilter({
-              ...tempProductInventoryFilter,
-              [filterKey as string]: filterState,
-            });
-          }}
-        />
-      </Drawer>
-    </div>
+    <FilterDrawerContainer<InventoryFilterKeys>
+      isFilterActive={isFilterActive}
+      filterSelector={inventoryFilterSelector}
+      setFilterAction={setInventoryFilter}
+      initialFilter={initialInventoryFilter}
+      drawerTitle={t('filter:INVENTORY.TITLE')}
+    >
+      {({ filter, onChange }) => (
+        <ProductInventoryFilterContent inventoryFilter={filter} onChange={onChange} />
+      )}
+    </FilterDrawerContainer>
   );
 };
 

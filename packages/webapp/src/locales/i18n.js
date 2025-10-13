65f24feddfe821105415a5273d2c116bd5bb6b04
Duncan-Brain <dbrain@litefarm.org>
2025-10-13 09:20:29 -0400

    LF-4950 Swap deprecated xhr backend for http backend

--- packages/webapp/src/locales/i18n.js
+++ packages/webapp/src/locales/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
-import backend from 'i18next-xhr-backend';
+import backend from 'i18next-http-backend';
 import { APP_VERSION } from '../util/constants';
 
 i18n

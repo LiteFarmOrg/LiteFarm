c9df926f3cfa1e5a77dc23ef2c28d754a9da1a23
Duncan-Brain <dbrain@litefarm.org>
2025-10-22 13:42:03 -0400

    LF-4988 Country code snuck in

--- packages/api/src/models/marketDirectoryInfoModel.js
+++ packages/api/src/models/marketDirectoryInfoModel.js
@@ -45,7 +45,6 @@ class MarketDirectoryInfo extends baseModel {
         contact_last_name: { type: 'string', maxLength: 255 },
         contact_email: { type: 'string', minLength: 1, maxLength: 255 },
         email: { type: 'string', maxLength: 255 },
-        country_code: { type: ['number', 'null'], format: 'integer', minimum: 1, maximum: 999 },
         phone_number: { type: 'string', maxLength: 255 },
         address: { type: 'string', minLength: 1, maxLength: 255 },
         website: { type: ['string', `null`], maxLength: 255 },

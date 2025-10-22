fda102b5f9dd836546f7f0356e25e612aa3b845c
Duncan-Brain <dbrain@litefarm.org>
2025-10-22 12:50:27 -0400

    LF-4988 Update table fields

--- packages/api/db/migration/20251014140509_create_market_directory_table.js
+++ packages/api/db/migration/20251014140509_create_market_directory_table.js
@@ -19,22 +19,22 @@
  */
 
 export const up = async (knex) => {
-  await knex.schema.createTable('market_directory_data', (table) => {
-    table.uuid('farm_id').references('farm_id').inTable('farm').primary();
+  await knex.schema.createTable('market_directory_info', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.uuid('farm_id').unique().references('farm_id').inTable('farm');
     table.string('farm_name').notNullable();
     table.string('logo');
     table.text('about');
-    table.string('representative_first_name').notNullable();
-    table.string('representative_last_name').notNullable().defaultTo('');
-    table.string('email').notNullable();
+    table.string('contact_first_name').notNullable();
+    table.string('contact_last_name').notNullable().defaultTo('');
+    table.string('contact_email').notNullable();
+    table.string('email');
     table.string('phone_number');
     table.string('address').notNullable();
     table.text('website');
     table.string('instagram');
     table.string('facebook');
     table.string('x');
-    table.string('youtube');
-    table.string('linkedin');
     table.boolean('deleted').notNullable().defaultTo(false);
     table
       .string('created_by_user_id')
@@ -58,5 +58,5 @@ export const up = async (knex) => {
  * @returns { Promise<void> }
  */
 export const down = async (knex) => {
-  await knex.schema.dropTable('market_directory_data');
+  await knex.schema.dropTable('market_directory_info');
 };

2c1e1b671db849c33c614b50880f367dbd1e8d83
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-10-24 13:18:22 -0700

    LF-4995 Add POST API skeleton for market directory info

---
+++ packages/api/src/controllers/marketDirectoryInfoController.ts
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Request, Response } from 'express';
+import { HttpError } from '../types.js';
+
+const marketDirectoryInfoController = {
+  addMarketDirectoryInfo() {
+    return async (_req: Request, res: Response) => {
+      try {
+        res.status(201).send();
+      } catch (error: unknown) {
+        console.error(error);
+        const err = error as HttpError;
+        const status = err.status || err.code || 500;
+        return res.status(status).json({
+          error: err.message || err,
+        });
+      }
+    };
+  },
+};
+
+export default marketDirectoryInfoController;

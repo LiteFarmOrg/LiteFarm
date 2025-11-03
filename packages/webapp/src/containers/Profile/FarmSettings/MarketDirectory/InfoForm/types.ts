7ac29367511c925efc2a801a898e189c999e5c03
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-11-03 13:46:56 -0800

    LF-4990 Update containers

---
+++ packages/webapp/src/containers/Profile/FarmSettings/MarketDirectory/InfoForm/types.ts
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+export const DIRECTORY_INFO_FIELDS = {
+  // Farm identity
+  FARM_NAME: 'farm_name',
+  ABOUT: 'about',
+  LOGO: 'logo',
+
+  // Farm representative
+  CONTACT_FIRST_NAME: 'contact_first_name',
+  CONTACT_LAST_NAME: 'contact_last_name',
+  CONTACT_EMAIL: 'contact_email',
+
+  // Farm store contact
+  ADDRESS: 'address',
+  COUNTRY_CODE: 'country_code',
+  PHONE_NUMBER: 'phone_number',
+  EMAIL: 'email',
+
+  // Online presence
+  WEBSITE: 'website',
+  INSTAGRAM: 'instagram',
+  FACEBOOK: 'facebook',
+  X: 'x',
+} as const;
+
+export type MarketDirectoryInfoFormFields = {
+  [DIRECTORY_INFO_FIELDS.FARM_NAME]?: string;
+  [DIRECTORY_INFO_FIELDS.ABOUT]?: string;
+  [DIRECTORY_INFO_FIELDS.LOGO]?: string;
+  [DIRECTORY_INFO_FIELDS.CONTACT_FIRST_NAME]?: string;
+  [DIRECTORY_INFO_FIELDS.CONTACT_LAST_NAME]?: string;
+  [DIRECTORY_INFO_FIELDS.CONTACT_EMAIL]?: string;
+  [DIRECTORY_INFO_FIELDS.ADDRESS]?: string;
+  [DIRECTORY_INFO_FIELDS.COUNTRY_CODE]?: number;
+  [DIRECTORY_INFO_FIELDS.PHONE_NUMBER]?: string;
+  [DIRECTORY_INFO_FIELDS.EMAIL]?: string;
+  [DIRECTORY_INFO_FIELDS.WEBSITE]?: string;
+  [DIRECTORY_INFO_FIELDS.INSTAGRAM]?: string;
+  [DIRECTORY_INFO_FIELDS.FACEBOOK]?: string;
+  [DIRECTORY_INFO_FIELDS.X]?: string;
+};

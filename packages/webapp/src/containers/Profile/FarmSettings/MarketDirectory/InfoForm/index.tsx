6f6363a9bc9a5ff5c62f51a0aa3d3bf4e018771e
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2025-11-04 14:48:02 -0800

    LF-4990 Add reverse geocoding of lat/lng addresses

--- packages/webapp/src/containers/Profile/FarmSettings/MarketDirectory/InfoForm/index.tsx
+++ packages/webapp/src/containers/Profile/FarmSettings/MarketDirectory/InfoForm/index.tsx
@@ -15,15 +15,18 @@
 
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+// @ts-expect-error untyped package
+import Script from 'react-load-script';
 import { FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import PureMarketDirectoryInfoForm from '../../../../../components/MarketDirectory/InfoForm';
 import useDefaultMarketDirectoryData from './useDefaultMarketDirectoryData';
-import { MarketDirectoryInfoFormFields } from './types';
+import { DIRECTORY_INFO_FIELDS, MarketDirectoryInfoFormFields } from './types';
 import useImagePickerUpload from '../../../../../components/ImagePicker/useImagePickerUpload';
 import { useAddMarketDirectoryInfoMutation } from '../../../../../store/api/marketDirectoryInfoApi';
 import { enqueueErrorSnackbar, enqueueSuccessSnackbar } from '../../../../Snackbar/snackbarSlice';
 import InFormButtons from '../../../../../components/Form/InFormButtons';
+import { isLatLng, reverseGeocode } from '../../../../../util/google-maps/reverseGeocode';
 
 export enum FormMode {
   ADD = 'add',
@@ -105,21 +108,42 @@ const MarketDirectoryInfoForm = ({ setIsComplete, close }: MarketDirectoryInfoFo
     setFormMode(FormMode.READONLY);
   };
 
+  // Geocode lat/lng address if needed
+  const handleGeocode = () => {
+    const address = defaultValues[DIRECTORY_INFO_FIELDS.ADDRESS];
+
+    if (address && isLatLng(address)) {
+      reverseGeocode(address).then((formattedAddress) => {
+        formMethods.setValue(DIRECTORY_INFO_FIELDS.ADDRESS, formattedAddress, {
+          shouldValidate: true,
+        });
+      });
+    }
+  };
+
   return (
-    <FormProvider {...formMethods}>
-      <PureMarketDirectoryInfoForm
-        close={close}
-        getOnFileUpload={getOnFileUpload}
-        formMode={formMode}
+    <>
+      <Script
+        url={`https://maps.googleapis.com/maps/api/js?key=${
+          import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+        }&libraries=places&language=en-US`}
+        onLoad={handleGeocode}
       />
-      <InFormButtons
-        confirmText={formMode === FormMode.READONLY ? t('common:EDIT') : t('common:SAVE')}
-        onCancel={formMode === FormMode.READONLY ? undefined : onCancel}
-        onConfirm={formMethods.handleSubmit(onSubmit)}
-        isDisabled={!formMethods.formState.isValid}
-        confirmButtonType="submit"
-      />
-    </FormProvider>
+      <FormProvider {...formMethods}>
+        <PureMarketDirectoryInfoForm
+          close={close}
+          getOnFileUpload={getOnFileUpload}
+          formMode={formMode}
+        />
+        <InFormButtons
+          confirmText={formMode === FormMode.READONLY ? t('common:EDIT') : t('common:SAVE')}
+          onCancel={formMode === FormMode.READONLY ? undefined : onCancel}
+          onConfirm={formMethods.handleSubmit(onSubmit)}
+          isDisabled={!formMethods.formState.isValid}
+          confirmButtonType="submit"
+        />
+      </FormProvider>
+    </>
   );
 };
 

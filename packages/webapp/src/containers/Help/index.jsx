bf711aec246e30b84fc51531570a040a168f2279
Sayaka Ono <sayaka.ono.81@gmail.com>
2025-12-09 16:32:37 -0800

    LF-4666 Replace help saga with RTK Query

--- packages/webapp/src/containers/Help/index.jsx
+++ packages/webapp/src/containers/Help/index.jsx
@@ -1,40 +1,47 @@
 import PureHelpRequestPage from '../../components/Help';
 import { useDispatch, useSelector } from 'react-redux';
-import { supportFileUpload } from './saga';
+import { useTranslation } from 'react-i18next';
 import {
-  isHelpLoadingSelector,
-  startSendHelp,
   dismissHelpRequestModal,
   showHelpRequestModalSelector,
+  postHelpRequestSuccess,
 } from '../Home/homeSlice';
 import { userFarmSelector } from '../userFarmSlice';
+import { enqueueErrorSnackbar } from '../Snackbar/snackbarSlice';
 import RequestConfirmationComponent from '../../components/Modals/RequestConfirmationModal';
+import { useAddSupportTicketMutation } from '../../store/api/supportTicketApi';
 
 export default function HelpRequest({ closeDrawer }) {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const [addSupportTicket, { isLoading }] = useAddSupportTicketMutation();
 
   const showHelpRequestModal = useSelector(showHelpRequestModalSelector);
   const showRequestConfirmationModalOnClick = () => dispatch(dismissHelpRequestModal());
 
-  const handleSubmit = (file, data, resetForm) => {
-    dispatch(startSendHelp());
-    dispatch(
-      supportFileUpload({
-        file,
-        form: data,
-        onSuccess: () => {
-          resetForm();
-          closeDrawer?.();
-        },
-      }),
-    );
+  const handleSubmit = async (file, data, resetForm) => {
+    try {
+      const result = await addSupportTicket({ file, data });
+
+      if (result.error) {
+        throw new Error(result.error.error);
+      }
+
+      dispatch(postHelpRequestSuccess());
+      resetForm();
+      closeDrawer?.();
+    } catch (error) {
+      console.error(error);
+      dispatch(enqueueErrorSnackbar(t('message:HELP_REQUEST.ERROR.SEND')));
+    }
   };
   const onCancel = () => {
     closeDrawer?.();
   };
 
   const { email, phone_number } = useSelector(userFarmSelector);
-  const loading = useSelector(isHelpLoadingSelector);
+
   return (
     <>
       <PureHelpRequestPage
@@ -42,7 +49,7 @@ export default function HelpRequest({ closeDrawer }) {
         onCancel={onCancel}
         email={email}
         phoneNumber={phone_number}
-        isLoading={loading}
+        isLoading={isLoading}
       />
       {showHelpRequestModal && (
         <RequestConfirmationComponent

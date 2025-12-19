5c650073cf0ace2ed28df4c42970825ab7ddac52
Duncan-Brain <dbrain@litefarm.org>
2025-12-19 12:21:59 -0500

    Add hook capable of replacing sensor selectors

---
+++ packages/webapp/src/hooks/location/useExternalLocations.ts
@@ -0,0 +1,51 @@
+/*
+ *  Copyright 2025 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { useGetSensorsQuery } from '../../store/api/apiSlice';
+
+const useExternalLocations = () => {
+  const { data: sensorData = { sensors: [], sensor_arrays: [] }, isLoading: isLoadingSensors } =
+    useGetSensorsQuery();
+
+  const isLoading = isLoadingSensors;
+
+  if (isLoading) {
+    return { locations: [], isLoading };
+  }
+
+  const allSensors = sensorData.sensors.map((sensor) => ({
+    ...sensor,
+    isAddonSensor: 'true',
+    type: 'sensor',
+  }));
+
+  const sensorArrays = sensorData.sensor_arrays.map((sensorArray) => ({
+    ...sensorArray,
+    isAddonSensor: 'true',
+    type: 'sensor_array',
+  }));
+
+  const standaloneSensors = allSensors?.filter((sensor) => sensor.sensor_array_id === null);
+
+  return {
+    locations: {
+      sensor: [...standaloneSensors],
+      sensor_array: [...sensorArrays],
+    },
+    isLoading,
+  };
+};
+
+export default useExternalLocations;

67ca71392d8ec6cfcf4ab1910f51c39a238e1cdd
Duncan-Brain <dbrain@litefarm.org>
2026-01-05 15:36:19 -0500

    Create a default filter settings constant that uses rtk API types

--- packages/webapp/src/stories/Pages/Map/SpotLight/Drawer.stories.jsx
+++ packages/webapp/src/stories/Pages/Map/SpotLight/Drawer.stories.jsx
@@ -1,29 +1,14 @@
-import React from 'react';
 import decorators from '../../config/Decorators';
 import PureMapFooter from '../../../../components/Map/Footer/';
-import { locationEnum } from '../../../../containers/Map/constants';
 import { chromaticSmallScreen } from '../../config/chromatic';
+import { MAP_LOCATION_TYPE_BY_FIGURE } from '../../../../containers/Map/constants';
 
 export default {
   title: 'Components/Map/SpotLight',
   component: PureMapFooter,
   decorators: decorators,
 };
-const availableFilterSettings = {
-  area: [
-    locationEnum.barn,
-    locationEnum.ceremonial_area,
-    locationEnum.farm_site_boundary,
-    locationEnum.field,
-    locationEnum.garden,
-    locationEnum.greenhouse,
-    locationEnum.surface_water,
-    locationEnum.natural_area,
-    locationEnum.residence,
-  ],
-  line: [locationEnum.buffer_zone, locationEnum.creek, locationEnum.fence],
-  point: [locationEnum.gate, locationEnum.water_valve],
-};
+const availableFilterSettings = MAP_LOCATION_TYPE_BY_FIGURE;
 const Template = (args) => (
   <div
     style={{

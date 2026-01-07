b2284a4f0c69c5ed884cde15fd588fcab86144bf
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2026-01-06 16:51:46 -0800

    LF-5107 Add survey displaying component

---
+++ packages/webapp/src/components/SurveyComponent/index.tsx
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2026 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+import { useCallback, useMemo } from 'react';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+import { DefaultLight } from 'survey-core/themes';
+import 'survey-core/survey-core.css';
+
+interface SurveyComponentProps {
+  surveyJson: any; // Survey JSON schema object
+  onComplete: (surveyData: any) => void;
+  initialData?: Record<string, any>;
+}
+
+export default function SurveyComponent({
+  surveyJson,
+  onComplete,
+  initialData,
+}: SurveyComponentProps) {
+  const survey = useMemo(() => {
+    const model = new Model(surveyJson);
+
+    model.applyTheme(DefaultLight);
+
+    // Set initial data if provided
+    if (initialData) {
+      model.data = initialData;
+    }
+
+    return model;
+  }, [surveyJson, initialData]);
+
+  // https://surveyjs.io/form-library/documentation/get-started-react
+  const handleComplete = useCallback(
+    (surveyModel: Model) => {
+      const surveyData = surveyModel.data;
+      onComplete(surveyData);
+    },
+    [onComplete],
+  );
+
+  survey.onComplete.add(handleComplete);
+
+  return <Survey model={survey} />;
+}

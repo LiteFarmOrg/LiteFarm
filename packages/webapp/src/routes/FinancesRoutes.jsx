fda1ea691461ca0961ebfe301a3e71439f39cd44
Sayaka Ono <sayaka.ono.81@gmail.com>
2026-01-13 11:50:21 -0800

    LF-4242 WIP - Refactor FinancesRoutes

--- packages/webapp/src/routes/FinancesRoutes.jsx
+++ packages/webapp/src/routes/FinancesRoutes.jsx
@@ -15,30 +15,6 @@
 
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import {
-  ACTUAL_REVENUE_URL,
-  ADD_CUSTOM_EXPENSE_URL,
-  ADD_CUSTOM_REVENUE_URL,
-  ADD_EXPENSE_URL,
-  ADD_REVENUE_URL,
-  createEditCustomExpenseURL,
-  createEditCustomRevenueUrl,
-  createEditExpenseDetailsUrl,
-  createEditRevenueDetailsUrl,
-  createExpenseDetailsUrl,
-  createManagementPlanEstimatedRevenueURL,
-  createReadonlyCustomExpenseURL,
-  createReadonlyCustomRevenueUrl,
-  createRevenueDetailsUrl,
-  ESTIMATED_REVENUE_URL,
-  EXPENSE_CATEGORIES_URL,
-  FINANCES_HOME_URL,
-  LABOUR_URL,
-  MANAGE_CUSTOM_EXPENSES_URL,
-  MANAGE_CUSTOM_REVENUES_URL,
-  OTHER_EXPENSE_URL,
-  REVENUE_TYPES_URL,
-} from '../util/siteMapConstants';
 import WithNavigation from './WithNavigation';
 const Finances = React.lazy(() => import('../containers/Finances'));
 const ActualRevenue = React.lazy(() => import('../containers/Finances/ActualRevenue'));
@@ -81,44 +57,34 @@ const EditCustomRevenue = React.lazy(() =>
   import('../containers/Finances/CustomRevenueType/EditCustomRevenue'),
 );
 
-// TODO: update to relative paths
 const FinancesRoutes = () => (
   <Routes>
-    <Route path={FINANCES_HOME_URL} element={<Finances />} />
-    <Route path={ACTUAL_REVENUE_URL} element={<ActualRevenue />} />
+    <Route path="transactions" element={<Finances />} />
+    <Route path="actual_revenue" element={<ActualRevenue />} />
     <Route
-      path={createManagementPlanEstimatedRevenueURL(':management_plan_id')}
+      path="estimated_revenue/plan/:management_plan_id"
       element={<UpdateEstimatedCropRevenue />}
     />
-    <Route path={REVENUE_TYPES_URL} element={<RevenueTypes />} />
-    <Route path={ADD_REVENUE_URL} element={<AddSale />} />
-    <Route path={MANAGE_CUSTOM_REVENUES_URL} element={<ManageRevenueTypes />} />
-    <Route path={createRevenueDetailsUrl(':sale_id')} element={<RevenueDetail />} />
-    <Route path={createEditRevenueDetailsUrl(':sale_id')} element={<RevenueDetail />} />
-    <Route path={ESTIMATED_REVENUE_URL} element={<EstimatedRevenue />} />
-    <Route path={LABOUR_URL} element={<Labour />} />
-    <Route path={OTHER_EXPENSE_URL} element={<OtherExpense />} />
-    <Route path={createExpenseDetailsUrl(':expense_id')} element={<ExpenseDetail />} />
-    <Route path={createEditExpenseDetailsUrl(':expense_id')} element={<ExpenseDetail />} />
-    <Route
-      path={EXPENSE_CATEGORIES_URL}
-      render={() => <WithNavigation component={ExpenseCategories} />}
-    />
-    <Route path={ADD_EXPENSE_URL} render={() => <WithNavigation component={AddExpense} />} />
-    <Route path={MANAGE_CUSTOM_EXPENSES_URL} element={<ManageExpenseTypes />} />
-    <Route path={ADD_CUSTOM_EXPENSE_URL} element={<AddCustomExpense />} />
-    <Route
-      path={createReadonlyCustomExpenseURL(':expense_type_id')}
-      element={<ReadOnlyCustomExpense />}
-    />
-    <Route path={createEditCustomExpenseURL(':expense_type_id')} element={<EditCustomExpense />} />
-    <Route path={ADD_CUSTOM_REVENUE_URL} element={<AddCustomRevenue />} />
-    <Route
-      path={createReadonlyCustomRevenueUrl(':revenue_type_id')}
-      element={<ReadOnlyCustomRevenue />}
-    />
-    <Route path={createEditCustomRevenueUrl(':revenue_type_id')} element={<EditCustomRevenue />} />
-    <Route render={() => <Navigate to={'/'} />} />
+    <Route path="revenue_types" element={<RevenueTypes />} />
+    <Route path="add_revenue" element={<AddSale />} />
+    <Route path="manage_custom_revenues" element={<ManageRevenueTypes />} />
+    <Route path="revenue/:sale_id" element={<RevenueDetail />} />
+    <Route path="revenue/:sale_id/edit" element={<RevenueDetail />} />
+    <Route path="estimated_revenue" element={<EstimatedRevenue />} />
+    <Route path="labour" element={<Labour />} />
+    <Route path="other_expense" element={<OtherExpense />} />
+    <Route path="expense/:expense_id" element={<ExpenseDetail />} />
+    <Route path="expense/:expense_id/edit" element={<ExpenseDetail />} />
+    <Route path="expense_categories" element={<WithNavigation component={ExpenseCategories} />} />
+    <Route path="add_expense" element={<WithNavigation component={AddExpense} />} />
+    <Route path="manage_custom_expenses" element={<ManageExpenseTypes />} />
+    <Route path="add_custom_expense" element={<AddCustomExpense />} />
+    <Route path="readonly_custom_expense/:expense_type_id" element={<ReadOnlyCustomExpense />} />
+    <Route path="edit_custom_expense/:expense_type_id" element={<EditCustomExpense />} />
+    <Route path="add_custom_revenue" element={<AddCustomRevenue />} />
+    <Route path="readonly_custom_revenue/:revenue_type_id" element={<ReadOnlyCustomRevenue />} />
+    <Route path="edit_custom_revenue/:revenue_type_id" element={<EditCustomRevenue />} />
+    <Route path="*" element={<Navigate to={'/'} />} />
   </Routes>
 );
 

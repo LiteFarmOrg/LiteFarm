2283ac814be66942d9ed4b87773c5c908cf53538
Sayaka Ono <sayaka.ono.81@gmail.com>
2026-02-12 13:51:56 -0800

    LF-5145 Add migration for offline_event_log table

---
+++ packages/api/db/migration/20260209223930_add_offline_event_log.js
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2026 LiteFarm.org
+ *  This file is part of LiteFarm.
+ *
+ *  LiteFarm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LiteFarm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async function (knex) {
+  return knex.schema.createTable('offline_event_log', function (table) {
+    table.increments('id').primary();
+    table.string('event_name').notNullable();
+    table.string('status');
+    table.integer('country_id').references('id').inTable('countries');
+    table.string('app_version');
+    table.boolean('authenticated');
+    table.dateTime('event_at');
+    table.dateTime('created_at').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async function (knex) {
+  return knex.schema.dropTable('offline_event_log');
+};

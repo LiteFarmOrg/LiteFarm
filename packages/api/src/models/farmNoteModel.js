7e08f9c5b83cde536678ee3198b92616fc0ca857
Sayaka Ono <sayaka.ono.81@gmail.com>
2026-03-23 11:19:11 -0700

    LF-5211 Trim note before insert/update

--- packages/api/src/models/farmNoteModel.js
+++ packages/api/src/models/farmNoteModel.js
@@ -14,6 +14,7 @@
  */
 
 import BaseModel from './baseModel.js';
+import { checkAndTrimString } from '../util/util.js';
 
 class FarmNoteModel extends BaseModel {
   static get tableName() {
@@ -27,6 +28,22 @@ class FarmNoteModel extends BaseModel {
   // Override to expose updated_at and created_by_user_id for display on the frontend.
   static get hidden() {
     return ['updated_by_user_id', 'created_at', 'deleted'];
+  }
+
+  async $beforeInsert(queryContext) {
+    await super.$beforeInsert(queryContext);
+    this.trimStringProperties();
+  }
+
+  async $beforeUpdate(opt, queryContext) {
+    await super.$beforeUpdate(opt, queryContext);
+    this.trimStringProperties();
+  }
+
+  trimStringProperties() {
+    if (this.note) {
+      this.note = checkAndTrimString(this.note);
+    }
   }
 
   static get jsonSchema() {

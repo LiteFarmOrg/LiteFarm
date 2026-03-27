e6bf4834083bee79a7984e70ea818509a2c6fa49
Joyce Yuki <82857964+kathyavini@users.noreply.github.com>
2026-03-27 13:13:33 -0700

    LF-5216 Move from makeStyles to SCSS classes to combine styles on the outer div and remove extra wrapping div on FilePickerWrapper
    The position: absolute was needed, but the extra div was making flexbox styling from the parent a big pain

--- packages/webapp/src/components/Form/FilePickerWrapper/index.tsx
+++ packages/webapp/src/components/Form/FilePickerWrapper/index.tsx
@@ -1,25 +1,7 @@
-import { makeStyles } from '@mui/styles';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { ComponentPropsWithoutRef } from 'react';
-
-const useStyles = makeStyles({
-  inputContainer: {
-    position: 'relative',
-    display: 'inline-block',
-    cursor: 'pointer',
-  },
-  input: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    minWidth: '10px',
-    opacity: 0,
-    cursor: 'pointer',
-    '&::-webkit-file-upload-button': {
-      cursor: 'pointer',
-    },
-  },
-});
+import styles from './styles.module.scss';
 
 export default function PureFilePickerWrapper({
   children,
@@ -29,21 +11,17 @@ export default function PureFilePickerWrapper({
   onChange,
   ...props
 }: ComponentPropsWithoutRef<'input'>) {
-  const classes = useStyles();
-
   return (
-    <div className={className} style={style}>
-      <div className={classes.inputContainer}>
-        <input
-          type={'file'}
-          size={1}
-          className={classes.input}
-          onChange={onChange}
-          disabled={disabled}
-          {...props}
-        />
-        {children}
-      </div>
+    <div className={clsx(styles.inputContainer, className)} style={style}>
+      <input
+        type="file"
+        size={1}
+        className={styles.input}
+        onChange={onChange}
+        disabled={disabled}
+        {...props}
+      />
+      {children}
     </div>
   );
 }

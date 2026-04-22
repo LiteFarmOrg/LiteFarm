a8a2c224e159b938f0841340d6908922c7f40d85
tomaioo <hongsaygio.com@gmail.com>
2026-04-22 11:14:51 -0700

    fix(security): 2 improvements across 2 files
    - Security: Predictable UUID version used for user identifiers
    - Security: Invitation/token values stored in plaintext
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- packages/api/db/migration/20201130123842_change_user_id_to_uuid.js
+++ packages/api/db/migration/20201130123842_change_user_id_to_uuid.js
@@ -1,6 +1,6 @@
 export const up = function (knex) {
   return Promise.all([
-    knex.raw('ALTER TABLE "users" ALTER COLUMN user_id SET DEFAULT uuid_generate_v1();'),
+    knex.raw('ALTER TABLE "users" ALTER COLUMN user_id SET DEFAULT uuid_generate_v4();'),
   ]);
 };
 
